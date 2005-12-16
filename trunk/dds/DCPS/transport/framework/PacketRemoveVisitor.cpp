2d134f8a93283a37179a2f798b593f7007457125
Trevor Fields <fields_t@ociweb.com>
2005-12-16 08:04:55 +0000

    ChangeLogTag:Fri Dec 16 01:03:41 MST 2005  Trevor Fields  <fields_t@ociweb.com>

--- trunk/dds/DCPS/transport/framework/PacketRemoveVisitor.cpp
+++ trunk/dds/DCPS/transport/framework/PacketRemoveVisitor.cpp
@@ -36,6 +36,23 @@ TAO::DCPS::PacketRemoveVisitor::PacketRemoveVisitor
 }
 
 
+TAO::DCPS::PacketRemoveVisitor::PacketRemoveVisitor
+                                    (RepoId              pub_id,
+                                     ACE_Message_Block*& unsent_head_block,
+                                     ACE_Message_Block*  header_block)
+  : sample_(0),
+    pub_id_(pub_id),
+    head_(unsent_head_block),
+    header_block_(header_block),
+    status_(0),
+    current_block_(0),
+    previous_block_(0),
+    replaced_element_allocator_(NUM_REPLACED_ELEMENT_CHUNKS)
+{
+  DBG_ENTRY("PacketRemoveVisitor","PacketRemoveVisitor");
+}
+
+
 TAO::DCPS::PacketRemoveVisitor::~PacketRemoveVisitor()
 {
   DBG_ENTRY("PacketRemoveVisitor","~PacketRemoveVisitor");
