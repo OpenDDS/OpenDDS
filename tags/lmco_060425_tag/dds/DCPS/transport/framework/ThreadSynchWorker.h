7e206d37e74981cab4a38a7c68f403078376e5c5
(no author) <unknown@unknown>
2006-04-25 16:54:22 +0000

    This commit was manufactured by cvs2svn to create tag 'lmco_060425_tag'.

---
+++ tags/lmco_060425_tag/dds/DCPS/transport/framework/ThreadSynchWorker.h
@@ -0,0 +1,43 @@
+// -*- C++ -*-
+//
+// $Id$
+#ifndef TAO_DCPS_THREADSYNCHWORKER_H
+#define TAO_DCPS_THREADSYNCHWORKER_H
+
+#include  "dds/DCPS/dcps_export.h"
+
+namespace TAO
+{
+  namespace DCPS
+  {
+
+    class TAO_DdsDcps_Export ThreadSynchWorker
+    {
+      public:
+
+        virtual ~ThreadSynchWorker();
+
+        enum WorkOutcome
+        {
+          WORK_OUTCOME_MORE_TO_DO,
+          WORK_OUTCOME_NO_MORE_TO_DO,
+          WORK_OUTCOME_ClOGGED_RESOURCE,
+          WORK_OUTCOME_BROKEN_RESOURCE
+        };
+
+        virtual WorkOutcome perform_work() = 0;
+
+
+      protected:
+
+        ThreadSynchWorker();
+    };
+
+  } /* namespace DCPS */
+} /* namespace TAO */
+
+#if defined (__ACE_INLINE__)
+#include "ThreadSynchWorker.inl"
+#endif /* __ACE_INLINE__ */
+
+#endif  /* TAO_DCPS_THREADSYNCHWORKER_H */
---
+++ tags/lmco_060425_tag/tests/transport/simple_bp_rm/SimpleDataReader.h
@@ -0,0 +1,38 @@
+// -*- C++ -*-
+//
+// $Id$
+#ifndef SIMPLEDATAREADER_H
+#define SIMPLEDATAREADER_H
+
+#include  "dds/DCPS/transport/framework/TransportReceiveListener.h"
+#include  "dds/DCPS/Definitions.h"
+
+
+class SimpleDataReader : public TAO::DCPS::TransportReceiveListener
+{
+  public:
+
+    SimpleDataReader();
+    virtual ~SimpleDataReader();
+
+    void init(TAO::DCPS::RepoId sub_id,unsigned num_messages_expected);
+
+    virtual void data_received(const TAO::DCPS::ReceivedDataSample& sample);
+
+    void transport_lost();
+
+    /// Returns 0 if the data_received() has not been called/completed.
+    /// Returns 1 if the data_received() has been called, and all of
+    /// the TransportReceiveListeners have been told of the data_received().
+    int received_test_message() const;
+
+
+  private:
+
+    TAO::DCPS::RepoId sub_id_;
+    int num_messages_expected_;
+    int num_messages_received_;
+    int sequence_;
+};
+
+#endif  /* SIMPLEDATAREADER_H */
