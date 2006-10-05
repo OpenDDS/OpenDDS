a4bb8e42e663321739c5855d9bd222f083584aa4
Ciju John <johnc@ociweb.com>
2006-10-05 20:31:45 +0000

    Thu Oct  5 19:55:42 UTC 2006  Ciju John  <johnc@ociweb.com>
            * tests/DCPS/Reconnect/README:
    	  Create a README for this test.
    
            * tests/DCPS/Reconnect/run_test.pl:
            * bin/dcps_tests.lst:
    	  Adding a new regression test to capture a failing scenario. The
              Subscriber is killed (simulating a crash) after
              initialization. The Publisher then crashes during its
              initialization.

--- tests/DCPS/Reconnect/run_test.pl
+++ tests/DCPS/Reconnect/run_test.pl
@@ -21,6 +21,7 @@ $read_delay_ms=0;
 $lost_publication_callback = 0;
 $lost_subscription_callback = 0;
 $end_with_publisher = 0;
+$kill_subscriber = 0;
 
 if ($ARGV[0] eq 'restart_sub') {
   # Increase the number of messages so that the publisher will last
@@ -29,42 +30,45 @@ if ($ARGV[0] eq 'restart_sub') {
   $restart_delay = 10;
   $num_reads_before_crash = 2;
   $num_lost_messages_estimate = $restart_delay * 1000/$write_delay_ms;
-  $num_expected_reads_restart_sub 
+  $num_expected_reads_restart_sub
     = $num_writes - $num_reads_before_crash - $num_lost_messages_estimate;
   $lost_publication_callback = 1;
-  
+
   # We can not give an exact number of reads we expected when subscriber crashes
-  # because we do not know how many messages are lost. We give a deviation on the 
+  # because we do not know how many messages are lost. We give a deviation on the
   # number of expected messages.
   $num_reads_deviation = 2;
-} 
-elsif ($ARGV[0] eq 'restart_pub') { 
+}
+elsif ($ARGV[0] eq 'restart_pub') {
   $restart_delay = 10;
   $num_writes_before_crash = 2;
   $num_expected_reads = $num_writes + $num_writes_before_crash;
   $lost_subscription_callback = 1;
 }
-elsif ($ARGV[0] eq 'bp_timeout') { 
+elsif ($ARGV[0] eq 'bp_timeout') {
   # no delay between writes.
   $write_delay_ms=0;
-  # delay 2 seconds between reads to make the 
+  # delay 2 seconds between reads to make the
   # transport have backpressure.
   $read_delay_ms=5000;
   $num_writes = 10000;
   $num_expected_reads = $num_writes;
   $lost_publication_callback = 1;
   $lost_subscription_callback = 1;
-  
+
   $end_with_publisher = 1;
 }
-elsif ($ARGV[0] eq '') { 
-  #default test - no reconnect 
+elsif ($ARGV[0] eq 'sub_init_crash') {
+    $kill_subscriber = 1;
+}
+elsif ($ARGV[0] eq '') {
+  #default test - no reconnect
 }
 else {
   print STDERR "ERROR: invalid parameter $ARGV[0]\n";
   exit 1;
 }
- 
+
 
 $domains_file = PerlACE::LocalFile ("domain_ids");
 $dcpsrepo_ior = PerlACE::LocalFile ("repo.ior");
@@ -82,19 +86,19 @@ unlink $publisher_ready;
 unlink $testoutputfilename;
 
 # Save the output to check after execution
-open(SAVEERR, ">&STDERR");
-open(STDERR, ">$testoutputfilename") || die "ERROR: Can't redirect stderr";
+#open(SAVEERR, ">&STDERR");
+#open(STDERR, ">$testoutputfilename") || die "ERROR: Can't redirect stderr";
 
 $DCPSREPO = new PerlACE::Process ("$ENV{DDS_ROOT}/dds/InfoRepo/DCPSInfoRepo",
 				  "-NOBITS -o $dcpsrepo_ior -d $domains_file -ORBSvcConf repo.conf");
-$Subscriber = new PerlACE::Process ("subscriber", 
+$Subscriber = new PerlACE::Process ("subscriber",
           "-DCPSConfigFile sub.ini -a $num_reads_before_crash -n $num_expected_reads"
           . " -i $read_delay_ms -l $lost_subscription_callback -e $end_with_publisher");
-$Publisher = new PerlACE::Process ("publisher", 
+$Publisher = new PerlACE::Process ("publisher",
           "-DCPSConfigFile pub.ini -a $num_writes_before_crash -n $num_writes "
           . "-i $write_delay_ms -l $lost_publication_callback");
 
-
+print $DCPSREPO->CommandLine () . "\n";
 $DCPSREPO->Spawn ();
 if (PerlACE::waitforfile_timed ($dcpsrepo_ior, 30) == -1) {
     print STDERR "ERROR: waiting for DCPSInfo IOR file\n";
@@ -102,26 +106,37 @@ if (PerlACE::waitforfile_timed ($dcpsrepo_ior, 30) == -1) {
     exit 1;
 }
 
-$Publisher->Spawn ();
-print $Publisher->CommandLine () . "\n";
-$Subscriber->Spawn ();
 print $Subscriber->CommandLine () . "\n";
+$Subscriber->Spawn ();
+
+if ($kill_subscriber > 0)
+{
+# Simulate a crashed subscriber by killing it.
+    #$Subscriber->WaitKill (5);
+    $Subscriber->Wait (5);
+    $Subscriber->Kill ();
+
+    print "Subscriber crash before Publisher initialization.\n\n";
+}
+
+print $Publisher->CommandLine () . "\n";
+$Publisher->Spawn ();
 
 # The subscriber crashes and we need restart the subscriber.
 if ($num_reads_before_crash > 0)
 {
   $SubscriberResult = $Subscriber->WaitKill (60);
-  
+
   # We will not check the status returned from WaitKill() since it returns
   # different status on windows and linux.
   print "Subscriber crashed and returned $SubscriberResult. \n";
-    
-  $Subscriber = new PerlACE::Process ("subscriber", 
+
+  $Subscriber = new PerlACE::Process ("subscriber",
           "-DCPSConfigFile sub.ini -n $num_expected_reads_restart_sub "
           . "-r $num_reads_deviation");
 
   sleep($restart_delay);
-         
+
   print "\n\n!!! Restart subscriber !!! \n\n";;
   print $Subscriber->CommandLine () . "\n";
   $Subscriber->Spawn ();
@@ -130,29 +145,31 @@ if ($num_reads_before_crash > 0)
 # The publisher crashes and we need restart the publisher.
 if ($num_writes_before_crash > 0) {
   $PublisherResult = $Publisher->WaitKill (60);
-  
+
   # We will not check the status returned from WaitKill() since it returns
   # different status on windows and linux.
   print "Publisher crashed and returned $PublisherResult. \n";
 
-  $Publisher = new PerlACE::Process ("publisher", 
+  $Publisher = new PerlACE::Process ("publisher",
           "-DCPSConfigFile pub.ini -n $num_writes");
-  
+
   sleep($restart_delay);
-    
+
   print "\n\n!!! Restart publisher !!! \n\n";;
   print $Publisher->CommandLine () . "\n";
   $Publisher->Spawn ();
 }
-  
 
-$SubscriberResult = $Subscriber->WaitKill (300);
-if ($SubscriberResult != 0) {
-    print STDERR "ERROR: subscriber returned $SubscriberResult \n";
-    $status = 1;
+if ($kill_subscriber == 0)
+{
+    $SubscriberResult = $Subscriber->WaitKill (10);
+    if ($SubscriberResult != 0) {
+	print STDERR "ERROR: subscriber returned $SubscriberResult \n";
+	$status = 1;
+    }
 }
 
-$PublisherResult = $Publisher->WaitKill (30);
+$PublisherResult = $Publisher->WaitKill (10);
 if ($PublisherResult != 0) {
     print STDERR "ERROR: publisher returned $PublisherResult \n";
     $status = 1;
