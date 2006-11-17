0dd1e64f9db320baba46368ecbabe9a9112f1659
Ciju John <johnc@ociweb.com>
2006-11-17 18:11:30 +0000

    Fri Nov 17 18:04:25 UTC 2006  Ciju John  <johnc@ociweb.com>
            * DevGuideExamples/DDS/Messenger/pub_udp.ini:
            * DevGuideExamples/DDS/Messenger/sub_udp.ini:
    	  Add udp specific pub/sub configuration files.
    
            * DevGuideExamples/DDS/Messenger/run_test.pl:
    	  Modifications to pass transport specific options to pub/sub.
    
            * DevGuideExamples/DDS/Messenger/publisher.cpp:
            * DevGuideExamples/DDS/Messenger/subscriber.cpp:
    	  Add transport selection option.
    
            * DevGuideExamples/DDS/Messenger/sub.ini:
            * DevGuideExamples/DDS/Messenger/pub.ini:
    	  Remove dangling spaces.

--- DevGuideExamples/DDS/Messenger/publisher.cpp
+++ DevGuideExamples/DDS/Messenger/publisher.cpp
@@ -16,76 +16,112 @@
 #include <dds/DCPS/PublisherImpl.h>
 #include <dds/DCPS/transport/framework/TheTransportFactory.h>
 #include <dds/DCPS/transport/simpleTCP/SimpleTcpConfiguration.h>
-#include <ace/streams.h>
 
-const TAO::DCPS::TransportIdType TCP_IMPL_ID = 1;
+#include <ace/streams.h>
+#include "ace/Get_Opt.h"
+
+TAO::DCPS::TransportIdType transport_impl_id = 1;
 
 const char* pub_ready_filename    = "publisher_ready.txt";
 const char* pub_finished_filename = "publisher_finished.txt";
 const char* sub_ready_filename    = "subscriber_ready.txt";
 const char* sub_finished_filename = "subscriber_finished.txt";
 
+int
+parse_args (int argc, char *argv[])
+{
+  ACE_Get_Opt get_opts (argc, argv, "t:");
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    {
+      switch (c)
+  {
+  case 't':
+    if (ACE_OS::strcmp (get_opts.opt_arg (), "udp") == 0) {
+      transport_impl_id = 2;
+    }
+    break;
+
+  case '?':
+  default:
+    ACE_ERROR_RETURN ((LM_ERROR,
+           "usage:  %s "
+           "-t <tcp/udp> "
+           "\n",
+           argv [0]),
+          -1);
+  }
+    }
+  // Indicates sucessful parsing of the command line
+  return 0;
+}
 
 int main (int argc, char *argv[]) {
-  try {
-    DDS::DomainParticipantFactory_var dpf =
-      TheParticipantFactoryWithArgs(argc, argv);
-    DDS::DomainParticipant_var participant =
-      dpf->create_participant(411,
-                              PARTICIPANT_QOS_DEFAULT,
-                              DDS::DomainParticipantListener::_nil());
-    if (CORBA::is_nil (participant.in ())) {
-      cerr << "create_participant failed." << endl;
-      return 1;
-    }
+  try
+    {
+      DDS::DomainParticipantFactory_var dpf =
+  TheParticipantFactoryWithArgs(argc, argv);
+      DDS::DomainParticipant_var participant =
+  dpf->create_participant(411,
+        PARTICIPANT_QOS_DEFAULT,
+        DDS::DomainParticipantListener::_nil());
+      if (CORBA::is_nil (participant.in ())) {
+  cerr << "create_participant failed." << endl;
+  return 1;
+      }
 
-    MessageTypeSupportImpl* servant = new MessageTypeSupportImpl();
-    PortableServer::ServantBase_var safe_servant = servant;
+      if (parse_args (argc, argv) == -1) {
+  return -1;
+      }
 
-    if (DDS::RETCODE_OK != servant->register_type(participant.in (), "")) {
-      cerr << "register_type failed." << endl;
-      exit(1);
-    }
-  
-    CORBA::String_var type_name = servant->get_type_name ();
+      MessageTypeSupportImpl* servant = new MessageTypeSupportImpl();
+      PortableServer::ServantBase_var safe_servant = servant;
 
-    DDS::TopicQos topic_qos;
-    participant->get_default_topic_qos(topic_qos);
-    DDS::Topic_var topic =
-      participant->create_topic ("Movie Discussion List",
-                                 type_name.in (),
-                                 topic_qos,
-                                 DDS::TopicListener::_nil());
-    if (CORBA::is_nil (topic.in ())) {
-      cerr << "create_topic failed." << endl;
-      exit(1);
-    }
+      if (DDS::RETCODE_OK != servant->register_type(participant.in (), "")) {
+  cerr << "register_type failed." << endl;
+  exit(1);
+      }
 
-    TAO::DCPS::TransportImpl_rch tcp_impl =
-      TheTransportFactory->create_transport_impl (TCP_IMPL_ID, 
-                                                  ::TAO::DCPS::AUTO_CONFIG);
+      CORBA::String_var type_name = servant->get_type_name ();
 
-    DDS::Publisher_var pub =
-      participant->create_publisher(PUBLISHER_QOS_DEFAULT,
-                                    DDS::PublisherListener::_nil());
-    if (CORBA::is_nil (pub.in ())) {
-      cerr << "create_publisher failed." << endl;
-      exit(1);
-    }
+      DDS::TopicQos topic_qos;
+      participant->get_default_topic_qos(topic_qos);
+      DDS::Topic_var topic =
+  participant->create_topic ("Movie Discussion List",
+           type_name.in (),
+           topic_qos,
+           DDS::TopicListener::_nil());
+      if (CORBA::is_nil (topic.in ())) {
+  cerr << "create_topic failed." << endl;
+  exit(1);
+      }
 
-    // Attach the publisher to the transport.
-    TAO::DCPS::PublisherImpl* pub_impl =
-      ::TAO::DCPS::reference_to_servant< TAO::DCPS::PublisherImpl, 
-                                         DDS::Publisher_ptr>(pub.in ());
-    if (0 == pub_impl) {
-      cerr << "Failed to obtain publisher servant" << endl;
-      exit(1);
-    }
+      TAO::DCPS::TransportImpl_rch tcp_impl =
+  TheTransportFactory->create_transport_impl (transport_impl_id,
+                ::TAO::DCPS::AUTO_CONFIG);
 
-    TAO::DCPS::AttachStatus status = pub_impl->attach_transport(tcp_impl.in());
-    if (status != TAO::DCPS::ATTACH_OK) {
-      std::string status_str;
-      switch (status) {
+      DDS::Publisher_var pub =
+  participant->create_publisher(PUBLISHER_QOS_DEFAULT,
+              DDS::PublisherListener::_nil());
+      if (CORBA::is_nil (pub.in ())) {
+  cerr << "create_publisher failed." << endl;
+  exit(1);
+      }
+
+      // Attach the publisher to the transport.
+      TAO::DCPS::PublisherImpl* pub_impl =
+  ::TAO::DCPS::reference_to_servant< TAO::DCPS::PublisherImpl,
+  DDS::Publisher_ptr>(pub.in ());
+      if (0 == pub_impl) {
+  cerr << "Failed to obtain publisher servant" << endl;
+  exit(1);
+      }
+
+      TAO::DCPS::AttachStatus status = pub_impl->attach_transport(tcp_impl.in());
+      if (status != TAO::DCPS::ATTACH_OK) {
+  std::string status_str;
+  switch (status) {
         case TAO::DCPS::ATTACH_BAD_TRANSPORT:
           status_str = "ATTACH_BAD_TRANSPORT";
           break;
@@ -98,51 +134,53 @@ int main (int argc, char *argv[]) {
         default:
           status_str = "Unknown Status";
           break;
-      }
-      cerr << "Failed to attach to the transport. Status == "
-           << status_str.c_str() << endl;
-      exit(1);
-    }
-
-    // Create the datawriter
-    DDS::DataWriterQos dw_qos;
-    pub->get_default_datawriter_qos (dw_qos);
-    DDS::DataWriter_var dw =
-      pub->create_datawriter(topic.in (),
-                             dw_qos,
-                             DDS::DataWriterListener::_nil());
-    if (CORBA::is_nil (dw.in ())) {
-      cerr << "create_datawriter failed." << endl;
-      exit(1);
-    }
-    Writer* writer = new Writer(dw.in());
-
-    // Indicate that the publisher is ready
-    FILE* writers_ready = ACE_OS::fopen (pub_ready_filename, "w");
-    if (writers_ready == 0) {
-      cerr << "ERROR Unable to create publisher ready file" << endl;
-      exit(1);
-    }
-    ACE_OS::fclose(writers_ready);
-
-    writer->start ();
-    while ( !writer->is_finished()) {
-      ACE_Time_Value small(0,250000);
-      ACE_OS::sleep (small);
-    }
-
-    // Cleanup
-    writer->end ();
-    delete writer;
-    participant->delete_contained_entities();
-    dpf->delete_participant(participant.in ());
-    TheTransportFactory->release();
-    TheServiceParticipant->shutdown ();
-  } catch (CORBA::Exception& e) {
-    cerr << "Exception caught in main.cpp:" << endl
-         << e << endl;
-    exit(1);
   }
+  cerr << "Failed to attach to the transport. Status == "
+       << status_str.c_str() << endl;
+  exit(1);
+      }
+
+      // Create the datawriter
+      DDS::DataWriterQos dw_qos;
+      pub->get_default_datawriter_qos (dw_qos);
+      DDS::DataWriter_var dw =
+  pub->create_datawriter(topic.in (),
+             dw_qos,
+             DDS::DataWriterListener::_nil());
+      if (CORBA::is_nil (dw.in ())) {
+  cerr << "create_datawriter failed." << endl;
+  exit(1);
+      }
+      Writer* writer = new Writer(dw.in());
+
+      // Indicate that the publisher is ready
+      FILE* writers_ready = ACE_OS::fopen (pub_ready_filename, "w");
+      if (writers_ready == 0) {
+  cerr << "ERROR Unable to create publisher ready file" << endl;
+  exit(1);
+      }
+      ACE_OS::fclose(writers_ready);
+
+      writer->start ();
+      while ( !writer->is_finished()) {
+  ACE_Time_Value small(0,250000);
+  ACE_OS::sleep (small);
+      }
+
+      // Cleanup
+      writer->end ();
+      delete writer;
+      participant->delete_contained_entities();
+      dpf->delete_participant(participant.in ());
+      TheTransportFactory->release();
+      TheServiceParticipant->shutdown ();
+  }
+  catch (CORBA::Exception& e)
+    {
+      cerr << "Exception caught in main.cpp:" << endl
+     << e << endl;
+      exit(1);
+    }
 
   return 0;
 }
