7ede4ea15228340508540d7d5a9d723ba72c52f0
Jonathan Pollack <pollack_j@ociweb.com>
2007-02-08 23:02:33 +0000

    Added a loader

---
+++ branches/ReliableMulticast/dds/DCPS/transport/ReliableMulticast/ReliableMulticastLoader.cpp
@@ -0,0 +1,43 @@
+// -*- C++ -*-
+//
+// $Id$
+
+#include "ReliableMulticastLoader.h"
+#include "ReliableMulticastTransportGenerator.h"
+#include "dds/DCPS/transport/framework/TheTransportFactory.h"
+
+#if !defined (__ACE_INLINE__)
+#include "ReliableMulticastLoader.inl"
+#endif /* __ACE_INLINE__ */
+
+int
+TAO_DCPS_ReliableMulticastLoader::init(int argc, ACE_TCHAR* argv[])
+{
+  TAO::DCPS::TransportGenerator* generator = 0;
+
+  ACE_NEW_RETURN(
+    generator,
+    TAO::DCPS::ReliableMulticastTransportGenerator,
+    -1
+    );
+
+  TheTransportFactory->register_generator(
+    "ReliableMulticast",
+    generator
+    );
+
+  return 0;
+}
+
+ACE_FACTORY_DEFINE(
+  ReliableMulticast,
+  TAO_DCPS_ReliableMulticastLoader
+  )
+ACE_STATIC_SVC_DEFINE (
+  TAO_DCPS_ReliableMulticastLoader,
+  ACE_TEXT ("DCPS_ReliableMulticastLoader"),
+  ACE_SVC_OBJ_T,
+  &ACE_SVC_NAME (TAO_DCPS_ReliableMulticastLoader),
+  ACE_Service_Type::DELETE_THIS | ACE_Service_Type::DELETE_OBJ,
+  0
+  )
