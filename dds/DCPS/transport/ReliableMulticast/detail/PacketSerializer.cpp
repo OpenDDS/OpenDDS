1b3780c87d5364bb7ad843b5806ff088f740f80a
Jonathan Pollack <pollack_j@ociweb.com>
2007-02-12 14:50:04 +0000

    Adding PacketSerializer stub

---
+++ dds/DCPS/transport/ReliableMulticast/detail/PacketSerializer.cpp
@@ -0,0 +1,11 @@
+// -*- C++ -*-
+//
+// $Id$
+
+#include "PacketSerializer.h"
+
+#if !defined (__ACE_INLINE__)
+#include "PacketSerializer.inl"
+#endif /* __ACE_INLINE__ */
+
+//@@todo: Add Code Here
