7e86b89e2410f433f6b6ec4c15860ab00c9ac837
Jonathan Pollack <pollack_j@ociweb.com>
2007-02-12 14:50:04 +0000

    Adding PacketSerializer stub

---
+++ branches/ReliableMulticast/dds/DCPS/transport/ReliableMulticast/detail/PacketSerializer.h
@@ -0,0 +1,45 @@
+// -*- C++ -*-
+//
+
+#ifndef TAO_DCPS_PACKETSERIALIZER_H
+#define TAO_DCPS_PACKETSERIALIZER_H
+
+#include /**/ "ace/pre.h"
+#include /**/ "ace/config-all.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+namespace TAO
+{
+
+  namespace DCPS
+  {
+
+    namespace ReliableMulticast
+    {
+
+      namespace detail
+      {
+
+        class PacketSerializer
+        {
+        //@@todo: Add Code Here
+        };
+
+      } /* namespace detail */
+
+    } /* namespace ReliableMulticast */
+
+  } /* namespace DCPS */
+
+} /* namespace TAO */
+
+#if defined (__ACE_INLINE__)
+#include "PacketSerializer.inl"
+#endif /* __ACE_INLINE__ */
+
+#include /**/ "ace/post.h"
+
+#endif /* TAO_DCPS_PACKETSERIALIZER_H */
