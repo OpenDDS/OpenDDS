b8b82f0648e4dfe7895e1d449d06f83cd93295a9
Scott Harris <harris_s@ociweb.com>
2007-05-01 23:24:39 +0000

    Tue May  1 23:22:14 UTC 2007  Scott Harris <harris_s@ociweb.com>

--- tests/DCPS/Messenger_ZeroCopy/DataReaderListener.cpp
+++ tests/DCPS/Messenger_ZeroCopy/DataReaderListener.cpp
@@ -19,62 +19,6 @@ DataReaderListenerImpl::~DataReaderListenerImpl ()
 {
 }
 
-/// This is an allocator that simply uses malloc and free.
-/// A real allocator would most likely use a pool of memory.
-template<class T, std::size_t N>
-class BogusExampleAllocator : public ACE_Allocator
-{
-public:
-    BogusExampleAllocator() {};
-    virtual void *malloc (size_t nbytes) { 
-        return ACE_OS::malloc(nbytes);
-    };
-    virtual void free (void *ptr) {
-        ACE_OS::free(ptr);
-    };
-
-  /// These methods are no-ops.
-  virtual void *calloc (size_t nbytes, char initial_value = '\0') 
-        {/* no-op */ return (void*)0;};
-  virtual void *calloc (size_t n_elem, size_t elem_size, char initial_value = '\0')
-        {/* no-op */ return (void*)0;};
-
-  virtual int remove (void)
-        {/* no-op */ return -1; };
-  virtual int bind (const char *name, void *pointer, int duplicates = 0)
-        {/* no-op */ return -1; };
-  virtual int trybind (const char *name, void *&pointer)
-        {/* no-op */ return -1; };
-  virtual int find (const char *name, void *&pointer)
-        {/* no-op */ return -1; };
-  virtual int find (const char *name)
-        {/* no-op */ return -1; };
-  virtual int unbind (const char *name)
-        {/* no-op */ return -1; };
-  virtual int unbind (const char *name, void *&pointer)
-        {/* no-op */ return -1; };
-  virtual int sync (ssize_t len = -1, int flags = MS_SYNC)
-        {/* no-op */ return -1; };
-  virtual int sync (void *addr, size_t len, int flags = MS_SYNC)
-        {/* no-op */ return -1; };
-  virtual int protect (ssize_t len = -1, int prot = PROT_RDWR)
-        {/* no-op */ return -1; };
-  virtual int protect (void *addr, size_t len, int prot = PROT_RDWR)
-        {/* no-op */ return -1; };
-#if defined (ACE_HAS_MALLOC_STATS)
-  virtual void print_stats (void) const
-        {/* no-op */ };
-#endif /* ACE_HAS_MALLOC_STATS */
-  virtual void dump (void) const
-        {/* no-op */ };
-
-private:
-    // do not allow copies. - I am not sure this restriction is necessary.
-    BogusExampleAllocator(const BogusExampleAllocator&);
-    BogusExampleAllocator& operator=(const BogusExampleAllocator&);
-};
-
-
 void DataReaderListenerImpl::on_data_available(DDS::DataReader_ptr reader)
   throw (CORBA::SystemException)
 {
@@ -91,54 +35,13 @@ void DataReaderListenerImpl::on_data_available(DDS::DataReader_ptr reader)
       }
 
       const CORBA::Long MAX_ELEMS_TO_RETURN = 1;
-#if 0
-      // type NOT supporting zero-copy read
-      ::DDS::SampleInfoSeq the_info(MAX_ELEMS_TO_RETURN);
-      MessageSeq the_data (MAX_ELEMS_TO_RETURN);
-#else
-
-      // types supporting zero-copy read 
+      // using types supporting zero-copy read 
+      // Note: the 0 enables zero-copy reads/takes.
+      //       A value > 0 will enable single-copy reads/takes
+      //       The default contructor enables zero-copy reads/takes.
+      MessageZCSeq the_data (0, MAX_ELEMS_TO_RETURN);
       ::TAO::DCPS::SampleInfoZCSeq the_info(0,MAX_ELEMS_TO_RETURN);
 
-  #if 0
-      // This is an example of the user-code providing its
-      // own allocator for the sequence of pointers 
-      // to the the samples.  
-      BogusExampleAllocator<Messenger::Message*,MAX_ELEMS_TO_RETURN> the_allocator;
-      MessageZCSeq the_data (0, MAX_ELEMS_TO_RETURN, &the_allocator);
-  #else      
-      // Note: the default allocator for a ZCSeq is very fast because
-      // it will allocate from a pool on the stack and thus avoid
-      // a heap allocation but it is limited to a size of
-      // DCPS_ZERO_COPY_SEQ_DEFAULT_SIZE.
-      // The user might create a pooled allocator at a higher scope
-      // (like a class member) that can reuse the allocator's pool 
-      // and thus allow for fast and large sequences.
-      MessageZCSeq the_data (0, MAX_ELEMS_TO_RETURN);
-  #endif
-
-      //======= begin old std::vector impl ======
-      //BogusExampleAllocator<Messenger::Message*,1> allocator;
-      // MessageZCSeq<std::allocator<Messenger::Message*> > the_data (1);
-
-      // this does not work because the_data is not of type MessageZCSeq<>
-      //MessageZCSeq<BogusExampleAllocator<Messenger::Message*,1> > the_data (1);
-
-      //DataReaderListener.cpp(66) : error C3861: 'the_data': identifier not found, even with argument-dependent lookup
-      // because ">>" needs a space like "> >"
-      //MessageZCSeq<BogusExampleAllocator<Messenger::Message*,1>> the_data (1);
-    
-      //======= end old std::vector impl ======
-#endif
-#if 0
-      // Use the zero-copy API to get data buffers directly.
-      DDS::ReturnCode_t status = dr_impl->take (the_data
-                                                , the_info
-                                                , MAX_ELEMS_TO_RETURN
-                                                , ::DDS::ANY_SAMPLE_STATE
-                                                , ::DDS::ANY_VIEW_STATE
-                                                , ::DDS::ANY_INSTANCE_STATE);
-#else
       // Use the zero-copy API to get data buffers directly.
       DDS::ReturnCode_t status = dr_impl->read (the_data
                                                 , the_info
@@ -146,11 +49,8 @@ void DataReaderListenerImpl::on_data_available(DDS::DataReader_ptr reader)
                                                 , ::DDS::ANY_SAMPLE_STATE
                                                 , ::DDS::ANY_VIEW_STATE
                                                 , ::DDS::ANY_INSTANCE_STATE);
-#endif
 
       if (status == DDS::RETCODE_OK) {
-        void* addr = &(the_data[0]);
-        ACE_DEBUG((LM_INFO,"a value at %x / %x -0x10\n", addr, (&(the_data[0].count) )));
         cout << "Message: subject    = " << the_data[0].subject.in() << endl
              << "         subject_id = " << the_data[0].subject_id   << endl
              << "         from       = " << the_data[0].from.in()    << endl
@@ -163,7 +63,7 @@ void DataReaderListenerImpl::on_data_available(DDS::DataReader_ptr reader)
         cerr << "ERROR: read Message: Error: " <<  status << endl;
       }
 
-      // return the loaned data
+      // the application is required to return the loaned samples
       dr_impl->return_loan (the_data, the_info);
     }
   catch (CORBA::Exception& e) {
