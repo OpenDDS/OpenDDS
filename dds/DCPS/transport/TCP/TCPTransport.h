0ba35845e58bcf3fa320a9725e03467319a0d81b
Chad Elliott <elliott_c@ociweb.com>
2007-05-03 17:13:18 +0000

    ChangeLogTag: Thu May  3 17:13:58 UTC 2007  Chad Elliott  <elliott_c@ociweb.com>

--- dds/DCPS/transport/TCP/TCPTransport.h
+++ dds/DCPS/transport/TCP/TCPTransport.h
@@ -3,6 +3,7 @@
 
 #include "dds/DCPS/transport/framework/Transport.h"
 #include <ace/SOCK_Stream.h>
+#include <ace/Task.h>
 
 /// Implements the TransportAPI::Transport interface using TCP/IP.
 class TCPTransport: public TransportAPI::Transport
@@ -20,18 +21,28 @@ public:
   virtual void destroyLink(TransportAPI::Transport::Link* link);
 
 private:
+  bool active_;
   std::string hostname_;
-  std::string port_;
+  unsigned short port_;
 
   class BLOB: public TransportAPI::BLOB
   {
   public:
-    BLOB(const std::string& hostname, const std::string& port);
+    BLOB(const std::string& hostname,
+         unsigned short port,
+         bool active);
     const std::string& getHostname() const;
     unsigned short getPort() const;
+    bool getActive() const;
+
+  private:
+    bool active_;
+    std::string hostname_;
+    unsigned short port_;
   };
 
-  class Link: public TransportAPI::Transport::Link
+  class Link: public TransportAPI::Transport::Link,
+              public ACE_Task_Base
   {
   public:
     Link();
@@ -44,7 +55,13 @@ private:
 
     virtual TransportAPI::Status send(const iovec buffers[], size_t iovecSize, const TransportAPI::Id& requestId);
 
+    virtual int svc();
+
+    /// Tell the thread to end itself and wait for it.
+    void finish();
+
   private:
+    bool done_;
     TransportAPI::LinkCallback* callback_;
     ACE_SOCK_Stream stream_;
   };
