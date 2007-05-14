8274dd0aa89831b4509d4b6ac34f0e17e2938457
Adam Mitz <mitza@ociweb.com>
2007-05-14 17:59:45 +0000

    ChangeLogTag: Mon May 14 17:57:07 UTC 2007  Adam Mitz  <mitza@ociweb.com>

--- trunk/dds/DCPS/transport/simpleTCP/SimpleTcp.cpp
+++ trunk/dds/DCPS/transport/simpleTCP/SimpleTcp.cpp
@@ -1,6 +1,7 @@
 // $Id$
 #include "SimpleTcp_pch.h"
 #include "SimpleTcp.h"
+#include "SimpleTcp_export.h"
 #include "SimpleTcpLoader.h"
 #include "tao/debug.h"
 #include "ace/Dynamic_Service.h"
@@ -9,10 +10,9 @@ DCPS_SimpleTcp_Initializer::DCPS_SimpleTcp_Initializer (void)
 {
   ACE_Service_Config::process_directive (ace_svc_desc_DCPS_SimpleTcpLoader);
 
-  //This was added for static builds but is causing problems from non-static
-  //builds.  Disabling for now.  The static build may need to use -ORBSvcConf
-  //with a static directive to work-around.
-  //ACE_Service_Config::process_directive (ACE_TEXT ("static "
-  //                                                 "DCPS_SimpleTcpLoader "
-  //                                                 "\"-type SimpleTcp\""));
+#if SIMPLETCP_HAS_DLL == 0
+  ACE_Service_Config::process_directive (ACE_TEXT ("static "
+                                                   "DCPS_SimpleTcpLoader "
+                                                   "\"-type SimpleTcp\""));
+#endif
 }
