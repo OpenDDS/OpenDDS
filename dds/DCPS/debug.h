9d33808f3226dd0323f2dc08570c779d4186c3b3
Scott Harris <harris_s@ociweb.com>
2007-05-16 15:12:13 +0000

    Wed May 16 15:09:05 UTC 2007  Scott Harris  <harris_s@ociweb.com>

--- dds/DCPS/debug.h
+++ dds/DCPS/debug.h
@@ -26,6 +26,10 @@ namespace TAO
     /// 10 - logs that may happen more than once per sample write/read
     extern TAO_DdsDcps_Export unsigned int DCPS_debug_level;
 
+    /// The proper way to set the DCPS_debug_level.
+    /// This function allows for possible side-effects of setting the level.
+    extern void TAO_DdsDcps_Export set_DCPS_debug_level(unsigned int lvl);
+
   } // namespace TAO
 } // namespace DCPS
 
