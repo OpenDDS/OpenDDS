d80255946d593e03e29836b655308ec6ec0474ee
Adam Mitz <mitza@ociweb.com>
2007-05-23 22:29:25 +0000

    ChangeLogTag: Wed May 23 22:25:40 UTC 2007  Adam Mitz  <mitza@ociweb.com>

--- branches/20070426_no_CORBA/dds/DCPS/LocalObject.h
+++ branches/20070426_no_CORBA/dds/DCPS/LocalObject.h
@@ -10,7 +10,7 @@ namespace TAO
 {
   namespace DCPS
   {
-    // support "TAO::DCPS" style _prt and _var
+    // support "TAO::DCPS" style _ptr and _var
     typedef CORBA::LocalObject_ptr LocalObject_ptr;
     typedef CORBA::LocalObject_var LocalObject_var;
 
@@ -22,10 +22,7 @@ namespace TAO
     template <class Stub>
     class LocalObject
       : public virtual Stub
-      , public virtual CORBA::LocalObject
-      // Note: used CORBA::LocalObject instead of TAO_Local_RefCounted_Object
-      // to avoid reference counting problems (as in the_type->_add_ref();
-      // in Data_Types_Register::register_type)
+      , public virtual TAO_Local_RefCounted_Object
     {
     public:
       typedef typename Stub::_ptr_type _ptr_type;
