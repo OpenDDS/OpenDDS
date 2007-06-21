dbaf65950826d30d41f40d0825e3705255e334ea
Yan Dai <dai_y@ociweb.com>
2007-06-21 20:50:24 +0000

    Thu Jun 21 20:47:18 UTC 2007  Yan Dai  <dai_y@ociweb.com>

--- trunk/dds/InfoRepo/DCPS_Entity_Id_Generator.cpp
+++ trunk/dds/InfoRepo/DCPS_Entity_Id_Generator.cpp
@@ -1,11 +1,9 @@
 #include "DcpsInfo_pch.h"
 #include "DCPS_Entity_Id_Generator.h"
-
 #include "ace/Log_Msg.h"
 
-DCPS_Entity_Id_Generator::DCPS_Entity_Id_Generator (void)
+DCPS_Entity_Id_Generator::DCPS_Entity_Id_Generator ()
 {
-  nextEntity_ = (CORBA::Long) 1;
 }
 
 DCPS_Entity_Id_Generator::~DCPS_Entity_Id_Generator (void)
@@ -14,33 +12,105 @@ DCPS_Entity_Id_Generator::~DCPS_Entity_Id_Generator (void)
 
 
 
-CORBA::Long DCPS_Entity_Id_Generator::get_next_id ()
+CORBA::Long DCPS_Entity_Id_Generator::get_next_part_id ()
 {
-  CORBA::Long retValue = nextEntity_;
+  static CORBA::Long nextEntity = 1;
 
-  ++nextEntity_;
+  CORBA::Long retValue = nextEntity;
 
-  if (nextEntity_ == 0)
+  ++nextEntity;
+
+  if (nextEntity == 0)
     {
       // We have rolled over and there can now exist objects with
       // the same key.
       ACE_ERROR((LM_ERROR,
-        ACE_TEXT("ERROR: DCPS_Entity_Key_Generator::get_next_key() Exceeded Maximum number of keys!\n")
+        ACE_TEXT("ERROR: DCPS_Entity_Key_Generator::get_next_part_id() Exceeded Maximum number of keys!\n")
         ACE_TEXT(" Next key will be a duplicate!\n")
         ));
 
-      nextEntity_ = (CORBA::Long) 1;
+      nextEntity = (CORBA::Long) 1;
+    }
+
+  return retValue;
+}
+
+CORBA::Long DCPS_Entity_Id_Generator::get_next_topic_id ()
+{
+  static CORBA::Long nextEntity = 1;
+
+  CORBA::Long retValue = nextEntity;
+
+  ++nextEntity;
+
+  if (nextEntity == 0)
+    {
+      // We have rolled over and there can now exist objects with
+      // the same key.
+      ACE_ERROR((LM_ERROR,
+        ACE_TEXT("ERROR: DCPS_Entity_Key_Generator::get_next_topic_id() Exceeded Maximum number of keys!\n")
+        ACE_TEXT(" Next key will be a duplicate!\n")
+        ));
+
+      nextEntity = (CORBA::Long) 1;
+    }
+
+  return retValue;
+}
+
+CORBA::Long DCPS_Entity_Id_Generator::get_next_sub_pub_id ()
+{
+  static CORBA::Long nextEntity = 1;
+
+  CORBA::Long retValue = nextEntity;
+
+  ++nextEntity;
+
+  if (nextEntity == 0)
+    {
+      // We have rolled over and there can now exist objects with
+      // the same key.
+      ACE_ERROR((LM_ERROR,
+        ACE_TEXT("ERROR: DCPS_Entity_Key_Generator::get_next_sub_pub_id() Exceeded Maximum number of keys!\n")
+        ACE_TEXT(" Next key will be a duplicate!\n")
+        ));
+
+      nextEntity = (CORBA::Long) 1;
     }
 
   return retValue;
 }
 
 bool
-DCPS_Entity_Id_Generator::set_base_id (CORBA::Long id)
+DCPS_Entity_Id_Generator::set_base_part_id (CORBA::Long id)
 {
-  if (id > nextEntity_) {
-    nextEntity_ = id;
-  }
+  static CORBA::Long nextEntity = 1;
 
+  if (id > nextEntity) {
+    nextEntity = id;
+  }
   return true;
 }
+
+bool
+DCPS_Entity_Id_Generator::set_base_topic_id (CORBA::Long id)
+{
+  static CORBA::Long nextEntity = 1;
+
+  if (id > nextEntity) {
+    nextEntity = id;
+  }
+  return true;
+}
+
+bool
+DCPS_Entity_Id_Generator::set_base_sub_pub_id (CORBA::Long id)
+{
+  static CORBA::Long nextEntity = 1;
+
+  if (id > nextEntity) {
+    nextEntity = id;
+  }
+  return true;
+}
+
