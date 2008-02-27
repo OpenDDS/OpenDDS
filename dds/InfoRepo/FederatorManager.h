7b60b5613f7ff3fc653e03cc064fa1e16fae1acd
Mike Martinez <martinez_m@ociweb.com>
2008-02-27 02:18:56 +0000

    Wed Feb 27 02:16:42 UTC 2008  Mike Martinez  <martinez_m@ociweb.com>

---
+++ dds/InfoRepo/FederatorManager.h
@@ -0,0 +1,44 @@
+// -*- C++ -*-
+//
+// $Id$
+#ifndef FEDERATORMANAGER_H
+#define FEDERATORMANAGER_H
+
+#include "FederatorS.h"
+
+namespace OpenDDS { namespace Federator {
+
+class FederatorManager : public virtual POA_OpenDDS::Federator::Manager {
+  public:
+    /// Default constructor.
+    FederatorManager();
+
+    /// Virtual destructor.
+    virtual ~FederatorManager();
+
+    // IDL methods.
+
+    virtual ::OpenDDS::Federator::Status join_federation (
+      const char * endpoint
+    )
+    ACE_THROW_SPEC ((
+      ::CORBA::SystemException,
+      ::OpenDDS::Federator::Unavailable
+    ));
+
+    virtual ::OpenDDS::Federator::Status remove_connection (
+      ::OpenDDS::Federator::RepoKey remoteId
+    )
+    ACE_THROW_SPEC ((
+      ::CORBA::SystemException,
+      ::OpenDDS::Federator::ConnectionBusy
+    ));
+
+  private:
+
+};
+
+}} // End namespace OpenDDS::Federator
+
+#endif // FEDERATORMANAGER_H
+
