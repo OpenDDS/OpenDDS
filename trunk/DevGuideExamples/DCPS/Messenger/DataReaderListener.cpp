80d41ab55821da3cf0cc6a41b956859a25fb819f
Yan Dai <dai_y@ociweb.com>
2008-03-18 17:25:42 +0000

    Tue Mar 18 17:24:37 UTC 2008  Yan Dai  <dai_y@ociweb.com>

--- trunk/DevGuideExamples/DCPS/Messenger/DataReaderListener.cpp
+++ trunk/DevGuideExamples/DCPS/Messenger/DataReaderListener.cpp
@@ -41,6 +41,8 @@ void DataReaderListenerImpl::on_data_available(DDS::DataReader_ptr reader)
     //DDS::ReturnCode_t status = dr_servant->take_next_sample(message, si) ;
 
     if (status == DDS::RETCODE_OK) {
+      cout << "SampleInfo.sample_rank = " << si.sample_rank << endl;
+      cout << "SampleInfo.instance_state = " << si.instance_state << endl;
       if (si.valid_data == 1)
       {
         cout << "Message: subject    = " << message.subject.in() << endl
@@ -48,12 +50,11 @@ void DataReaderListenerImpl::on_data_available(DDS::DataReader_ptr reader)
           << "         from       = " << message.from.in()    << endl
           << "         count      = " << message.count        << endl
           << "         text       = " << message.text.in()    << endl;
-        cout << "SampleInfo.sample_rank = " << si.sample_rank << endl;
       }
       else
       {
         ACE_DEBUG ((LM_DEBUG, "(%P|%t)DataReaderListenerImpl::on_data_available:"
-                             " received DISPOSE_INSTANCE\n"));
+                             " received DISPOSE_INSTANCE message.\n"));
       }
     } else if (status == DDS::RETCODE_NO_DATA) {
       cerr << "ERROR: reader received DDS::RETCODE_NO_DATA!" << endl;
