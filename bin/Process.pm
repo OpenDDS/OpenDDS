4d265e02d20c8eddf02ea99f27a21d4b39c9d542
Brian Johnson <johnsonb@ociweb.com>
2008-04-09 15:56:23 +0000

    Wed Apr  9 15:53:00 UTC 2008  Brian Johnson  <johnsonb@ociweb.com>

--- bin/Process.pm
+++ bin/Process.pm
@@ -1,4 +1,5 @@
 # $Id$
+use PerlACE::Process;
 
 package PerlDDS::Process;
 
@@ -6,97 +7,32 @@ use strict;
 use English;
 use POSIX qw(:time_h);
 
-sub create {
-  my $executable = shift;
-  my $arguments = shift;
-  my $created;
+our @ISA = qw(PerlACE::Process);
 
-  if ((PerlACE::is_vxworks_test()) &&
-      (is_process_remote($executable))) {
-    $created = new PerlACE::ProcessVX($executable, $arguments);
-  }
-  elsif ((PerlDDS::is_coverage_test()) &&
-         (is_process_remote($executable))) {
-    my $cov_executable = $executable;
-    my $local_dir = $ENV{"DDS_ROOT"};
-    my $cov_dir = $ENV{"COV_DDS_ROOT"};
-    # try and substitute the coverage dir for the local dir
-    if($cov_executable !~ s/$local_dir/$cov_dir/)
-    {
-      # since it didn't match the executable must be 
-      # a relative path so just add the cov_dir
-	  $cov_executable = $cov_dir . '/' . $executable;
-    }
-    $created = new PerlACE::Process($cov_executable, $arguments);
-  }
-  else {
-    $created = new PerlACE::Process($executable, $arguments);
-  }
-  return $created;
-}
-
-sub is_process_remote {
-  my $executable = shift;
-  
-  # skip all this if we already have a remote process
-  # NOTE: may want to move this and log a message if we are trying to start 2
-  if(!PerlDDS::remote_process_created())
-  {
-    if(PerlDDS::is_remote_InfoRepo_test()) {
-      if(match($executable, "DCPSInfoRepo")) {
-        return 1;
-      }
-    }
-    elsif(PerlDDS::is_remote_sub_test()) {
-      if(match($executable, "sub")) {
-        return 1;
-      }
-    }
-    elsif(PerlDDS::is_remote_pub_test()) {
-      if(match($executable, "pub")) {
-        return 1;
-      }
-    }
-    elsif(PerlDDS::is_remote_other_test()) {
-      if(!match($executable, "DCPSInfoRepo") &&
-         !match($executable, "sub") &&
-         !match($executable, "pub")) {
-        return 1;
-      }
-    }
-  }
-}
-
-sub match {
-  my $executable = lc(shift);
-  my $to_match = lc(shift);
-  return ($executable =~ /$to_match[^\\\/]*$/);
+sub new {
+    my $proto = shift;
+    my $class = ref ($proto) || $proto;
+    my $executable = shift;
+    my $arguments = shift;
+    my $self = PerlACE::Process->new($executable, $arguments);
+	bless($self, $class);
+	return $self;
 }
 
 sub Spawn {
-  my $process = shift;
-  my $is_remote = is_process_remote($process->{EXECUTABLE});
-  if($is_remote) {
-    add_lib_path('$ENV{"COV_DDS_ROOT"}/lib');
-  }
-  my $ret_value = $process->Spawn();
-  if($is_remote) {
-    remove_lib_path('$ENV{"COV_DDS_ROOT"}/lib');
-  }
+  my $self = shift;
+  add_lib_path('$ENV{"COV_DDS_ROOT"}/lib');
+  my $ret_value = $self->SUPER::Spawn();
+  remove_lib_path('$ENV{"COV_DDS_ROOT"}/lib');
   return $ret_value;
 }
 
 sub SpawnWaitKill {
-  my $process = shift;
+  my $self = shift;
   my $seconds = shift;
-  my $is_remote = is_process_remote($process->{EXECUTABLE});
-  if($is_remote) {
-    add_lib_path('$ENV{"COV_DDS_ROOT"}/lib');
-  }
-  my $ret_value = $process->SpawnWaitKill($seconds);
-  if($is_remote) {
-    remove_lib_path('$ENV{"COV_DDS_ROOT"}/lib');
-  }
+  add_lib_path('$ENV{"COV_DDS_ROOT"}/lib');
+  my $ret_value = $self->SUPER::SpawnWaitKill($seconds);
+  remove_lib_path('$ENV{"COV_DDS_ROOT"}/lib');
   return $ret_value;
 }
 
