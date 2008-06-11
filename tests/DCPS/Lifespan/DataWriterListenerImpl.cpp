a25d2828c463cb9dca78f7d70333e41fd8f37c1d
Ossama Othman <othmano@ociweb.com>
2008-06-11 06:00:41 +0000

    ChangeLogTag:Wed Jun 11 05:58:20 UTC 2008  Ossama Othman  <othmano@ociweb.com>

--- tests/DCPS/Lifespan/DataWriterListenerImpl.cpp
+++ tests/DCPS/Lifespan/DataWriterListenerImpl.cpp
@@ -1,116 +1,81 @@
+// $Id$
+
 #include "DataWriterListenerImpl.h"
 #include "dds/DdsDcpsPublicationC.h"
-#include "dds/DCPS/Service_Participant.h"
 
 DataWriterListenerImpl::DataWriterListenerImpl ()
-: publication_matched_(false)
+  : publication_matched_ (false)
 {
 }
 
-// Implementation skeleton destructor
 DataWriterListenerImpl::~DataWriterListenerImpl ()
 {
 }
 
-void DataWriterListenerImpl::on_offered_deadline_missed (
-      ::DDS::DataWriter_ptr writer,
-      const ::DDS::OfferedDeadlineMissedStatus & status
-    )
-    ACE_THROW_SPEC ((
-      ::CORBA::SystemException
-    ))
+void
+DataWriterListenerImpl::on_offered_deadline_missed (
+    ::DDS::DataWriter_ptr /* writer */,
+    ::DDS::OfferedDeadlineMissedStatus const & /* status */)
+  ACE_THROW_SPEC ((::CORBA::SystemException))
 {
-  ACE_UNUSED_ARG(writer) ;
-  ACE_UNUSED_ARG(status) ;
 }
 
-void DataWriterListenerImpl::on_offered_incompatible_qos (
-      ::DDS::DataWriter_ptr writer,
-      const ::DDS::OfferedIncompatibleQosStatus & status
-    )
-    ACE_THROW_SPEC ((
-      ::CORBA::SystemException
-    ))
+void
+DataWriterListenerImpl::on_offered_incompatible_qos (
+    ::DDS::DataWriter_ptr /* writer */,
+    ::DDS::OfferedIncompatibleQosStatus const & /* status */)
+  ACE_THROW_SPEC ((::CORBA::SystemException))
 {
-  ACE_UNUSED_ARG(writer) ;
-  ACE_UNUSED_ARG(status) ;
-
-  ACE_ERROR ((LM_DEBUG,
-         ACE_TEXT("(%P|%t) DataWriterListenerImpl::on_offered_incompatible_qos "
-         "This should appear when the test is designed to be incompatible.\n")));
 }
 
-void DataWriterListenerImpl::on_liveliness_lost (
-      ::DDS::DataWriter_ptr writer,
-      const ::DDS::LivelinessLostStatus & status
-    )
-    ACE_THROW_SPEC ((
-      ::CORBA::SystemException
-    ))
+void
+DataWriterListenerImpl::on_liveliness_lost (
+    ::DDS::DataWriter_ptr /* writer */,
+    ::DDS::LivelinessLostStatus const & /* status */)
+  ACE_THROW_SPEC ((::CORBA::SystemException))
 {
-  ACE_UNUSED_ARG(writer) ;
-  ACE_UNUSED_ARG(status) ;
 }
   
-void DataWriterListenerImpl::on_publication_match (
-      ::DDS::DataWriter_ptr writer,
-      const ::DDS::PublicationMatchStatus & status
-    )
-    ACE_THROW_SPEC ((
-      ::CORBA::SystemException
-    ))
+void
+DataWriterListenerImpl::on_publication_match (
+    ::DDS::DataWriter_ptr /* writer */,
+    ::DDS::PublicationMatchStatus const & /* status */)
+  ACE_THROW_SPEC ((::CORBA::SystemException))
 {
-  ACE_UNUSED_ARG(writer) ;
-  ACE_UNUSED_ARG(status) ;
-
-  publication_matched_ = true;
+  this->publication_matched_ = true;
 
   ACE_DEBUG((LM_DEBUG,
-    ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_publication_match \n")));
+             ACE_TEXT("(%P|%t) DataReaderListenerImpl::")
+             ACE_TEXT("on_publication_match\n")));
 }
 
-void DataWriterListenerImpl::on_publication_disconnected (
-      ::DDS::DataWriter_ptr writer,
-      const ::OpenDDS::DCPS::PublicationDisconnectedStatus & status
-    )
-    ACE_THROW_SPEC ((
-      ::CORBA::SystemException
-    ))
+void
+DataWriterListenerImpl::on_publication_disconnected (
+    ::DDS::DataWriter_ptr /* writer */,
+    ::OpenDDS::DCPS::PublicationDisconnectedStatus const & /* status */)
+  ACE_THROW_SPEC ((::CORBA::SystemException))
 {
-  ACE_UNUSED_ARG(writer) ;
-  ACE_UNUSED_ARG(status) ;
 }
 
-void DataWriterListenerImpl::on_publication_reconnected (
-      ::DDS::DataWriter_ptr writer,
-      const ::OpenDDS::DCPS::PublicationReconnectedStatus & status
-    )
-    ACE_THROW_SPEC ((
-      ::CORBA::SystemException
-    ))
+void
+DataWriterListenerImpl::on_publication_reconnected (
+    ::DDS::DataWriter_ptr /* writer */,
+    ::OpenDDS::DCPS::PublicationReconnectedStatus const & /* status */)
+  ACE_THROW_SPEC ((::CORBA::SystemException))
 {
-  ACE_UNUSED_ARG(writer) ;
-  ACE_UNUSED_ARG(status) ;
 }
 
-void DataWriterListenerImpl::on_publication_lost (
-      ::DDS::DataWriter_ptr writer,
-      const ::OpenDDS::DCPS::PublicationLostStatus & status
-    )
-    ACE_THROW_SPEC ((
-      ::CORBA::SystemException
-    ))
+void
+DataWriterListenerImpl::on_publication_lost (
+    ::DDS::DataWriter_ptr /* writer */,
+    ::OpenDDS::DCPS::PublicationLostStatus const & /* status */)
+  ACE_THROW_SPEC ((::CORBA::SystemException))
 {
-  ACE_UNUSED_ARG(writer) ;
-  ACE_UNUSED_ARG(status) ;
 }
 
-void DataWriterListenerImpl::on_connection_deleted (
-      ::DDS::DataWriter_ptr writer
-    )
-    ACE_THROW_SPEC ((
-      ::CORBA::SystemException
-    ))
+void
+DataWriterListenerImpl::on_connection_deleted (
+    ::DDS::DataWriter_ptr /* writer */)
+  ACE_THROW_SPEC ((::CORBA::SystemException))
 {
-  ACE_UNUSED_ARG(writer) ;
 }
