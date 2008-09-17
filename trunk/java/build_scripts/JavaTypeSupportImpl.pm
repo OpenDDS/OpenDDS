e5900182df29955afd332e294459d0041c6bced3
Adam Mitz <mitza@ociweb.com>
2008-09-17 19:02:22 +0000

    ChangeLogTag: Wed Sep 17 18:47:00 UTC 2008  Adam Mitz  <mitza@ociweb.com>

--- trunk/java/build_scripts/JavaTypeSupportImpl.pm
+++ trunk/java/build_scripts/JavaTypeSupportImpl.pm
@@ -6,8 +6,17 @@ use File::Basename;
 sub generate {
     my $pkg = shift;
     my $aref = shift;
+    my $ext_args = shift;
+    my $file_names = shift;
+
+    my $cpp_file = undef;
+    if ($ext_args =~ /cpp_file=(\S+)/) {
+      $cpp_file = $1;
+    }
+
     my %extra;
-    $extra{'cpp'} = '#include "idl2jni_jni.h"' . "\n";
+    $extra{'cpp'} = '#include "idl2jni_jni.h"' . "\n\n";
+    $extra{'cpp'}.= '#include "' . $file_names->[1] . "\"\n\n" if $cpp_file;
     for my $type (@$aref) {
         my @path = split ('::', $type);
         my $file = join ('/', @path) . 'TypeSupportImpl.java';
@@ -37,8 +46,17 @@ Java_${jniclass}TypeSupportImpl__1jni_1init (JNIEnv *, jclass)
 {
   return reinterpret_cast<jlong> (CORBA::Object::_duplicate (new ${type}TypeSupportImpl));
 }
+
 EOT
+    } # for each type...
+
+    if ($cpp_file) {
+      open (CPP, ">$cpp_file") or die "Failed to open $file\n";
+      print CPP $extra{'cpp'};
+      close CPP;
+      delete $extra{'cpp'};
     }
+
     return \%extra;
 }
 
