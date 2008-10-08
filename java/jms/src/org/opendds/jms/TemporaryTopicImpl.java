ee6ab7a0d0694d59394bb44ce2e90fc638a4cdda
Steven Stallion <stallions@ociweb.com>
2008-10-08 15:43:48 +0000

    Wed Oct  8 15:42:20 UTC 2008  Steven Stallion  <stallions@ociweb.com>

--- java/jms/src/org/opendds/jms/TemporaryTopicImpl.java
+++ java/jms/src/org/opendds/jms/TemporaryTopicImpl.java
@@ -5,23 +5,23 @@
 package org.opendds.jms;
 
 import javax.jms.JMSException;
-
-import org.opendds.jms.impl.DDSTopic;
+import javax.jms.Topic;
 
 /**
  * @author  Steven Stallion
  * @version $Revision$
  */
-public class TemporaryTopicImpl implements DDSTopic {
-
-    public String getDestinationName() {
-        return null;
-    }
+public class TemporaryTopicImpl implements Topic {
 
     public String getTopicName() throws JMSException {
         return null;
     }
 
     public void delete() throws JMSException {
+    }
+
+    @Override
+    public String toString() {
+        return null;
     }
 }
