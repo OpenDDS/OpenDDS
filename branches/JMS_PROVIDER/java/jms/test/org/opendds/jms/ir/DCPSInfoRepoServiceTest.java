b25aafeda24a9a676c149a1849e7df8a87c17b91
Steven Stallion <stallions@ociweb.com>
2008-10-20 19:23:15 +0000

    Mon Oct 20 19:21:43 UTC 2008  Steven Stallion  <stallions@ociweb.com>

---
+++ branches/JMS_PROVIDER/java/jms/test/org/opendds/jms/ir/DCPSInfoRepoServiceTest.java
@@ -0,0 +1,61 @@
+/*
+ * $Id$
+ */
+
+package org.opendds.jms.ir;
+
+import org.junit.Test;
+
+/**
+ * @author  Steven Stallion
+ * @version $Revision$
+ */
+public class DCPSInfoRepoServiceTest {
+
+    @Test(expected = NullPointerException.class)
+    public void errorWithNullArgs() {
+        new DCPSInfoRepoService(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void errorWithNullArg() {
+        new DCPSInfoRepoService(new String[] { "1", "2", null });
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void errorRunAfterFini() {
+        DCPSInfoRepoService service =
+            new DCPSInfoRepoService(new String[] { "-NOBITS" });
+
+        service.fini();
+        service.run();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void errorShutdownAfterFini() {
+        DCPSInfoRepoService service =
+            new DCPSInfoRepoService(new String[] { "-NOBITS" });
+
+        service.fini();
+        service.shutdown();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUsage() {
+        new DCPSInfoRepoService(new String[] { "-?" });
+    }
+
+    @Test
+    public void testRunWithShutdown() throws Exception {
+        DCPSInfoRepoService service =
+            new DCPSInfoRepoService(new String[] { "-NOBITS" });
+
+        Thread t = new Thread(service);
+        t.start();
+
+        Thread.sleep(10000);
+
+        service.shutdown(true);
+        t.join();
+    }
+}
