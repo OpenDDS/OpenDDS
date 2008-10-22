f80b51ca05ca0545a629de8903f5374451d0bfbf
Friedhelm Wolf <fwolf@dre.vanderbilt.edu>
2008-10-22 16:33:49 +0000

    add portability headers

---
+++ wrapper_subscription.h
@@ -0,0 +1,26 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    wrapper_subscription.h
+ *
+ *  $Id$
+ *
+ * @author   Friedhelm Wolf (fwolf@dre.vanderbilt.edu)
+ */
+//=============================================================================
+
+#ifndef _DDS_WRAPPER_SUBSCRIPTION_H_
+#define _DDS_WRAPPER_SUBSCRIPTION_H_
+
+#include "wrapper_config.h"
+
+#if defined (OPEN_SPLICE_CONFIG)
+
+#elif defined (OPEN_DDS_CONFIG)
+
+#include <dds/DdsDcpsSubscriptionC.h>
+
+#endif
+
+#endif /* _DDS_WRAPPER_SUBSCRIPTION_H_ */
