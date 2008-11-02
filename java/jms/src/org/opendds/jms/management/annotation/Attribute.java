e81d181a8fc3c17d1ac741945feed543b4159655
Steven Stallion <stallions@ociweb.com>
2008-11-02 20:46:29 +0000

    TEMPORARY COMMIT

--- java/jms/src/org/opendds/jms/management/annotation/Attribute.java
+++ java/jms/src/org/opendds/jms/management/annotation/Attribute.java
@@ -1,16 +1,25 @@
 /*
- * $
+ * $Id$
  */
 
 package org.opendds.jms.management.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
+ * Indicates an MBean attribute.  This type should be applied to
+ * accessor (getter) methods only.
+ *
  * @author  Steven Stallion
  * @version $Revision$
+ *
+ * @see     javax.management.MBeanAttributeInfo
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Attribute {
-}
+@Target(ElementType.METHOD)
+public @interface Attribute {}
