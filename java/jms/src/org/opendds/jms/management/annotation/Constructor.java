3becd4247b0236fd6231b2583fec9d62db506ba3
Steven Stallion <stallions@ociweb.com>
2008-11-05 03:15:31 +0000

    Wed Nov  5 02:08:06 UTC 2008  Steven Stallion  <stallions@ociweb.com>

--- java/jms/src/org/opendds/jms/management/annotation/Constructor.java
+++ java/jms/src/org/opendds/jms/management/annotation/Constructor.java
@@ -5,10 +5,10 @@
 package org.opendds.jms.management.annotation;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Indicates an MBean constructor.  This is only required for
