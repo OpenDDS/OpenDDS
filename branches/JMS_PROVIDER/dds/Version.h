1ced8032847a10dffb7b085c615504be22daf0ff
Adam Mitz <mitza@ociweb.com>
2008-11-07 17:53:56 +0000

    ChangeLogTag: Fri Nov  7 17:49:09 UTC 2008  Adam Mitz  <mitza@ociweb.com>

--- branches/JMS_PROVIDER/dds/Version.h
+++ branches/JMS_PROVIDER/dds/Version.h
@@ -2,6 +2,7 @@
 // $Id$
 
 #define DDS_MAJOR_VERSION 1
-#define DDS_MINOR_VERSION 1
-#define DDS_VERSION "1.1"
+#define DDS_MINOR_VERSION 2
+#define DDS_MICRO_VERSION 1
+#define DDS_VERSION "1.2.1"
 
