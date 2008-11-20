c6c4b5c76d22aeca729a6b9a5fac2f4ba8984641
Adam Mitz <mitza@ociweb.com>
2008-11-20 21:25:59 +0000

    ChangeLogTag: Thu Nov 20 21:24:39 UTC 2008  Adam Mitz  <mitza@ociweb.com>

--- java/dds/OpenDDS/DCPS/transport/AttachStatus.java
+++ java/dds/OpenDDS/DCPS/transport/AttachStatus.java
@@ -4,22 +4,35 @@ import java.io.Serializable;
 
 public class AttachStatus implements Serializable {
 
+  private static AttachStatus[] __values = {
+    new AttachStatus(0),
+    new AttachStatus(1),
+    new AttachStatus(2),
+    new AttachStatus(3)
+  };
+
   public static final int _ATTACH_BAD_TRANSPORT = 0;
-  public static final AttachStatus ATTACH_BAD_TRANSPORT = new AttachStatus(_ATTACH_BAD_TRANSPORT);
+  public static final AttachStatus ATTACH_BAD_TRANSPORT = __values[0];
 
   public static final int _ATTACH_ERROR = 1;
-  public static final AttachStatus ATTACH_ERROR = new AttachStatus(_ATTACH_ERROR);
+  public static final AttachStatus ATTACH_ERROR = __values[1];
 
   public static final int _ATTACH_INCOMPATIBLE_QOS = 2;
-  public static final AttachStatus ATTACH_INCOMPATIBLE_QOS = new AttachStatus(_ATTACH_INCOMPATIBLE_QOS);
+  public static final AttachStatus ATTACH_INCOMPATIBLE_QOS = __values[2];
 
   public static final int _ATTACH_OK = 3;
-  public static final AttachStatus ATTACH_OK = new AttachStatus(_ATTACH_OK);
+  public static final AttachStatus ATTACH_OK = __values[3];
 
   public int value() { return _value; }
   private int _value;
   public static AttachStatus from_int(int value) {
-    return new AttachStatus (value);
+    return __values[value];
   }
   protected AttachStatus(int value) { _value = value; }
+
+  public Object readResolve()
+      throws java.io.ObjectStreamException {
+    return from_int(value());
+  }
+
 }
