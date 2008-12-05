0edcffd22c293038da47f277cecd985628aad71a
Steven Stallion <stallions@ociweb.com>
2008-12-05 00:44:43 +0000

    Fri Dec  5 00:41:34 UTC 2008  Steven Stallion  <stallions@ociweb.com>

--- branches/JMS_PROVIDER/java/jms/src/org/opendds/jms/management/PersistenceManagerService.java
+++ branches/JMS_PROVIDER/java/jms/src/org/opendds/jms/management/PersistenceManagerService.java
@@ -4,13 +4,10 @@
 
 package org.opendds.jms.management;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 import org.opendds.jms.common.util.JndiHelper;
 import org.opendds.jms.common.util.Logger;
-import org.opendds.jms.common.util.PropertiesHelper;
 import org.opendds.jms.management.annotation.Attribute;
 import org.opendds.jms.management.annotation.Constructor;
 import org.opendds.jms.management.annotation.Description;
@@ -35,27 +32,27 @@ public class PersistenceManagerService extends DynamicMBeanSupport implements Se
 
     @Constructor
     public PersistenceManagerService() {
-        registerAttribute("CacheProviderClass", String.class);
-        registerAttribute("CacheRegionPrefix", String.class);
-        registerAttribute("CacheUseMinimalPuts", Boolean.class);
-        registerAttribute("CacheUseQueryCache", Boolean.class);
-        registerAttribute("DataSource", String.class);
-        registerAttribute("DefaultSchema", String.class);
-        registerAttribute("Dialect", String.class);
-        registerAttribute("GenerateStatistics", Boolean.class);
-        registerAttribute("Hbm2ddlAuto", Boolean.class);
-        registerAttribute("JdbcBatchSize", Integer.class);
-        registerAttribute("JdbcBatchVersionedData", Boolean.class);
-        registerAttribute("JdbcFetchSize", Integer.class);
-        registerAttribute("JdbcUseGetGeneratedKeys", Boolean.class);
-        registerAttribute("JdbcUseScrollableResultSet", Boolean.class);
-        registerAttribute("JdbcUseStreamsForBinary", Boolean.class);
-        registerAttribute("MaxFetchDepth", Integer.class);
-        registerAttribute("Password", String.class);
-        registerAttribute("QuerySubstitutions", String.class);
-        registerAttribute("ShowSql", Boolean.class);
-        registerAttribute("UseSqlComments", Boolean.class);
-        registerAttribute("Username", String.class);
+        registerAttribute("CacheProviderClass", "hibernate.cache.provider_class", String.class);
+        registerAttribute("CacheRegionPrefix", "hibernate.cache.region_prefix", String.class);
+        registerAttribute("CacheUseMinimalPuts", "hibernate.cache.use_minimal_puts", Boolean.class);
+        registerAttribute("CacheUseQueryCache", "hibernate.cache.use_query_cache", Boolean.class);
+        registerAttribute("DataSource", "hibernate.connection.datasource", String.class);
+        registerAttribute("DefaultSchema", "hibernate.default_schema", String.class);
+        registerAttribute("Dialect", "hibernate.dialect", String.class);
+        registerAttribute("GenerateStatistics", "hibernate.generate_statistics", Boolean.class);
+        registerAttribute("Hbm2ddlAuto", "hibernate.jdbc.batch_size", String.class);
+        registerAttribute("JdbcBatchSize", "hibernate.jdbc.batch_versioned_data", Integer.class);
+        registerAttribute("JdbcBatchVersionedData", "hibernate.jdbc.fetch_size", Boolean.class);
+        registerAttribute("JdbcFetchSize", "hibernate.jdbc.use_get_generated_keys", Integer.class);
+        registerAttribute("JdbcUseGetGeneratedKeys", "hibernate.jdbc.use_scrollable_resultset", Boolean.class);
+        registerAttribute("JdbcUseScrollableResultSet", "hibernate.jdbc.use_streams_for_binary", Boolean.class);
+        registerAttribute("JdbcUseStreamsForBinary", "hibernate.hbm2ddl.auto", Boolean.class);
+        registerAttribute("MaxFetchDepth", "hibernate.max_fetch_depth", Integer.class);
+        registerAttribute("Password", "hibernate.connection.password", String.class);
+        registerAttribute("QuerySubstitutions", "hibernate.query.substitutions", String.class);
+        registerAttribute("ShowSql", "hibernate.show_sql", Boolean.class);
+        registerAttribute("UseSqlComments", "hibernate.use_sql_comments", Boolean.class);
+        registerAttribute("Username", "hibernate.connection.username", String.class);
     }
 
     @Attribute(readOnly = true)
@@ -82,34 +79,6 @@ public class PersistenceManagerService extends DynamicMBeanSupport implements Se
         return started;
     }
 
-    protected Map<String, String> mappedPropertyNames() {
-        Map<String, String> names = new HashMap<String, String>();
-
-        names.put("CacheProviderClass", "hibernate.cache.provider_class");
-        names.put("CacheRegionPrefix", "hibernate.cache.region_prefix");
-        names.put("CacheUseMinimalPuts", "hibernate.cache.use_minimal_puts");
-        names.put("CacheUseQueryCache", "hibernate.cache.use_query_cache");
-        names.put("DataSource", "hibernate.connection.datasource");
-        names.put("DefaultSchema", "hibernate.default_schema");
-        names.put("Dialect", "hibernate.dialect");
-        names.put("GenerateStatistics", "hibernate.generate_statistics");
-        names.put("JdbcBatchSize", "hibernate.jdbc.batch_size");
-        names.put("JdbcBatchVersionedData", "hibernate.jdbc.batch_versioned_data");
-        names.put("JdbcFetchSize", "hibernate.jdbc.fetch_size");
-        names.put("JdbcUseGetGeneratedKeys", "hibernate.jdbc.use_get_generated_keys");
-        names.put("JdbcUseScrollableResultSet", "hibernate.jdbc.use_scrollable_resultset");
-        names.put("JdbcUseStreamsForBinary", "hibernate.jdbc.use_streams_for_binary");
-        names.put("Hbm2ddlAuto", "hibernate.hbm2ddl.auto");
-        names.put("MaxFetchDepth", "hibernate.max_fetch_depth");
-        names.put("Password", "hibernate.connection.password");
-        names.put("QuerySubstitutions", "hibernate.query.substitutions");
-        names.put("ShowSql", "hibernate.show_sql");
-        names.put("UseSqlComments", "hibernate.use_sql_comments");
-        names.put("Username", "hibernate.connection.username");
-
-        return names;
-    }
-
     @Operation
     public void start() throws Exception {
         if (isStarted()) {
@@ -121,7 +90,6 @@ public class PersistenceManagerService extends DynamicMBeanSupport implements Se
         logger = Logger.getLogger(service);
 
         Properties properties = attributes.toProperties();
-        PropertiesHelper.remap(properties, mappedPropertyNames());
 
         logger.debug("Initializing with properties: %s", properties);
         instance = new PersistenceManager(properties);
