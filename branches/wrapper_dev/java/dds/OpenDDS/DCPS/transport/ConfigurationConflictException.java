230440ccbc4b083dfc8b70124bfd8d71c5bf1592
Friedhelm Wolf <fwolf@dre.vanderbilt.edu>
2008-12-18 21:17:59 +0000

    creating new developer branch for the wrapper libraries

---
+++ branches/wrapper_dev/java/dds/OpenDDS/DCPS/transport/ConfigurationConflictException.java
@@ -0,0 +1,8 @@
+package OpenDDS.DCPS.transport;
+
+public class ConfigurationConflictException extends TransportException {
+
+    public ConfigurationConflictException(String message) {
+        super(message);
+    }
+}
