0bf448cb34c09b96e9e6ae8be5befab344db757d
Steven Stallion <stallions@ociweb.com>
2008-12-23 18:56:08 +0000

    Tue Dec 23 18:55:25 UTC 2008  Steven Stallion  <stallions@ociweb.com>

--- branches/ERLANG_SUPPORT/erlang/lib/opendds_ic/cpp_src/main.cpp
+++ branches/ERLANG_SUPPORT/erlang/lib/opendds_ic/cpp_src/main.cpp
@@ -5,7 +5,7 @@
 // The following files are included which allows us to define our
 // own BE while reusing the original tao_idl FE.
 
-#include "TAO_IDL/tao_idl.cpp"
+#include "tao_idl.cpp"
 
-#include "TAO_IDL/driver/drv_args.cpp"
-#include "TAO_IDL/driver/drv_preproc.cpp"
+#include "driver/drv_args.cpp"
+#include "driver/drv_preproc.cpp"
