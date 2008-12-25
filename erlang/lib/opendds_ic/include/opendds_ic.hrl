e2038237e5449e6ede8b4bd208943f37ae6c047b
Steven Stallion <stallions@ociweb.com>
2008-12-25 05:29:22 +0000

    Thu Dec 25 05:27:56 UTC 2008  Steven Stallion  <stallions@ociweb.com>

---
+++ erlang/lib/opendds_ic/include/opendds_ic.hrl
@@ -0,0 +1,9 @@
+%%%---------------------------------------------------------------------
+%%% $Id$
+%%%---------------------------------------------------------------------
+
+%% Port command to spawn.
+-define(COMMAND, "opendds_ic").
+
+%% Maximum number of characters per line read.
+-define(LINE_MAX, 4096).
