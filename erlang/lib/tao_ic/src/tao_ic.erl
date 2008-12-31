12fbd4d7ffcfd8a383d97dd5ee5b30762418c260
Steven Stallion <stallions@ociweb.com>
2008-12-31 20:49:28 +0000

    Wed Dec 31 20:45:06 UTC 2008  Steven Stallion  <stallions@ociweb.com>

--- erlang/lib/tao_ic/src/tao_ic.erl
+++ erlang/lib/tao_ic/src/tao_ic.erl
@@ -12,7 +12,7 @@ gen(File) ->
     gen(File, []).
 
 gen(File, Opts) ->
-    gen0(map_options(Opts) ++ [to_list(File)]).
+    gen0(map_options(Opts) ++ [lists_:to_list(File)]).
 
 version() ->
     gen0(["-V"]).
@@ -29,42 +29,45 @@ loop(Port) ->
     receive
         {Port, {exit_status, 0}} ->
             ok;
-
         {Port, {exit_status, _Status}} ->
             error; % non-zero exit status
-
         {Port, {data, {_Flag, Line}}} ->
             io:format("~s~n", [Line]),
             loop(Port) % tail-recursive
     end.
 
 command(Opts) ->
-    string:join([?COMMAND|Opts], " ").
+    Command = os:find_executable(?COMMAND),
+    case Command of
+        false ->
+            error:badcmd(?COMMAND);
+        _Else ->
+            string:join([Command|Opts], " ")
+    end.    
 
 map_options(Opts) ->
-    lists:map(fun map_option/1, Opts).
+    [map_option(Opt) || Opt <- Opts].
 
 map_option(Opt) ->
     case Opt of
-        % frontend options
         {local_escape, Esc} ->
-            string:concat("-A", Esc);
+            "-A" ++ Esc;
         {dump, true} ->
             "-d";
         {preproc_cmd, Cmd} ->
-            string:concat("-Yp,", Cmd);
+            "-Yp," ++ Cmd;
         {preproc_flags, Flags} ->
-            string:concat("-Wp,", string:join(Flags, ","));
+            "-Wp," ++ string:join(Flags, ",");
         {preproc_to_stdout, true} ->
             "-E";
         {include, Dir} ->
-            string:concat("-I", Dir);
+            "-I" ++ Dir;
         {define, Name} ->
-            string:concat("-D", Name);
+            "-D" ++ Name;
         {define, Name, Value} ->
-            string:concat("-D", Name) ++ string:concat("=", Value);
+            "-D" ++ Name ++ "=" ++ Value;
         {undef, Name} ->
-            string:concat("-U", Name);
+            "-U" ++ Name;
         {trace, true} ->
             "-v";
         {silent, true} ->
@@ -74,39 +77,32 @@ map_option(Opt) ->
         {error_case, true} ->
             "-Ce";
         {tempdir, Dir} ->
-            string:concat("-t ", Dir);
-
-        % backend options
+            "-t " ++ Dir;
         {be, T} ->
             "-Wb," ++
             case T of
                 {port_driver_name, Name} ->
-                    string:concat("port_driver_name=", Name);
+                    "port_driver_name=" ++ Name;
                 {skel_export_macro, Macro} ->
-                    string:concat("skel_export_macro=", Macro);
+                    "skel_export_macro=" ++ Macro;
                 {skel_export_include, Include} ->
-                    string:concat("skel_export_include=", Include);
+                    "skel_export_include=" ++ Include;
                 {stub_export_macro, Macro} ->
-                    string:concat("stub_export_macro=", Macro);
+                    "stub_export_macro=" ++ Macro;
                 {stub_export_include, Include} ->
-                    string:concat("stub_export_include=", Include);
+                    "stub_export_include=" ++ Include;
                 _Else ->
-                    throw({invalid_be_option, T})
+                    error:badarg(Opt)
             end;
         {suppress_skel, true} ->
             "-SS";
         {otp, true} ->
             "-otp";
         {outputdir, Dir} ->
-            string:concat("-o ", Dir);
+            "-o " ++ Dir;
 
         {_A, false} ->
             ""; % ignore option
         _Else ->
-            throw({invalid_option, Opt})
+            error:badarg(Opt)
     end.
-
-to_list(A) when is_atom(A) ->
-    atom_to_list(A);
-to_list(L) when is_list(L) ->
-    L.
