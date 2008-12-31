9821d8386f3a28bcb306ffae63fa2ca252ea10ca
Steven Stallion <stallions@ociweb.com>
2008-12-31 20:49:28 +0000

    Wed Dec 31 20:45:06 UTC 2008  Steven Stallion  <stallions@ociweb.com>

---
+++ branches/ERLANG_SUPPORT/erlang/lib/common/src/lists_.erl
@@ -0,0 +1,21 @@
+%%%---------------------------------------------------------------------
+%%% $Id$
+%%%---------------------------------------------------------------------
+
+-module(lists_).
+
+-export([to_list/1]).
+
+to_list(L) when is_list(L) ->
+    L;
+to_list(B) when is_binary(B) ->
+    binary_to_list(B);
+to_list(A) when is_atom(A) ->
+    atom_to_list(A);
+to_list(T) when is_tuple(T) ->
+    tuple_to_list(T);
+to_list(X) when is_integer(X) ->
+    integer_to_list(X);
+to_list(X) when is_float(X) ->
+    float_to_list(X).
+
