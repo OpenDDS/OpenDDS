674e01b5070060486f59c96f39f2a7d4b56cd76f
Steven Stallion <stallions@ociweb.com>
2008-12-31 22:59:59 +0000

    TEMPORARY COMMIT

--- erlang/lib/tao_ic/cpp_src/be_extern.h
+++ erlang/lib/tao_ic/cpp_src/be_extern.h
@@ -13,8 +13,8 @@ extern BE_GlobalData *be_global;
 int BE_init(int &, char **);
 void BE_post_init(BE_PI_CONST char **, long);
 void BE_version(void);
-void BE_produce(void);
 void BE_abort(void);
 void BE_cleanup(void);
+void BE_produce(void);
 
 #endif /* TAO_IC_BE_EXTERN_H */
