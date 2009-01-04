8ddfb37f697c042c7c3bb897e9ace5fd3cc9bfe6
Steven Stallion <stallions@ociweb.com>
2009-01-04 18:52:29 +0000

    Sun Jan  4 18:51:09 UTC 2009  Steven Stallion  <stallions@ociweb.com>

--- branches/ERLANG_SUPPORT/erlang/bin/Erlang/EDoc.pm
+++ branches/ERLANG_SUPPORT/erlang/bin/Erlang/EDoc.pm
@@ -7,27 +7,64 @@ package Erlang::EDoc;
 use strict;
 use warnings;
 
+use File::Find;
+
 sub new {
   my $self = {};
   bless $self, shift;
-
-  $self->{application} = shift or die;
-  $self->{filename} = shift or ".";
-  $self->{options} = shift or {};
-  
-  return $self; 
+  return $self->reset();
 }
 
-sub generate {
+sub reset {
   my $self = shift;
-  my @options = ();
+  $self->command qw(erl -noshell -run edoc_run);
+  return $self;
+}
 
-  my $cmd = "erl -noshell -run edoc_run application" .
-    " \"'$self->{application}'\"" .
-    " \"'$self->{filename}'\"" .
-    " \"'[" . join ',', $options . "]'\"";
-  #system($cmd)
-  print "$cmd\n";
+sub files {
+  my $self = shift;
+  my ($filename, $options) = @_;
+
+  $self->push('files');
+
+  my @filenames = ();
+  File::Find::find(
+    sub {
+      /\.erl$/ or return;
+      push @filenames, "\"$File::Find::name\"";
+    }, $filename);
+  
+  $self->push_list(@filenames);
+  $self->push($options);
+
+  $self->run();
+}
+
+sub command {
+  my $self = shift;
+  if (@_) { @{$self->{COMMAND}} = @_ }
+  return @{$self->{COMMAND}};
+}
+
+sub push {
+  my $self = shift;
+  push @{$self->{COMMAND}}, @_;
+}
+
+sub push_list {
+  my $self = shift;
+  $self->push('[' . join(',', @_) . ']');
+}
+
+sub print {
+  my $self = shift;
+  print join(' ', $self->command) . "\n";
+}
+
+sub run {
+  my $self = shift;
+  $self->print();
+  system $self->command;
 }
 
 1;
