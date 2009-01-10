b58e56ecf3fac8086c2ca7b02adcdaf84bb858ea
Steven Stallion <stallions@ociweb.com>
2009-01-10 16:49:00 +0000

    TEMPORARY COMMIT

---
+++ branches/ERLANG_SUPPORT/erlang/lib/tao_ic/cpp_src/drv_args.cpp
@@ -0,0 +1,8 @@
+/*
+ * $Id$
+ */
+
+// The following file is included to reuse the original tao_idl
+// driver with TAO_IDL_FE and our own custom BE (see: be_extern.h).
+
+#include "driver/drv_args.cpp"
