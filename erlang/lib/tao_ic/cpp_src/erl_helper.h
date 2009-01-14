edf77297f0fb58feaf2bc89e60de6d9b48aeaade
Steven Stallion <stallions@ociweb.com>
2009-01-14 01:05:24 +0000

    TEMPORARY COMMIT

--- erlang/lib/tao_ic/cpp_src/erl_helper.h
+++ erlang/lib/tao_ic/cpp_src/erl_helper.h
@@ -6,17 +6,20 @@
 #define TAO_IC_ERL_HELPER_H
 
 #include <fstream>
+#include <ostream>
 #include <string>
 #include <vector>
 
 #include "ast_decl.h"
 
-class erl_helper {
+class erl_module {
 public:
   static const char *ext;
 
-  explicit erl_helper(AST_Decl *);
-  ~erl_helper(void);
+  explicit erl_module(AST_Decl *);
+  ~erl_module(void);
+
+  const char *name(void) const;
 
   void add_export(const char *);
 
@@ -32,22 +35,46 @@ public:
 
   void generate_includes(void);
 
-  std::ofstream &open_stream(bool = true);
+  std::ostream &open_stream(bool = true);
 
   static const char *to_list(std::vector<const char *> &);
 
-  static const char *to_module(AST_Decl *);
+  static const char *to_name(AST_Decl *);
 
 private:
-  const char *module_;
+  const char *name_;
 
   std::vector<const char *> exports_;
-
   std::vector<const char *> imports_;
 
   std::vector<const char *> includes_;
 
   std::ofstream out_;
 };
+
+enum erl_type {
+  ERL_INTEGER,
+  ERL_FLOAT,
+  ERL_STRING,
+  ERL_CHAR,
+  ERL_BINARY,
+  ERL_RECORD
+};
+
+class erl_literal {
+public:
+  explicit erl_literal(AST_Expression *);
+  ~erl_literal(void);
+
+  erl_type type(void) const;
+
+  const char *c_str(void) const;
+
+private:
+  const char *data_;
+  erl_type type_;  
+};
+
+ostream &operator<<(ostream &os, const erl_value &value);
 
 #endif /* TAO_IC_ERL_HELPER_H */
--- erlang/lib/tao_ic/cpp_src/generator_erl.cpp
+++ erlang/lib/tao_ic/cpp_src/generator_erl.cpp
@@ -21,74 +21,71 @@ generator_erl::~generator_erl()
 bool
 generator_erl::generate_constant(AST_Constant *node)
 {
-  erl_helper helper(node);
+  erl_module module(node);
 
-  helper.add_export("value/0");
+  module.add_export("value/0");
 
-  ofstream &out = helper.open_stream();
-  if (!out) {
-    return false;
-  }
+  ostream &out = module.open_stream();
+  if (!out) return false; // bad stream
 
-  out << "value() -> ";
-
-  AST_Expression::AST_ExprValue *ev = node->constant_value()->ev();
-  switch (ev->et) {
-  case AST_Expression::EV_short:
-    out << ev->u.sval;
-    break;
-  case AST_Expression::EV_ushort:
-    out << ev->u.usval;
-    break;
-  case AST_Expression::EV_long:
-    out << ev->u.lval;
-    break;
-  case AST_Expression::EV_ulong:
-    out << ev->u.ulval;
-    break;
-#ifndef ACE_LACKS_LONGLONG_T
-  case AST_Expression::EV_longlong:
-    out << ev->u.llval;
-    break;
-  case AST_Expression::EV_ulonglong:
-    out << ev->u.ullval;
-    break;
-#endif /* ACE_LACKS_LONGLONG_T */
-
-  case AST_Expression::EV_float:
-    out << showpoint << ev->u.fval;
-    break;
-  case AST_Expression::EV_double:
-    out << showpoint << ev->u.dval;
-    break;
-  
-  case AST_Expression::EV_char:
-    out << '$' << ev->u.cval;
-    break;
-  case AST_Expression::EV_wchar:
-    out << '$' << ev->u.wcval;
-    break;
-
-  case AST_Expression::EV_octet:
-    out << "<<" << static_cast<unsigned>(ev->u.oval) << ">>";
-    break;
-
-  case AST_Expression::EV_bool:
-    out << boolalpha << ev->u.bval;
-    break;
-
-  case AST_Expression::EV_string:
-    out << '"' << ev->u.strval->get_string() << '"';
-    break;
-  case AST_Expression::EV_wstring:
-    out << '"' << ev->u.wstrval << '"';
-    break;
-
-  default:
-    return false; // not supported
-  }
-  
-  out << '.' << endl;
-
+  out << "value() -> " << erl_literal(node->constant_value()) << '.' << endl;
   return true;
 }
+
+//
+//  AST_Expression::AST_ExprValue *ev = node->constant_value()->ev();
+//  switch (ev->et) {
+//  case AST_Expression::EV_short:
+//    out << ev->u.sval;
+//    break;
+//  case AST_Expression::EV_ushort:
+//    out << ev->u.usval;
+//    break;
+//  case AST_Expression::EV_long:
+//    out << ev->u.lval;
+//    break;
+//  case AST_Expression::EV_ulong:
+//    out << ev->u.ulval;
+//    break;
+//#ifndef ACE_LACKS_LONGLONG_T
+//  case AST_Expression::EV_longlong:
+//    out << ev->u.llval;
+//    break;
+//  case AST_Expression::EV_ulonglong:
+//    out << ev->u.ullval;
+//    break;
+//#endif /* ACE_LACKS_LONGLONG_T */
+//
+//  case AST_Expression::EV_float:
+//    out << showpoint << ev->u.fval;
+//    break;
+//  case AST_Expression::EV_double:
+//    out << showpoint << ev->u.dval;
+//    break;
+//
+//  case AST_Expression::EV_char:
+//    out << '$' << ev->u.cval;
+//    break;
+//  case AST_Expression::EV_wchar:
+//    out << '$' << ev->u.wcval;
+//    break;
+//
+//  case AST_Expression::EV_octet:
+//    out << "<<" << unsigned(ev->u.oval) << ">>";
+//    break;
+//
+//  case AST_Expression::EV_bool:
+//    out << boolalpha << ev->u.bval;
+//    break;
+//
+//  case AST_Expression::EV_string:
+//    out << '"' << ev->u.strval->get_string() << '"';
+//    break;
+//  case AST_Expression::EV_wstring:
+//    out << '"' << ev->u.wstrval << '"';
+//    break;
+//
+//  default:
+//    return false; // not supported
+//  }
+
