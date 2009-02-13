18699ace8269ca28e53cd953a9b34bc7139715ec
Steven Stallion <stallions@ociweb.com>
2009-02-13 05:39:31 +0000

    Fri Feb 13 05:37:18 UTC 2009  Steven Stallion  <stallions@ociweb.com>

---
+++ trunk/tests/DCPS/Thrasher/DataReaderListenerImpl.cpp
@@ -0,0 +1,83 @@
+/*
+ * $Id$
+ */
+
+#include "DataReaderListenerImpl.h"
+#include "FooTypeTypeSupportC.h"
+
+DataReaderListenerImpl::DataReaderListenerImpl(int& received_samples)
+  : received_samples_(received_samples)
+{}
+
+DataReaderListenerImpl::~DataReaderListenerImpl()
+{}
+
+void
+DataReaderListenerImpl::on_data_available(
+    DDS::DataReader_ptr reader)
+  throw (CORBA::SystemException)
+{
+  FooDataReader_var reader_i = FooDataReader::_narrow(reader);
+  if (CORBA::is_nil(reader_i))
+  {
+    ACE_ERROR((LM_ERROR,
+               ACE_TEXT("%N:%l: on_data_available()")
+               ACE_TEXT(" _narrow failed!\n")));
+    return;
+  }
+
+  Foo foo;
+  DDS::SampleInfo si;
+
+  if (reader_i->take_next_sample(foo, si) != DDS::RETCODE_OK)
+  {
+    ACE_ERROR((LM_ERROR,
+               ACE_TEXT("%N:%l: on_data_available()")
+               ACE_TEXT(" take_next_sample failed!\n")));
+    return;
+  }
+
+  ++this->received_samples_;
+}
+
+void
+DataReaderListenerImpl::on_requested_deadline_missed(
+    DDS::DataReader_ptr reader,
+    const DDS::RequestedDeadlineMissedStatus& status)
+  throw (CORBA::SystemException)
+{}
+
+void
+DataReaderListenerImpl::on_requested_incompatible_qos(
+    DDS::DataReader_ptr reader,
+    const DDS::RequestedIncompatibleQosStatus& status)
+  throw (CORBA::SystemException)
+{}
+
+void
+DataReaderListenerImpl::on_liveliness_changed(
+    DDS::DataReader_ptr reader,
+    const DDS::LivelinessChangedStatus& status)
+  throw (CORBA::SystemException)
+{}
+
+void
+DataReaderListenerImpl::on_subscription_match(
+    DDS::DataReader_ptr reader,
+    const DDS::SubscriptionMatchStatus& status)
+  throw (CORBA::SystemException)
+{}
+
+void
+DataReaderListenerImpl::on_sample_rejected(
+    DDS::DataReader_ptr reader,
+    const DDS::SampleRejectedStatus& status)
+  throw (CORBA::SystemException)
+{}
+
+void
+DataReaderListenerImpl::on_sample_lost(
+    DDS::DataReader_ptr reader,
+    const DDS::SampleLostStatus& status)
+  throw (CORBA::SystemException)
+{}
--- trunk/tests/DCPS/Thrasher/Subscriber.cpp
+++ trunk/tests/DCPS/Thrasher/Subscriber.cpp
@@ -2,33 +2,36 @@
  * $Id$
  */
 
-#include <cstdlib>
-
 #include <ace/Arg_Shifter.h>
 #include <ace/Log_Msg.h>
+#include <ace/OS_main.h>
+#include <ace/OS_NS_stdlib.h>
 
+#include <dds/DdsDcpsInfrastructureC.h>
 #include <dds/DCPS/Service_Participant.h>
 #include <dds/DCPS/Marked_Default_Qos.h>
 #include <dds/DCPS/SubscriberImpl.h>
 #include <dds/DCPS/transport/framework/TheTransportFactory.h>
+#include <dds/DCPS/WaitSet.h>
 
+#include "DataReaderListenerImpl.h"
 #include "FooTypeTypeSupportImpl.h"
 
 int expected_samples = 0;
+int received_samples = 0;
 
 void
-parse_args(int& argc, char** argv)
+parse_args(int& argc, ACE_TCHAR** argv)
 {
-  ACE_Arg_Shifter_T<char> shifter(argc, argv);
-
+  ACE_Arg_Shifter shifter(argc, argv);
+ 
   while (shifter.is_anything_left())
   {
-    const char* arg;
+    const ACE_TCHAR* arg;
 
-    // expected samples
     if ((arg = shifter.get_the_parameter("-n")))
     {
-      expected_samples = std::atoi(arg);
+      expected_samples = ACE_OS::atoi(arg);
       shifter.consume_arg();
     }
     else
@@ -39,83 +42,130 @@ parse_args(int& argc, char** argv)
 }
 
 int
-main(int argc, char** argv)
+ACE_TMAIN(int argc, ACE_TCHAR** argv)
 {
   parse_args(argc, argv);
-
-  ACE_DEBUG((LM_DEBUG,
-             ACE_TEXT("(%P|%t) Initializing subscriber; expected_samples: %s\n"),
-             expected_samples));
-
-  DDS::DomainParticipantFactory_var dpf =
-    TheParticipantFactoryWithArgs(argc, argv);
-
-  // Create Participant
-  DDS::DomainParticipant_var participant =
-    dpf->create_participant(42,
-                            PARTICIPANT_QOS_DEFAULT,
-                            DDS::DomainParticipantListener::_nil());
-
-  if (CORBA::is_nil(participant.in()))
-    ACE_ERROR_RETURN((LM_ERROR,
-                      ACE_TEXT("%N:%l: main()")
-                      ACE_TEXT(" create_participant failed!\n")), 1);
-
-  // Create Subscriber
-  DDS::Subscriber_var subscriber =
-    participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT,
-                                   DDS::SubscriberListener::_nil());
-
-  if (CORBA::is_nil(subscriber.in()))
-    ACE_ERROR_RETURN((LM_ERROR,
-                      ACE_TEXT("%N:%l: main()")
-                      ACE_TEXT(" create_subscriber failed!\n")), 1);
-
-  // Attach Transport
-  OpenDDS::DCPS::TransportImpl_rch transport =
-    TheTransportFactory->create_transport_impl(1);
-
-  OpenDDS::DCPS::SubscriberImpl* subscriber_i =
-    dynamic_cast<OpenDDS::DCPS::SubscriberImpl*>(subscriber.in());
-
-  if (subscriber_i == 0)
-    ACE_ERROR_RETURN((LM_ERROR,
-                      ACE_TEXT("%N:%l: main()")
-                      ACE_TEXT(" dynamic_cast failed!\n")), 1);
-
-  OpenDDS::DCPS::AttachStatus status =
-    subscriber_i->attach_transport(transport.in());
   
-  if (status != OpenDDS::DCPS::ATTACH_OK)
-    ACE_ERROR_RETURN((LM_ERROR,
-                      ACE_TEXT("%N:%l: main()")
-                      ACE_TEXT(" attach_transport failed!\n")), 1);
+  ACE_DEBUG((LM_INFO,
+             ACE_TEXT("(%P|%t) Initializing subscriber; expected_samples: %d\n"),
+             expected_samples));
+  try
+  {
+    DDS::DomainParticipantFactory_var dpf =
+      TheParticipantFactoryWithArgs(argc, argv);
 
-  // Register Type (FooType)
-  FooTypeSupport_var ts = new FooTypeSupportImpl;
-  if (ts->register_type(participant.in(), "") != DDS::RETCODE_OK)
-    ACE_ERROR_RETURN((LM_ERROR,
-                      ACE_TEXT("%N:%l: main()")
-                      ACE_TEXT(" register_type failed!\n")), 1);
+    // Create Participant
+    DDS::DomainParticipant_var participant =
+      dpf->create_participant(42,
+                              PARTICIPANT_QOS_DEFAULT,
+                              DDS::DomainParticipantListener::_nil());
 
-  // Create Topic (FooTopic)
-  DDS::Topic_var topic =
-    participant->create_topic("FooTopic",
-                              ts->get_type_name(),
-                              TOPIC_QOS_DEFAULT,
-                              DDS::TopicListener::_nil());
+    if (CORBA::is_nil(participant.in()))
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("%N:%l: main()")
+                        ACE_TEXT(" create_participant failed!\n")), 1);
 
-  if (CORBA::is_nil(topic.in()))
-    ACE_ERROR_RETURN((LM_ERROR,
-                      ACE_TEXT("%N:%l: main()")
-                      ACE_TEXT(" create_topic failed!\n")), 1);
+    // Create Subscriber
+    DDS::Subscriber_var subscriber =
+      participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT,
+                                     DDS::SubscriberListener::_nil());
 
-  // ...
+    if (CORBA::is_nil(subscriber.in()))
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("%N:%l: main()")
+                        ACE_TEXT(" create_subscriber failed!\n")), 1);
 
-  participant->delete_contained_entities();
-  dpf->delete_participant(participant.in());
+    // Attach Transport
+    OpenDDS::DCPS::TransportImpl_rch transport =
+      TheTransportFactory->create_transport_impl(1); // subscriber.ini
 
-  TheServiceParticipant->shutdown();
+    OpenDDS::DCPS::SubscriberImpl* subscriber_i =
+      dynamic_cast<OpenDDS::DCPS::SubscriberImpl*>(subscriber.in());
 
-  return 0;
+    if (subscriber_i == 0)
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("%N:%l: main()")
+                        ACE_TEXT(" dynamic_cast failed!\n")), 1);
+
+    OpenDDS::DCPS::AttachStatus status =
+      subscriber_i->attach_transport(transport.in());
+    
+    if (status != OpenDDS::DCPS::ATTACH_OK)
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("%N:%l: main()")
+                        ACE_TEXT(" attach_transport failed!\n")), 1);
+
+    // Register Type (FooType)
+    FooTypeSupport_var ts = new FooTypeSupportImpl;
+    if (ts->register_type(participant.in(), "") != DDS::RETCODE_OK)
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("%N:%l: main()")
+                        ACE_TEXT(" register_type failed!\n")), 1);
+
+    // Create Topic (FooTopic)
+    DDS::Topic_var topic =
+      participant->create_topic("FooTopic",
+                                ts->get_type_name(),
+                                TOPIC_QOS_DEFAULT,
+                                DDS::TopicListener::_nil());
+
+    if (CORBA::is_nil(topic.in()))
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("%N:%l: main()")
+                        ACE_TEXT(" create_topic failed!\n")), 1);
+
+    // Create DataReader
+    DDS::DataReaderListener_var listener =
+      new DataReaderListenerImpl(received_samples);
+
+    DDS::DataReader_var reader =
+      subscriber->create_datareader(topic.in(),
+                                    DATAREADER_QOS_DEFAULT,
+                                    listener.in());
+
+    if (CORBA::is_nil(reader.in()))
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("%N:%l: main()")
+                        ACE_TEXT(" create_datareader failed!\n")), 1);
+
+    // Block until Publisher completes
+    DDS::StatusCondition_var cond = reader->get_statuscondition();
+    cond->set_enabled_statuses(DDS::SUBSCRIPTION_MATCH_STATUS);
+
+    DDS::WaitSet_var ws = new DDS::WaitSet;
+    ws->attach_condition(cond);
+    
+    DDS::ConditionSeq conditions; 
+  
+    DDS::Duration_t timeout =
+      { DDS::DURATION_INFINITY_SEC, DDS::DURATION_INFINITY_NSEC };
+
+    DDS::SubscriptionMatchStatus matches = {0};
+    do
+    {
+      if (ws->wait(conditions, timeout) != DDS::RETCODE_OK)
+        ACE_ERROR_RETURN((LM_ERROR,
+                          ACE_TEXT("%N:%l: main()")
+                          ACE_TEXT(" wait failed!\n")), 1);
+
+      matches = reader->get_subscription_match_status();
+    }
+    while (matches.current_count > 0);
+   
+    ws->detach_condition(cond);
+
+    // Cleanup!
+    participant->delete_contained_entities();
+    dpf->delete_participant(participant.in());
+
+    TheTransportFactory->release();
+    TheServiceParticipant->shutdown();
+  }
+  catch (const CORBA::Exception& e)
+  {
+    e._tao_print_exception("caught in main()");
+    return 1;
+  }
+
+  return !!received_samples != expected_samples; 
 }
