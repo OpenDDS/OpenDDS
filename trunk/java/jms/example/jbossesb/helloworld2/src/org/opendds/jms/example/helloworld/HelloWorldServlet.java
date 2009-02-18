83c33d80d521e2f31c037e78b8f8edaa79b9a763
Steven Stallion <stallions@ociweb.com>
2009-02-18 23:49:41 +0000

    Wed Feb 18 23:43:20 UTC 2009  Steven Stallion  <stallions@ociweb.com>

---
+++ trunk/java/jms/example/jbossesb/helloworld2/src/org/opendds/jms/example/helloworld/HelloWorldServlet.java
@@ -0,0 +1,61 @@
+/*
+ * $Id$
+ */
+
+package org.opendds.jms.example.helloworld;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jboss.soa.esb.client.ServiceInvoker;
+import org.jboss.soa.esb.message.Message;
+import org.jboss.soa.esb.message.format.MessageFactory;
+
+/**
+ * @author  Steven Stallion
+ * @version $Revision$
+ */
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        forwardToView(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        int numberOfSamples = Integer.parseInt(request.getParameter("numberOfSamples"));
+        long delay = Long.parseLong(request.getParameter("delay"));
+        
+        try {
+            Message message = MessageFactory.getInstance().getMessage();
+            message.getBody().add("Hello World!");
+           
+            ServiceInvoker invoker =
+                new ServiceInvoker("OpenDDS", "HelloWorld");
+
+            for (int i = 0; i < numberOfSamples; ++i) {
+                invoker.deliverAsync(message);
+                Thread.sleep(delay);
+            }
+
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+        forwardToView(request, response);
+    }
+
+    //
+
+    private void forwardToView(ServletRequest request, ServletResponse response) throws IOException, ServletException {
+        RequestDispatcher rd = request.getRequestDispatcher("/view.jsp");
+        rd.forward(request, response);
+    }
+}
