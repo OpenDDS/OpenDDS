4d35c73d6ef20894b30fe6fe7a2b439f74a919a1
Steven Stallion <stallions@ociweb.com>
2009-04-02 23:19:26 +0000

    Thu Apr  2 23:18:56 UTC 2009  Steven Stallion  <stallions@ociweb.com>

--- dds/DCPS/InstanceHandle.cpp
+++ dds/DCPS/InstanceHandle.cpp
@@ -8,6 +8,16 @@
 
 #include "InstanceHandle.h"
 
+namespace
+{
+inline bool
+is_kind(DDS::InstanceHandle_t handle, CORBA::Octet kind)
+{
+  return (handle & kind) == kind;
+}
+
+} // namespace
+
 namespace OpenDDS
 {
 namespace DCPS
@@ -24,33 +34,33 @@ InstanceHandleHelper::~InstanceHandleHelper()
 bool
 InstanceHandleHelper::is_topic()
 {
-  return handle_ & ENTITYKIND_OPENDDS_TOPIC;
+  return is_kind(handle_, ENTITYKIND_OPENDDS_TOPIC);
 }
 
 bool
 InstanceHandleHelper::is_subscriber()
 {
-  return handle_ & ENTITYKIND_OPENDDS_SUBSCRIBER;
+  return is_kind(handle_, ENTITYKIND_OPENDDS_SUBSCRIBER);
 }
 
 bool
 InstanceHandleHelper::is_publisher()
 {
-  return handle_ & ENTITYKIND_OPENDDS_PUBLISHER;
+  return is_kind(handle_, ENTITYKIND_OPENDDS_PUBLISHER);
 }
 
 bool
 InstanceHandleHelper::is_datareader()
 {
-  return handle_ & ENTITYKIND_USER_READER_WITH_KEY
-      || handle_ & ENTITYKIND_USER_READER_NO_KEY;
+  return is_kind(handle_, ENTITYKIND_USER_READER_WITH_KEY)
+      || is_kind(handle_, ENTITYKIND_USER_READER_NO_KEY);
 }
 
 bool
 InstanceHandleHelper::is_datawriter()
 {
-  return handle_ & ENTITYKIND_USER_WRITER_WITH_KEY
-      || handle_ & ENTITYKIND_USER_WRITER_NO_KEY;
+  return is_kind(handle_, ENTITYKIND_USER_WRITER_WITH_KEY)
+      || is_kind(handle_, ENTITYKIND_USER_WRITER_NO_KEY);
 }
 
 
