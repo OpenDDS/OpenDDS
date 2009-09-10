57b07a4f3d177e32d682088216938ee36b44187b
Steven Stallion <stallions@ociweb.com>
2009-09-10 18:00:16 +0000

    Thu Sep 10 17:59:21 UTC 2009  Steven Stallion  <stallions@ociweb.com>

--- trunk/tools/copyright/update_copyright.sh
+++ trunk/tools/copyright/update_copyright.sh
@@ -3,28 +3,28 @@
 # $Id$
 #
 
-if [ ! $DDS_ROOT ]; then
+if [ ! "$DDS_ROOT" ]; then
   echo "ERROR: DDS_ROOT environment variable not set!"
   exit 1
 fi 
 
-TOOLS_HOME=$DDS_ROOT/tools
+TOOLS_HOME="$DDS_ROOT/tools"
 
 if [ $# = 0 ]; then
-  echo "Usage: $0 file..."
+  echo "Usage: `basename $0` file..."
   exit 1
 fi
 
 DATE=`date +%Y`
-for FILE in $FILES; do
-  echo "Processing $FILE"
-
+for FILE in "$FILES"; do
   if [ ! -f "$FILE" ]; then
     echo "$FILE: No such file"
     exit 1
   fi
+  
+  echo "Processing $FILE"
 
-  cp $FILE $FILE.orig
+  cp "$FILE" "$FILE.orig" # create working copy
   sed -e "s/\(Copyright\).*\(Object Computing, Inc.\)\$/\1 $DATE \2/g" \
-         $FILE.orig > $FILE
+         "$FILE.orig" > "$FILE"
 done
