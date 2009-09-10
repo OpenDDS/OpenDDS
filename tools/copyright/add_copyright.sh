06e0ee08f00769320635c80e06698b976d7c0ef6
Steven Stallion <stallions@ociweb.com>
2009-09-10 18:00:16 +0000

    Thu Sep 10 17:59:21 UTC 2009  Steven Stallion  <stallions@ociweb.com>

--- tools/copyright/add_copyright.sh
+++ tools/copyright/add_copyright.sh
@@ -3,26 +3,26 @@
 # $Id$
 #
 
-if [ ! $DDS_ROOT ]; then
+if [ ! "$DDS_ROOT" ]; then
   echo "ERROR: DDS_ROOT environment variable not set!"
   exit 1
 fi 
 
-TOOLS_HOME=$DDS_ROOT/tools
+TOOLS_HOME="$DDS_ROOT/tools"
 
 if [ $# = 0 ]; then
-  echo "Usage: $0 file..."
+  echo "Usage: `basename $0` file..."
   exit 1
 fi
 
 for FILE in "$@"; do
-  echo "Processing $FILE"
-
   if [ ! -f "$FILE" ]; then
     echo "$FILE: No such file"
     exit 1
   fi
 
-  cp $FILE $FILE.orig
-  cat $TOOLS_HOME/copyright/COPYRIGHT $FILE.orig > $FILE
+  echo "Processing $FILE"
+
+  cp "$FILE" "$FILE.orig" # create working copy
+  cat "$TOOLS_HOME/copyright/COPYRIGHT" "$FILE.orig" > "$FILE"
 done
--- tools/style/style.sh
+++ tools/style/style.sh
@@ -3,36 +3,35 @@
 # $Id$
 #
 
-if [ ! $DDS_ROOT ]; then
+if [ ! "$DDS_ROOT" ]; then
   echo "ERROR: DDS_ROOT environment variable not set!"
   exit 1
 fi 
 
-TOOLS_HOME=$DDS_ROOT/tools
+TOOLS_HOME="$DDS_ROOT/tools"
 
 if [ $# = 0 ]; then
-  echo "Usage: $0 file..."
+  echo "Usage: `basename $0` file..."
   exit 1
 fi
 
 # sed(1) scripts to apply to target files
-SCRIPTS=`find $TOOLS_HOME/style/style.d -type f \
-        -name '*.sed'`
+SCRIPTS=`find "$TOOLS_HOME/style/style.d" -type f -name '*.sed'`
 
 for FILE in "$@"; do
-  echo "Processing $FILE"
-
   if [ ! -f "$FILE" ]; then
     echo "$FILE: No such file"
     exit 1
   fi
+  
+  echo "Processing $FILE"
 
   # Apply astyle(1):
-  astyle --quiet --options=$TOOLS_HOME/style/astyle.options $FILE
+  astyle --quiet --options="$TOOLS_HOME/style/astyle.options" "$FILE"
 
   # Apply individual sed(1) scripts:
   for SCRIPT in $SCRIPTS; do
-    cp $FILE $FILE.orig
-    sed -f $SCRIPT $FILE.orig > $FILE
+    cp "$FILE" "$FILE.orig"
+    sed -f "$SCRIPT" "$FILE.orig" > "$FILE"
   done
 done
