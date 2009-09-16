548f10e73b43b7bd199b6f142fb8fb156d41f63a
Steven Stallion <stallions@ociweb.com>
2009-09-16 18:46:57 +0000

    Wed Sep 16 17:49:34 UTC 2009  Steven Stallion  <stallions@ociweb.com>

--- DevGuideExamples/DCPS/Messenger_ZeroCopy/subscriber.cpp
+++ DevGuideExamples/DCPS/Messenger_ZeroCopy/subscriber.cpp
@@ -20,6 +20,12 @@
 #include <dds/DCPS/transport/framework/TheTransportFactory.h>
 #include <dds/DCPS/transport/simpleTCP/SimpleTcpConfiguration.h>
 
+#ifdef ACE_AS_STATIC_LIBS
+#include <dds/DCPS/transport/simpleTCP/SimpleTcp.h>
+#include <dds/DCPS/transport/simpleUnreliableDgram/SimpleUnreliableDgram.h>
+#include <dds/DCPS/transport/ReliableMulticast/ReliableMulticast.h>
+#endif
+
 #include "DataReaderListener.h"
 #include "MessengerTypeSupportImpl.h"
 
