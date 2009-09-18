e5af52ac89bec91f4ec932e019b0181071e31892
Steven Stallion <stallions@ociweb.com>
2009-09-18 16:21:41 +0000

    Fri Sep 18 16:20:54 UTC 2009  Steven Stallion  <stallions@ociweb.com>

--- DevGuideExamples/DCPS/Messenger/DataReaderListenerImpl.cpp
+++ DevGuideExamples/DCPS/Messenger/DataReaderListenerImpl.cpp
@@ -20,6 +20,7 @@ void
 DataReaderListenerImpl::on_requested_deadline_missed(
   DDS::DataReader_ptr /* reader */,
   const DDS::RequestedDeadlineMissedStatus& /* status */)
+ACE_THROW_SPEC((CORBA::SystemException))
 {
 }
 
@@ -27,6 +28,7 @@ void
 DataReaderListenerImpl::on_requested_incompatible_qos(
   DDS::DataReader_ptr /* reader */,
   const DDS::RequestedIncompatibleQosStatus& /* status */)
+ACE_THROW_SPEC((CORBA::SystemException))
 {
 }
 
@@ -34,6 +36,7 @@ void
 DataReaderListenerImpl::on_sample_rejected(
   DDS::DataReader_ptr /* reader */,
   const DDS::SampleRejectedStatus& /* status */)
+ACE_THROW_SPEC((CORBA::SystemException))
 {
 }
 
@@ -41,11 +44,13 @@ void
 DataReaderListenerImpl::on_liveliness_changed(
   DDS::DataReader_ptr /* reader */,
   const DDS::LivelinessChangedStatus& /* status */)
+ACE_THROW_SPEC((CORBA::SystemException))
 {
 }
 
 void
 DataReaderListenerImpl::on_data_available(DDS::DataReader_ptr reader)
+ACE_THROW_SPEC((CORBA::SystemException))
 {
   Messenger::MessageDataReader_var reader_i =
     Messenger::MessageDataReader::_narrow(reader);
@@ -86,6 +91,7 @@ void
 DataReaderListenerImpl::on_subscription_matched(
   DDS::DataReader_ptr /* reader */,
   const DDS::SubscriptionMatchedStatus& /* status */)
+ACE_THROW_SPEC((CORBA::SystemException))
 {
 }
 
@@ -93,5 +99,6 @@ void
 DataReaderListenerImpl::on_sample_lost(
   DDS::DataReader_ptr /* reader */,
   const DDS::SampleLostStatus& /* status */)
+ACE_THROW_SPEC((CORBA::SystemException))
 {
 }
--- DevGuideExamples/DCPS/Messenger_ZeroCopy/DataReaderListenerImpl.h
+++ DevGuideExamples/DCPS/Messenger_ZeroCopy/DataReaderListenerImpl.h
@@ -10,6 +10,8 @@
 #ifndef DATAREADER_LISTENER_IMPL_H
 #define DATAREADER_LISTENER_IMPL_H
 
+#include <ace/Global_Macros.h>
+
 #include <dds/DdsDcpsSubscriptionS.h>
 #include <dds/DCPS/LocalObject.h>
 
@@ -18,30 +20,37 @@ class DataReaderListenerImpl
 public:
   virtual void on_requested_deadline_missed(
     DDS::DataReader_ptr reader,
-    const DDS::RequestedDeadlineMissedStatus& status);
+    const DDS::RequestedDeadlineMissedStatus& status)
+  ACE_THROW_SPEC((CORBA::SystemException));
 
   virtual void on_requested_incompatible_qos(
     DDS::DataReader_ptr reader,
-    const DDS::RequestedIncompatibleQosStatus& status);
+    const DDS::RequestedIncompatibleQosStatus& status)
+  ACE_THROW_SPEC((CORBA::SystemException));
 
   virtual void on_sample_rejected(
     DDS::DataReader_ptr reader,
-    const DDS::SampleRejectedStatus& status);
+    const DDS::SampleRejectedStatus& status)
+  ACE_THROW_SPEC((CORBA::SystemException));
 
   virtual void on_liveliness_changed(
     DDS::DataReader_ptr reader,
-    const DDS::LivelinessChangedStatus& status);
+    const DDS::LivelinessChangedStatus& status)
+  ACE_THROW_SPEC((CORBA::SystemException));
 
   virtual void on_data_available(
-    DDS::DataReader_ptr reader);
+    DDS::DataReader_ptr reader)
+  ACE_THROW_SPEC((CORBA::SystemException));
 
   virtual void on_subscription_matched(
     DDS::DataReader_ptr reader,
-    const DDS::SubscriptionMatchedStatus& status);
+    const DDS::SubscriptionMatchedStatus& status)
+  ACE_THROW_SPEC((CORBA::SystemException));
 
   virtual void on_sample_lost(
     DDS::DataReader_ptr reader,
-    const DDS::SampleLostStatus& status);
+    const DDS::SampleLostStatus& status)
+  ACE_THROW_SPEC((CORBA::SystemException));
 };
 
 #endif /* DATAREADER_LISTENER_IMPL_H */
