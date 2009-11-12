23b417fe3ddf71e54c7379b3815667e97e21f37c
Steven Stallion <stallions@ociweb.com>
2009-11-12 00:29:50 +0000

    Thu Nov 12 00:25:11 UTC 2009  Steven Stallion  <stallions@ociweb.com>

--- branches/MCAST_REDUX/dds/DCPS/transport/multicast/MulticastConfiguration.cpp
+++ branches/MCAST_REDUX/dds/DCPS/transport/multicast/MulticastConfiguration.cpp
@@ -13,15 +13,15 @@
 
 namespace {
 
-const bool  DEFAULT_DEFAULT_TO_IPV6(false);
+const bool DEFAULT_DEFAULT_TO_IPV6(false);
 
 const char* DEFAULT_IPV4_GROUP_ADDRESS("224.0.36.0");
 const char* DEFAULT_IPV6_GROUP_ADDRESS("FF36::8000:0");
 
-const long  DEFAULT_HANDSHAKE_TIMEOUT(30000);
+const long DEFAULT_HANDSHAKE_TIMEOUT(30000);
 
-const long    DEFAULT_NAK_TIMEOUT(30000);
-const size_t  DEFAULT_NAK_REPAIR_DEPTH(32);
+const long DEFAULT_NAK_TIMEOUT(30000);
+const size_t DEFAULT_NAK_REPAIR_DEPTH(32);
 
 } // namespace
 
