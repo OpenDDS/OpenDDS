c782e6bea8acdabe566103facd6da71cb9d89c85
Steven Stallion <stallions@ociweb.com>
2009-11-29 07:34:27 +0000

    Sun Nov 29 07:32:21 UTC 2009  Steven Stallion  <stallions@ociweb.com>

---
+++ dds/DCPS/transport/multicast/Multicast.cpp
@@ -0,0 +1,29 @@
+/*
+ * $Id$
+ *
+ * Copyright 2009 Object Computing, Inc.
+ *
+ * Distributed under the OpenDDS License.
+ * See: http://www.opendds.org/license.html
+ */
+
+#include "Multicast.h"
+#include "MulticastLoader.h"
+
+#include "ace/Dynamic_Service.h"
+#include "ace/Service_Config.h"
+
+namespace OpenDDS {
+namespace DCPS {
+
+MulticastInitializer::MulticastInitializer()
+{
+  ACE_Service_Config::process_directive(ace_svc_desc_MulticastLoader);
+
+#if (OPENDDS_MULTICAST_HAS_DLL == 0)
+  ACE_Service_Config::process_directive(ACE_TEXT("static MulticastLoader"));
+#endif  /* OPENDDS_MULTICAST_HAS_DLL */
+}
+
+} // namespace DCPS
+} // namespace OpenDDS
