0ad19e39019c48cd5d759c68822086645d38eac0
Steven Stallion <stallions@ociweb.com>
2009-11-29 07:34:27 +0000

    Sun Nov 29 07:32:21 UTC 2009  Steven Stallion  <stallions@ociweb.com>

---
+++ trunk/dds/DCPS/transport/multicast/Multicast.h
@@ -0,0 +1,28 @@
+/*
+ * $Id$
+ *
+ * Copyright 2009 Object Computing, Inc.
+ *
+ * Distributed under the OpenDDS License.
+ * See: http://www.opendds.org/license.html
+ */
+
+#ifndef DCPS_MULTICAST_H
+#define DCPS_MULTICAST_H
+
+#include "Multicast_Export.h"
+
+namespace OpenDDS {
+namespace DCPS {
+
+class OpenDDS_Multicast_Export MulticastInitializer {
+public:
+  MulticastInitializer();
+};
+
+static MulticastInitializer __multicast_initializer;
+
+} // namespace DCPS
+} // namespace OpenDDS
+
+#endif  /* DCPS_MULTICAST_H */
