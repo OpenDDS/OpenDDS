6b3ed18c4553192105552b3fe355c3a7d42965c2
Steven Stallion <stallions@ociweb.com>
2010-02-16 17:04:30 +0000

    Tue Feb 16 17:01:23 UTC 2010  Steven Stallion  <stallions@ociweb.com>

--- trunk/tools/odds_dissector/odds_packet.cpp
+++ trunk/tools/odds_dissector/odds_packet.cpp
@@ -298,6 +298,7 @@ dissect_odds(tvbuff_t* tvb, packet_info* pinfo, proto_tree* tree)
   if (check_col(pinfo->cinfo, COL_PROTOCOL)) {
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "OpenDDS");
   }
+
   if (check_col(pinfo->cinfo, COL_INFO)) {
     col_clear(pinfo->cinfo, COL_INFO);
   }
