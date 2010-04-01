d71c65095b1c83d3d39824f806ec44fe7355a28d
Trevor Fields <fields_t@ociweb.com>
2010-04-01 22:38:17 +0000

    ChangeLogTag: Thu Apr 01 22:36:25 UTC 2010  Trevor Fields  <fields_t@ociweb.com>

--- performance-tests/Bench/bin/extract-throughput.pl
+++ performance-tests/Bench/bin/extract-throughput.pl
@@ -17,14 +17,49 @@ $Id$
 
 =head1 DESCRIPTION
 
+This script processes the logfiles created by executing the througput
+tests and generates a series of indexed data sets suitable for plotting
+by GNUPlot.
+
+All input files named on the command line are processed and the transport
+used, the message sizes and rates, as well as the elapsed test time and
+actual number of messages received are extracted.
+
+The input files are expected to be in logfile format as produced by the
+testprocess C<-v> option.  Logfiles produced by the subscription end of
+the testing contain the data required to produce plottable data sets.
+
+Each index set in the output represents one transport type with each
+entry within the index representing a single size/rate test result.
+
+The data within an index is formatted as a CSV, with the following
+fields:
+
+=begin html
+
+<table border>
+  <tr><th>Field</th><th>Description</th></tr>
+  <tr><td>1</td><td>transport type</td></tr>
+  <tr><td>2</td><td>test message size</td></tr>
+  <tr><td>3</td><td>test message rate</td></tr>
+  <tr><td>4</td><td>actual (measured) bandwidth</td></tr>
+  <tr><td>5</td><td>nominal (specified) bandwidth</td></tr>
+</table>
+
+=end html
+
+=for text
+      Field  1: transport type
+      Field  2: test message size
+      Field  3: test message rate
+      Field  4: actual (measured) bandwidth
+      Field  5: nominal (specified) bandwidth
+
 =head1 EXAMPLE
 
-  extract-throughput.pl */*.data > data/throughput.csv
+  extract-throughput.pl */*.results > data/throughput.csv
 
 =cut
-
-# Filename extension to elide.
-use constant EXTENSION => ".data";
 
 # current   - current filename being processed.
 # transport - current transport being processed.
@@ -45,7 +80,7 @@ if( not defined $current or $ARGV ne "$current") {
 
 /Options::loadPublication\(\) -   \[publication\/\S+] MessageRate == (\d+)\.$/ and do { $rate = $1; };
 /Options::loadPublication\(\) -   \[publication\/\S+] MessageSize == (\d+)\.$/ and do { $size = $1; };
-/Publication::enable\(\) - publication \S+: (created|obtained) (.+) transport with index \d+\.$/ and do { $transport = $2; };
+/::enable\(\) - (publication|subscription) \S+: obtained (.+) transport with index \d+\.$/ and do { $transport = $2; };
 
 # No need to examine data if we have not found required information.
 next if ((not defined $transport) or (not defined $rate) or (not defined $size));
@@ -56,6 +91,8 @@ next if ((not defined $transport) or (not defined $rate) or (not defined $size))
 
 
 END {
+  die "No actual data gathered." if not $data or not scalar keys %$data;
+
   # Format output as CSV data.
   my $index = 0;
   foreach my $transport (sort keys %$data) {
@@ -68,7 +105,9 @@ END {
     foreach my $size (sort { $a <=> $b; } keys %{$data->{$transport}}) {
       foreach my $rate (sort { $a <=> $b; } keys %{$data->{$transport}->{$size}}) {
         my $expected = $size * $rate;
-        my $actual = ($data->{$transport}->{$size}->{$rate}->{actual} * $size / $data->{ $transport}->{ $size}->{ $rate}->{time});
+        my $elapsed = $data->{ $transport}->{ $size}->{ $rate}->{time};
+        next if not $elapsed;
+        my $actual = ($data->{$transport}->{$size}->{$rate}->{actual} * $size / $elapsed);
         print "$transport,";
         print "$size,";
         print "$rate,";
