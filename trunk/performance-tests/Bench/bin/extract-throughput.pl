5195c35ad7a9a06c9d3bc22797220fdf6af515bb
Mike Martinez <martinez_m@ociweb.com>
2010-04-19 19:22:51 +0000

    Mon Apr 19 19:19:09 UTC 2010  Mike Martinez  <martinez_m@ociweb.com>

--- trunk/performance-tests/Bench/bin/extract-throughput.pl
+++ trunk/performance-tests/Bench/bin/extract-throughput.pl
@@ -68,7 +68,26 @@ fields:
 # time      - current time of test being processed.
 # data      - data that has been processed.
 # test      - current test number being processed.
-our ($current, $transport, $size, $rate, $time, $data, $test);
+# type      - type of current test being processed.
+our ($current, $transport, $size, $rate, $time, $data, $test, $type);
+
+BEGIN {
+  # Seed the test types and the transports so that we can maintain a
+  # consistent set of indices in the output for our plotting scripts.
+  $data = { bidir =>    { 'SimpleTcp'             => undef,
+                          'best effort multicast' => undef, 
+                          'reliable multicast'    => undef, 
+                          'udp'                   => undef},
+            pubbound => { 'SimpleTcp'             => undef,
+                          'best effort multicast' => undef, 
+                          'reliable multicast'    => undef, 
+                          'udp'                   => undef},
+            subbound => { 'SimpleTcp'             => undef,
+                          'best effort multicast' => undef, 
+                          'reliable multicast'    => undef, 
+                          'udp'                   => undef}
+          };
+}
 
 if( not defined $current or $ARGV ne "$current") {
   # Starting a new file.
@@ -78,12 +97,21 @@ if( not defined $current or $ARGV ne "$current") {
   undef $transport;
   undef $time;
   undef $test;
-  if ($current =~ /bidir(\d+)-.*/){
+  if ($current =~ /bidir(\d+).*/){
     $test = $1;
+    $type = "bidir";
   }
-#  elsif ($current =~ /test(\d+)-.*/) {
-#    $test = $1 + 13;
-#  }
+ elsif ($current =~ /test(\d+)-(pub|sub)(\d*)/) {
+   $test = $1;
+   if( $2 eq "pub") {
+     $type = "pubbound";
+     $type = "subbound" if $3;
+   } elsif( $2 eq "sub") {
+     $type = "subbound";
+     $type = "pubbound" if $3;
+   }
+ }
+ # print "$current: $type test $test.\n";
 }
 
 
@@ -96,81 +124,88 @@ next if ((not defined $transport) or (not defined $test));
 
 
 # Store data as it is recognized.
-/DataReaderListener::on_data_available\(\) - received a non-data sample.  After messages: total: \d+, valid: (\d+)./ and do {$data->{ $transport}->{ $test}->{actual} = $1; };
-/^Valid Messages Received: (\d+)$/ and do {$data->{ $transport}->{ $test}->{actual} = $1; };
+/DataReaderListener::on_data_available\(\) - received a non-data sample.  After messages: total: \d+, valid: (\d+)./ and do {$data->{ $type}->{ $transport}->{ $test}->{actual} += $1; };
+/^Valid Messages Received: (\d+)$/ and do {$data->{ $type}->{ $transport}->{ $test}->{actual} = $1; };
 /test elapsed time: (\d+\.?\d+)/ and do {
-  $data->{ $transport}->{ $test}->{time} = $1;
-  $data->{ $transport}->{ $test}->{size} = $size;
-  $data->{ $transport}->{ $test}->{rate} = $rate;
+  $data->{ $type}->{ $transport}->{ $test}->{time} = $1;
+  $data->{ $type}->{ $transport}->{ $test}->{size} = $size;
+  $data->{ $type}->{ $transport}->{ $test}->{rate} = $rate;
 };
 
-sub printtestresult ($$$$) {
-  my $size = shift;
-  my $rate = shift;
-  my $messages = shift;
-  my $time = shift;
-  my $actual = $messages * $size * 8 / $time;
-  print "$size $rate $actual\n";
+sub testName {
+  # Lookup table for test types.
+  my $type = shift;
+  return "UNKNOWN TEST TYPE"  if not $type;
+  return "Bidirectional"      if $type eq "bidir";
+  return "Publication Bound"  if $type eq "pubbound";
+  return "Subscription Bound" if $type eq "subbound";
+  return "UNKNOWN TEST TYPE";
 }
 
+sub testGroups {
+  # Lookup table for test numbers.
+  return { bidir    => [ [  1,  2,  3,  4,  5], [  1,  6,  7,  8,  9], [  1, 10, 11, 12, 13]],
+           pubbound => [ [  1,  2,  3,  4,  5], [  1,  6,  7,  8,  9], [  1, 10, 11, 12, 13]],
+           subbound => [ [ 14, 15, 16, 17, 18], [ 19, 20, 21, 15, 22], [ 14, 20, 23, 24, 25]]}->{ $_[0]};
+}
+
+sub print_header {
+  my $index     = shift;
+  my $transport = shift || "UNKNOWN TRANSPORT";
+  my $type      = shift || "UNKNOWN TEST TYPE";
+  my $subtype   = shift || "UNKNOWN TEST SUBTYPE";
+  print "#\n";
+  print "# Index " . $index++ . " data for $transport\n";
+  print "#\n";
+  print "# size (bytes per sample),\n";
+  print "# rate (samples per second),\n";
+  print "# throughput per second (bits per second)\n";
+  print "#\n";
+  print "# expected throughput per second == size * rate * 8\n";
+  print "#  -- using x:(column(1)*column(2)*8))\n";
+  print "#\n";
+  print "# $type / $subtype\n";
+}
+
+sub dataline {
+  my $dataline = shift;
+  my $scale    = shift || 8;
+  return "# NO DATA\n"        if not $dataline;
+  return "# NO SIZE\n"        if not $dataline->{size};
+  return "# NO RATE\n"        if not $dataline->{rate};
+  return "# NO MEASUREMENT\n" if not $dataline->{actual};
+  return "$dataline->{size} $dataline->{rate} "
+       . ($dataline->{actual} * $dataline->{size} * $scale / ($dataline->{time} || 120))
+       . "\n";
+}
 
 END {
   die "No actual data gathered." if not $data or not scalar keys %$data;
 
   my $index = 0;
-  foreach my $transport (sort keys %$data) {
-    print "#\n";
-    print "# Index " . $index++ . " data for $transport\n";
-    print "#\n";
-    print "# size (bytes per sample),\n";
-    print "# rate (samples per second),\n";
-    print "# throughput per second (bits per second)\n";
-    print "#\n";
-    print "# expected throughput per second == size * rate * 8\n";
-    print "#  -- using x:(column(1)*column(2)*8))\n";
-    print "#\n";
-    print "# Bidirectional / steepest ascent\n";
-    printtestresult ($data->{ $transport}->{ 1}->{size}, $data->{ $transport}->{ 1}->{rate}, $data->{ $transport}->{ 1}->{actual}, $data->{ $transport}->{ 1}->{time});
-    printtestresult ($data->{ $transport}->{ 2}->{size}, $data->{ $transport}->{ 2}->{rate}, $data->{ $transport}->{ 2}->{actual}, $data->{ $transport}->{ 2}->{time});
-    printtestresult ($data->{ $transport}->{ 3}->{size}, $data->{ $transport}->{ 3}->{rate}, $data->{ $transport}->{ 3}->{actual}, $data->{ $transport}->{ 3}->{time});
-    printtestresult ($data->{ $transport}->{ 4}->{size}, $data->{ $transport}->{ 4}->{rate}, $data->{ $transport}->{ 4}->{actual}, $data->{ $transport}->{ 4}->{time});
-    printtestresult ($data->{ $transport}->{ 5}->{size}, $data->{ $transport}->{ 5}->{rate}, $data->{ $transport}->{ 5}->{actual}, $data->{ $transport}->{ 5}->{time});
-    print "\n\n";
 
-    print "#\n";
-    print "# Index " . $index++ . " data for $transport\n";
-    print "#\n";
-    print "# size (bytes per sample),\n";
-    print "# rate (samples per second),\n";
-    print "# throughput per second (bits per second)\n";
-    print "#\n";
-    print "# expected throughput per second == size * rate * 8\n";
-    print "#  -- using x:(column(1)*column(2)*8))\n";
-    print "#\n";
-    print "# Bidirectional / fixed rate\n";
-    printtestresult ($data->{ $transport}->{ 1}->{size}, $data->{ $transport}->{ 1}->{rate}, $data->{ $transport}->{ 1}->{actual}, $data->{ $transport}->{ 1}->{time});
-    printtestresult ($data->{ $transport}->{ 6}->{size}, $data->{ $transport}->{ 6}->{rate}, $data->{ $transport}->{ 6}->{actual}, $data->{ $transport}->{ 6}->{time});
-    printtestresult ($data->{ $transport}->{ 7}->{size}, $data->{ $transport}->{ 7}->{rate}, $data->{ $transport}->{ 7}->{actual}, $data->{ $transport}->{ 7}->{time});
-    printtestresult ($data->{ $transport}->{ 8}->{size}, $data->{ $transport}->{ 8}->{rate}, $data->{ $transport}->{ 8}->{actual}, $data->{ $transport}->{ 8}->{time});
-    printtestresult ($data->{ $transport}->{ 9}->{size}, $data->{ $transport}->{ 9}->{rate}, $data->{ $transport}->{ 9}->{actual}, $data->{ $transport}->{ 9}->{time});
-    print "\n\n";
+  # Process each test type in turn.
+  foreach my $type (sort keys %$data) {
+    my $scale;
+    my $groups;
+    SWITCH:{
+      $type =~ /bidir/    && do { $scale = 16; $groups = &testGroups($type); last SWITCH; };
+      $type =~ /pubbound/ && do { $scale =  8; $groups = &testGroups($type); last SWITCH; };
+      $type =~ /subbound/ && do { $scale =  8; $groups = &testGroups($type); last SWITCH; };
+    }
 
-    print "#\n";
-    print "# Index " . $index++ . " data for $transport\n";
-    print "#\n";
-    print "# size (bytes per sample),\n";
-    print "# rate (samples per second),\n";
-    print "# throughput per second (bits per second)\n";
-    print "#\n";
-    print "# expected throughput per second == size * rate * 8\n";
-    print "#  -- using x:(column(1)*column(2)*8))\n";
-    print "#\n";
-    print "# Bidirectional / fixed size\n";
-    printtestresult ($data->{ $transport}->{ 1}->{size}, $data->{ $transport}->{ 1}->{rate}, $data->{ $transport}->{ 1}->{actual}, $data->{ $transport}->{ 1}->{time});
-    printtestresult ($data->{ $transport}->{ 10}->{size}, $data->{ $transport}->{ 10}->{rate}, $data->{ $transport}->{ 10}->{actual}, $data->{ $transport}->{ 10}->{time});
-    printtestresult ($data->{ $transport}->{ 11}->{size}, $data->{ $transport}->{ 11}->{rate}, $data->{ $transport}->{ 11}->{actual}, $data->{ $transport}->{ 11}->{time});
-    printtestresult ($data->{ $transport}->{ 12}->{size}, $data->{ $transport}->{ 12}->{rate}, $data->{ $transport}->{ 12}->{actual}, $data->{ $transport}->{ 12}->{time});
-    printtestresult ($data->{ $transport}->{ 13}->{size}, $data->{ $transport}->{ 13}->{rate}, $data->{ $transport}->{ 13}->{actual}, $data->{ $transport}->{ 13}->{time});
-    print "\n\n";
+    # Then process each transport in turn.
+    foreach my $transport (sort keys %{$data->{$type}}) {
+      # Each test type has three arrangements.
+      for my $current (0 .. 2) {
+        my $subtype = [ "steepest ascent", "fixed rate", "fixed size"]->[$current];
+        &print_header( $index++, $transport, &testName($type), $subtype);
+        foreach my $test (@{$groups->[$current]}) {
+          print &dataline( $data->{$type}->{$transport}->{$test}, $scale);
+        }
+        print "\n\n";
+      }
+    }
   }
 }
+
