55aefbbb43eb6ccced690e0737e115996295c9a8
Steven Stallion <stallions@ociweb.com>
2010-04-26 18:24:50 +0000

    Mon Apr 26 18:23:57 UTC 2010  Steven Stallion  <stallions@ociweb.com>

--- branches/RT13927/tools/modeling/plugins/org.opendds.modeling.diagram.application/src/org/opendds/modeling/diagram/application/edit/helpers/ApplicationModelEditHelper.java
+++
@@ -1,7 +0,0 @@
-package org.opendds.modeling.diagram.application.edit.helpers;
-
-/**
- * @generated
- */
-public class ApplicationModelEditHelper extends OpenDDSBaseEditHelper {
-}
--- branches/RT13927/tools/modeling/plugins/org.opendds.modeling.diagram.application/src/org/opendds/modeling/diagram/application/edit/helpers/ApplicationTargetEditHelper.java
+++
@@ -1,7 +0,0 @@
-package org.opendds.modeling.diagram.application.edit.helpers;
-
-/**
- * @generated
- */
-public class ApplicationTargetEditHelper extends OpenDDSBaseEditHelper {
-}
