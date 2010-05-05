058ee364b7ae763bb3a7033e50695191005bc420
Trevor Fields <fields_t@ociweb.com>
2010-05-05 23:19:23 +0000

    ChangeLogTag:Wed May 05 23:16:41 UTC 2010  Trevor Fields  <fields_t@ociweb.com>

--- trunk/performance-tests/Bench/bin/gen-throughput-tables.pl
+++ trunk/performance-tests/Bench/bin/gen-throughput-tables.pl
@@ -38,8 +38,11 @@ sub transportName {
   # Lookup table for transport types.
   my $type = shift;
   return "UNKNOWN TRANSPORT TYPE"  if not $type;
+  return "TCP"      if $type eq "tcp";
   return "TCP"      if $type eq "SimpleTcp";
+  return "Multicast Best Effort"  if $type eq "mbe";
   return "Multicast Best Effort"  if $type eq "best effort multicast";
+  return "Multicast Reliable" if $type eq "mre";
   return "Multicast Reliable" if $type eq "reliable multicast";
   return "UDP" if $type eq "udp";
   return "UNKNOWN TRANSPORT TYPE";
@@ -75,7 +78,7 @@ die "Unable to open output: $thru_filename - $!"
           print THROUT "<tr class=\"data_tables\">" .
                        "<td class=\"data_tables\">$size</td>" .
                        "<td class=\"data_tables\">$rate</td>" .
-                       "<td class=\"data_tables\">" . $data->{$type}->{$transport}->{$size}->{$rate} . "</td>" .
+                       "<td class=\"data_tables\">" . sprintf("%.2e", $data->{$type}->{$transport}->{$size}->{$rate}) . "</td>" .
                        "<td class=\"data_tables\">" .
                        sprintf("%.2e", 100 * $data->{$type}->{$transport}->{$size}->{$rate} / $test_max) . "</td></tr>\n";
 
