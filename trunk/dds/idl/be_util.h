4d0dfc4292466f616675d90cea7c4b3fe8dfacda
Jeff Parsons <j.parsons@vanderbilt.edu>
2010-07-12 08:47:58 +0000

    ChangeLogTag: Mon Jul 12 08:44:58 UTC 2010  Jeff Parsons <j.parsons@vanderbilt.edu>

---
+++ trunk/dds/idl/be_util.h
@@ -0,0 +1,46 @@
+/* -*- c++ -*- */
+
+//=============================================================================
+/**
+ *  @file    be_util.h
+ *
+ *  $Id$
+ *
+ *  Static helper methods used by multiple visitors.
+ *
+ *
+ *  @author Gary Maxey
+ *  @author Jeff Parsons
+ */
+//=============================================================================
+
+#ifndef TAO_BE_UTIL_H
+#define TAO_BE_UTIL_H
+
+class TAO_OutStream;
+class be_module;
+class AST_Decl;
+class AST_Generator;
+
+class be_util
+{
+public:
+  /// Special BE arg call factored out of DRV_args.
+  static void
+  prep_be_arg (char *s);
+
+  /// Checks made after parsing args.
+  static void
+  arg_post_proc (void);
+
+  /// Display usage of BE-specific options.
+  static void
+  usage (void);
+
+  /// Create an AST node generator.
+  static AST_Generator *
+  generator_init (void);
+};
+
+#endif // if !defined
+
