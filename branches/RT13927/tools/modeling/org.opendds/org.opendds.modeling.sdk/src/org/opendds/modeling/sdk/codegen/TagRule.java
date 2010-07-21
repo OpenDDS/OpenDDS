fb3b15c2898b3f679165309ba11a535d461b84eb
Mike Martinez <martinez_m@ociweb.com>
2010-07-21 00:05:32 +0000

    Created code generation editor infrastructure.  This includes a code generation file creation wizard and the multi-page editor framework itself.  Currently the default example editor is included.

---
+++ branches/RT13927/tools/modeling/org.opendds/org.opendds.modeling.sdk/src/org/opendds/modeling/sdk/codegen/TagRule.java
@@ -0,0 +1,31 @@
+package org.opendds.modeling.sdk.codegen;
+
+import org.eclipse.jface.text.rules.*;
+
+public class TagRule extends MultiLineRule {
+
+	public TagRule(IToken token) {
+		super("<", ">", token);
+	}
+	protected boolean sequenceDetected(
+		ICharacterScanner scanner,
+		char[] sequence,
+		boolean eofAllowed) {
+		int c = scanner.read();
+		if (sequence[0] == '<') {
+			if (c == '?') {
+				// processing instruction - abort
+				scanner.unread();
+				return false;
+			}
+			if (c == '!') {
+				scanner.unread();
+				// comment - abort
+				return false;
+			}
+		} else if (sequence[0] == '>') {
+			scanner.unread();
+		}
+		return super.sequenceDetected(scanner, sequence, eofAllowed);
+	}
+}
