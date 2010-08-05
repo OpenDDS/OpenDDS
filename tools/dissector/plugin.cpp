89acfa9cf9984f893df89ca7613e12c5676cc4ae
Adam Mitz <mitza@ociweb.com>
2010-08-05 20:57:40 +0000

    ChangeLogTag: Thu Aug  5 20:57:04 UTC 2010  Adam Mitz  <mitza@ociweb.com>

--- tools/dissector/plugin.cpp
+++ tools/dissector/plugin.cpp
@@ -18,25 +18,25 @@ extern "C" {
 
 #include <dds/Version.h>
 
-#include "odds_export.h"
+#include "dissector_export.h"
 
 #ifndef ACE_AS_STATIC_LIBS
 extern "C"
-odds_Export const gchar version[] = DDS_VERSION;
+dissector_Export const gchar version[] = DDS_VERSION;
 
 extern "C"
-odds_Export void
+dissector_Export void
 plugin_register()
 {
-  extern void proto_register_odds();
-  proto_register_odds();
+  extern void proto_register_opendds();
+  proto_register_opendds();
 }
 
 extern "C"
-odds_Export void
+dissector_Export void
 plugin_reg_handoff()
 {
-  extern void proto_reg_handoff_odds();
-  proto_reg_handoff_odds();
+  extern void proto_reg_handoff_opendds();
+  proto_reg_handoff_opendds();
 }
 #endif  /* ACE_AS_STATIC_LIBS */
