002b5dc94c77e04e7ac8a43b0b5a7e88ce896e23
Jeff Schmitz <schmitzj@ociweb.com>
2010-11-12 17:38:25 +0000

    Fri Nov 12 17:35:52 UTC 2010  Jeff Schmitz  <schmitz_j@ociweb.com>

--- branches/RT13927/tools/modeling/plugins/org.opendds.modeling.diagram.dcpslib/src/org/opendds/modeling/model/opendds/diagram/dcpslib/edit/commands/ReliabilityQosPolicyCreateCommand.java
+++ branches/RT13927/tools/modeling/plugins/org.opendds.modeling.diagram.dcpslib/src/org/opendds/modeling/model/opendds/diagram/dcpslib/edit/commands/ReliabilityQosPolicyCreateCommand.java
@@ -13,7 +13,6 @@ import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
 import org.eclipse.gmf.runtime.notation.View;
 import org.opendds.modeling.model.opendds.DcpsLib;
 import org.opendds.modeling.model.opendds.OpenDDSFactory;
-import org.opendds.modeling.model.opendds.diagram.dcpslib.providers.ElementInitializers;
 import org.opendds.modeling.model.opendds.diagram.dcpslib.providers.OpenDDSDcpsLibElementTypes;
 
 /**
