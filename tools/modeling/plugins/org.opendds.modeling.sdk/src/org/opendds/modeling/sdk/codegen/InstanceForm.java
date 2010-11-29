33b56793a9c190879128ce3e31e55ede3c9aa762
Jeff Schmitz <schmitzj@ociweb.com>
2010-11-29 22:14:47 +0000

    Mon Nov 29 22:05:17 UTC 2010  Jeff Schmitz  <schmitz_j@ociweb.com>

--- tools/modeling/plugins/org.opendds.modeling.sdk/src/org/opendds/modeling/sdk/codegen/InstanceForm.java
+++ tools/modeling/plugins/org.opendds.modeling.sdk/src/org/opendds/modeling/sdk/codegen/InstanceForm.java
@@ -1,15 +1,10 @@
 package org.opendds.modeling.sdk.codegen;
 
-import java.awt.Color;
-
-import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.editor.FormPage;
 import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.opendds.modeling.sdk.InstanceTraitsContentProvider;
-import org.opendds.modeling.sdk.presentation.InstancetraitsEditorPlugin;
 
 public class InstanceForm extends FormPage {
 
@@ -25,10 +20,6 @@ public class InstanceForm extends FormPage {
 		Composite parent = managedForm.getForm().getBody();
 		FormToolkit toolkit = managedForm.getToolkit();
 		Composite leftPanel = toolkit.createComposite(parent);
-		TreeViewer treeViewer = new TreeViewer(parent);
-		treeViewer.setContentProvider(new InstanceTraitsContentProvider());
-		treeViewer.setInput(null);
-
 	}
 	
 }
