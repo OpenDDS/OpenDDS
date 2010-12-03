c7393807787d8b654f2a30a74f9970e08e6191de
Byron Harris <harris_b@ociweb.com>
2010-12-03 13:23:48 +0000

    Fri Dec  3 13:15:15 UTC 2010  Byron Harris  <harrisb@ociweb.com>

--- branches/RT13927/tools/modeling/plugins/org.opendds.modeling.diagram.datalib/src/org/opendds/modeling/model/opendds/diagram/datalib/edit/parts/StructKeysEditPart.java
+++ branches/RT13927/tools/modeling/plugins/org.opendds.modeling.diagram.datalib/src/org/opendds/modeling/model/opendds/diagram/datalib/edit/parts/StructKeysEditPart.java
@@ -27,7 +27,7 @@ public class StructKeysEditPart extends ListCompartmentEditPart {
 	/**
 	 * @generated
 	 */
-	public static final int VISUAL_ID = 7004;
+	public static final int VISUAL_ID = 7007;
 
 	/**
 	 * @generated
