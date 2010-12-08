c196bebdd08284415d162270263fb2b0a009dd78
Byron Harris <harris_b@ociweb.com>
2010-12-08 14:43:00 +0000

    Wed Dec  8 14:40:53 UTC 2010  Byron Harris  <harrisb@ociweb.com>

--- branches/RT13927/tools/modeling/plugins/org.opendds.modeling.diagram.dcpslib/src/org/opendds/modeling/model/opendds/diagram/dcpslib/part/QosPolicyLabeler.java
+++ branches/RT13927/tools/modeling/plugins/org.opendds.modeling.diagram.dcpslib/src/org/opendds/modeling/model/opendds/diagram/dcpslib/part/QosPolicyLabeler.java
@@ -1,13 +1,24 @@
 package org.opendds.modeling.model.opendds.diagram.dcpslib.part;
 
+import org.eclipse.emf.ecore.EObject;
+import org.opendds.modeling.model.opendds.PolicyLib;
 import org.opendds.modeling.model.qos.QosPolicy;
 
 
 public class QosPolicyLabeler extends com.ociweb.gmf.dialogs.ObjectLabeler<QosPolicy> {
 
 	@Override
-	public String getLabel(QosPolicy feature) {
-		return feature.eClass().getName() + ": " + feature.getName();
+	public String getLabel(QosPolicy policy) {
+		String policyName = "";
+		
+		// Qualify by policy owner since a policy library may be in the same resource
+		// as the element using the policy.
+		EObject policyOwner = policy.eContainer();
+		if (policyOwner instanceof PolicyLib ) {
+			policyName = ((PolicyLib) policyOwner).getName() + ".";
+		}
+		policyName += policy.getName();
+		return "«" + policy.eClass().getName() + "» " + policyName;
 	}
 
 }
