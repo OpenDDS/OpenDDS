6f64a506e12c7945dcacd094aee29178e045c67f
Byron Harris <harris_b@ociweb.com>
2011-02-07 17:55:43 +0000

    Mon Feb  7 17:51:05 UTC 2011  Byron Harris  <harrisb@ociweb.com>

---
+++ branches/RT13927/tools/modeling/plugins/org.opendds.modeling.diagram.dcpslib/src/org/opendds/modeling/model/opendds/diagram/dcpslib/part/OpenDDSDcpsLibRefTypeActionContentFilteredTopic.java
@@ -0,0 +1,18 @@
+package org.opendds.modeling.model.opendds.diagram.dcpslib.part;
+
+import org.opendds.modeling.model.opendds.diagram.dcpslib.edit.parts.ContentFilteredTopicDataTypeEditPart;
+import org.opendds.modeling.model.opendds.diagram.dcpslib.edit.parts.ContentFilteredTopicEditPart;
+import org.opendds.modeling.model.opendds.diagram.dcpslib.edit.parts.TopicDataTypeEditPart;
+import org.opendds.modeling.model.opendds.diagram.dcpslib.edit.parts.TopicEditPart;
+
+/**
+ * @generated NOT
+ */
+public class OpenDDSDcpsLibRefTypeActionContentFilteredTopic extends
+OpenDDSDcpsLibRefTypeAction<ContentFilteredTopicDataTypeEditPart> {
+
+	public OpenDDSDcpsLibRefTypeActionContentFilteredTopic() {
+		super(ContentFilteredTopicEditPart.class);
+	}
+
+}
