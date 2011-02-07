6b9c06fdc34188d13d86dfb6e326fad68e2741dc
Jeff Schmitz <schmitzj@ociweb.com>
2011-02-07 18:03:18 +0000

    Mon Feb  7 18:00:21 UTC 2011  Jeff Schmitz  <schmitz_j@ociweb.com>

---
+++ tools/modeling/codegen/model/Sync.h
@@ -0,0 +1,31 @@
+#ifndef SYNC_H
+#define SYNC_H
+
+#include "model_export.h"
+#include "dds/DdsDcpsC.h"
+
+namespace OpenDDS {
+  namespace Model {
+    class OpenDDS_Model_Export WriterSync {
+    public:
+      WriterSync(DDS::DataWriter_var& writer);
+      ~WriterSync();
+      static int wait_match(DDS::DataWriter_var& writer);
+      static int wait_ack(DDS::DataWriter_var& writer);
+    private:
+      DDS::DataWriter_var& writer_;
+    };
+
+    class OpenDDS_Model_Export ReaderSync {
+    public:
+      ReaderSync(DDS::DataReader_var& reader);
+      ~ReaderSync();
+      static int wait_match(DDS::DataReader_var& reader);
+      static int wait_unmatch(DDS::DataReader_var& reader);
+    private:
+      DDS::DataReader_var& reader_;
+    };
+  };
+};
+
+#endif
