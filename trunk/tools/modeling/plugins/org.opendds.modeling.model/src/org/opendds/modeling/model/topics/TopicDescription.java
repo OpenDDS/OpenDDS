47038c110bd22796a32b283e4420585ca43aa085
Byron Harris <harris_b@ociweb.com>
2011-02-14 20:25:03 +0000

    Mon Feb 14 20:19:02 UTC 2011  Byron Harris  <harrisb@ociweb.com>

--- trunk/tools/modeling/plugins/org.opendds.modeling.model/src/org/opendds/modeling/model/topics/TopicDescription.java
+++ trunk/tools/modeling/plugins/org.opendds.modeling.model/src/org/opendds/modeling/model/topics/TopicDescription.java
@@ -15,44 +15,12 @@ import org.opendds.modeling.model.types.Struct;
  * A representation of the model object '<em><b>Topic Description</b></em>'.
  * <!-- end-user-doc -->
  *
- * <p>
- * The following features are supported:
- * <ul>
- *   <li>{@link org.opendds.modeling.model.topics.TopicDescription#getDatatype <em>Datatype</em>}</li>
- * </ul>
- * </p>
  *
  * @see org.opendds.modeling.model.topics.TopicsPackage#getTopicDescription()
  * @model abstract="true"
  * @generated
  */
 public interface TopicDescription extends Entity {
-	/**
-	 * Returns the value of the '<em><b>Datatype</b></em>' reference.
-	 * <!-- begin-user-doc -->
-	 * <p>
-	 * If the meaning of the '<em>Datatype</em>' reference isn't clear,
-	 * there really should be more of a description here...
-	 * </p>
-	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Datatype</em>' reference.
-	 * @see #setDatatype(Struct)
-	 * @see org.opendds.modeling.model.topics.TopicsPackage#getTopicDescription_Datatype()
-	 * @model required="true"
-	 * @generated
-	 */
-	Struct getDatatype();
-
-	/**
-	 * Sets the value of the '{@link org.opendds.modeling.model.topics.TopicDescription#getDatatype <em>Datatype</em>}' reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Datatype</em>' reference.
-	 * @see #getDatatype()
-	 * @generated
-	 */
-	void setDatatype(Struct value);
-
 	/**
 	 * Needed because datatype is a non-containment reference but will be
 	 * the domain element behind a sub-figure of the Topic figure and GMF
