4e62b731d348a92eb409a12aedf339a46c79ec67
Mike Martinez <martinez_m@ociweb.com>
2011-02-14 19:36:36 +0000

    Added SearchLocation support for table view items.

---
+++ tools/modeling/plugins/org.opendds.modeling.sdk.model.edit/src/org/opendds/modeling/sdk/model/GeneratorSpecification/Provider/SearchLocationItemProvider.java
@@ -0,0 +1,224 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.opendds.modeling.sdk.model.GeneratorSpecification.Provider;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.ResourceLocator;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IChildCreationExtender;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITableItemLabelProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+import org.opendds.modeling.common.Plugin;
+import org.opendds.modeling.sdk.model.GeneratorSpecification.GeneratorFactory;
+import org.opendds.modeling.sdk.model.GeneratorSpecification.GeneratorPackage;
+import org.opendds.modeling.sdk.model.GeneratorSpecification.LocationPath;
+import org.opendds.modeling.sdk.model.GeneratorSpecification.LocationVariable;
+import org.opendds.modeling.sdk.model.GeneratorSpecification.SearchLocation;
+import org.opendds.modeling.sdk.model.GeneratorSpecification.SearchPaths;
+
+/**
+ * This is the item provider adapter for a {@link org.opendds.modeling.sdk.model.GeneratorSpecification.SearchLocation} object.
+ * <!-- begin-user-doc -->
+ * @implements ITableItemLabelProvider
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class SearchLocationItemProvider extends ItemProviderAdapter implements
+		IEditingDomainItemProvider, IStructuredItemContentProvider,
+		ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource,
+		ITableItemLabelProvider {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public SearchLocationItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(
+			Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures
+					.add(GeneratorPackage.Literals.SEARCH_LOCATION__VARIABLE);
+			childrenFeatures
+					.add(GeneratorPackage.Literals.SEARCH_LOCATION__PATH);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
+	}
+
+	/**
+	 * This returns SearchLocation.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage(
+				"full/obj16/" + Plugin.INSTANCE.imageMapping("SearchLocation")));
+	}
+
+	@Override
+	public Object getColumnImage(Object object, int columnIndex) {
+		IItemLabelProvider itemLabelProvider;
+
+		switch( columnIndex) {
+		case 0:
+			LocationVariable variable = ((SearchLocation)object).getVariable();
+			if( variable == null) {
+				return null;
+			}
+			itemLabelProvider = (IItemLabelProvider)adapterFactory.adapt(variable, IItemLabelProvider.class);
+			return itemLabelProvider.getImage(variable);
+
+		case 1:
+			LocationPath path = ((SearchLocation)object).getPath();
+			if( path == null) {
+				return null;
+			}
+			itemLabelProvider = (IItemLabelProvider)adapterFactory.adapt(path, IItemLabelProvider.class);
+			return itemLabelProvider.getImage(path);
+
+		default: return overlayImage(object, getResourceLocator().getImage(
+				"full/obj16/" + Plugin.INSTANCE.imageMapping("SearchLocation")));
+		}
+	}
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String getText(Object object) {
+		return getString("_UI_SearchLocation_type");
+	}
+
+	@Override
+	public String getColumnText(Object object, int columnIndex) {
+		switch (columnIndex) {
+		case 0:
+			LocationVariable variable = ((SearchLocation) object).getVariable();
+			return variable != null? variable.getValue(): "";
+
+		case 1:
+			LocationPath path = ((SearchLocation) object).getPath();
+			return path != null? path.getValue(): "";
+
+		default:
+			return "";
+		}
+	}
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(SearchLocation.class)) {
+		case GeneratorPackage.SEARCH_LOCATION__VARIABLE:
+		case GeneratorPackage.SEARCH_LOCATION__PATH:
+			fireNotifyChanged(new ViewerNotification(notification, notification
+					.getNotifier(), true, false));
+			return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+	 * that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected void collectNewChildDescriptors(
+			Collection<Object> newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add(createChildParameter(
+				GeneratorPackage.Literals.SEARCH_LOCATION__VARIABLE,
+				GeneratorFactory.eINSTANCE.createLocationVariable()));
+
+		newChildDescriptors.add(createChildParameter(
+				GeneratorPackage.Literals.SEARCH_LOCATION__PATH,
+				GeneratorFactory.eINSTANCE.createLocationPath()));
+	}
+
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getResourceLocator() {
+		return ((IChildCreationExtender) adapterFactory).getResourceLocator();
+	}
+
+}
