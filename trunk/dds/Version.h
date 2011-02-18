b36e90101ccc640cf0bb796bda0d471929fe246b
Adam Mitz <mitza@ociweb.com>
2011-02-18 22:45:39 +0000

    ChangeLogTag: Fri Feb 18 22:43:48 UTC 2011  Adam Mitz  <mitza@ociweb.com>

--- trunk/dds/Version.h
+++ trunk/dds/Version.h
@@ -1,13 +1,12 @@
 /*
  * $Id$
  *
- * Copyright 2009 Object Computing, Inc.
  *
  * Distributed under the OpenDDS License.
  * See: http://www.opendds.org/license.html
  */
 
 #define DDS_MAJOR_VERSION 2
-#define DDS_MINOR_VERSION 2
+#define DDS_MINOR_VERSION 3
 #define DDS_MICRO_VERSION 0
-#define DDS_VERSION "2.2"
+#define DDS_VERSION "2.3"
