504c57a4f1f182d24031cb94532dfaefba42946c
Byron Harris <harris_b@ociweb.com>
2011-04-01 21:06:16 +0000

    Fri Apr  1 21:00:46 UTC 2011  Byron Harris  <harrisb@ociweb.com>

--- tools/modeling/plugins/org.opendds.modeling.diagram.dcpslib/src/org/opendds/modeling/diagram/dcpslib/edit/commands/QosPolicyCreationHelper.java
+++ tools/modeling/plugins/org.opendds.modeling.diagram.dcpslib/src/org/opendds/modeling/diagram/dcpslib/edit/commands/QosPolicyCreationHelper.java
@@ -1,8 +1,11 @@
 package org.opendds.modeling.diagram.dcpslib.edit.commands;
 
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.opendds.modeling.model.opendds.DcpsLib;
+import org.opendds.modeling.model.opendds.OpenDDSPackage;
 import org.opendds.modeling.model.opendds.qosPolicy;
 
 /**
@@ -13,29 +16,18 @@ class QosPolicyCreationHelper {
 	static <PolicyType extends qosPolicy> void addPolicy(PolicyType newElement, String policyAssociationName, EObject referrer) {
 
 		// Add to owner, which we know must be a DcpsLib
-		DcpsLib owner = null;
-
-		EObject container = referrer.eContainer();
-		if (container instanceof DcpsLib) {
-			owner = (DcpsLib) container;
-		}
-		else {
-			container = container.eContainer();
-			if (container instanceof DcpsLib) {
-				owner = (DcpsLib) container;
-			}
-			else {
-				container = container.eContainer();
-				if (container instanceof DcpsLib) {
-					owner = (DcpsLib) container;
-				}
-			}
-		}
+		DcpsLib owner = (DcpsLib) com.ociweb.emf.util.ObjectsFinder.findObjectInContainmentTree(referrer, getDcpsLibClass());
 		owner.getPolicies().add(newElement);
 
 		// Add to referrer
 		EStructuralFeature structuralFeature = referrer.eClass().getEStructuralFeature(policyAssociationName);
 		referrer.eSet(structuralFeature, newElement);
-		
+
 	}
+
+	private static EClass getDcpsLibClass() {
+		EPackage dcpsLibsPackage = EPackage.Registry.INSTANCE.getEPackage(OpenDDSPackage.eNS_URI);
+		return (EClass) dcpsLibsPackage.getEClassifier("DcpsLib");
+	}
+
 }
