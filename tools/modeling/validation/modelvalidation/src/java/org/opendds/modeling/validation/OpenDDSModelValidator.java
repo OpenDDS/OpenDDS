31200dcc611a64f2e4227963dc9367ea069c073f
Dean Wette <wette_d@ociweb.com>
2011-06-28 20:54:02 +0000

    Tue Jun 28 20:52:44 UTC 2011  Dean Wette  <wette_d@ociweb.com>
            * tools/modeling/validation/modelvalidation/docs/README:
            * tools/modeling/validation/modelvalidation/src/java/org/opendds/modeling/validation/OpenDDSModelValidator.java:
    
              Refactor validation tool for better ease of use.

--- tools/modeling/validation/modelvalidation/src/java/org/opendds/modeling/validation/OpenDDSModelValidator.java
+++ tools/modeling/validation/modelvalidation/src/java/org/opendds/modeling/validation/OpenDDSModelValidator.java
@@ -2,6 +2,7 @@ package org.opendds.modeling.validation;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,51 +32,80 @@ public class OpenDDSModelValidator {
 
 	static final String APP_NAME_PROPERTY = "app.name";
 	static final String FILE_ARG = "file";
-	static final String DIR_ARG = "dir";
 	static final String RECURSIVE_ARG = "recursive";
+	static final String QUIET_ARG = "quiet";
 
-	static final Map<String,String> schemas = new HashMap<String,String>();
+	private final Map<String,String> schemas = new HashMap<String,String>();
+
+	private final List<File> files;
+	private final boolean recursive;
+	private final boolean quiet;
+
+	public OpenDDSModelValidator(boolean recursive, boolean quiet, List<File> files) {
+	    this.files = files;
+	    this.recursive = recursive;
+	    this.quiet = quiet;
+
+	    String openddsXsd = System.getProperty(OPENDDS_XSD_FILE_PROPERTY, DEFAULT_OPENDDS_XSD);
+	    schemas.put(OPENDDS_FILE_EXT, openddsXsd);
+	    String codegenXsd = System.getProperty(CODEGEN_XSD_FILE_PROPERTY, DEFAULT_CODEGEN_XSD);
+	    schemas.put(CODEGEN_FILE_EXT, codegenXsd);
+	}
 
 	public static void main(String[] args) {
-
 		CommandLineParser parser = new PosixParser();
 		Options options = new Options();
-		Option fileOpt = new Option("f", FILE_ARG, true, "opendds file(s) to validate");
+		Option fileOpt = new Option("f", FILE_ARG, true, "opendds file(s) and/or directory(s) to validate");
 		fileOpt.setArgs(Integer.MAX_VALUE);
 		options.addOption(fileOpt);
-		options.addOption("d", DIR_ARG, true, "directory of opendds files to validate");
 		options.addOption("r", RECURSIVE_ARG, false, "search directory recursively for opendds files");
+		options.addOption("q", QUIET_ARG, false, "suppress output except for error reporting");
 
 		try {
-			String openddsXsd = System.getProperty(OPENDDS_XSD_FILE_PROPERTY, DEFAULT_OPENDDS_XSD);
-			schemas.put(OPENDDS_FILE_EXT, openddsXsd);
-			String codegenXsd = System.getProperty(CODEGEN_XSD_FILE_PROPERTY, DEFAULT_CODEGEN_XSD);
-			schemas.put(CODEGEN_FILE_EXT, codegenXsd);
-
-			CommandLine line = parser.parse(options, args);
-
-			if (!(line.hasOption(FILE_ARG) || line.hasOption(DIR_ARG))) {
+			CommandLine line = parser.parse(options, args, false);
+			if (!line.hasOption(FILE_ARG)) {
 				HelpFormatter formatter = new HelpFormatter();
 				formatter.printHelp(
 						System.getProperty(APP_NAME_PROPERTY, OpenDDSModelValidator.class.getSimpleName()),
 						options);
+				System.exit(-1);
 			}
-			String[] files = line.getOptionValues(FILE_ARG);
-			String dir = line.getOptionValue(DIR_ARG);
+
+			String[] filenames = line.getOptionValues(FILE_ARG);
 			boolean recursive = line.hasOption(RECURSIVE_ARG);
-			if (files != null) {
-				validateFiles(files);
-			}
-			if (dir != null) {
-				validateDir(new File(dir), recursive);
-			}
-		}
-		catch( ParseException exp ) {
+			boolean quiet = line.hasOption(QUIET_ARG);
+			List<File> files = new ArrayList<File>();
+			for (String filename : filenames) {
+			    files.add(new File(filename));
+            }
+			OpenDDSModelValidator validator = new OpenDDSModelValidator(recursive, quiet, files);
+			int numErrs = validator.validate();
+			System.exit(numErrs);
+		} catch (ParseException exp) {
 			logger.error("Encountered an unexpected error:" + exp.getMessage());
 		}
 	}
 
-	static void validateFile(final File xml) {
+    public int validate() {
+        if (quiet) {
+            logger.info("Starting validation in quiet mode (error reporting only)...");
+        } else {
+            logger.info("Starting validation...");
+        }
+        int numInvalidFiles = 0;
+        for (File file : files) {
+            if (file.isDirectory()) {
+                numInvalidFiles += validateDir(file);
+            } else {
+                numInvalidFiles += validateFile(file) ? 0 : 1;
+            }
+        }
+        logger.info("Validation completed.");
+        return numInvalidFiles;
+    }
+
+	private boolean validateFile(File xml) {
+	    boolean valid = false;
 		try {
 		    String ext = xml.getName().replaceAll(".+(\\..+)", "$1");
 		    String xsdFile = schemas.get(ext);
@@ -91,23 +121,23 @@ public class OpenDDSModelValidator {
 					        se.getColumnNumber() + ": " + se.getMessage());
 				}
 			} else {
-				logger.info("Validated " + xml);
+			    if (!quiet) {
+			        logger.info("Validated " + xml);
+			    }
+			    valid = true;
 			}
+			return valid;
 		} catch (RuntimeException e) {
 			logger.fatal("Unexpected error",e);
 			throw e;
 		} catch (Exception e) {
 		    logger.error("Fatal validation Error: " + e.getMessage());
+		    return false;
 		}
 	}
 
-	static void validateFiles(final String[] files) {
-		for (String filename : files) {
-			validateFile(new File(filename));
-		}
-	}
-
-	static void validateDir(final File parent, final boolean recursive){
+	private int validateDir(File parent){
+	    int numInvalidFiles = 0;
         FileFilter filter = new FileFilter() {
             @Override
             public boolean accept(File file) {
@@ -120,11 +150,12 @@ public class OpenDDSModelValidator {
         };
 		for (File f: parent.listFiles(filter)) {
 			logger.debug("Checking " + f.getPath());
-			if (f.isDirectory() && recursive) {
-				validateDir(f, true);
+			if (recursive && f.isDirectory()) {
+			    numInvalidFiles += validateDir(f);
 			} else {
-				validateFile(f);
+			    numInvalidFiles += validateFile(f) ? 0 : 1;
 			}
 		}
+		return numInvalidFiles;
 	}
 }
