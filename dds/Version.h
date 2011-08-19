9821861e6cf579f3ac86b16e7f064711886b91f4
Adam Mitz <mitza@ociweb.com>
2011-08-19 13:51:46 +0000

    ChangeLogTag: Fri Aug 19 13:47:18 UTC 2011  Adam Mitz  <mitza@ociweb.com>

--- dds/Version.h
+++ dds/Version.h
@@ -6,7 +6,7 @@
  * See: http://www.opendds.org/license.html
  */
 
-#define DDS_MAJOR_VERSION 2
-#define DDS_MINOR_VERSION 4
+#define DDS_MAJOR_VERSION 3
+#define DDS_MINOR_VERSION 0
 #define DDS_MICRO_VERSION 0
-#define DDS_VERSION "2.4"
+#define DDS_VERSION "3.0"
