9d5178e6e75117e66082c9685af04c37966af9a4
Adam Mitz <mitza@ociweb.com>
2011-09-09 21:04:46 +0000

    ChangeLogTag: Fri Sep  9 21:00:15 UTC 2011  Adam Mitz  <mitza@ociweb.com>

--- dds/DCPS/transport/rtps_udp/RtpsUdpReceiveStrategy.cpp
+++ dds/DCPS/transport/rtps_udp/RtpsUdpReceiveStrategy.cpp
@@ -9,6 +9,8 @@
 #include "RtpsUdpReceiveStrategy.h"
 #include "RtpsUdpDataLink.h"
 
+#include "dds/DCPS/RTPS/BaseMessageTypes.h"
+
 #include "ace/Reactor.h"
 
 namespace OpenDDS {
@@ -17,6 +19,7 @@ namespace DCPS {
 RtpsUdpReceiveStrategy::RtpsUdpReceiveStrategy(RtpsUdpDataLink* link)
   : link_(link)
   , last_received_()
+  , receiver_(OpenDDS::DCPS::GUIDPREFIX_UNKNOWN) //TODO: get local prefix from link
 {
 }
 
@@ -90,6 +93,124 @@ bool
 RtpsUdpReceiveStrategy::check_header(const TransportHeader& header)
 {
   return true;
+}
+
+RtpsUdpReceiveStrategy::MessageReceiver::MessageReceiver(
+  const OpenDDS::RTPS::GuidPrefix_t& local)
+{
+  assign(local_, local);
+}
+
+void
+RtpsUdpReceiveStrategy::MessageReceiver::reset(const ACE_INET_Addr& addr,
+                                               const OpenDDS::RTPS::Header& hdr)
+{
+  using namespace OpenDDS::RTPS;
+  // see RTPS spec v2.1 section 8.3.4 table 8.16 and section 8.3.6.4
+  source_version_ = hdr.version;
+  source_vendor_ = hdr.vendorId;
+
+  assign(source_guid_prefix_, hdr.guidPrefix);
+  assign(dest_guid_prefix_, local_);
+
+  unicast_reply_locator_list_.length(0);
+  unicast_reply_locator_list_[0].kind =
+    addr.get_type() == AF_INET6 ? LOCATOR_KIND_UDPv6 : LOCATOR_KIND_UDPv4;
+  unicast_reply_locator_list_[0].port = LOCATOR_PORT_INVALID;
+  assign(unicast_reply_locator_list_[0].address, addr);
+
+  multicast_reply_locator_list_.length(0);
+  multicast_reply_locator_list_[0].kind =
+    addr.get_type() == AF_INET6 ? LOCATOR_KIND_UDPv6 : LOCATOR_KIND_UDPv4;
+  multicast_reply_locator_list_[0].port = LOCATOR_PORT_INVALID;
+  assign(multicast_reply_locator_list_[0].address, LOCATOR_ADDRESS_INVALID);
+
+  have_timestamp_ = false;
+  timestamp_ = TIME_INVALID;
+}
+
+void
+RtpsUdpReceiveStrategy::MessageReceiver::submsg(
+  const OpenDDS::RTPS::InfoDestinationSubmessage& id)
+{
+  // see RTPS spec v2.1 section 8.3.7.7.4
+  for (size_t i = 0; i < sizeof(GuidPrefix_t); ++i) {
+    if (id.guidPrefix[i]) { // if some byte is > 0, it's not UNKNOWN
+      assign(dest_guid_prefix_, id.guidPrefix);
+      return;
+    }
+  }
+  assign(dest_guid_prefix_, local_);
+}
+
+void
+RtpsUdpReceiveStrategy::MessageReceiver::submsg(
+  const OpenDDS::RTPS::InfoReplySubmessage& ir)
+{
+  // see RTPS spec v2.1 section 8.3.7.8.4
+  unicast_reply_locator_list_.length(ir.unicastLocatorList.length());
+  for (CORBA::ULong i = 0; i < ir.unicastLocatorList.length(); ++i) {
+    unicast_reply_locator_list_[i] = ir.unicastLocatorList[i];
+  }
+
+  if (ir.smHeader.flags & 2 /* MulticastFlag */) {
+    multicast_reply_locator_list_.length(ir.multicastLocatorList.length());
+    for (CORBA::ULong i = 0; i < ir.multicastLocatorList.length(); ++i) {
+      multicast_reply_locator_list_[i] = ir.multicastLocatorList[i];
+    }
+
+  } else {
+    multicast_reply_locator_list_.length(0);
+  }
+}
+
+void
+RtpsUdpReceiveStrategy::MessageReceiver::submsg(
+  const OpenDDS::RTPS::InfoReplyIp4Submessage& iri4)
+{
+  // see RTPS spec v2.1 sections 8.3.7.8.4 and 9.4.5.14
+  unicast_reply_locator_list_.length(1);
+  unicast_reply_locator_list_[0].kind = OpenDDS::RTPS::LOCATOR_KIND_UDPv4;
+  unicast_reply_locator_list_[0].port = iri4.unicastLocator.port;
+  assign(unicast_reply_locator_list_[0].address, iri4.unicastLocator.address);
+
+  if (iri4.smHeader.flags & 2 /* MulticastFlag */) {
+    multicast_reply_locator_list_.length(1);
+    multicast_reply_locator_list_[0].kind = OpenDDS::RTPS::LOCATOR_KIND_UDPv4;
+    multicast_reply_locator_list_[0].port = iri4.multicastLocator.port;
+    assign(multicast_reply_locator_list_[0].address,
+           iri4.multicastLocator.address);
+  } else {
+    multicast_reply_locator_list_.length(0);
+  }
+}
+
+void
+RtpsUdpReceiveStrategy::MessageReceiver::submsg(
+  const OpenDDS::RTPS::InfoTimestampSubmessage& it)
+{
+  // see RTPS spec v2.1 section 8.3.7.9.10
+  if (!(it.smHeader.flags & 2 /* InvalidateFlag */)) {
+    have_timestamp_ = true;
+    timestamp_ = it.timestamp;
+  } else {
+    have_timestamp_ = false;
+  }
+}
+
+void
+RtpsUdpReceiveStrategy::MessageReceiver::submsg(
+  const OpenDDS::RTPS::InfoSourceSubmessage& is)
+{
+  // see RTPS spec v2.1 section 8.3.7.9.4
+  assign(source_guid_prefix_, is.guidPrefix);
+  source_version_ = is.version;
+  source_vendor_ = is.vendorId;
+  unicast_reply_locator_list_.length(1);
+  unicast_reply_locator_list_[0] = OpenDDS::RTPS::LOCATOR_INVALID;
+  multicast_reply_locator_list_.length(1);
+  multicast_reply_locator_list_[0] = OpenDDS::RTPS::LOCATOR_INVALID;
+  have_timestamp_ = false;
 }
 
 } // namespace DCPS
--- dds/DCPS/transport/rtps_udp/RtpsUdpReceiveStrategy.h
+++ dds/DCPS/transport/rtps_udp/RtpsUdpReceiveStrategy.h
@@ -11,9 +11,15 @@
 
 #include "Rtps_Udp_Export.h"
 
-#include "ace/Event_Handler.h"
-
 #include "dds/DCPS/transport/framework/TransportReceiveStrategy.h"
+
+#include "dds/DCPS/RTPS/RtpsBaseMessageTypesC.h"
+#include "dds/DCPS/RTPS/RtpsMessageTypesC.h"
+
+#include "ace/Event_Handler.h"
+#include "ace/INET_Addr.h"
+
+#include <cstring>
 
 namespace OpenDDS {
 namespace DCPS {
@@ -50,7 +56,89 @@ protected:
 private:
   RtpsUdpDataLink* link_;
   SequenceNumber last_received_;
+
+  struct MessageReceiver {
+
+    explicit MessageReceiver(const OpenDDS::RTPS::GuidPrefix_t& local);
+
+    void reset(const ACE_INET_Addr& remote_address,
+               const OpenDDS::RTPS::Header& hdr);
+
+    void submsg(const OpenDDS::RTPS::InfoDestinationSubmessage& id);
+    void submsg(const OpenDDS::RTPS::InfoReplySubmessage& ir);
+    void submsg(const OpenDDS::RTPS::InfoReplyIp4Submessage& iri4);
+    void submsg(const OpenDDS::RTPS::InfoTimestampSubmessage& it);
+    void submsg(const OpenDDS::RTPS::InfoSourceSubmessage& is);
+
+    static inline void assign(OpenDDS::RTPS::GuidPrefix_t& dest,
+                              const OpenDDS::RTPS::GuidPrefix_t& src);
+
+    static inline void assign(OpenDDS::RTPS::OctetArray16& dest,
+                              const OpenDDS::RTPS::OctetArray16& src);
+
+    static inline void assign(OpenDDS::RTPS::OctetArray16& dest,
+                              const ACE_INET_Addr& addr);
+
+    static inline void assign(OpenDDS::RTPS::OctetArray16& dest,
+                              const ACE_CDR::ULong& ipv4addr_be);
+
+    OpenDDS::RTPS::GuidPrefix_t local_;
+    OpenDDS::RTPS::ProtocolVersion_t source_version_;
+    OpenDDS::RTPS::VendorId_t source_vendor_;
+    OpenDDS::RTPS::GuidPrefix_t source_guid_prefix_;
+    OpenDDS::RTPS::GuidPrefix_t dest_guid_prefix_;
+    OpenDDS::RTPS::LocatorSeq unicast_reply_locator_list_;
+    OpenDDS::RTPS::LocatorSeq multicast_reply_locator_list_;
+    bool have_timestamp_;
+    OpenDDS::RTPS::Time_t timestamp_;
+  };
+
+  MessageReceiver receiver_;
 };
+
+inline void
+RtpsUdpReceiveStrategy::MessageReceiver::assign(
+  OpenDDS::RTPS::GuidPrefix_t& dest,
+  const OpenDDS::RTPS::GuidPrefix_t& src)
+{
+  std::memcpy(&dest[0], &src[0], sizeof(GuidPrefix_t));
+}
+
+inline void
+RtpsUdpReceiveStrategy::MessageReceiver::assign(
+  OpenDDS::RTPS::OctetArray16& dest,
+  const OpenDDS::RTPS::OctetArray16& src)
+{
+  std::memcpy(&dest[0], &src[0], sizeof(OpenDDS::RTPS::OctetArray16));
+}
+
+inline void
+RtpsUdpReceiveStrategy::MessageReceiver::assign(
+  OpenDDS::RTPS::OctetArray16& dest,
+  const ACE_INET_Addr& addr)
+{
+  // ACE does not make this easy...
+  const void* raw = addr.get_addr();
+  if (addr.get_type() == AF_INET6) {
+    const sockaddr_in6* in = static_cast<const sockaddr_in6*>(raw);
+    std::memcpy(&dest[0], &in->sin6_addr, 16);
+  } else {
+    const sockaddr_in* in = static_cast<const sockaddr_in*>(raw);
+    std::memset(&dest[0], 0, 12);
+    std::memcpy(&dest[12], &in->sin_addr, 4);
+  }
+}
+
+inline void
+RtpsUdpReceiveStrategy::MessageReceiver::assign(
+  OpenDDS::RTPS::OctetArray16& dest, const ACE_CDR::ULong& ipv4addr_be)
+{
+  std::memset(&dest[0], 0, 12);
+  dest[12] = ipv4addr_be >> 24;
+  dest[13] = ipv4addr_be >> 16;
+  dest[14] = ipv4addr_be >> 8;
+  dest[15] = ipv4addr_be;
+}
 
 } // namespace DCPS
 } // namespace OpenDDS
