aff883d79b4e3c6c201a988253f75f972bee83db
Adam Mitz <mitza@ociweb.com>
2012-01-23 19:17:19 +0000

    ChangeLogTag: Mon Jan 23 19:02:17 UTC 2012  Adam Mitz  <mitza@ociweb.com>

--- dds/DCPS/RTPS/RtpsDiscovery.cpp
+++ dds/DCPS/RTPS/RtpsDiscovery.cpp
@@ -25,7 +25,7 @@
 namespace {
   u_short get_default_d0(u_short fallback)
   {
-#ifndef ACE_LACKS_GETENV
+#if !defined ACE_LACKS_GETENV && !defined ACE_LACKS_ENV
     const char* from_env = std::getenv("OPENDDS_RTPS_DEFAULT_D0");
     if (from_env) {
       return static_cast<u_short>(std::atoi(from_env));
