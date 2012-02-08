766c010db0f1584ef021dffd89f258a78bdf6eb6
Adam Mitz <mitza@ociweb.com>
2012-02-08 16:46:40 +0000

    ChangeLogTag: Wed Feb  8 16:45:07 UTC 2012  Adam Mitz  <mitza@ociweb.com>

--- java/tests/messenger/publisher/run_test.pl
+++ java/tests/messenger/publisher/run_test.pl
@@ -9,8 +9,7 @@ use lib "$DDS_ROOT/bin";
 use lib "$ACE_ROOT/bin";
 use PerlDDS::Run_Test;
 use PerlDDS::Process_Java;
-use strict;
-
+use strict; 
 my $status = 0;
 my $debug = '0';
 
@@ -20,76 +19,80 @@ foreach my $i (@ARGV) {
     }
 }
 
-my $transport = pop;
-if ($transport eq "") {
-    $transport = 'tcp';
+my $config = pop;
+if ($config eq '') {
+    $config = 'tcp';
 }
 
-my $opts = "-DCPSBit 0";
-my $debug_opt = ($debug eq '0') ? ''
-    : "-ORBDebugLevel $debug -DCPSDebugLevel $debug " .
-      "-DCPSTransportDebugLevel $debug";
+my $use_repo = ($config !~ /^rtps_disc/);
 
-my $pub_opts = "$opts $debug_opt ".
-    "-ORBLogFile pub.log -DCPSConfigFile pub_$transport.ini";
-my $sub_opts = "$opts $debug_opt ".
-    "-ORBLogFile sub.log -DCPSConfigFile sub_$transport.ini";
+my $opts = "-DCPSBit 0 -DCPSConfigFile ../$config.ini";
+my $pub_opts = $opts;
+my $sub_opts = $opts;
+if ($debug ne '0') {
+    my $debug_opt = "-ORBDebugLevel $debug -DCPSDebugLevel $debug " .
+                    "-DCPSTransportDebugLevel $debug";
+    $pub_opts .= " $debug_opt -ORBLogFile pub.log";
+    $sub_opts .= " $debug_opt -ORBLogFile sub.log";
+}
 
-my $dcpsrepo_ior = "repo.ior";
+my $dcpsrepo_ior = 'repo.ior';
 
 unlink $dcpsrepo_ior;
 unlink qw/pub.log sub.log DCPSInfoRepo.log/;
 
-my $DCPSREPO = PerlDDS::create_process ("$DDS_ROOT/bin/DCPSInfoRepo",
-               "-NOBITS".
-               "-ORBDebugLevel $debug ".
-               "-ORBLogFile DCPSInfoRepo.log -o $dcpsrepo_ior");
+my $DCPSREPO = PerlDDS::create_process("$DDS_ROOT/bin/DCPSInfoRepo",
+                 '-NOBITS' . (($debug eq '0' ? '' : " -ORBDebugLevel $debug" .
+                 ' -ORBLogFile DCPSInfoRepo.log')) . " -o $dcpsrepo_ior");
 
-my $SUB = PerlDDS::create_process ("$DDS_ROOT/tests/DCPS/Messenger".
-                                   "/subscriber", "$sub_opts");
+my $SUB = PerlDDS::create_process("$DDS_ROOT/tests/DCPS/Messenger/subscriber",
+                                  $sub_opts);
 
-PerlACE::add_lib_path ("$DDS_ROOT/java/tests/messenger/messenger_idl");
-PerlACE::add_lib_path ("$DDS_ROOT/tests/DCPS/Messenger");
+PerlACE::add_lib_path("$DDS_ROOT/java/tests/messenger/messenger_idl");
+PerlACE::add_lib_path("$DDS_ROOT/tests/DCPS/Messenger");
 
-my $PUB = new PerlDDS::Process_Java ('TestPublisher', $pub_opts,
-                           ["$DDS_ROOT/java/tests/messenger/messenger_idl/".
-                            "messenger_idl_test.jar"]);
+my $PUB = new PerlDDS::Process_Java('TestPublisher', $pub_opts,
+            ["$DDS_ROOT/java/tests/messenger/messenger_idl/".
+             'messenger_idl_test.jar']);
 
-$DCPSREPO->Spawn ();
-if (PerlACE::waitforfile_timed ($dcpsrepo_ior, 30) == -1) {
-    print STDERR "ERROR: waiting for DCPSInfo IOR file\n";
-    $DCPSREPO->Kill ();
-    exit 1;
+if ($use_repo) {
+    $DCPSREPO->Spawn();
+    if (PerlACE::waitforfile_timed($dcpsrepo_ior, 30) == -1) {
+        print STDERR "ERROR: waiting for DCPSInfo IOR file\n";
+        $DCPSREPO->Kill();
+        exit 1;
+    }
 }
 
-$PUB->Spawn ();
+$PUB->Spawn();
 
-$SUB->Spawn ();
+$SUB->Spawn();
 
-my $PublisherResult = $PUB->WaitKill (300);
+my $PublisherResult = $PUB->WaitKill(300);
 if ($PublisherResult != 0) {
-    print STDERR "ERROR: publisher returned $PublisherResult \n";
+    print STDERR "ERROR: publisher returned $PublisherResult\n";
     $status = 1;
 }
 
-my $SubscriberResult = $SUB->WaitKill (15);
+my $SubscriberResult = $SUB->WaitKill(15);
 if ($SubscriberResult != 0) {
-    print STDERR "ERROR: subscriber returned $SubscriberResult \n";
+    print STDERR "ERROR: subscriber returned $SubscriberResult\n";
     $status = 1;
 }
 
-my $ir = $DCPSREPO->TerminateWaitKill(5);
-if ($ir != 0) {
-    print STDERR "ERROR: DCPSInfoRepo returned $ir\n";
-    $status = 1;
+if ($use_repo) {
+    my $ir = $DCPSREPO->TerminateWaitKill(5);
+    if ($ir != 0) {
+        print STDERR "ERROR: DCPSInfoRepo returned $ir\n";
+        $status = 1;
+    }
+    unlink $dcpsrepo_ior;
 }
-
-unlink $dcpsrepo_ior;
 
 if ($status == 0) {
-  print "test PASSED.\n";
+    print "test PASSED.\n";
 } else {
-  print STDERR "test FAILED.\n";
+    print STDERR "test FAILED.\n";
 }
 
 exit $status;
--- java/tests/messenger/run_test.pl
+++ java/tests/messenger/run_test.pl
@@ -9,8 +9,7 @@ use lib "$DDS_ROOT/bin";
 use lib "$ACE_ROOT/bin";
 use PerlDDS::Run_Test;
 use PerlDDS::Process_Java;
-use strict;
-
+use strict; 
 my $status = 0;
 my $debug = '0';
 
@@ -20,75 +19,80 @@ foreach my $i (@ARGV) {
     }
 }
 
-my $transport = pop;
-if ($transport eq "") {
-    $transport = 'tcp';
+my $config = pop;
+if ($config eq '') {
+    $config = 'tcp';
 }
 
-my $opts = "-DCPSBit 0";
-my $debug_opt = ($debug eq '0') ? ''
-    : "-ORBDebugLevel $debug -DCPSDebugLevel $debug";
-my $pub_dir = "publisher";
-my $sub_dir = "subscriber";
-my $pub_opts = "$opts -ORBListenEndpoints iiop://127.0.0.1:12346 $debug_opt ".
-    "-ORBLogFile pub.log -DCPSConfigFile $pub_dir/pub_$transport.ini";
-my $sub_opts = "$opts -ORBListenEndpoints iiop://127.0.0.1:12347 $debug_opt ".
-    "-ORBLogFile sub.log -DCPSConfigFile $sub_dir/sub_$transport.ini";
+my $use_repo = ($config !~ /^rtps_disc/);
 
-my $dcpsrepo_ior = "repo.ior";
+my $opts = "-DCPSBit 0 -DCPSConfigFile $config.ini";
+my $pub_opts = $opts;
+my $sub_opts = $opts;
+if ($debug ne '0') {
+    my $debug_opt = "-ORBDebugLevel $debug -DCPSDebugLevel $debug " .
+                    "-DCPSTransportDebugLevel $debug";
+    $pub_opts .= " $debug_opt -ORBLogFile pub.log";
+    $sub_opts .= " $debug_opt -ORBLogFile sub.log";
+}
+
+my $dcpsrepo_ior = 'repo.ior';
 
 unlink $dcpsrepo_ior;
+unlink qw/pub.log sub.log DCPSInfoRepo.log/;
 
-my $DCPSREPO = PerlDDS::create_process ("$DDS_ROOT/bin/DCPSInfoRepo",
-               "-NOBITS ".
-               "-ORBListenEndpoints iiop://127.0.0.1:1111 -ORBDebugLevel 10 ".
-               "-ORBLogFile DCPSInfoRepo.log -o $dcpsrepo_ior ");
+my $DCPSREPO = PerlDDS::create_process("$DDS_ROOT/bin/DCPSInfoRepo",
+                 '-NOBITS' . (($debug eq '0' ? '' : " -ORBDebugLevel $debug" .
+                 ' -ORBLogFile DCPSInfoRepo.log')) . " -o $dcpsrepo_ior");
 
-PerlACE::add_lib_path ("$DDS_ROOT/java/tests/messenger/messenger_idl");
+my $SUB = new PerlDDS::Process_Java('TestSubscriber', $sub_opts,
+            ["$DDS_ROOT/java/tests/messenger/messenger_idl/".
+             'messenger_idl_test.jar', 'subscriber/classes']);
 
-my $PUB = new PerlDDS::Process_Java ('TestPublisher', $pub_opts,
-                           ["$DDS_ROOT/java/tests/messenger/messenger_idl/".
-                            "messenger_idl_test.jar", "$pub_dir/classes"]);
+PerlACE::add_lib_path("$DDS_ROOT/java/tests/messenger/messenger_idl");
 
-my $SUB = new PerlDDS::Process_Java ('TestSubscriber', $sub_opts,
-                           ["$DDS_ROOT/java/tests/messenger/messenger_idl/".
-                            "messenger_idl_test.jar", "$sub_dir/classes"]);
+my $PUB = new PerlDDS::Process_Java('TestPublisher', $pub_opts,
+            ["$DDS_ROOT/java/tests/messenger/messenger_idl/".
+             'messenger_idl_test.jar', 'publisher/classes']);
 
-$DCPSREPO->Spawn ();
-if (PerlACE::waitforfile_timed ($dcpsrepo_ior, 30) == -1) {
-    print STDERR "ERROR: waiting for DCPSInfo IOR file\n";
-    $DCPSREPO->Kill ();
-    exit 1;
+if ($use_repo) {
+    $DCPSREPO->Spawn();
+    if (PerlACE::waitforfile_timed($dcpsrepo_ior, 30) == -1) {
+        print STDERR "ERROR: waiting for DCPSInfo IOR file\n";
+        $DCPSREPO->Kill();
+        exit 1;
+    }
 }
 
-$PUB->Spawn ();
+$PUB->Spawn();
 
-$SUB->Spawn ();
+$SUB->Spawn();
 
-my $PublisherResult = $PUB->WaitKill (300);
+my $PublisherResult = $PUB->WaitKill(300);
 if ($PublisherResult != 0) {
-    print STDERR "ERROR: publisher returned $PublisherResult \n";
+    print STDERR "ERROR: publisher returned $PublisherResult\n";
     $status = 1;
 }
 
-my $SubscriberResult = $SUB->WaitKill (15);
+my $SubscriberResult = $SUB->WaitKill(15);
 if ($SubscriberResult != 0) {
-    print STDERR "ERROR: subscriber returned $SubscriberResult \n";
+    print STDERR "ERROR: subscriber returned $SubscriberResult\n";
     $status = 1;
 }
 
-my $ir = $DCPSREPO->TerminateWaitKill(5);
-if ($ir != 0) {
-    print STDERR "ERROR: DCPSInfoRepo returned $ir\n";
-    $status = 1;
+if ($use_repo) {
+    my $ir = $DCPSREPO->TerminateWaitKill(5);
+    if ($ir != 0) {
+        print STDERR "ERROR: DCPSInfoRepo returned $ir\n";
+        $status = 1;
+    }
+    unlink $dcpsrepo_ior;
 }
-
-unlink $dcpsrepo_ior;
 
 if ($status == 0) {
-  print "test PASSED.\n";
+    print "test PASSED.\n";
 } else {
-  print STDERR "test FAILED.\n";
+    print STDERR "test FAILED.\n";
 }
 
 exit $status;
