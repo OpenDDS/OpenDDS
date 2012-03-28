39556fb5ff471afb5289a5dbae464926069db3b2
Adam Mitz <mitza@ociweb.com>
2012-03-28 18:00:28 +0000

    ChangeLogTag: Wed Mar 28 17:41:16 UTC 2012  Adam Mitz  <mitza@ociweb.com>

--- dds/Version.h
+++ dds/Version.h
@@ -7,6 +7,6 @@
  */
 
 #define DDS_MAJOR_VERSION 3
-#define DDS_MINOR_VERSION 0
-#define DDS_MICRO_VERSION 1
-#define DDS_VERSION "3.0.1"
+#define DDS_MINOR_VERSION 1
+#define DDS_MICRO_VERSION 0
+#define DDS_VERSION "3.1"
