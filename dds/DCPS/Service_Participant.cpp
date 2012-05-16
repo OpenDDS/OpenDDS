164d5c8c508ee21ab819ec4873b0e8c8568b80e5
Marcel Smit <msmit@remedy.nl>
2012-05-16 09:19:11 +0000

    Wed May 16 09:17:43 UTC 2012  Marcel Smit  <msmit@remedy.nl>
            * dds/DCPS/Service_Participant.cpp:
              Changed the default recovery method back to REPO.

--- dds/DCPS/Service_Participant.cpp
+++ dds/DCPS/Service_Participant.cpp
@@ -81,7 +81,7 @@ static bool got_persistent_data_dir = false;
 static bool got_default_discovery = false;
 
 #if !defined (DDS_DEFAULT_DISCOVERY_METHOD)
-# define DDS_DEFAULT_DISCOVERY_METHOD Discovery::DEFAULT_RTPS
+# define DDS_DEFAULT_DISCOVERY_METHOD Discovery::DEFAULT_REPO
 #endif
 
 Service_Participant::Service_Participant()
