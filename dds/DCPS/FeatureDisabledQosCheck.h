ee5ce83683941ef8070ec37391b92df5d46c2a13
Byron Harris <harris_b@ociweb.com>
2012-05-23 15:57:30 +0000

    Wed May 23 15:55:26 UTC 2012  Byron Harris  <harrisb@ociweb.com>

--- dds/DCPS/FeatureDisabledQosCheck.h
+++ dds/DCPS/FeatureDisabledQosCheck.h
@@ -9,12 +9,12 @@
 #ifndef OPENDDS_DCPS_FEATURE_DISABLED_QOS_CHECK_H
 #define OPENDDS_DCPS_FEATURE_DISABLED_QOS_CHECK_H
 
-#ifdef OPENDDS_NO_OBJECT_MODEL_PROFILE
+#ifdef OPENDDS_NO_OWNERSHIP_PROFILE
 #define OPENDDS_NO_OWNERSHIP_PROFILE_COMPATIBILITY_CHECK(qos) \
   if (qos.history.depth > 1) { \
     ACE_ERROR_RETURN((LM_ERROR, \
                       ACE_TEXT("(%P|%t) ERROR: ") \
-                      ACE_TEXT("Feature object_model_profile disabled, ") \
+                      ACE_TEXT("Feature ownership_profile disabled, ") \
                       ACE_TEXT("therefore history depth must be 1. \n")), \
                      DDS::RETCODE_NOT_SUPPORTED) \
   }
@@ -28,14 +28,14 @@
       TheServiceParticipant->initial_OwnershipStrengthQosPolicy()) { \
     ACE_ERROR_RETURN((LM_ERROR, \
                       ACE_TEXT("(%P|%t) ERROR: ") \
-                      ACE_TEXT("Feature ownership_profile disabled, ") \
+                      ACE_TEXT("Feature ownership_kind_exclusive disabled, ") \
                       ACE_TEXT("therefore ownership strength must be the default. \n")), \
                      DDS::RETCODE_NOT_SUPPORTED) \
   } \
   if (qos.ownership.kind == ::DDS::EXCLUSIVE_OWNERSHIP_QOS) { \
     ACE_ERROR_RETURN((LM_ERROR, \
                       ACE_TEXT("(%P|%t) ERROR: ") \
-                      ACE_TEXT("Feature ownership_profile disabled, ") \
+                      ACE_TEXT("Feature ownership_kind_exclusive disabled, ") \
                       ACE_TEXT("therefore ownership kind must be SHARED. \n")), \
                      DDS::RETCODE_NOT_SUPPORTED) \
   }
