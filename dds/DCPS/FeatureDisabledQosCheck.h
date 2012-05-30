e16ed3995d6b59702b9a7540f106023eff9ac3a5
Byron Harris <harris_b@ociweb.com>
2012-05-30 21:20:34 +0000

    Wed May 30 21:19:21 UTC 2012  Byron Harris  <harrisb@ociweb.com>

--- dds/DCPS/FeatureDisabledQosCheck.h
+++ dds/DCPS/FeatureDisabledQosCheck.h
@@ -14,11 +14,11 @@
 #ifdef  OPENDDS_NO_OWNERSHIP_PROFILE
 #define OPENDDS_NO_OWNERSHIP_PROFILE_COMPATIBILITY_CHECK(qos, error_rtn_value) \
   if (qos.history.depth > 1) { \
-    ACE_ERROR_RETURN((LM_ERROR, \
-                      ACE_TEXT("(%P|%t) ERROR: ") \
-                      ACE_TEXT("Feature ownership_profile disabled, ") \
-                      ACE_TEXT("therefore history depth must be 1. \n")), \
-                     error_rtn_value); \
+    ACE_ERROR((LM_ERROR, \
+              ACE_TEXT("(%P|%t) ERROR: ") \
+              ACE_TEXT("Feature ownership_profile disabled, ") \
+              ACE_TEXT("therefore history depth must be 1. \n"))); \
+    return error_rtn_value; \
   }
 #else
 #define OPENDDS_NO_OWNERSHIP_PROFILE_COMPATIBILITY_CHECK(qos, error_rtn_value)
@@ -27,19 +27,19 @@
 #ifdef  OPENDDS_NO_OWNERSHIP_KIND_EXCLUSIVE
 #define OPENDDS_NO_OWNERSHIP_KIND_EXCLUSIVE_COMPATIBILITY_CHECK(qos, error_rtn_value) \
   if (qos.ownership.kind == ::DDS::EXCLUSIVE_OWNERSHIP_QOS) { \
-    ACE_ERROR_RETURN((LM_ERROR, \
-                      ACE_TEXT("(%P|%t) ERROR: ") \
-                      ACE_TEXT("Feature ownership_kind_exclusive disabled, ") \
-                      ACE_TEXT("therefore ownership kind must be SHARED. \n")), \
-                     error_rtn_value); \
+    ACE_ERROR((LM_ERROR, \
+              ACE_TEXT("(%P|%t) ERROR: ") \
+              ACE_TEXT("Feature ownership_kind_exclusive disabled, ") \
+              ACE_TEXT("therefore ownership kind must be SHARED. \n"))); \
+    return error_rtn_value; \
   }
 #define OPENDDS_NO_OWNERSHIP_STRENGTH_COMPATIBILITY_CHECK(qos, error_rtn_value) \
   if (qos.ownership_strength.value != 0) { \
-    ACE_ERROR_RETURN((LM_ERROR, \
-                      ACE_TEXT("(%P|%t) ERROR: ") \
-                      ACE_TEXT("Feature ownership_kind_exclusive disabled, ") \
-                      ACE_TEXT("therefore ownership strength must be the default. \n")), \
-                     error_rtn_value); \
+    ACE_ERROR((LM_ERROR, \
+              ACE_TEXT("(%P|%t) ERROR: ") \
+              ACE_TEXT("Feature ownership_kind_exclusive disabled, ") \
+              ACE_TEXT("therefore ownership strength must be the default. \n"))); \
+    return error_rtn_value; \
   }
 #else
 #define OPENDDS_NO_OWNERSHIP_KIND_EXCLUSIVE_COMPATIBILITY_CHECK(qos, error_rtn_value)
@@ -49,19 +49,19 @@
 #ifdef  OPENDDS_NO_OBJECT_MODEL_PROFILE
 #define OPENDDS_NO_OBJECT_MODEL_PROFILE_COMPATIBILITY_CHECK(qos, error_rtn_value) \
   if (qos.presentation.access_scope == ::DDS::GROUP_PRESENTATION_QOS) { \
-    ACE_ERROR_RETURN((LM_ERROR, \
-                      ACE_TEXT("(%P|%t) ERROR: ") \
-                      ACE_TEXT("Feature object_model_profile disabled, ") \
-                      ACE_TEXT("therefore presentation access scope cannot be GROUP. \n")), \
-                     error_rtn_value); \
+    ACE_ERROR((LM_ERROR, \
+              ACE_TEXT("(%P|%t) ERROR: ") \
+              ACE_TEXT("Feature object_model_profile disabled, ") \
+              ACE_TEXT("therefore presentation access scope cannot be GROUP. \n"))); \
+    return error_rtn_value; \
   } \
   if (qos.presentation.access_scope == ::DDS::TOPIC_PRESENTATION_QOS) { \
-    ACE_ERROR_RETURN((LM_ERROR, \
-                      ACE_TEXT("(%P|%t) ERROR: ") \
-                      ACE_TEXT("Feature object_model_profile disabled. ") \
-                      ACE_TEXT("Although not part of the profile, currently ") \
-                      ACE_TEXT("this means presentation access scope cannot be TOPIC. \n")), \
-                     error_rtn_value); \
+    ACE_ERROR((LM_ERROR, \
+              ACE_TEXT("(%P|%t) ERROR: ") \
+              ACE_TEXT("Feature object_model_profile disabled. ") \
+              ACE_TEXT("Although not part of the profile, currently ") \
+              ACE_TEXT("this means presentation access scope cannot be TOPIC. \n"))); \
+    return error_rtn_value; \
   }
 #else  
 #define OPENDDS_NO_OBJECT_MODEL_PROFILE_COMPATIBILITY_CHECK(qos, error_rtn_value)    
@@ -71,22 +71,22 @@
 
 #define OPENDDS_NO_DURABILITY_SERVICE_COMPATIBILITY_CHECK(qos, error_rtn_value) \
   if (qos.durability_service != TheServiceParticipant->initial_DurabilityServiceQosPolicy()) { \
-    ACE_ERROR_RETURN((LM_ERROR, \
-                      ACE_TEXT("(%P|%t) ERROR: ") \
-                      ACE_TEXT("Feature persistence_profile disabled, ") \
-                      ACE_TEXT("therefore durability service must be the default. \n")), \
-                     error_rtn_value); \
+    ACE_ERROR((LM_ERROR, \
+              ACE_TEXT("(%P|%t) ERROR: ") \
+              ACE_TEXT("Feature persistence_profile disabled, ") \
+              ACE_TEXT("therefore durability service must be the default. \n"))); \
+    return error_rtn_value; \
   }
 
 #define OPENDDS_NO_DURABILITY_KIND_TRANSIENT_PERSISTENT_COMPATIBILITY_CHECK(qos, error_rtn_value) \
   if (qos.durability.kind == ::DDS::TRANSIENT_DURABILITY_QOS || \
       qos.durability.kind == ::DDS::PERSISTENT_DURABILITY_QOS) { \
-    ACE_ERROR_RETURN((LM_ERROR, \
-                      ACE_TEXT("(%P|%t) ERROR: ") \
-                      ACE_TEXT("Feature persistence_profile disabled, ") \
-                      ACE_TEXT("therefore durability kind must not ") \
-                      ACE_TEXT("be TRANSIENT_DURABILITY_QOS or PERSISTENT_DURABILITY_QOS. \n")), \
-                     error_rtn_value); \
+    ACE_ERROR((LM_ERROR, \
+              ACE_TEXT("(%P|%t) ERROR: ") \
+              ACE_TEXT("Feature persistence_profile disabled, ") \
+              ACE_TEXT("therefore durability kind must not ") \
+              ACE_TEXT("be TRANSIENT_DURABILITY_QOS or PERSISTENT_DURABILITY_QOS. \n"))); \
+    return error_rtn_value; \
   }
   
 #else
