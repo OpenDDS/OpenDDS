32178423f32657e3ee0a89e2804f3542b6691e99
Adam Mitz <mitza@ociweb.com>
2012-12-04 17:23:51 +0000

    ChangeLogTag: Tue Dec  4 17:21:03 UTC 2012  Adam Mitz  <mitza@ociweb.com>

--- tests/DCPS/MultiTopic/run_test.pl
+++ tests/DCPS/MultiTopic/run_test.pl
@@ -9,27 +9,37 @@ use lib "$ENV{DDS_ROOT}/bin";
 use PerlDDS::Run_Test;
 use strict;
 
-
-my $dcpsrepo_ior = "repo.ior";
-unlink $dcpsrepo_ior;
-
-my $DCPSREPO = PerlDDS::create_process ("$ENV{DDS_ROOT}/bin/DCPSInfoRepo",
-                                        "-NOBITS -o $dcpsrepo_ior");
-
-$DCPSREPO->Spawn ();
-if (PerlACE::waitforfile_timed ($dcpsrepo_ior, 30) == -1) {
-    print STDERR "ERROR: waiting for Info Repo IOR file\n";
-    $DCPSREPO->Kill ();
-    exit 1;
+my $is_rtps_disc = 0;
+if ($ARGV[0] eq 'rtps_disc') {
+  $is_rtps_disc = 1;
 }
 
-my $TEST = PerlDDS::create_process ('MultiTopicTest', '-DCPSBit 0');
+my $DCPSREPO;
+if (!$is_rtps_disc) {
+  my $dcpsrepo_ior = 'repo.ior';
+  unlink $dcpsrepo_ior;
+  $DCPSREPO = PerlDDS::create_process("$ENV{DDS_ROOT}/bin/DCPSInfoRepo",
+                                      "-NOBITS -o $dcpsrepo_ior");
+  $DCPSREPO->Spawn();
+  if (PerlACE::waitforfile_timed($dcpsrepo_ior, 30) == -1) {
+    print STDERR "ERROR: waiting for Info Repo IOR file\n";
+    $DCPSREPO->Kill();
+    exit 1;
+  }
+}
+
+my $TEST = PerlDDS::create_process('MultiTopicTest',
+                                   $is_rtps_disc
+                                   ? '-DCPSConfigFile rtps_disc.ini'
+                                   : '-DCPSBit 0');
 
 my $result = $TEST->SpawnWaitKill(60);
 if ($result != 0) {
   print STDERR "ERROR: test returned $result\n";
 }
 
-$DCPSREPO->TerminateWaitKill(5);
+if ($DCPSREPO) {
+  $DCPSREPO->TerminateWaitKill(5);
+}
 
 exit (($result == 0) ? 0 : 1);
