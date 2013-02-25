b002ced7ee60f8a38aea61b4dcbf8caa12003cf2
Adam Mitz <mitza@ociweb.com>
2013-02-25 16:08:51 +0000

    ChangeLogTag: Mon Feb 25 16:08:32 UTC 2013  Adam Mitz  <mitza@ociweb.com>

--- tests/DCPS/ManyTopicTest/subscriber.cpp
+++ tests/DCPS/ManyTopicTest/subscriber.cpp
@@ -25,6 +25,12 @@
 #include "tests/DCPS/ManyTopicTypes/Foo3DefTypeSupportImpl.h"
 #include "dds/DCPS/transport/framework/EntryExit.h"
 
+#include "dds/DCPS/StaticIncludes.h"
+#ifdef ACE_AS_STATIC_LIBS
+#include "dds/DCPS/RTPS/RtpsDiscovery.h"
+#include "dds/DCPS/transport/rtps_udp/RtpsUdp.h"
+#endif
+
 #include "ace/Arg_Shifter.h"
 
 #include "common.h"
