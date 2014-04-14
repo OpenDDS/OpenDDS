f55fd816c20d51f44ae89f91c8983b9971d7d8b6
Brian Johnson <johnsonb@ociweb.com>
2014-04-14 19:22:25 +0000

    ChangeLogTag: Mon Apr 14 19:20:55 UTC 2014  Brian Johnson  <johnsonb@ociweb.com>

--- tests/DCPS/Priority/publisher_main.cpp
+++ tests/DCPS/Priority/publisher_main.cpp
@@ -36,22 +36,11 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
     publisher.run();
 
     // this can only fail if ACE_HAS_PTHREADS is defined
-    if (TheServiceParticipant->scheduler() != THR_SCHED_RR) {
-      std::string scheduler;
-      if (TheServiceParticipant->scheduler() == THR_SCHED_FIFO)
-        scheduler = "THR_SCHED_FIFO";
-      else if (TheServiceParticipant->scheduler() == THR_SCHED_DEFAULT)
-        scheduler = "THR_SCHED_DEFAULT";
-      else {
-        std::stringstream buffer;
-        buffer << TheServiceParticipant->scheduler();
-        scheduler = "<Unkown scheduler val - " + buffer.str() + ">";
-      }
+    if (TheServiceParticipant->scheduler() == -1) {
       ACE_ERROR((LM_ERROR,
         ACE_TEXT("(%P|%t) ERROR: publisher_main() - ")
-        ACE_TEXT("scheduler should be THR_SCHED_RR, but ")
-        ACE_TEXT("instead it is %C.\n"),
-        scheduler.c_str()
+        ACE_TEXT("scheduler should be set by transport ini ")
+        ACE_TEXT("file.\n")
       ));
       return 1;
     }
