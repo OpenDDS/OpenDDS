14561463ee16d2354d8951f61570c3d59a40be3e
Byron Harris <harris_b@ociweb.com>
2014-06-23 19:12:57 +0000

    Mon Jun 23 19:09:32 UTC 2014  Byron Harris  <harrisb@ociweb.com>

---
+++ dds/DCPS/MessageTracker.cpp
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ */
+
+#include "DCPS/DdsDcps_pch.h" //Only the _pch include should start with DCPS/
+#include <dds/DCPS/MessageTracker.h>
+#include <dds/DCPS/Service_Participant.h>
+#include "ace/Guard_T.h"
+
+using namespace OpenDDS::DCPS;
+
+MessageTracker::MessageTracker()
+: dropped_count_(0)
+, delivered_count_(0)
+, sent_count_(0)
+, done_condition_(lock_)
+{
+}
+
+bool
+MessageTracker::pending_messages()
+{
+  return sent_count_ > delivered_count_ + dropped_count_;
+}
+
+void
+MessageTracker::message_sent()
+{
+  ACE_GUARD(ACE_Thread_Mutex, guard, lock_);
+  ++sent_count_;
+}
+
+void
+MessageTracker::message_delivered()
+{
+  ACE_GUARD(ACE_Thread_Mutex, guard, lock_);
+  ++delivered_count_;
+
+  if (!pending_messages())
+    done_condition_.broadcast();
+}
+
+void
+MessageTracker::message_dropped()
+{
+  ACE_GUARD(ACE_Thread_Mutex, guard, lock_);
+  ++dropped_count_;
+
+  if (!pending_messages())
+    done_condition_.broadcast();
+}
+
+void
+MessageTracker::wait_messages_pending()
+{
+  ACE_Time_Value pending_timeout =
+    TheServiceParticipant->pending_timeout();
+
+  ACE_Time_Value* pTimeout = 0;
+
+  if (pending_timeout != ACE_Time_Value::zero) {
+    pTimeout = &pending_timeout;
+    pending_timeout += ACE_OS::gettimeofday();
+  }
+
+  ACE_GUARD(ACE_Thread_Mutex, guard, this->lock_);
+  while (true) {
+    if (!pending_messages())
+      break;
+
+    if (done_condition_.wait(pTimeout) == -1) {
+      ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) MessageTracker::wait_messages_pending %p\n")
+        ACE_TEXT("Timed out waiting for messages to be transported")));
+    }
+  }
+}
+
