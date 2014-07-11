f8025b28437b61821485ac544e6e339341c3f5b7
Jeff Schmitz <schmitzj@ociweb.com>
2014-07-11 19:00:27 +0000

    ChangeLogTag: Fri Jul 11 18:58:28 UTC 2014  Jeff Schmitz <schmitzj@ociweb.com>

--- dds/DCPS/DataSampleElement.cpp
+++ dds/DCPS/DataSampleElement.cpp
@@ -6,6 +6,7 @@
  * See: http://www.opendds.org/license.html
  */
 
+#include "DCPS/DdsDcps_pch.h" //Only the _pch include should start with DCPS/
 #include "DataSampleElement.h"
 
 #if !defined (__ACE_INLINE__)
