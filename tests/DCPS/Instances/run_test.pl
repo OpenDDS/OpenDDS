da670bc9090d1d18e693ab350593a77acf7f071b
Phillip LaBanca <labancap@ociweb.com>
2014-07-18 23:06:49 +0000

    Fri Jul 18 23:04:50 UTC 2014  Phillip LaBanca  <labancap@ociweb.com>

--- tests/DCPS/Instances/run_test.pl
+++ tests/DCPS/Instances/run_test.pl
@@ -16,49 +16,55 @@ PerlDDS::add_lib_path('../../Utils/');
 
 my $status=0;
 
-# process control parameters
-my $num_instances=1;
-my $num_writers=1;
 
-# const process control parameters
+# process control parameters
 my $keyed_data=1;
 my $multiple_instance=0;
+my $num_instances=1;
+my $num_writers=1;
+my $debug_level=0;
+
+# const process control parameters
 my $num_threads_to_write=5;
 my $num_writes_per_thread=2;
 my $max_samples_per_instance= 12345678;
 my $history_depth=100;
-my $blocking_write=0;
 my $write_delay_msec=0;
 my $receive_delay_msec=0;
-
-# script control parameters
-my $check_data_dropped=0;
 my $publisher_running_sec=60;
 my $subscriber_running_sec=30;
 my $repo_bit_conf = "-NOBITS";
 my $app_bit_conf = "-DCPSBit 0";
 my $cfgfile = '';
-my $nokey = 0;
 
-if ($#ARGV >= 0) {
-  for (my $i = 0; $i <= $#ARGV; $i++) {
-    if ($ARGV[$i] eq '-num_instances') {
-      $i++;
-      $num_instances = $ARGV[$i];
+for my $arg (@ARGV) {
+    if ($arg eq 'keyed') {
+        $keyed_data=1;
     }
-    elsif ($ARGV[$i] eq '-num_writers') {
-      $i++;
-      $num_writers = $ARGV[$i];
+    elsif ($arg eq 'nokey') {
+        $keyed_data=0;
     }
-    elsif ($ARGV[$i] eq '-keyed_data') {
-      $i++;
-      $keyed_data = $ARGV[$i];
+    elsif ($arg eq 'single_instance') {
+        $multiple_instance=0;
     }
-    else {
-      print STDERR "ERROR: invalid parameter $ARGV[$i] \n";
-      exit 1;
+    elsif ($arg eq 'multiple_instance') {
+        $multiple_instance=1;
     }
-  }
+    elsif ($arg eq 'single_datawriter') {
+        $num_writers=1;
+        $num_instances=$num_threads_to_write;
+    }
+    elsif ($arg eq 'multiple_datawriter') {
+        $num_writers=4;
+        $num_instances=$num_threads_to_write * $num_writers;
+    }
+    elsif ($arg eq 'debug') {
+        $debug_level=4;
+    }
+    elsif ($arg ne '') {
+        print STDERR "ERROR: invalid test case\n";
+        exit 1;
+    }
 }
 
 my $num_writes=$num_threads_to_write * $num_writes_per_thread * $num_writers;
@@ -74,6 +80,7 @@ print $DCPSREPO->CommandLine(), "\n";
 my $publisher=PerlDDS::create_process ("publisher",
                                        "$app_bit_conf $cfgfile"
                                        . " -DCPSInfoRepo file://$dcpsrepo_ior"
+                                       . " -DCPSDebugLevel $debug_level"
                                        . " -keyed_data $keyed_data"
                                        . " -num_writers $num_writers"
                                        . " -history_depth $history_depth"
@@ -82,8 +89,6 @@ my $publisher=PerlDDS::create_process ("publisher",
                                        . " -num_writes_per_thread $num_writes_per_thread "
                                        . " -max_samples_per_instance $max_samples_per_instance"
                                        . " -write_delay_msec $write_delay_msec"
-                                       #. " -r $check_data_dropped "
-                                       #. " -b $blocking_write "
 );
 
 print $publisher->CommandLine(), "\n";
@@ -91,7 +96,8 @@ print $publisher->CommandLine(), "\n";
 my $subscriber=PerlDDS::create_process ("subscriber",
                                         "$app_bit_conf $cfgfile"
                                         . " -DCPSInfoRepo file://$dcpsrepo_ior"
-                                       . " -keyed_data $keyed_data"
+                                        . " -DCPSDebugLevel $debug_level"
+                                        . " -keyed_data $keyed_data"
                                         . " -num_writes $num_writes"
                                         . " -receive_delay_msec $receive_delay_msec");
 
@@ -115,21 +121,11 @@ if ($result != 0) {
     $status=1;
 }
 
-if ($check_data_dropped == 0) {
-    $result=$subscriber->WaitKill($subscriber_running_sec);
+$result=$subscriber->WaitKill($subscriber_running_sec);
 
-    if ($result != 0) {
-        print STDERR "ERROR: $subscriber returned $result  \n";
-        $status=1;
-    }
-}
-else {
-    $result=$subscriber->TerminateWaitKill(5);
-
-    if ($result != 0) {
-        print STDERR "ERROR: subscriber returned $result\n";
-        $status=1;
-    }
+if ($result != 0) {
+    print STDERR "ERROR: $subscriber returned $result  \n";
+    $status=1;
 }
 
 my $ir=$DCPSREPO->TerminateWaitKill(5);
