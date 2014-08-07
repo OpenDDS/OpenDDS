f5a5626b30cbb84ab8203b66f5fd40a9a73c3f90
Jeff Schmitz <schmitzj@ociweb.com>
2014-08-07 17:10:16 +0000

    ChangeLogTag: Thu Aug  7 17:09:50 UTC 2014  Jeff Schmitz  <schmitzj@ociweb.com>

--- tests/DCPS/ManyToMany/publisher.cpp
+++ tests/DCPS/ManyToMany/publisher.cpp
@@ -19,6 +19,7 @@
 #ifdef ACE_AS_STATIC_LIBS
 #include <dds/DCPS/transport/udp/Udp.h>
 #include <dds/DCPS/transport/multicast/Multicast.h>
+#include <dds/DCPS/RTPS/RtpsDiscovery.h>
 #include <dds/DCPS/transport/shmem/Shmem.h>
 #include <dds/DCPS/transport/rtps_udp/RtpsUdp.h>
 #endif
