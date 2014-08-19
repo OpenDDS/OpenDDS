8e10fb28b6e18e45476bb62b6b5642fe87144bbd
Jeff Schmitz <schmitzj@ociweb.com>
2014-08-19 16:43:06 +0000

    ChangeLogTag: Tue Aug 19 16:39:54 UTC 2014  Jeff Schmitz  <schmitzj@ociweb.com>

--- dds/DCPS/DataReaderImpl.cpp
+++ dds/DCPS/DataReaderImpl.cpp
@@ -2715,6 +2715,7 @@ DataReaderImpl::filter_sample(const DataSampleHeader& header)
 
   // Ignore this sample if it is NOT a historic sample, and we are
   // waiting for historic sample from this writer
+/* Commenting out for 3.5.1 release - not backward compatible with 3.5.0
   if (!header.historic_sample_) {
     ACE_READ_GUARD_RETURN(
         ACE_RW_Thread_Mutex, read_guard, this->writers_lock_, false);
@@ -2723,7 +2724,7 @@ DataReaderImpl::filter_sample(const DataSampleHeader& header)
     if (writers_.end() != where) {
       return where->second->awaiting_historic_samples_;
     }
-  }
+  } */
 
   return false;
 }
