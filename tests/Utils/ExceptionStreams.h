456232ece707d8a1be10149c155b6305c1364860
Jeff Schmitz <schmitzj@ociweb.com>
2014-12-29 21:59:01 +0000

    BranchChangeLogTag: Mon Dec 29 21:58:30 UTC 2014  Jeff Schmitz  <schmitzj@ociweb.com>

--- tests/Utils/ExceptionStreams.h
+++ tests/Utils/ExceptionStreams.h
@@ -1,23 +1,26 @@
 #ifndef EXCEPTION_STREAMS_H
 #define EXCEPTION_STREAMS_H
 
-//#ifdef ACE_LACKS_IOSTREAMS_TOTALLY
+#ifdef ACE_LACKS_IOSTREAM_TOTALLY
+
 // In case we are lacking iostreams, we will want to be able to stream to 
 // standard iostreams in tests
+namespace CORBA {
 
-std::ostream& operator<< (std::ostream &os, const CORBA::Exception &e)
+inline std::ostream& operator<< (std::ostream &os, const CORBA::Exception &e)
 {
     os << e._name () << " (" << e._rep_id () << ')';
     return os;
 }
 
-std::ostream& operator<< (std::ostream &os, const CORBA::Exception *e)
+inline std::ostream& operator<< (std::ostream &os, const CORBA::Exception *e)
 {
     os << e->_name () << " (" << e->_rep_id () << ')';
     return os;
 }
 
-//#endif
+}
+#endif
 
 #endif
 
