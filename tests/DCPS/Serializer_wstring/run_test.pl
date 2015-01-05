f120a584245090f454326ad38869a3cd415682e3
Adam Mitz <mitza@ociweb.com>
2015-01-05 23:30:06 +0000

    ChangeLogTag: Mon Jan  5 23:29:10 UTC 2015  Adam Mitz  <mitza@ociweb.com>

--- tests/DCPS/Serializer_wstring/run_test.pl
+++ tests/DCPS/Serializer_wstring/run_test.pl
@@ -10,57 +10,17 @@ use lib "$DDS_ROOT/bin";
 use Env (ACE_ROOT);
 use lib "$ACE_ROOT/bin";
 use PerlDDS::Run_Test;
+use strict;
 
-$status = 0;
+my $test = new PerlDDS::TestFramework();
+$test->{'nobits'} = 1;
 
+$test->process('sub', 'subscriber');
+$test->process('pub', 'publisher');
 
+$test->setup_discovery();
 
+$test->start_process('pub');
+$test->start_process('sub');
 
-$dcpsrepo_ior = "repo.ior";
-
-unlink $dcpsrepo_ior;
-
-$DCPSREPO = PerlDDS::create_process ("$ENV{DDS_ROOT}/bin/DCPSInfoRepo",
-                                    "-NOBITS -o $dcpsrepo_ior ");
-$Subscriber = PerlDDS::create_process ("subscriber", "-DCPSBit 0 -DCPSConfigFile sub.ini");
-$Publisher = PerlDDS::create_process ("publisher", "-DCPSBit 0 -DCPSConfigFile pub.ini");
-
-$DCPSREPO->Spawn ();
-if (PerlACE::waitforfile_timed ($dcpsrepo_ior, 30) == -1) {
-    print STDERR "ERROR: waiting for Info Repo IOR file\n";
-    $DCPSREPO->Kill ();
-    exit 1;
-}
-
-$Publisher->Spawn ();
-
-$Subscriber->Spawn ();
-
-
-$PublisherResult = $Publisher->WaitKill (60);
-if ($PublisherResult != 0) {
-    print STDERR "ERROR: publisher returned $PublisherResult \n";
-    $status = 1;
-}
-
-$SubscriberResult = $Subscriber->WaitKill (15);
-if ($SubscriberResult != 0) {
-    print STDERR "ERROR: subscriber returned $SubscriberResult \n";
-    $status = 1;
-}
-
-$ir = $DCPSREPO->TerminateWaitKill(5);
-if ($ir != 0) {
-    print STDERR "ERROR: DCPSInfoRepo returned $ir\n";
-    $status = 1;
-}
-
-unlink $dcpsrepo_ior;
-
-if ($status == 0) {
-  print "test PASSED.\n";
-} else {
-  print STDERR "test FAILED.\n";
-}
-
-exit $status;
+exit $test->finish(300);
