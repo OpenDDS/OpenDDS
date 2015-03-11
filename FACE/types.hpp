b564b9de24d90ef506e176abbd7675f9e65a0bfe
Adam Mitz <mitza@ociweb.com>
2015-03-11 09:46:28 -0500

    Use basic types typedefs in FACE API.

--- FACE/types.hpp
+++ FACE/types.hpp
@@ -3,8 +3,14 @@
 
 #include <ace/CDR_Base.h>
 
-namespace FACE
-{
+namespace OpenDDS {
+  namespace FaceTypes {
+    template<typename CharT> class String_var;
+    template<typename CharT> class String_out;
+  }
+}
+
+namespace FACE {
   typedef ACE_CDR::Boolean Boolean;
   typedef ACE_CDR::Octet Octet;
   typedef ACE_CDR::Short Short;
@@ -19,8 +25,24 @@ namespace FACE
   typedef ACE_CDR::Char Char;
   typedef ACE_CDR::WChar WChar;
 
-  // _out types
-  // {,W}String{_var,_out}
+  typedef Boolean& Boolean_out;
+  typedef Octet& Octet_out;
+  typedef Short& Short_out;
+  typedef UnsignedShort& UnsignedShort_out;
+  typedef Long& Long_out;
+  typedef UnsignedLong& UnsignedLong_out;
+  typedef LongLong& LongLong_out;
+  typedef UnsignedLongLong& UnsignedLongLong_out;
+  typedef Float& Float_out;
+  typedef Double& Double_out;
+  typedef LongDouble& LongDouble_out;
+  typedef Char& Char_out;
+  typedef WChar& WChar_out;
+
+  typedef OpenDDS::FaceTypes::String_var<Char> String_var;
+  typedef OpenDDS::FaceTypes::String_out<Char> String_out;
+  typedef OpenDDS::FaceTypes::String_var<WChar> WString_var;
+  typedef OpenDDS::FaceTypes::String_out<WChar> WString_out;
 }
 
 #endif /* FACE_TYPES_HPP_HEADER_FILE */
