8b8e178fb1e6417981ee81c5b7fc6172c1cf7f51
Peter Oschwald <oschwaldp@ociweb.com>
2015-03-11 16:33:52 -0500

    Fixed ReaderCondSync to not wait_unmatch since it is the signalling of the condition that should allow it to proceed. Added wait_unmatch for writer side to react to the sub ending when ReaderCondSync ends the sub.  Modified MultiInstance test to take advantage of this and let the first pub instance wait for the first sub instance to end before continuing to second instance

--- tools/modeling/codegen/model/Sync.cpp
+++ tools/modeling/codegen/model/Sync.cpp
@@ -84,6 +84,56 @@ OpenDDS::Model::WriterSync::wait_ack(const DDS::DataWriter_var& writer)
   return 0;
 }
 
+int
+OpenDDS::Model::WriterSync::wait_unmatch(const DDS::DataWriter_var& writer,
+                                         unsigned int num_readers)
+{
+  DDS::ReturnCode_t stat;
+  DDS::StatusCondition_var condition = writer->get_statuscondition();
+  condition->set_enabled_statuses(DDS::PUBLICATION_MATCHED_STATUS);
+  DDS::WaitSet_var ws = new DDS::WaitSet;
+  ws->attach_condition(condition);
+  DDS::ConditionSeq conditions;
+  DDS::PublicationMatchedStatus ms = { 0, 0, 0, 0, 0 };
+  DDS::Duration_t timeout = { 1, 0 };
+  do {
+    if (DCPS_debug_level > 4) {
+      ACE_DEBUG((LM_NOTICE, ACE_TEXT("WriterSync: pub checking unmatched\n")));
+    }
+    stat = writer->get_publication_matched_status(ms);
+    if (stat != DDS::RETCODE_OK) {
+      ACE_ERROR_RETURN((
+                  LM_ERROR,
+                  ACE_TEXT("(%P|%t) ERROR: %N:%l: wait_unmatch() -")
+                  ACE_TEXT(" get_publication_matched_status failed!\n")),
+                 -1);
+    } else if (ms.current_count == 0 && (unsigned int)ms.total_count >= num_readers) {
+      if (DCPS_debug_level > 4) {
+        ACE_DEBUG((LM_NOTICE, ACE_TEXT("WriterSync: pub match count %d total count %d\n"),
+                                       ms.current_count, ms.total_count));
+      }
+      break;  // unmatched
+    }
+    if (DCPS_debug_level > 4) {
+      ACE_DEBUG((LM_NOTICE, ACE_TEXT("WriterSync: pub match count %d total count %d\n"),
+                                     ms.current_count, ms.total_count));
+    }
+    // wait for a change
+    stat = ws->wait(conditions, timeout);
+    if ((stat != DDS::RETCODE_OK) && (stat != DDS::RETCODE_TIMEOUT)) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("(%P|%t) ERROR: %N:%l: wait_unmatch() -")
+                        ACE_TEXT(" wait failed!\n")),
+                       -1);
+    }
+  } while (true);
+  ws->detach_condition(condition);
+  if (DCPS_debug_level > 4) {
+    ACE_DEBUG((LM_NOTICE, ACE_TEXT("WriterSync: pub unmatched\n")));
+  }
+  return 0;
+}
+
 OpenDDS::Model::ReaderSync::ReaderSync(DDS::DataReader_var reader,
                                        unsigned int num_writers) :
 reader_(reader),
@@ -122,10 +172,10 @@ OpenDDS::Model::ReaderSync::wait_unmatch(const DDS::DataReader_var& reader,
                   ACE_TEXT(" get_subscription_matched_status failed!\n")),
                  -1);
     } else if (ms.current_count == 0 && (unsigned int)ms.total_count >= num_writers) {
-//      if (DCPS_debug_level > 4) {
+      if (DCPS_debug_level > 4) {
         ACE_DEBUG((LM_NOTICE, ACE_TEXT("ReaderSync: sub match count %d total count %d\n"),
                                        ms.current_count, ms.total_count));
-//      }
+      }
       break;  // unmatched
     }
     if (DCPS_debug_level > 4) {
@@ -159,9 +209,6 @@ condition_(condition)
 
 OpenDDS::Model::ReaderCondSync::~ReaderCondSync()
 {
-  if (ReaderSync::wait_unmatch(reader_)) {
-    throw std::runtime_error("wait_unmatch failure");
-  }
   ACE_GUARD(ACE_SYNCH_MUTEX, conditionGuard, condition_.mutex());
   while (!complete_) {
     condition_.wait();
