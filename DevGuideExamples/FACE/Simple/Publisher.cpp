812cac40ba0e8d83b81c3951b9d8048786685784
Jeff Schmitz <jeffrey.j.schmitz@gmail.com>
2015-06-10 10:07:41 -0500

    Updates for dev guide

--- DevGuideExamples/FACE/Simple/Publisher.cpp
+++ DevGuideExamples/FACE/Simple/Publisher.cpp
@@ -1,26 +1,32 @@
 #include "FaceMessage_TS.hpp"
+#include "ace/Log_Msg.h"
 
 #ifdef ACE_AS_STATIC_LIBS
 # include "dds/DCPS/RTPS/RtpsDiscovery.h"
 # include "dds/DCPS/transport/rtps_udp/RtpsUdp.h"
 #endif
 
-#include "ace/Log_Msg.h"
-
-int ACE_TMAIN(int, ACE_TCHAR*[])
+int main(int, const char*[])
 {
   // Initialize the TS interface
   FACE::RETURN_CODE_TYPE status;
   FACE::TS::Initialize("face_config.ini", status);
-  if (status != FACE::RC_NO_ERROR) return static_cast<int>(status);
+
+  if (status != FACE::RC_NO_ERROR) {
+    return static_cast<int>(status);
+  }
 
   // Create the pub connection
   FACE::CONNECTION_ID_TYPE connId;
   FACE::CONNECTION_DIRECTION_TYPE dir;
   FACE::MESSAGE_SIZE_TYPE size;
-  FACE::TS::Create_Connection("pub", FACE::PUB_SUB, connId, dir, size,
-                              FACE::INF_TIME_VALUE, status);
-  if (status != FACE::RC_NO_ERROR) return static_cast<int>(status);
+  FACE::TS::Create_Connection(
+    "pub", FACE::PUB_SUB, connId, dir,
+    size, FACE::INF_TIME_VALUE, status);
+
+  if (status != FACE::RC_NO_ERROR) {
+    return static_cast<int>(status);
+  }
 
   // Message to send
   Messenger::Message msg = {"Hello, world.", 14, 1};
@@ -28,14 +34,20 @@ int ACE_TMAIN(int, ACE_TCHAR*[])
   // Send message
   FACE::TRANSACTION_ID_TYPE txn;
   ACE_DEBUG((LM_INFO, "Publisher: about to Send_Message()\n"));
-  FACE::TS::Send_Message(connId, FACE::INF_TIME_VALUE, txn, msg, size, status);
-  if (status != FACE::RC_NO_ERROR) return static_cast<int>(status);
+  FACE::TS::Send_Message(
+    connId, FACE::INF_TIME_VALUE, txn, msg, size, status);
+  if (status != FACE::RC_NO_ERROR) {
+    return static_cast<int>(status);
+  }
 
-  // If not a reliable connection, must wait for message to hit the wire
+  // Give message time to be processed before exiting
   ACE_OS::sleep(15);
 
+  // Destroy the pub connection
   FACE::TS::Destroy_Connection(connId, status);
-  if (status != FACE::RC_NO_ERROR) return static_cast<int>(status);
+  if (status != FACE::RC_NO_ERROR) {
+    return static_cast<int>(status);
+  }
 
   return EXIT_SUCCESS;
 }
--- DevGuideExamples/FACE/Simple/Subscriber.cpp
+++ DevGuideExamples/FACE/Simple/Subscriber.cpp
@@ -1,50 +1,53 @@
 #include "FaceMessage_TS.hpp"
+#include "ace/Log_Msg.h"
 
 #ifdef ACE_AS_STATIC_LIBS
 # include "dds/DCPS/RTPS/RtpsDiscovery.h"
 # include "dds/DCPS/transport/rtps_udp/RtpsUdp.h"
 #endif
 
-#include "ace/Log_Msg.h"
-
 int ACE_TMAIN(int, ACE_TCHAR*[])
 {
+  // Initialize the TS interface
   FACE::RETURN_CODE_TYPE status;
   FACE::TS::Initialize("face_config.ini", status);
-  if (status != FACE::RC_NO_ERROR) return static_cast<int>(status);
 
+  if (status != FACE::RC_NO_ERROR) {
+    return static_cast<int>(status);
+  }
+
+  // Create the sub connection
   FACE::CONNECTION_ID_TYPE connId;
   FACE::CONNECTION_DIRECTION_TYPE dir;
   FACE::MESSAGE_SIZE_TYPE size;
-  FACE::TS::Create_Connection("sub", FACE::PUB_SUB, connId, dir, size,
-                              FACE::INF_TIME_VALUE, status);
-  if (status != FACE::RC_NO_ERROR) return static_cast<int>(status);
+  FACE::TS::Create_Connection(
+    "sub", FACE::PUB_SUB, connId, dir, size,
+    FACE::INF_TIME_VALUE, status);
+
+  if (status != FACE::RC_NO_ERROR) {
+    return static_cast<int>(status);
+  }
 
   const FACE::TIMEOUT_TYPE timeout = FACE::INF_TIME_VALUE;
   FACE::TRANSACTION_ID_TYPE txn;
   Messenger::Message msg;
 
   ACE_DEBUG((LM_INFO, "Subscriber: about to Receive_Message()\n"));
-  FACE::TS::Receive_Message(connId, timeout, txn, msg, size, status);
+  FACE::TS::Receive_Message(
+    connId, timeout, txn, msg, size, status);
 
-  if (status != FACE::RC_NO_ERROR) return static_cast<int>(status);
+  if (status != FACE::RC_NO_ERROR) {
+    return static_cast<int>(status);
+  }
 
   ACE_DEBUG((LM_INFO, "%C\t%d\n", msg.from.in(), msg.count));
 
-  FACE::CONNECTION_NAME_TYPE name = {};
-  FACE::TRANSPORT_CONNECTION_STATUS_TYPE connectionStatus;
-  FACE::TS::Get_Connection_Parameters(name, connId, connectionStatus, status);
-  if (status != FACE::RC_NO_ERROR) return static_cast<int>(status);
-
-  if (connectionStatus.LAST_MSG_VALIDITY != FACE::VALID) {
-    ACE_ERROR((LM_ERROR,
-               "ERROR: unexpected value in connection parameters after receiving\n"));
-    return EXIT_FAILURE;
-  }
-
-  // Destroy the connection
+  // Destroy the sub connection
   FACE::TS::Destroy_Connection(connId, status);
-  if (status != FACE::RC_NO_ERROR) return static_cast<int>(status);
+
+  if (status != FACE::RC_NO_ERROR) {
+    return static_cast<int>(status);
+  }
 
   return EXIT_SUCCESS;
 }
