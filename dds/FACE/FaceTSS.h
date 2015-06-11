822e335badcc668840aabfc4c81155cdd6f77f93
Peter Oschwald <oschwaldp@ociweb.com>
2015-06-11 13:16:56 -0500

    Fix callback to pass correct max message size parameter

--- dds/FACE/FaceTSS.h
+++ dds/FACE/FaceTSS.h
@@ -303,7 +303,7 @@ private:
         }
         for (size_t i = 0; i < callbacks_.size(); ++i) {
           retcode = FACE::RC_NO_ERROR;
-          callbacks_.at(i)(transaction_id /*Transaction_ID*/, sample, status.MESSAGE, status.MAX_MESSAGE, 0 /*WAITSET_TYPE*/, retcode);
+          callbacks_.at(i)(transaction_id /*Transaction_ID*/, sample, status.MESSAGE, status.MAX_MESSAGE_SIZE, 0 /*WAITSET_TYPE*/, retcode);
           if (retcode != FACE::RC_NO_ERROR) {
             ACE_ERROR((LM_ERROR, "ERROR: Listener::on_data_available - callback %d returned retcode: %d\n", i, retcode));
           }
