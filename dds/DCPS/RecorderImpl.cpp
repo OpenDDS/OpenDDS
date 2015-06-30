52dbc606085b2fe4d620d5c9d652cdb3525ad6b6
Peter Oschwald <oschwaldp@ociweb.com>
2015-06-30 11:50:55 -0500

    Fix compiler error

--- dds/DCPS/RecorderImpl.cpp
+++ dds/DCPS/RecorderImpl.cpp
@@ -710,7 +710,7 @@ RecorderImpl::remove_associations_i(const WriterIdSeq& writers,
       // DDS::DataReaderListener_var listener
       // = listener_for(DDS::SUBSCRIPTION_MATCHED_STATUS);
 
-      if (!CORBA::is_nil(listener_.in())) {
+      if (listener_.in()) {
         listener_->on_recorder_matched(
           this,
           this->subscription_match_status_);
