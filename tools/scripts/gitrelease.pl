ab57453a84d686fd8276849044ba9ddb28dc6939
Jeff Schmitz <jeffrey.j.schmitz@gmail.com>
2015-09-14 16:19:28 -0500

    fix ChangeLog generation

--- tools/scripts/gitrelease.pl
+++ tools/scripts/gitrelease.pl
@@ -304,7 +304,7 @@ sub remedy_changelog {
 
   open(CHANGELOG, ">$settings->{changelog}") or die "Opening $!";
 
-  open(GITLOG, "git log $prev_tag..$remote --name-status --date=raw |") or die "Opening $!";
+  open(GITLOG, "git log $prev_tag..$branch --name-status --date=raw |") or die "Opening $!";
   while (<GITLOG>) {
     chomp;
     if (/^commit /) {
