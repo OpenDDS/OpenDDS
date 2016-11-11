95efddd2a5015a10450e438744b0ada1a8cabf59
Huang-Ming Huang <huangh@ociweb.com>
2016-11-11 15:16:58 -0600

    deals with the case where MPC is not installed inside ACE_wrappers directory and utilizes clone_build_tree.pl to create separate the build tree inside docker.

--- tools/scripts/create_universal_linux_dds_idl_binaries.sh
+++ tools/scripts/create_universal_linux_dds_idl_binaries.sh
@@ -9,6 +9,7 @@
 
 buildbits=32
 docker_image=phusion/holy-build-box-${buildbits}
+MOUNT_DIR=/OpenDDS
 
 set -e
 
@@ -21,12 +22,62 @@ if [ ! -d ACE_wrappers ]; then
   curl http://download.ociweb.com/TAO-${tao_version}/ACE+TAO-${tao_version}_with_latest_patches_NO_makefiles.tar.gz | tar xz
 fi
 
-cat << 'EOF' > ACE_wrappers/ace/config.h
+
+if [ ! -d ACE_wrappers/MPC ]; then
+  if [ ! -f $MPC_ROOT/mwc.pl ]; then
+    echo "Cannnot find MPC installation, please set the MPC_ROOT environment variable."
+    exit 1
+  else
+    EXTRA_DOCKER_FLAGS="-v $MPC_ROOT:/MPC -e MPC_ROOT=/MPC"
+  fi
+else
+  EXTRA_DOCKER_FLAGS=" -e MPC_ROOT=$MOUNT_DIR/ACE_Wrappers/MPC"
+fi
+
+
+cat << 'EOF' > build_tao_dds_idl.sh
+#!/bin/bash
+set -e
+
+# Activate Holy Build Box environment.
+source /hbb_exe/activate
+
+cd `dirname "${BASH_SOURCE[0]}"`
+
+mkdir -p build
+
+$MPC_ROOT/clone_build_tree.pl universal_linux_idls_build
+
+export DDS_ROOT=$PWD/build/universal_linux_idls_build
+export ACE_ROOT=$PWD/build/universal_linux_idls_build/ACE_wrappers
+export TAO_ROOT=$PWD/build/universal_linux_idls_build/ACE_wrappers/TAO
+
+cd $DDS_ROOT
+
+files=( "ACE_wrappers/ace/config.h"
+        "ACE_wrappers/include/makeinclude/platform_macros.GNU"
+        "ACE_wrappers/apps/gperf/src/ace_gperf"
+        "ACE_wrappers/TAO/TAO_IDL/tao_idl"
+        "dds/idl/opendds_idl"
+)
+
+for i in "${files[@]}"
+do
+    if [ -h $i ]; then
+      rm $i
+    fi
+done
+
+
+if [ ! -f ACE_wrappers/ace/config.h ]; then
+cat << 'EEOF' > ACE_wrappers/ace/config.h
 #include "ace/config-linux.h"
 #undef ACE_HAS_SVR4_DYNAMIC_LINKING
-EOF
+EEOF
+fi
 
-cat << EOF > ACE_wrappers/include/makeinclude/platform_macros.GNU
+if [ ! -f ACE_wrappers/include/makeinclude/platform_macros.GNU ]; then
+cat << EEOF > ACE_wrappers/include/makeinclude/platform_macros.GNU
 static_libs_only = 1
 buildbits = $buildbits
 threads = 0
@@ -34,9 +85,10 @@ debug = 0
 static_stdlibs = 1
 dynamic_loader = 0
 include \$(ACE_ROOT)/include/makeinclude/platform_linux.GNU
-EOF
+EEOF
+fi
 
-cat << 'EOF' > tao_dds_idl.mwc
+cat << 'EEOF' > tao_dds_idl.mwc
 workspace {
   $(ACE_ROOT)/ace/ace.mpc
   $(ACE_ROOT)/apps/gperf/src
@@ -45,31 +97,24 @@ workspace {
   $(TAO_ROOT)/TAO_IDL/tao_idl_be.mpc
   $(DDS_ROOT)/dds/idl/opendds_idl.mpc
 }
-EOF
+EEOF
 
-MOUNT_DIR=/OpenDDS
-
-cat << EOF > build_tao_dds_idl.sh
-#!/bin/bash
-set -e
-
-# Activate Holy Build Box environment.
-source /hbb_exe/activate
-
-export DDS_ROOT=$MOUNT_DIR
-export ACE_ROOT=$MOUNT_DIR/ACE_wrappers
-export TAO_ROOT=$MOUNT_DIR/ACE_wrappers/TAO
-export MPC_ROOT=$MOUNT_DIR/ACE_wrappers/MPC
-
-# make sure no existing shared library exists;
-# otherwise, the build would fail to link
-rm -f ACE_wrappers/lib/*.so
-
-cd $MOUNT_DIR
 ACE_wrappers/bin/mwc.pl -type gnuace tao_dds_idl.mwc
 make
 
+mkdir -p ../install/bin
+cp ACE_wrappers/apps/gperf/src/ace_gperf ../install/bin
+cp ACE_wrappers/TAO/TAO_IDL/tao_idl ../install/bin
+cp dds/idl/opendds_idl ../install/bin
+
+for f in ../install/bin/* ;
+do
+  strip $f
+done
+
 EOF
 
-docker run -it --rm -u $UID -v $PWD:$MOUNT_DIR $docker_image bash $MOUNT_DIR/build_tao_dds_idl.sh
+docker run -it --rm -u $UID -e "buildbits=$buildbits" -v $PWD:$MOUNT_DIR $EXTRA_DOCKER_FLAGS $docker_image bash $MOUNT_DIR/build_tao_dds_idl.sh
+echo ""
+echo "build is done, all binaries are in the $PWD/build/install/bin directory"
 
