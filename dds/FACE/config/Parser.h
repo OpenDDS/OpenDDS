81559aa72e3cd0bb6f4d0b0aad32ab6fa5341822
Johnny Willemsen <jwillemsen@remedy.nl>
2016-11-14 11:45:02 +0100

    Missing versioned namespace macros around forward declared ACE classes
        * dds/FACE/config/Parser.h:

--- dds/FACE/config/Parser.h
+++ dds/FACE/config/Parser.h
@@ -6,8 +6,11 @@
 #include "ConnectionSettings.h"
 #include "TopicSettings.h"
 
+ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 class ACE_Configuration_Heap;
 class ACE_Configuration_Section_Key;
+ACE_END_VERSIONED_NAMESPACE_DECL
+
 
 namespace OpenDDS { namespace FaceTSS { namespace config {
 
