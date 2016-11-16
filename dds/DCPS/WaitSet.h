11b932ff096764f355e681296daf67b4a33ac1d1
Johnny Willemsen <jwillemsen@remedy.nl>
2016-11-16 19:21:13 +0100

    Zapped space     * dds/DCPS/WaitSet.h:

--- dds/DCPS/WaitSet.h
+++ dds/DCPS/WaitSet.h
@@ -98,7 +98,7 @@ namespace TAO {
 #define DDS_WAITSET__TRAITS_
 
 template<>
-struct OpenDDS_Dcps_Export Objref_Traits< DDS::WaitSet> {
+struct OpenDDS_Dcps_Export Objref_Traits<DDS::WaitSet> {
   static DDS::WaitSet_ptr duplicate(DDS::WaitSet_ptr p);
   static void release(DDS::WaitSet_ptr p);
   static DDS::WaitSet_ptr nil();
