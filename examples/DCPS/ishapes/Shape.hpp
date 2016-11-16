d5df7f184496bc1369f66461226eacd747b15861
Adam Mitz <mitza@ociweb.com>
2016-11-16 10:37:41 -0600

    configure and MPC support for C++11 version of ishapes; fixed a bug

--- examples/DCPS/ishapes/Shape.hpp
+++ examples/DCPS/ishapes/Shape.hpp
@@ -4,14 +4,17 @@
 #include <QtCore/QRect>
 #include <QtGui/QPen>
 #include <QtGui/QBrush>
+
+#include "ace/config-all.h"
+
 #if defined (ACE_HAS_CPP11)
 # include <memory>
 using std::shared_ptr;
-#define STD_TO_STRING std::to_string
+# define STD_TO_STRING std::to_string
 #else
 # include <boost/shared_ptr.hpp>
 using boost::shared_ptr;
-#define STD_TO_STRING boost::lexical_cast<std::string>
+# define STD_TO_STRING boost::lexical_cast<std::string>
 #endif /* ACE_HAS_CPP11 */
 
 
