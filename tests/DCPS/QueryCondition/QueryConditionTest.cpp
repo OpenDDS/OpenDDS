4308bf53e8b7d744d0f27e825eb4ba8f1f09df10
Johnny Willemsen <jwillemsen@remedy.nl>
2017-04-03 12:38:40 +0200

    Extend QC test with a more complex test in attempt to reproduce a dds4ccm crash when using filters
        * tests/DCPS/QueryCondition/rtps_disc.ini:
          Added.
    
        * bin/dcps_tests.lst:
        * tests/DCPS/Priority/DataReaderListener.h:
        * tests/DCPS/Priority/Subscriber.cpp:
        * tests/DCPS/QueryCondition/Messenger.idl:
        * tests/DCPS/QueryCondition/QueryConditionTest.cpp:
        * tests/DCPS/QueryCondition/run_test.pl:
        * tests/DCPS/UnitTests/UnitTests.mpc:

--- tests/DCPS/QueryCondition/QueryConditionTest.cpp
+++ tests/DCPS/QueryCondition/QueryConditionTest.cpp
@@ -14,6 +14,48 @@ using namespace DDS;
 using namespace OpenDDS::DCPS;
 using namespace Messenger;
 
+class MessengerListener
+  : public virtual OpenDDS::DCPS::LocalObject<DDS::DataReaderListener>
+{
+public:
+  MessengerListener(DDS::ReadCondition_ptr rd) : rc_ (DDS::ReadCondition::_duplicate(rd))  {};
+
+  virtual void on_requested_deadline_missed (
+    DDS::DataReader_ptr /*reader*/,
+    const DDS::RequestedDeadlineMissedStatus & /*status*/) {}
+
+  virtual void on_requested_incompatible_qos (
+    DDS::DataReader_ptr /*reader*/,
+    const DDS::RequestedIncompatibleQosStatus & /*status*/) {}
+
+  virtual void on_liveliness_changed (
+    DDS::DataReader_ptr /*reader*/,
+    const DDS::LivelinessChangedStatus & /*status*/) {}
+
+  virtual void on_subscription_matched (
+    DDS::DataReader_ptr /*reader*/,
+    const DDS::SubscriptionMatchedStatus & /*status*/) {}
+
+  virtual void on_sample_rejected(
+    DDS::DataReader_ptr /*reader*/,
+    const DDS::SampleRejectedStatus& /*status*/) {}
+
+  virtual void on_data_available(DDS::DataReader_ptr reader)
+  {
+    MessageDataReader_var mdr = MessageDataReader::_narrow(reader);
+    MessageSeq data;
+    SampleInfoSeq infoseq;
+    if (mdr->read_w_condition (data, infoseq, LENGTH_UNLIMITED, rc_.in ()) == RETCODE_ERROR) {
+      cout << "ERROR: read_w_condition failed" << endl;
+    }
+  }
+
+  virtual void on_sample_lost(DDS::DataReader_ptr /*reader*/,
+                              const DDS::SampleLostStatus& /*status*/) {}
+private:
+  DDS::ReadCondition_var rc_;
+};
+
 void test_setup(const DomainParticipant_var& dp,
   const MessageTypeSupport_var& ts, const Publisher_var& pub,
   const Subscriber_var& sub, const char* topicName, DataWriter_var& dw,
@@ -33,6 +75,41 @@ void test_setup(const DomainParticipant_var& dp,
   sub->get_default_datareader_qos(dr_qos);
   dr_qos.history.kind = KEEP_ALL_HISTORY_QOS;
   dr = sub->create_datareader(topic, dr_qos, 0, DEFAULT_STATUS_MASK);
+
+  StatusCondition_var dw_sc = dw->get_statuscondition();
+  dw_sc->set_enabled_statuses(PUBLICATION_MATCHED_STATUS);
+  WaitSet_var ws = new WaitSet;
+  ws->attach_condition(dw_sc);
+  Duration_t infinite = {DURATION_INFINITE_SEC, DURATION_INFINITE_NSEC};
+  ConditionSeq active;
+  ws->wait(active, infinite);
+  ws->detach_condition(dw_sc);
+}
+
+void complex_test_setup(const DomainParticipant_var& dp,
+  const MessageTypeSupport_var& ts, const Publisher_var& pub,
+  const Subscriber_var& sub, const char* topicName, DataWriter_var& dw,
+  DataReader_var& dr1, DataReader_var& dr2)
+{
+  CORBA::String_var typeName = ts->get_type_name();
+  Topic_var topic = dp->create_topic(topicName, typeName,
+                                     TOPIC_QOS_DEFAULT, 0,
+                                     DEFAULT_STATUS_MASK);
+
+  DataWriterQos dw_qos;
+  pub->get_default_datawriter_qos(dw_qos);
+  dw_qos.history.kind = KEEP_ALL_HISTORY_QOS;
+  dw_qos.durability.kind = TRANSIENT_DURABILITY_QOS;
+  dw_qos.reliability.kind = RELIABLE_RELIABILITY_QOS;
+  dw = pub->create_datawriter(topic, dw_qos, 0, DEFAULT_STATUS_MASK);
+
+  DataReaderQos dr_qos;
+  sub->get_default_datareader_qos(dr_qos);
+  dr_qos.history.kind = KEEP_ALL_HISTORY_QOS;
+  dr_qos.durability.kind = TRANSIENT_LOCAL_DURABILITY_QOS  ;
+  dr_qos.reliability.kind = RELIABLE_RELIABILITY_QOS;
+  dr1 = sub->create_datareader(topic, dr_qos, 0, DEFAULT_STATUS_MASK);
+  dr2 = sub->create_datareader(topic, dr_qos, 0, DEFAULT_STATUS_MASK);
 
   StatusCondition_var dw_sc = dw->get_statuscondition();
   dw_sc->set_enabled_statuses(PUBLICATION_MATCHED_STATUS);
@@ -129,6 +206,118 @@ bool run_filtering_test(const DomainParticipant_var& dp,
   }
 
   dr->delete_readcondition(dr_qc);
+  return true;
+}
+
+bool run_complex_filtering_test(const DomainParticipant_var& dp,
+  const MessageTypeSupport_var& ts, const Publisher_var& pub,
+  const Subscriber_var& sub)
+{
+  DataWriter_var dw;
+  DataReader_var dr1;
+  DataReader_var dr2;
+  complex_test_setup(dp, ts, pub, sub, "MyTopicComplex", dw, dr1, dr2);
+
+  ReturnCode_t ret;// = mdw->write(sample, HANDLE_NIL);
+//  if (ret != RETCODE_OK) return false;
+//   if (!waitForSample(dr)) return false;
+
+  DDS::StringSeq params(2);
+  params.length (2);
+  params[0] = "5";
+  params[1] = "10";
+  ReadCondition_var dr_qc1 = dr1->create_querycondition(ANY_SAMPLE_STATE,
+    ANY_VIEW_STATE, ALIVE_INSTANCE_STATE, "( (iteration > %0) AND (iteration < %1) )", params);
+  if (CORBA::is_nil(dr_qc1.in())) {
+    cout << "ERROR: failed to create QueryCondition 1" << endl;
+    return false;
+  }
+  ReadCondition_var dr_qc2 = dr2->create_querycondition(ANY_SAMPLE_STATE,
+    ANY_VIEW_STATE, ALIVE_INSTANCE_STATE, "( (iteration < %0) OR (iteration > %1) )", params);
+  if (CORBA::is_nil(dr_qc2.in())) {
+    cout << "ERROR: failed to create QueryCondition 2" << endl;
+    return false;
+  }
+
+  DDS::DataReaderListener_var ml1 = new MessengerListener(dr_qc1.in ());
+  DDS::DataReaderListener_var ml2 = new MessengerListener(dr_qc2.in ());
+  MessageDataReader_var mdr1 = MessageDataReader::_narrow(dr1);
+  MessageDataReader_var mdr2 = MessageDataReader::_narrow(dr2);
+  mdr1->set_listener (ml1.in (), DDS::DATA_AVAILABLE_STATUS);
+  mdr2->set_listener (ml2.in (), DDS::DATA_AVAILABLE_STATUS);
+  MessageSeq data;
+  SampleInfoSeq infoseq;
+  ret = mdr1->take_w_condition(data, infoseq, LENGTH_UNLIMITED, dr_qc1);
+  if (ret != RETCODE_NO_DATA) {
+    cout << "ERROR: take_w_condition(qc1) shouldn't have returned data" << endl;
+    return false;
+  }
+  ret = mdr2->take_w_condition(data, infoseq, LENGTH_UNLIMITED, dr_qc2);
+  if (ret != RETCODE_NO_DATA) {
+    cout << "ERROR: take_w_condition(qc2) shouldn't have returned data" << endl;
+    return false;
+  }
+
+  MessageDataWriter_var mdw = MessageDataWriter::_narrow(dw);
+  Message sample;
+  for (CORBA::Long i = 0; i < 6; i++) {
+    sample.key = i;
+    if (mdw->register_instance (sample) == HANDLE_NIL) {
+      cout << "ERROR: Registering instance failed" << endl;
+      return false;
+    }
+  }
+
+//   sample.key = 2;
+//   if (mdw->write(sample, HANDLE_NIL) != RETCODE_OK) return false;
+//   if (!waitForSample(dr)) return false;
+//
+//   ws->attach_condition(dr_qc);
+//   ret = ws->wait(active, three_sec);
+//   if (ret != RETCODE_OK) {
+//     cout << "ERROR: wait(qc) should not time out" << endl;
+//     return false;
+//   }
+//   ws->detach_condition(dr_qc);
+//
+//   ret = mdr->take_w_condition(data, infoseq, LENGTH_UNLIMITED, dr_qc);
+//   if (ret != RETCODE_OK) {
+//     cout << "ERROR: take_w_condition(qc) should have returned data" << endl;
+//     return false;
+//   }
+
+  for (CORBA::Long i = 0; i < 6; i++) {
+    for (CORBA::Long j = 0; j < 15; j++) {
+      sample.key = i;
+      sample.iteration = j;
+      DDS::InstanceHandle_t const hnd = mdw->lookup_instance(sample);
+      if (hnd == HANDLE_NIL) {
+        cout << "ERROR: Lookup instance failed" << endl;
+        return false;
+      }
+      if (mdw->write(sample, hnd) != RETCODE_OK) return false;
+    }
+  }
+
+  for (CORBA::Long i = 0; i < 6; i++) {
+    sample.key = i;
+    DDS::InstanceHandle_t const hnd = mdw->lookup_instance(sample);
+    if (hnd == HANDLE_NIL) {
+      cout << "ERROR: Lookup instance failed" << endl;
+      return false;
+    }
+    if (mdw->dispose (sample, hnd) != RETCODE_OK) {
+      cout << "ERROR: Dispose instance failed" << endl;
+      return false;
+    }
+    if (mdw->unregister_instance (sample, hnd) != RETCODE_OK) {
+      cout << "ERROR: Unregistering instance failed" << endl;
+      return false;
+    }
+  }
+
+  dr1->delete_readcondition(dr_qc1);
+  dr2->delete_readcondition(dr_qc2);
   return true;
 }
 
@@ -375,6 +564,7 @@ int run_test(int argc, ACE_TCHAR *argv[])
   bool passed = run_sorting_test(dp, ts, pub, sub);
   passed &= run_filtering_test(dp, ts, pub, sub);
   passed &= run_change_parameter_test(dp, ts, pub, sub);
+  passed &= run_complex_filtering_test(dp, ts, pub, sub);
 
   dp->delete_contained_entities();
   dpf->delete_participant(dp);
