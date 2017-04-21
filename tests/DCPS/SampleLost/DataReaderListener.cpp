5a60736ba6084b1f05cf0ce5dc06c8b920234530
Adam Mitz <mitza@ociweb.com>
2017-04-21 15:03:13 -0500

    Reworked this test to reproduce issue #489
        * tests/DCPS/SampleLost/DataReaderListener.cpp:
        * tests/DCPS/SampleLost/DataReaderListener.h:
        * tests/DCPS/SampleLost/DataWriterListenerImpl.cpp:
        * tests/DCPS/SampleLost/DataWriterListenerImpl.h:
        * tests/DCPS/SampleLost/Messenger.idl:
        * tests/DCPS/SampleLost/README:
        * tests/DCPS/SampleLost/SampleLost.mpc:
        * tests/DCPS/SampleLost/SampleLost_export.h:
        * tests/DCPS/SampleLost/Writer.cpp:
        * tests/DCPS/SampleLost/Writer.h:
        * tests/DCPS/SampleLost/pub.ini:
        * tests/DCPS/SampleLost/publisher.cpp:
        * tests/DCPS/SampleLost/run_test.pl:
        * tests/DCPS/SampleLost/sub.ini:
        * tests/DCPS/SampleLost/subscriber.cpp:
          Added.
    
    (cherry picked from commit 1a4d3e975f82ad8aebc79d367fdce75748e14d11)

---
+++ tests/DCPS/SampleLost/DataReaderListener.cpp
@@ -0,0 +1,134 @@
+// -*- C++ -*-
+//
+
+#include "DataReaderListener.h"
+#include "MessengerTypeSupportC.h"
+#include "MessengerTypeSupportImpl.h"
+#include <dds/DCPS/Service_Participant.h>
+#include <ace/streams.h>
+
+using namespace Messenger;
+
+DataReaderListenerImpl::DataReaderListenerImpl()
+  : num_reads_(0)
+{
+}
+
+DataReaderListenerImpl::~DataReaderListenerImpl ()
+{
+}
+
+void
+DataReaderListenerImpl::on_data_available(DDS::DataReader_ptr reader)
+{
+  ++this->num_reads_;
+
+  try {
+    MessageDataReader_var message_dr = MessageDataReader::_narrow(reader);
+    if (CORBA::is_nil (message_dr.in ())) {
+      cerr << "read: _narrow failed." << endl;
+      exit(1);
+    }
+
+    Messenger::Message message;
+    DDS::SampleInfo si ;
+    DDS::ReturnCode_t status = message_dr->take_next_sample(message, si) ;
+
+    if (status == DDS::RETCODE_OK) {
+      cout << "Message: subject    = " << message.subject.in() << endl
+           << "         subject_id = " << message.subject_id   << endl
+           << "         from       = " << message.from.in()    << endl
+           << "         count      = " << message.count        << endl
+           << "         text       = " << message.text.in()    << endl;
+      cout << "SampleInfo.sample_rank = " << si.sample_rank << endl;
+    } else if (status == DDS::RETCODE_NO_DATA) {
+      cerr << "ERROR: reader received DDS::RETCODE_NO_DATA!" << endl;
+    } else {
+      cerr << "ERROR: read Message: Error: " <<  status << endl;
+    }
+  } catch (CORBA::Exception& e) {
+    cerr << "Exception caught in read:" << endl << e << endl;
+    exit(1);
+  }
+}
+
+void
+DataReaderListenerImpl::on_requested_deadline_missed (
+    DDS::DataReader_ptr,
+    const DDS::RequestedDeadlineMissedStatus &)
+{
+  cerr << "DataReaderListenerImpl::on_requested_deadline_missed" << endl;
+}
+
+void
+DataReaderListenerImpl::on_requested_incompatible_qos (
+    DDS::DataReader_ptr,
+    const DDS::RequestedIncompatibleQosStatus &)
+{
+  cerr << "DataReaderListenerImpl::on_requested_incompatible_qos" << endl;
+}
+
+void
+DataReaderListenerImpl::on_liveliness_changed (
+    DDS::DataReader_ptr,
+    const DDS::LivelinessChangedStatus &)
+{
+  cerr << "DataReaderListenerImpl::on_liveliness_changed" << endl;
+}
+
+void
+DataReaderListenerImpl::on_subscription_matched (
+    DDS::DataReader_ptr,
+    const DDS::SubscriptionMatchedStatus &)
+{
+  cerr << "DataReaderListenerImpl::on_subscription_matched" << endl;
+}
+
+void
+DataReaderListenerImpl::on_sample_rejected (
+    DDS::DataReader_ptr,
+    const DDS::SampleRejectedStatus&)
+{
+  cerr << "DataReaderListenerImpl::on_sample_rejected" << endl;
+}
+
+void DataReaderListenerImpl::on_sample_lost(
+  DDS::DataReader_ptr,
+  const DDS::SampleLostStatus&)
+{
+  cerr << "DataReaderListenerImpl::on_sample_lost" << endl;
+}
+
+void DataReaderListenerImpl::on_subscription_disconnected (
+  DDS::DataReader_ptr,
+  const ::OpenDDS::DCPS::SubscriptionDisconnectedStatus &)
+{
+  cerr << "DataReaderListenerImpl::on_subscription_disconnected" << endl;
+}
+
+void DataReaderListenerImpl::on_subscription_reconnected (
+  DDS::DataReader_ptr,
+  const ::OpenDDS::DCPS::SubscriptionReconnectedStatus &)
+{
+  cerr << "DataReaderListenerImpl::on_subscription_reconnected" << endl;
+}
+
+void DataReaderListenerImpl::on_subscription_lost (
+  DDS::DataReader_ptr,
+  const ::OpenDDS::DCPS::SubscriptionLostStatus &)
+{
+  cerr << "DataReaderListenerImpl::on_subscription_lost" << endl;
+}
+
+void DataReaderListenerImpl::on_budget_exceeded (
+  DDS::DataReader_ptr,
+  const ::OpenDDS::DCPS::BudgetExceededStatus&)
+{
+  cerr << "DataReaderListenerImpl::on_budget_exceeded" << endl;
+}
+
+void DataReaderListenerImpl::on_connection_deleted (
+  DDS::DataReader_ptr)
+{
+  cerr << "DataReaderListenerImpl::on_connection_deleted" << endl;
+}
---
+++ tests/DCPS/SampleLost/DataReaderListener.h
@@ -0,0 +1,77 @@
+// -*- C++ -*-
+//
+#ifndef DATAREADER_LISTENER_IMPL
+#define DATAREADER_LISTENER_IMPL
+
+#include <dds/DdsDcpsSubscriptionExtC.h>
+#include <dds/DCPS/LocalObject.h>
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+class DataReaderListenerImpl
+  : public virtual OpenDDS::DCPS::LocalObject<OpenDDS::DCPS::DataReaderListener>
+{
+public:
+  DataReaderListenerImpl ();
+
+  virtual ~DataReaderListenerImpl (void);
+
+  virtual void on_requested_deadline_missed (
+    DDS::DataReader_ptr reader,
+    const DDS::RequestedDeadlineMissedStatus & status);
+
+  virtual void on_requested_incompatible_qos (
+    DDS::DataReader_ptr reader,
+    const DDS::RequestedIncompatibleQosStatus & status);
+
+  virtual void on_liveliness_changed (
+    DDS::DataReader_ptr reader,
+    const DDS::LivelinessChangedStatus & status);
+
+  virtual void on_subscription_matched (
+    DDS::DataReader_ptr reader,
+    const DDS::SubscriptionMatchedStatus & status);
+
+  virtual void on_sample_rejected(
+    DDS::DataReader_ptr reader,
+    const DDS::SampleRejectedStatus& status);
+
+  virtual void on_data_available(
+    DDS::DataReader_ptr reader);
+
+  virtual void on_sample_lost(
+    DDS::DataReader_ptr reader,
+    const DDS::SampleLostStatus& status);
+
+  virtual void on_subscription_disconnected (
+    DDS::DataReader_ptr reader,
+    const ::OpenDDS::DCPS::SubscriptionDisconnectedStatus & status);
+
+  virtual void on_subscription_reconnected (
+    DDS::DataReader_ptr reader,
+    const ::OpenDDS::DCPS::SubscriptionReconnectedStatus & status);
+
+  virtual void on_subscription_lost (
+    DDS::DataReader_ptr reader,
+    const ::OpenDDS::DCPS::SubscriptionLostStatus & status);
+
+  virtual void on_budget_exceeded (
+    DDS::DataReader_ptr,
+    const ::OpenDDS::DCPS::BudgetExceededStatus& status);
+
+  virtual void on_connection_deleted (
+    DDS::DataReader_ptr);
+
+  long num_reads() const {
+    return num_reads_;
+  }
+
+private:
+
+  DDS::DataReader_var reader_;
+  long                  num_reads_;
+};
+
+#endif /* DATAREADER_LISTENER_IMPL  */
