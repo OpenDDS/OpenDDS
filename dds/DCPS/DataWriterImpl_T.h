a4769aeb8be6b71cd1982935a67ae67049241f1f
Johnny Willemsen <jwillemsen@remedy.nl>
2017-06-07 13:45:17 +0200

    According to the DDS specification get_key_value should return RETCODE_BAD_PARAMETER at the moment an unknown handle is passed. This fixes issue #592
        * dds/DCPS/DataReaderImpl_T.h:
        * dds/DCPS/DataWriterImpl_T.h:
        * tests/DCPS/FooTest3_0/PubDriver.cpp:
        * tests/DCPS/FooTest4/Reader.cpp:
        * tests/DCPS/MultiTopic/MultiTopicTest.cpp:

--- dds/DCPS/DataWriterImpl_T.h
+++ dds/DCPS/DataWriterImpl_T.h
@@ -267,7 +267,7 @@ namespace OpenDDS {
             }
         }
 
-      return DDS::RETCODE_ERROR;
+      return DDS::RETCODE_BAD_PARAMETER;
     }
 
   virtual DDS::InstanceHandle_t lookup_instance (
