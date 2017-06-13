411a8ef9075a49583cb4967892719d9b9425ae2d
Johnny Willemsen <jwillemsen@remedy.nl>
2017-06-13 16:12:16 +0200

    Layout changes
        * tests/DCPS/Reconnect/DataWriterListener.cpp:
        * tests/DCPS/Reconnect/DataWriterListener.h:

--- tests/DCPS/Reconnect/DataWriterListener.h
+++ tests/DCPS/Reconnect/DataWriterListener.h
@@ -22,45 +22,36 @@ public:
 
   virtual void on_offered_deadline_missed (
       ::DDS::DataWriter_ptr writer,
-      const ::DDS::OfferedDeadlineMissedStatus & status
-    );
+      const ::DDS::OfferedDeadlineMissedStatus & status);
 
   virtual void on_offered_incompatible_qos (
       ::DDS::DataWriter_ptr writer,
-      const ::DDS::OfferedIncompatibleQosStatus & status
-    );
+      const ::DDS::OfferedIncompatibleQosStatus & status);
 
   virtual void on_liveliness_lost (
       ::DDS::DataWriter_ptr writer,
-      const ::DDS::LivelinessLostStatus & status
-    );
+      const ::DDS::LivelinessLostStatus & status);
 
   virtual void on_publication_matched (
       ::DDS::DataWriter_ptr writer,
-      const ::DDS::PublicationMatchedStatus & status
-    );
+      const ::DDS::PublicationMatchedStatus & status);
 
   virtual void on_publication_disconnected (
     ::DDS::DataWriter_ptr writer,
-    const ::OpenDDS::DCPS::PublicationDisconnectedStatus & status
-    );
+    const ::OpenDDS::DCPS::PublicationDisconnectedStatus & status);
 
   virtual void on_publication_reconnected (
     ::DDS::DataWriter_ptr writer,
-    const ::OpenDDS::DCPS::PublicationReconnectedStatus & status
-    );
+    const ::OpenDDS::DCPS::PublicationReconnectedStatus & status);
 
   virtual void on_publication_lost (
     ::DDS::DataWriter_ptr writer,
-    const ::OpenDDS::DCPS::PublicationLostStatus & status
-    );
+    const ::OpenDDS::DCPS::PublicationLostStatus & status);
 
   virtual void on_connection_deleted (
-    ::DDS::DataWriter_ptr writer
-    );
+    ::DDS::DataWriter_ptr writer);
 
 private:
-
   DDS::DataWriter_var reader_;
 };
 
