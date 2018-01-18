2cd702516ffd6e7c3be9a57199ae106042d7045a
Jeremy Bratton <brattonj@objectcomputing.com>
2018-01-18 10:30:42 -0600

    This commit adds an initial stub version of the CryptoKeyFactory interface as well as an mpb file used for building unit tests with googlemock

---
+++ dds/DCPS/security/CommonUtilities.h
@@ -0,0 +1,36 @@
+#ifndef DDS_SECURITY_UTILS_H
+#define DDS_SECURITY_UTILS_H
+
+#include "dds/DCPS/security/DdsSecurityCoreC.h"
+#include "dds/Versioned_Namespace.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+class DDS_TEST;
+
+OPENDDS_BEGIN_VERSIONED_NAMESPACE_DECL
+
+namespace OpenDDS {
+namespace Security {
+
+/**
+* @class CommonUtilities
+*
+* @brief Provides several simple utility functions common to the Security plugins
+*
+*/
+class CommonUtilities 
+{
+public:
+  static void set_security_error(::DDS::Security::SecurityException& ex, 
+                                 int code, 
+                                 int minor_code, 
+                                 const char* message);
+};
+
+} // Security
+} // OpenDDS
+
+#endif
