91aff94a335d5df0423d96691c080a6bcbb0207a
Adam Mitz <mitza@ociweb.com>
2018-03-02 09:49:02 -0600

    fixed build error with precompiled header

--- dds/DCPS/security/framework/SecurityPluginInst.cpp
+++ dds/DCPS/security/framework/SecurityPluginInst.cpp
@@ -5,6 +5,7 @@
  * See: http://www.opendds.org/license.html
  */
 
+#include "DCPS/DdsDcps_pch.h" //Only the _pch include should start with DCPS/
 #include "SecurityPluginInst.h"
 
 namespace OpenDDS {
