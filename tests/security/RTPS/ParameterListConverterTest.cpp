ceea63e51ebadc41d05274acb4739fe7bceb3b82
Mason McParlane <mcparlanem@objectcomputing.com>
2018-03-06 12:23:48 -0600

    Removed largely-unused utils from tests/security

--- tests/security/RTPS/ParameterListConverterTest.cpp
+++ tests/security/RTPS/ParameterListConverterTest.cpp
@@ -5,14 +5,97 @@
 #include <memory>
 #include <cstring>
 
-#include "../Utils.h"
-
 using namespace OpenDDS::RTPS;
 using namespace OpenDDS::Security;
 using namespace DDS::Security;
 using namespace std;
 
 namespace {
+
+  SPDPdiscoveredParticipantData spdp_participant(
+    const void* user_data = NULL,
+    CORBA::ULong user_data_len = 0,
+    char major_protocol_version = 0,
+    char minor_protocol_version = 0,
+    char* vendor_id = NULL,
+    OpenDDS::DCPS::GUID_t* guid = NULL,
+    bool expects_inline_qos = false,
+    unsigned long builtin_endpoints = 0,
+    OpenDDS::DCPS::Locator_t* mtu_locs = NULL,
+    CORBA::ULong num_mtu_locs = 0,
+    OpenDDS::DCPS::Locator_t* mtm_locs = NULL,
+    CORBA::ULong num_mtm_locs = 0,
+    OpenDDS::DCPS::Locator_t* du_locs = NULL,
+    CORBA::ULong num_du_locs = 0,
+    OpenDDS::DCPS::Locator_t* dm_locs = NULL,
+    CORBA::ULong num_dm_locs = 0,
+    long liveliness_count = 0,
+    long lease_dur_seconds = 100,
+    unsigned long lease_dur_fraction = 0
+  )
+  {
+    SPDPdiscoveredParticipantData result;
+    if (user_data_len && user_data) {
+      result.ddsParticipantData.user_data.value.length(user_data_len);
+      for (CORBA::ULong i = 0; i < user_data_len; ++i) {
+        result.ddsParticipantData.user_data.value[i] = ((char*)user_data)[i];
+      }
+    }
+    if (major_protocol_version && minor_protocol_version) {
+      result.participantProxy.protocolVersion.major = major_protocol_version;
+      result.participantProxy.protocolVersion.minor = minor_protocol_version;
+    }
+    if (vendor_id) {
+      result.participantProxy.vendorId.vendorId[0] = vendor_id[0];
+      result.participantProxy.vendorId.vendorId[1] = vendor_id[1];
+    }
+    if (guid) {
+      memcpy(result.participantProxy.guidPrefix,
+             guid->guidPrefix,
+             sizeof(guid->guidPrefix));
+    }
+    result.participantProxy.expectsInlineQos = expects_inline_qos;
+    result.participantProxy.availableBuiltinEndpoints = builtin_endpoints;
+
+    if (num_mtu_locs && mtu_locs) {
+      result.participantProxy.metatrafficUnicastLocatorList.length(num_mtu_locs);
+      for (CORBA::ULong i = 0; i < num_mtu_locs; ++i) {
+        result.participantProxy.metatrafficUnicastLocatorList[i] =
+            mtu_locs[i];
+      }
+    }
+
+    if (num_mtm_locs && mtm_locs) {
+      result.participantProxy.metatrafficMulticastLocatorList.length(num_mtm_locs);
+      for (CORBA::ULong i = 0; i < num_mtm_locs; ++i) {
+        result.participantProxy.metatrafficMulticastLocatorList[i] =
+            mtm_locs[i];
+      }
+    }
+
+    if (num_du_locs && du_locs) {
+      result.participantProxy.defaultUnicastLocatorList.length(num_du_locs);
+      for (CORBA::ULong i = 0; i < num_du_locs; ++i) {
+        result.participantProxy.defaultUnicastLocatorList[i] = du_locs[i];
+      }
+    }
+
+    if (num_dm_locs && dm_locs) {
+      result.participantProxy.defaultMulticastLocatorList.length(num_dm_locs);
+      for (CORBA::ULong i = 0; i < num_dm_locs; ++i) {
+        result.participantProxy.defaultMulticastLocatorList[i] = dm_locs[i];
+      }
+    }
+
+    if (liveliness_count) {
+      result.participantProxy.manualLivelinessCount.value = liveliness_count;
+    }
+
+    result.leaseDuration.seconds = lease_dur_seconds;
+    result.leaseDuration.fraction = lease_dur_fraction;
+
+    return result;
+  }
 
   Token token(string classid = "Test-Class-Id",
                    size_t proplen = 1,
@@ -88,7 +171,7 @@ TEST(ToParamListTest, From_SPDPdiscoveredParticipantData_SecurityWrapper_Identit
 {
   SPDPdiscoveredParticipantData_SecurityWrapper w1, w2;
 
-  w1.data = Factory::spdp_participant("Test-Test-Test", 10);
+  w1.data = spdp_participant("Test-Test-Test", 10);
   w1.identity_status_token = token();
 
   ParameterList p;
