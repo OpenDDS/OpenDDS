bf1985dd2fef3dc3426b09846c21f32c998f46bb
Mason McParlane <mcparlanem@objectcomputing.com>
2018-03-11 15:36:54 -0500

    Implemented test for signature-verification failure

--- tests/security/SSL/PrivateKeyTest.cpp
+++ tests/security/SSL/PrivateKeyTest.cpp
@@ -54,3 +54,30 @@ TEST_F(PrivateKeyTest, SignAndVerify_Success)
 
   ASSERT_EQ(0, verify_result);
 }
+
+TEST_F(PrivateKeyTest, SignAndVerify_WrongData_Failure)
+{
+  DDS::OctetSeq hello;
+  size_t hello_len = std::strlen("hello");
+  hello.length(hello_len);
+  std::memcpy(hello.get_buffer(), "hello", hello_len);
+
+  DDS::OctetSeq world;
+  size_t world_len = std::strlen("world");
+  world.length(world_len);
+  std::memcpy(world.get_buffer(), "world", world_len);
+
+  std::vector<const DDS::OctetSeq*> sign_these;
+  sign_these.push_back(&hello);
+  sign_these.push_back(&world);
+
+  std::vector<const DDS::OctetSeq*> verify_these;
+  verify_these.push_back(&hello);
+
+  DDS::OctetSeq tmp;
+  privkey_.sign(sign_these, tmp);
+
+  int verify_result = pubkey_.verify_signature(tmp, verify_these);
+
+  ASSERT_EQ(1, verify_result);
+}
