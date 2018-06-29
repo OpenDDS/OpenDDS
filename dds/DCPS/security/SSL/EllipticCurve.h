95f0d4a1db1fe6da5d0094185c7b769381734fef
Doug Lewis <lewisdo@objectcomputing.com>
2018-06-29 09:55:24 -0500

    Modifications from code review process.

--- dds/DCPS/security/SSL/EllipticCurve.h
+++ dds/DCPS/security/SSL/EllipticCurve.h
@@ -12,114 +12,109 @@
 #include <openssl/ossl_typ.h>
 
 namespace OpenDDS {
-    namespace Security {
-        namespace SSL {
+namespace Security {
+namespace SSL {
 
-            class DdsSecurity_Export ECAlgorithm
-            {
-            public:
-                typedef DCPS::unique_ptr<ECAlgorithm> unique_ptr;
+  class DdsSecurity_Export ECAlgorithm
+  {
+  public:
+    typedef DCPS::unique_ptr<ECAlgorithm> unique_ptr;
 
-                ECAlgorithm() : k_(NULL)
-                {
+    ECAlgorithm() : 
+      k_(NULL)
+    {
 
-                }
-
-                virtual ~ECAlgorithm();
-
-                virtual int init() = 0;
-                virtual int pub_key(DDS::OctetSeq& dst) = 0;
-                virtual int gen_shared_secret(const DDS::OctetSeq& pub_key)
-                {
-                    int err = compute_shared_secret(pub_key) || hash_shared_secret();
-                    return err;
-                }
-                virtual const DDS::OctetSeq& get_shared_secret() const
-                {
-                    return shared_secret_;
-                }
-                virtual bool cmp_shared_secret(const ECAlgorithm& other) const;
-                virtual const char* kagree_algo() = 0;
-
-            protected:
-                virtual int compute_shared_secret(const DDS::OctetSeq& pub_key) = 0;
-                int hash_shared_secret();
-
-                EVP_PKEY* k_;
-                DDS::OctetSeq shared_secret_;
-            };
-
-            class DdsSecurity_Export EC_PRIME_256_V1_CEUM : public ECAlgorithm
-            {
-            public:
-                EC_PRIME_256_V1_CEUM();
-                ~EC_PRIME_256_V1_CEUM();
-
-                int init();
-
-                int pub_key(DDS::OctetSeq& dst);
-
-                int compute_shared_secret(const DDS::OctetSeq& pub_key);
-
-                const char* kagree_algo() {
-                    return "EC+prime256v1-CEUM";
-                }
-            };
-
-            class DdsSecurity_Export EllipticCurve
-            {
-            public:
-                typedef DCPS::unique_ptr<EllipticCurve> unique_ptr;
-
-                static EllipticCurve* factory(const DDS::OctetSeq& kagree_algo);
-
-                EllipticCurve(ECAlgorithm* algorithm) :
-                    algo_(algorithm)
-                {
-
-                }
-
-                ~EllipticCurve()
-                {
-
-                }
-
-                void load()
-                {
-                    if (algo_) algo_->init();
-                }
-
-                int pub_key(DDS::OctetSeq& dst)
-                {
-                    return algo_->pub_key(dst);
-                }
-
-                int gen_shared_secret(const DDS::OctetSeq& pub_key)
-                {
-                    return algo_->gen_shared_secret(pub_key);
-                }
-
-                const DDS::OctetSeq& get_shared_secret()
-                {
-                    return algo_->get_shared_secret();
-                }
-
-                bool cmp_shared_secret(const EllipticCurve& other)
-                {
-                    return algo_->cmp_shared_secret(*other.algo_);
-                }
-
-                const char* kagree_algo() const
-                {
-                    return algo_->kagree_algo();
-                }
-
-            private:
-
-                ECAlgorithm::unique_ptr algo_;
-            };
-        }
     }
+
+    virtual ~ECAlgorithm();
+
+    virtual int init() = 0;
+    virtual int pub_key(DDS::OctetSeq& dst) = 0;
+    virtual int gen_shared_secret(const DDS::OctetSeq& pub_key)
+    {
+      int err = compute_shared_secret(pub_key) || hash_shared_secret();
+      return err;
+    }
+    virtual const DDS::OctetSeq& get_shared_secret() const
+    {
+      return shared_secret_;
+    }
+    virtual bool cmp_shared_secret(const ECAlgorithm& other) const;
+    virtual const char* kagree_algo() = 0;
+
+  protected:
+    virtual int compute_shared_secret(const DDS::OctetSeq& pub_key) = 0;
+    int hash_shared_secret();
+
+    EVP_PKEY* k_;
+    DDS::OctetSeq shared_secret_;
+  };
+
+  class DdsSecurity_Export EC_PRIME_256_V1_CEUM : public ECAlgorithm
+  {
+  public:
+    EC_PRIME_256_V1_CEUM();
+    ~EC_PRIME_256_V1_CEUM();
+
+    int init();
+
+    int pub_key(DDS::OctetSeq& dst);
+
+    int compute_shared_secret(const DDS::OctetSeq& pub_key);
+
+    const char* kagree_algo() {
+      return "EC+prime256v1-CEUM";
+    }
+  };
+
+  class DdsSecurity_Export EllipticCurve
+  {
+  public:
+    typedef DCPS::unique_ptr<EllipticCurve> unique_ptr;
+
+    static EllipticCurve* factory(const DDS::OctetSeq& kagree_algo);
+
+    EllipticCurve(ECAlgorithm* algorithm) :
+      algo_(algorithm)
+    {
+
+    }
+
+    ~EllipticCurve()
+    {
+
+    }
+
+    void load() {
+      if (algo_) algo_->init();
+    }
+
+    int pub_key(DDS::OctetSeq& dst) {
+      return algo_->pub_key(dst);
+    }
+
+    int gen_shared_secret(const DDS::OctetSeq& pub_key) {
+      return algo_->gen_shared_secret(pub_key);
+    }
+
+    const DDS::OctetSeq& get_shared_secret() {
+      return algo_->get_shared_secret();
+    }
+
+    bool cmp_shared_secret(const EllipticCurve& other) {
+      return algo_->cmp_shared_secret(*other.algo_);
+    }
+
+    const char* kagree_algo() const {
+      return algo_->kagree_algo();
+    }
+
+  private:
+
+    ECAlgorithm::unique_ptr algo_;
+  };
+}
+}
 }
 
 #endif
