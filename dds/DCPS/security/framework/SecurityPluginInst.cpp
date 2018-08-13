f789bd5b191b9921cc6d8de5717fac2cdcec248f
Adam Mitz <mitza@ociweb.com>
2018-08-13 09:45:20 -0500

    versioned namespace for new security class

--- dds/DCPS/security/framework/SecurityPluginInst.cpp
+++ dds/DCPS/security/framework/SecurityPluginInst.cpp
@@ -8,6 +8,8 @@
 #include "DCPS/DdsDcps_pch.h" //Only the _pch include should start with DCPS/
 #include "SecurityPluginInst.h"
 
+OPENDDS_BEGIN_VERSIONED_NAMESPACE_DECL
+
 namespace OpenDDS {
 namespace Security {
 
@@ -22,3 +24,5 @@ SecurityPluginInst::~SecurityPluginInst()
 
 }
 }
+
+OPENDDS_END_VERSIONED_NAMESPACE_DECL
