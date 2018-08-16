7988bc13769ab2ac2ace83c27a7b8c6ced5d4036
Adam Mitz <mitza@ociweb.com>
2018-08-16 15:31:18 -0500

    Set up OpenSSL in unit tests (for 1.0.x compat)

--- tests/security/Authentication/AuthenticationTest.cpp
+++ tests/security/Authentication/AuthenticationTest.cpp
@@ -5,6 +5,7 @@
 
 #include "gtest/gtest.h"
 #include "dds/DCPS/security/AuthenticationBuiltInImpl.h"
+#include "dds/DCPS/security/OpenSSL_init.h"
 #include "dds/DCPS/GuidUtils.h"
 
 #include "dds/DCPS/security/SSL/Certificate.h"
@@ -1068,6 +1069,7 @@ TEST_F(AuthenticationTest, GetSharedSecret_InitiatorAndReplier_Match)
 
 int main(int argc, char** argv)
 {
+  openssl_init();
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
