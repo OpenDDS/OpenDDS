28b9eaa31f3d714ec1a733c1ef02e04e37aff5e8
Jose Morato <jose.morato@swisstiming.com>
2018-09-23 13:34:06 +0200

    Wrong entity_refs_ counter when deleting data reader in the TopicDescriptionImpl

--- dds/DCPS/DataReaderImpl.h
+++ dds/DCPS/DataReaderImpl.h
@@ -27,6 +27,7 @@
 #include "Stats_T.h"
 #include "OwnershipManager.h"
 #include "ContentFilteredTopicImpl.h"
+#include "MultiTopicImpl.h"
 #include "GroupRakeData.h"
 #include "CoherentChangeControl.h"
 #include "AssociationData.h"
@@ -475,6 +476,12 @@ public:
 
 #endif
 
+#ifndef OPENDDS_NO_MULTI_TOPIC
+
+  void enable_multi_topic(MultiTopicImpl* mt);
+
+#endif
+
   void update_subscription_params(const DDS::StringSeq& params) const;
 
   typedef OPENDDS_VECTOR(void*) GenericSeq;
@@ -641,6 +648,9 @@ protected:
   TopicDescriptionPtr<ContentFilteredTopicImpl> content_filtered_topic_;
 #endif
 
+#ifndef OPENDDS_NO_MULTI_TOPIC
+  TopicDescriptionPtr<MultiTopicImpl> multi_topic_;
+#endif
 
   /// Is accessing to Group coherent changes ?
   bool coherent_;
@@ -753,11 +763,11 @@ private:
       , liveliness_timer_id_(-1)
     { }
 
-    void check_liveliness()
-    {
-      CheckLivelinessCommand c(this);
-      execute_or_enqueue(c);
-    }
+	void check_liveliness()
+	{
+		CheckLivelinessCommand c(this);
+		execute_or_enqueue(c);
+	}
 
     void cancel_timer()
     {
