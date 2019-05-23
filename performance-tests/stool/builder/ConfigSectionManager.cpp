e4fa1e2e24d3d07e502a39b1b3b787e65c21a321
Timothy Simpson <simpsont@objectcomputing.com>
2019-05-23 10:11:02 -0500

    fixing bug in subsection / key parsing for builder config sections

--- performance-tests/stool/builder/ConfigSectionManager.cpp
+++ performance-tests/stool/builder/ConfigSectionManager.cpp
@@ -10,14 +10,23 @@ namespace Builder {
 
 ConfigSectionManager::ConfigSectionManager(const ConfigSectionSeq& seq) {
   ACE_Configuration_Heap ach;
-  ACE_Configuration_Section_Key sect_key;
   ach.open();
   for (CORBA::ULong i = 0; i < seq.length(); ++i) {
     const ConfigSection& section = seq[i];
-    ach.open_section(ach.root_section(), section.name.in(), 1, sect_key);
+    std::vector<char> buff(std::strlen(section.name.in()) + 1);
+    std::strcpy(&(buff[0]), section.name.in());
+    char* token = std::strtok(&(buff[0]), "/");
+    std::vector<ACE_Configuration_Section_Key> keys;
+    keys.push_back(ach.root_section());
+    while (token != NULL) {
+      ACE_Configuration_Section_Key sect_key;
+      ach.open_section(keys.back(), token, 1, sect_key);
+      keys.push_back(sect_key);
+      token = std::strtok(NULL, "/");
+    }
     for (CORBA::ULong j = 0; j < section.properties.length(); ++j) {
       std::cout << "Adding '" << section.properties[j].name << "' = '" << section.properties[j].value << "' to [" << section.name << "] configuration section" << std::endl;
-      ach.set_string_value(sect_key, section.properties[j].name.in(), section.properties[j].value.in());
+      ach.set_string_value(keys.back(), section.properties[j].name.in(), section.properties[j].value.in());
     }
   }
   TheServiceParticipant->load_configuration(ach, "");
