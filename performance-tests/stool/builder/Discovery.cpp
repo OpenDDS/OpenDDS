50a6b345bb592940b66075b846e662e0735b3f96
Timothy Simpson <simpsont@objectcomputing.com>
2019-06-07 09:19:22 -0500

    adding support for info repo discovery to bench builder / bench, adding example config file (obviously, requires running an instance of DCPSInfoRepo)

--- performance-tests/stool/builder/Discovery.cpp
+++ performance-tests/stool/builder/Discovery.cpp
@@ -2,7 +2,8 @@
 
 #include "dds/DCPS/Service_Participant.h"
 
-#include <dds/DCPS/RTPS/RtpsDiscovery.h>
+#include "dds/DCPS/RTPS/RtpsDiscovery.h"
+#include "dds/DCPS/InfoRepoDiscovery/InfoRepoDiscovery.h"
 
 #include <iostream>
 
@@ -14,6 +15,10 @@ Discovery::Discovery(const DiscoveryConfig& config)
   std::cout << "Creating discovery '" << name_ << "' of type " << config.type << std::endl;
   if (std::string(config.type.in()) == "rtps") {
     OpenDDS::RTPS::RtpsDiscovery_rch disc = OpenDDS::DCPS::make_rch<OpenDDS::RTPS::RtpsDiscovery>(config.name.in());
+    TheServiceParticipant->add_discovery(OpenDDS::DCPS::static_rchandle_cast<OpenDDS::DCPS::Discovery>(disc));
+    TheServiceParticipant->set_repo_domain(config.domain, disc->key());
+  } else if (std::string(config.type.in()) == "repo") {
+    OpenDDS::DCPS::InfoRepoDiscovery_rch disc = OpenDDS::DCPS::make_rch<OpenDDS::DCPS::InfoRepoDiscovery>(config.name.in(), config.ior.in());
     TheServiceParticipant->add_discovery(OpenDDS::DCPS::static_rchandle_cast<OpenDDS::DCPS::Discovery>(disc));
     TheServiceParticipant->set_repo_domain(config.domain, disc->key());
   } else {
