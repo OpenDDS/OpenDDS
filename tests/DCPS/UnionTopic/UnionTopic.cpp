368bac0d35a97c0b3d5f3b14809f106735bc5e0d
Fred Hornsey <hornseyf@objectcomputing.com>
2019-08-05 18:38:29 -0500

    UnionTopic: Fix Static, Require Ownership Profile

--- tests/DCPS/UnionTopic/UnionTopic.cpp
+++ tests/DCPS/UnionTopic/UnionTopic.cpp
@@ -3,6 +3,11 @@
 #include <dds/DCPS/Marked_Default_Qos.h>
 #include <dds/DCPS/WaitSet.h>
 #include <dds/DCPS/SafetyProfileStreams.h>
+#include "dds/DCPS/StaticIncludes.h"
+#ifdef ACE_AS_STATIC_LIBS
+#  include "dds/DCPS/RTPS/RtpsDiscovery.h"
+#  include "dds/DCPS/transport/rtps_udp/RtpsUdp.h"
+#endif
 
 using OpenDDS::DCPS::retcode_to_string;
 
