0c0c5bef94bf2893769cdd2bafb17178f5e54170
Timothy Simpson <simpsont@objectcomputing.com>
2019-08-15 09:34:06 -0500

    updating bng start/stop scripts for new worker params / behavior

--- performance-tests/bench/worker/start_bng_workers.sh
+++ performance-tests/bench/worker/start_bng_workers.sh
@@ -7,12 +7,14 @@ else
 fi
 
 pid_file="/tmp/bench_worker_pids"
+log_file="/tmp/bench_worker_logs"
 
 function gather_pids()
 {
   new_job_started="$(jobs -n)"
   if [ -n "$new_job_started" ];then
       echo $! >> ${pid_file}
+      echo $1 >> ${log_file}
   fi
 }
 
@@ -28,18 +30,38 @@ else
   site_count=$1
 fi
 
-#echo "site count = $site_count"
+echo "site count = $site_count"
 
 jobs &>/dev/null
 exec &>/dev/null
 
-#LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${BNG_ROOT}/lib
-#PATH=${PATH}:${BNG_ROOT}/bin
+do_mutrace=0
+mutrace_args="mutrace --hash-size=2000000"
 
+j=1
 for (( i=1; i<=$site_count; i++ ))
 do
-  ./worker configs/bng_site_config.json true &
-  gather_pids
+  if [ x$do_mutrace = x1 ] && [ x$i = x1 ]; then
+    log_file_name="/tmp/bench_worker_log_$j.txt"
+    $mutrace_args ./worker configs/bng_site_config.json --log $log_file_name &> mutrace_site_output.txt &
+    gather_pids "$log_file_name"
+    j=$( echo "$j + 1" | bc )
+  else
+    log_file_name="/tmp/bench_worker_log_$j.txt"
+    ./worker configs/bng_site_config.json --log $log_file_name &
+    gather_pids "$log_file_name"
+    j=$( echo "$j + 1" | bc )
+  fi
 done
-./worker configs/bng_ops_center_config.json true &
-gather_pids
+
+if [ x$do_mutrace = x1 ]; then
+  log_file_name="/tmp/bench_worker_log_$j.txt"
+  $mutrace_args ./worker configs/bng_ops_center_config.json --log $log_file_name &> mutrace_ops_center_output.txt &
+  gather_pids "$log_file_name"
+  j=$( echo "$j + 1" | bc )
+else
+  log_file_name="/tmp/bench_worker_log_$j.txt"
+  ./worker configs/bng_ops_center_config.json --log $log_file_name &
+  gather_pids "$log_file_name"
+  j=$( echo "$j + 1" | bc )
+fi
