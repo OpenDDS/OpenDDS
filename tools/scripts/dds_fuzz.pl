c8ee13c0d3c0b5458d7055b98facb394f0d76f95
Fred Hornsey <fred@hornsey.us>
2019-08-29 20:26:35 -0500

    Moved gettimeofday check to dds_fuzz.pl

--- tools/scripts/dds_fuzz.pl
+++ tools/scripts/dds_fuzz.pl
@@ -1,9 +1,48 @@
 #!/usr/bin/perl
 
-# Run fuzz.pl (from ACE) passing in the list of tests applicable to OpenDDS
+use strict;
 
 use File::Spec::Functions;
-use strict;
+use File::Find qw/find/;
+
+my $opendds_checks_failed = 0;
+my $dds_root_len = length($ENV{'DDS_ROOT'});
+
+sub process_file {
+  my $full_filename = $_;
+  my $filename = substr($full_filename, $dds_root_len);
+  my $cpp_source = $filename =~ /\.(cpp|h|inl)$/;
+  my $in_dds_dcps = $filename =~ /^\/dds\/DCPS/;
+  if (!($cpp_source && $in_dds_dcps)) {
+    return;
+  }
+
+  my $gettimeofday = 0;
+
+  open(my $fd, $full_filename);
+  while (my $line = <$fd>) {
+    if ($line =~ /gettimeofday|ACE_Time_Value\(\)\.now\(\)/) {
+      $gettimeofday = 1;
+    }
+  }
+  close($fd);
+
+  my $failed_checks = "";
+  if ($gettimeofday) {
+    $failed_checks .=
+      " - ACE_OS::gettimeofday() and \"ACE_Time_Value().now()\" are forbidden in the core libraries.\n" .
+      "   Please use MonotonicTimePoint or SystemTimePoint defined in dds/DCPS/TimeTypes.h.\n";
+  }
+
+  if (length($failed_checks)) {
+    print STDERR "$filename has failed the following checks:\n$failed_checks";
+    $opendds_checks_failed = 1;
+  }
+}
+
+find({wanted => \&process_file, follow => 0, no_chdir => 1}, $ENV{'DDS_ROOT'});
+
+# Run fuzz.pl (from ACE) passing in the list of tests applicable to OpenDDS
 
 my $fuzz = catfile($ENV{'ACE_ROOT'}, 'bin', 'fuzz.pl');
 my $tests = join(',', qw/
@@ -21,4 +60,4 @@ check_for_trailing_whitespace
 # check_for_improper_main_declaration
 # check_for_long_file_names
 
-exit(system("$fuzz -t $tests @ARGV") >> 8);
+exit((system("$fuzz -t $tests @ARGV") >> 8) || $opendds_checks_failed);
