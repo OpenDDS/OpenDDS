0df288ffdaf72419ce264783a09e2612baa7a74a
Fred Hornsey <hornseyf@objectcomputing.com>
2019-09-09 17:47:53 -0500

    Dont Use Monotonic on Apple/Darwin/macOS

--- dds/DCPS/TimeTypes.h
+++ dds/DCPS/TimeTypes.h
@@ -8,6 +8,7 @@
 
 #include "ace/Monotonic_Time_Policy.h"
 #include "ace/Condition_Attributes.h"
+#include "ace/Time_Policy.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 #  pragma once
@@ -22,14 +23,19 @@ namespace DCPS {
  * ACE_Time_Policy that OpenDDS uses for internal timing.
  *
  * ACE_Monotonic_Time_Policy protects OpenDDS from being effected by changes to
- * the system clock to a certain degree. This is what will be passed from the
- * Reactor in handle_timeout.
- *
- * ConditionTime will have to be passed to ACE_Conditions to enable the
- * monotonic behavior.
+ * the system clock to a certain degree.
  */
 ///@{
+#if defined(__APPLE__) && defined(__MACH__)
+/*
+ * As of writing, ACE_Monotonic_Time_Policy doesn't support Darwin systems like
+ * macOS. Use ACE_System_Time_Policy instead, because ACE_Monotonic_Time_Policy
+ * falls back to returing ACE_Time_Value::zero for some reason.
+ */
+typedef ACE_System_Time_Policy MonotonicClock;
+#else
 typedef ACE_Monotonic_Time_Policy MonotonicClock;
+#endif
 inline
 ACE_Time_Value_T<MonotonicClock>
 monotonic_time()
@@ -37,8 +43,13 @@ monotonic_time()
   static MonotonicClock clock;
   return clock();
 }
-typedef ACE_Condition_Attributes_T<MonotonicClock> ConditionTime;
 ///@}
+
+/**
+ * ConditionTime will have to be passed to ACE_Condition for it to intrepret
+ * the ACE_Time_Value* argument of wait as monotonic time.
+ */
+typedef ACE_Condition_Attributes_T<MonotonicClock> ConditionTime;
 
 } // namespace DCPS
 } // namespace OpenDDS
