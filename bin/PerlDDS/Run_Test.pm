429199c97c90698eca73e4ccce84c53c2a1878ab
Mike Kuznetsov <kuznetsovm@objectcomputing.com>
2020-01-21 14:57:30 -0600

    Improved error message wording for missing installed OpenDDS path prefix

--- bin/PerlDDS/Run_Test.pm
+++ bin/PerlDDS/Run_Test.pm
@@ -622,7 +622,9 @@ sub setup_discovery {
     $executable = "$ENV{DDS_ROOT}/bin/DCPSInfoRepo";
     if (!(-e $executable) && !(-e "${executable}.exe")) {
       if (!defined($ENV{OPENDDS_INSTALL_PREFIX})) {
-        print STDERR "ERROR: OPENDDS_INSTALL_PREFIX is not defined. Couldn't find $ENV{DDS_ROOT}/bin/DCPSInfoRepo\n";
+        print STDERR "ERROR: Couldn't find \$DDS_ROOT/bin/DCPSInfoRepo. It " .
+          "needs to be built or \$OPENDDS_INSTALL_PREFIX needs to be defined " .
+          "if OpenDDS is installed.\n";
         $self->{status} = -1;
         return;
       }
