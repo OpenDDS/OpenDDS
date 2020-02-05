cb25809efe7289836e871a863c70fde65716293c
Fred Hornsey <hornseyf@objectcomputing.com>
2020-02-05 12:44:17 -0600

    ParameterListConverter: Fix Indentation
    Fix from changing `int`s to `bool`s in #1453. This is why I don't like
    indentation that's not based on tab stops.

--- dds/DCPS/RTPS/ParameterListConverter.h
+++ dds/DCPS/RTPS/ParameterListConverter.h
@@ -32,150 +32,150 @@ namespace ParameterListConverter {
 
 OpenDDS_Rtps_Export
 bool to_param_list(const DDS::ParticipantBuiltinTopicData& pbtd,
-                  ParameterList& param_list);
+                   ParameterList& param_list);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    DDS::ParticipantBuiltinTopicData& pbtd);
+                     DDS::ParticipantBuiltinTopicData& pbtd);
 
 #ifdef OPENDDS_SECURITY
 // DDS::Security::ParticipantBuiltinTopicData
 
 OpenDDS_Rtps_Export
 bool to_param_list(const DDS::Security::ParticipantBuiltinTopicData& pbtd,
-                  ParameterList& param_list);
+                   ParameterList& param_list);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    DDS::Security::ParticipantBuiltinTopicData& pbtd);
+                     DDS::Security::ParticipantBuiltinTopicData& pbtd);
 
 // DDS::Security::ParticipantBuiltinTopicDataSecure
 
 OpenDDS_Rtps_Export
 bool to_param_list(const DDS::Security::ParticipantBuiltinTopicDataSecure& pbtds,
-                  ParameterList& param_list);
+                   ParameterList& param_list);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    DDS::Security::ParticipantBuiltinTopicDataSecure& pbtds);
+                     DDS::Security::ParticipantBuiltinTopicDataSecure& pbtds);
 #endif
 
 // OpenDDS::RTPS::ParticipantProxy_t
 
 OpenDDS_Rtps_Export
 bool to_param_list(const ParticipantProxy_t& proxy,
-                  ParameterList& param_list);
+                   ParameterList& param_list);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    ParticipantProxy_t& proxy);
+                     ParticipantProxy_t& proxy);
 
 // OpenDDS::RTPS::Duration_t
 
 OpenDDS_Rtps_Export
 bool to_param_list(const Duration_t& duration,
-                  ParameterList& param_list);
+                   ParameterList& param_list);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    Duration_t& duration);
+                     Duration_t& duration);
 
 // OpenDDS::RTPS::SPDPdiscoveredParticipantData
 
 OpenDDS_Rtps_Export
 bool to_param_list(const SPDPdiscoveredParticipantData& participant_data,
-                  ParameterList& param_list);
+                   ParameterList& param_list);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    SPDPdiscoveredParticipantData& participant_data);
+                     SPDPdiscoveredParticipantData& participant_data);
 
 #ifdef OPENDDS_SECURITY
 // OpenDDS::Security::SPDPdiscoveredParticipantData
 
 OpenDDS_Rtps_Export
 bool to_param_list(const OpenDDS::Security::SPDPdiscoveredParticipantData& participant_data,
-                  ParameterList& param_list);
+                   ParameterList& param_list);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    OpenDDS::Security::SPDPdiscoveredParticipantData& participant_data);
+                     OpenDDS::Security::SPDPdiscoveredParticipantData& participant_data);
 #endif
 
 // OpenDDS::DCPS::DiscoveredWriterData
 
 OpenDDS_Rtps_Export
 bool to_param_list(const DCPS::DiscoveredWriterData& writer_data,
-                  ParameterList& param_list,
-                  bool map = false /*map IPV4 to IPV6 addr*/);
+                   ParameterList& param_list,
+                   bool map = false /*map IPV4 to IPV6 addr*/);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                   DCPS::DiscoveredWriterData& writer_data);
+                    DCPS::DiscoveredWriterData& writer_data);
 
 // OpenDDS::DCPS::DiscoveredReaderData
 
 OpenDDS_Rtps_Export
 bool to_param_list(const DCPS::DiscoveredReaderData& reader_data,
-                  ParameterList& param_list,
-                  bool map = false /*map IPV4 to IPV6 addr*/);
+                   ParameterList& param_list,
+                   bool map = false /*map IPV4 to IPV6 addr*/);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    DCPS::DiscoveredReaderData& reader_data);
+                     DCPS::DiscoveredReaderData& reader_data);
 
 #ifdef OPENDDS_SECURITY
 // DDS::Security::EndpointSecurityInfo
 
 OpenDDS_Rtps_Export
 bool to_param_list(const DDS::Security::EndpointSecurityInfo& info,
-                  ParameterList& param_list);
+                   ParameterList& param_list);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    DDS::Security::EndpointSecurityInfo& info);
+                     DDS::Security::EndpointSecurityInfo& info);
 
 // DDS::Security::DataTags data_tags
 
 OpenDDS_Rtps_Export
 bool to_param_list(const DDS::Security::DataTags& tags,
-                  ParameterList& param_list);
+                   ParameterList& param_list);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    DDS::Security::DataTags& tags);
+                     DDS::Security::DataTags& tags);
 
 // DiscoveredPublication_SecurityWrapper
 
 OpenDDS_Rtps_Export
 bool to_param_list(const DiscoveredPublication_SecurityWrapper& wrapper,
-                  ParameterList& param_list,
-                  bool map = false /*map IPV4 to IPV6 addr*/);
+                   ParameterList& param_list,
+                   bool map = false /*map IPV4 to IPV6 addr*/);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    DiscoveredPublication_SecurityWrapper& wrapper);
+                     DiscoveredPublication_SecurityWrapper& wrapper);
 
 // DiscoveredSubscription_SecurityWrapper
 
 OpenDDS_Rtps_Export
 bool to_param_list(const DiscoveredSubscription_SecurityWrapper& wrapper,
-                  ParameterList& param_list,
-                  bool map = false /*map IPV4 to IPV6 addr*/);
+                   ParameterList& param_list,
+                   bool map = false /*map IPV4 to IPV6 addr*/);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    DiscoveredSubscription_SecurityWrapper& wrapper);
+                     DiscoveredSubscription_SecurityWrapper& wrapper);
 
 // Extensions for ICE
 
 OpenDDS_Rtps_Export
 bool to_param_list(const ICE::AgentInfoMap& ai_map,
-                  ParameterList& param_list);
+                   ParameterList& param_list);
 
 OpenDDS_Rtps_Export
 bool from_param_list(const ParameterList& param_list,
-                    ICE::AgentInfoMap& ai_map);
+                     ICE::AgentInfoMap& ai_map);
 #endif
 
 }
