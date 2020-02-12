4ee0e1d8656d8f8441e2f30c50f20094e9d0760e
Mike Kuznetsov <kuznetsovm@objectcomputing.com>
2020-02-12 16:51:52 -0600

    Fixes for Secure Participant Discovery

--- dds/DCPS/RTPS/Sedp.cpp
+++ dds/DCPS/RTPS/Sedp.cpp
@@ -3145,11 +3145,7 @@ Sedp::Writer::write_dcps_participant_secure(const Security::SPDPdiscoveredPartic
     return DDS::RETCODE_ERROR;
   }
 
-  DDS::ReturnCode_t return_code = write_parameter_list(plist, reader, seq_);
-  if (return_code == DDS::RETCODE_OK) {
-    seq_++;
-  }
-  return return_code;
+return write_parameter_list(plist, reader, DCPS::SequenceNumber::SEQUENCENUMBER_UNKNOWN());
 }
 #endif
 
