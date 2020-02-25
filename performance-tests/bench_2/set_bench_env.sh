562e192e32463b3ee6d82de4eec4b9882d536bd7
Timothy Simpson <simpsont@objectcomputing.com>
2020-02-25 12:26:09 -0600

    move bench libout to fix install prefix builds

---
+++ performance-tests/bench_2/set_bench_env.sh
@@ -0,0 +1,3 @@
+export BENCH_ROOT=${DDS_ROOT}/performance-tests/bench_2
+export BENCH_BUILDER_ROOT=${DDS_ROOT}/performance-tests/bench_2
+export LD_LIBRARY_PATH=${LD_LIBRARY_ROOT}:${BENCH_BUILDER_ROOT}/lib:${BENCH_ROOT}/lib
