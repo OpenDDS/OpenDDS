b3ae90cb835634c97c267f8016df5ae7c3b289fa
Adam Mitz <mitza@objectcomputing.com>
2020-05-07 11:50:32 -0500

    extended test

--- java/idl2jni/tests/union/TestUnion.cpp
+++ java/idl2jni/tests/union/TestUnion.cpp
@@ -1,9 +1,19 @@
 #include "UnionTestJC.h"
 
 extern "C" JNIEXPORT jobject JNICALL
-Java_TestUnion_getUnion(JNIEnv* jni, jclass, jobject src)
+Java_TestUnion_getUnionND(JNIEnv* jni, jclass, jobject src)
 {
-  U u;
+  NoDefault u;
+  copyToCxx(jni, u, src);
+  jobject u2;
+  copyToJava(jni, u2, u, true);
+  return u2;
+}
+
+extern "C" JNIEXPORT jobject JNICALL
+Java_TestUnion_getUnionWD(JNIEnv* jni, jclass, jobject src)
+{
+  WithDefault u;
   copyToCxx(jni, u, src);
   jobject u2;
   copyToJava(jni, u2, u, true);
--- java/idl2jni/tests/union/TestUnion.java
+++ java/idl2jni/tests/union/TestUnion.java
@@ -1,6 +1,7 @@
 public class TestUnion {
 
-  private static native U getUnion(U u);
+  private static native NoDefault getUnionND(NoDefault u);
+  private static native WithDefault getUnionWD(WithDefault u);
 
   public static void main(String[] args) {
     String lib = "idl2jni_test_union";
@@ -13,13 +14,39 @@ public class TestUnion {
       lib += "d";
     System.loadLibrary(lib);
 
-    U[] us = {new U(), new U(), new U()};
-    us[0].__default(E.E1);
-    us[1].o((byte) 1);
-    us[2].s((short) 2);
+    NoDefault[] nds = {new NoDefault(), new NoDefault(), new NoDefault(), new NoDefault()};
+    nds[0].__default(E.E1);
+    nds[1].o((byte) 1);
+    nds[2].s((short) 2);
+    nds[3].s(E.E4, (short) 3);
 
-    for (U u : us) {
-      System.out.println("Discriminator: " + getUnion(u).discriminator().value());
+    boolean failed = false;
+    for (NoDefault u1 : nds) {
+      final NoDefault u2 = getUnionND(u1);
+      if (u1.discriminator() != u2.discriminator()) {
+        System.out.println("ERROR: NoDefault expected discriminator " +
+                           u1.discriminator() + " != actual " +
+                           u2.discriminator());
+        failed = true;
+      }
     }
+
+    WithDefault[] wds = {new WithDefault(), new WithDefault(), new WithDefault(), new WithDefault()};
+    wds[0].b(true);
+    wds[1].s(E.E2, "");
+    wds[2].s(E.E3, "");
+    wds[3].s(E.E4, "");
+
+    for (WithDefault u1 : wds) {
+      final WithDefault u2 = getUnionWD(u1);
+      if (u1.discriminator() != u2.discriminator()) {
+        System.out.println("ERROR: WithDefault expected discriminator " +
+                           u1.discriminator() + " != actual " +
+                           u2.discriminator());
+        failed = true;
+      }
+    }
+
+    System.exit(failed ? 1 : 0);
   }
 }
