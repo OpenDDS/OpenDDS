1c8d8d646c13163d77239d1360e2fdf67e525f03
Son Dinh <dinhs@objectcomputing.com>
2020-06-04 17:28:21 -0500

    Use new public interfaces

--- tests/DCPS/UnitTests/XtypesAssignability.cpp
+++ tests/DCPS/UnitTests/XtypesAssignability.cpp
@@ -78,19 +78,19 @@ TEST(PrimitiveTypesTest, Assignable)
   MinimalBitmaskType bitmask;
   bitmask.header = header;
   MinimalTypeObject tob(bitmask);
-  EXPECT_TRUE(test.assignable_primitive(tia, tob));
+  EXPECT_TRUE(test.assignable(tia, TypeObject(tob)));
 
   tia.kind = TK_UINT16;
   tob.bitmask_type.header.common.bit_bound = 16;
-  EXPECT_TRUE(test.assignable_primitive(tia, tob));
+  EXPECT_TRUE(test.assignable(tia, TypeObject(tob)));
 
   tia.kind = TK_UINT32;
   tob.bitmask_type.header.common.bit_bound = 32;
-  EXPECT_TRUE(test.assignable_primitive(tia, tob));
+  EXPECT_TRUE(test.assignable(tia, TypeObject(tob)));
 
   tia.kind = TK_UINT64;
   tob.bitmask_type.header.common.bit_bound = 64;
-  EXPECT_TRUE(test.assignable_primitive(tia, tob));
+  EXPECT_TRUE(test.assignable(tia, TypeObject(tob)));
 }
 
 TEST(PrimitiveTypesTest, NotAssignable)
@@ -167,19 +167,19 @@ TEST(PrimitiveTypesTest, NotAssignable)
   MinimalBitmaskType bitmask;
   bitmask.header = header;
   MinimalTypeObject tob(bitmask);
-  EXPECT_FALSE(test.assignable_primitive(tia, tob));
+  EXPECT_FALSE(test.assignable(tia, TypeObject(tob)));
 
   tia.kind = TK_UINT16;
   tob.bitmask_type.header.common.bit_bound = 17;
-  EXPECT_FALSE(test.assignable_primitive(tia, tob));
+  EXPECT_FALSE(test.assignable(tia, TypeObject(tob)));
 
   tia.kind = TK_UINT32;
   tob.bitmask_type.header.common.bit_bound = 33;
-  EXPECT_FALSE(test.assignable_primitive(tia, tob));
+  EXPECT_FALSE(test.assignable(tia, TypeObject(tob)));
 
   tia.kind = TK_UINT64;
   tob.bitmask_type.header.common.bit_bound = 31;
-  EXPECT_FALSE(test.assignable_primitive(tia, tob));
+  EXPECT_FALSE(test.assignable(tia, TypeObject(tob)));
 }
 
 TEST(StringTypesTest, Assignable)
@@ -188,90 +188,90 @@ TEST(StringTypesTest, Assignable)
   TypeIdentifier tia, tib;
   tia.kind = TI_STRING8_SMALL;
   tib.kind = TI_STRING8_SMALL;
-  EXPECT_TRUE(test.assignable_string(tia, tib));
+  EXPECT_TRUE(test.assignable(tia, tib));
   tib.kind = TI_STRING8_LARGE;
-  EXPECT_TRUE(test.assignable_string(tia, tib));
+  EXPECT_TRUE(test.assignable(tia, tib));
 
   tia.kind = TI_STRING8_LARGE;
-  EXPECT_TRUE(test.assignable_string(tia, tib));
+  EXPECT_TRUE(test.assignable(tia, tib));
   tib.kind = TI_STRING8_SMALL;
-  EXPECT_TRUE(test.assignable_string(tia, tib));
+  EXPECT_TRUE(test.assignable(tia, tib));
 
   tia.kind = TI_STRING16_SMALL;
   tib.kind = TI_STRING16_SMALL;
-  EXPECT_TRUE(test.assignable_string(tia, tib));
+  EXPECT_TRUE(test.assignable(tia, tib));
   tib.kind = TI_STRING16_LARGE;
-  EXPECT_TRUE(test.assignable_string(tia, tib));
+  EXPECT_TRUE(test.assignable(tia, tib));
 
   tia.kind = TI_STRING16_LARGE;
-  EXPECT_TRUE(test.assignable_string(tia, tib));
+  EXPECT_TRUE(test.assignable(tia, tib));
   tib.kind = TI_STRING16_SMALL;
-  EXPECT_TRUE(test.assignable_string(tia, tib));
+  EXPECT_TRUE(test.assignable(tia, tib));
 }
 
 void string_expect_false(const TypeAssignability& test, const TypeIdentifier& tia, TypeIdentifier& tib)
 {
   tib.kind = TK_BOOLEAN;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_BYTE;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_INT16;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_INT32;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_INT64;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_UINT16;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_UINT32;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_UINT64;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_FLOAT32;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_FLOAT64;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_FLOAT128;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_INT8;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_UINT8;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_CHAR8;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TK_CHAR16;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
 
   if (TI_STRING8_SMALL == tia.kind || TI_STRING8_LARGE == tia.kind) {
     tib.kind = TI_STRING16_SMALL;
-    EXPECT_FALSE(test.assignable_string(tia, tib));
+    EXPECT_FALSE(test.assignable(tia, tib));
     tib.kind = TI_STRING16_LARGE;
-    EXPECT_FALSE(test.assignable_string(tia, tib));
+    EXPECT_FALSE(test.assignable(tia, tib));
   } else {
     tib.kind = TI_STRING8_SMALL;
-    EXPECT_FALSE(test.assignable_string(tia, tib));
+    EXPECT_FALSE(test.assignable(tia, tib));
     tib.kind = TI_STRING8_LARGE;
-    EXPECT_FALSE(test.assignable_string(tia, tib));
+    EXPECT_FALSE(test.assignable(tia, tib));
   }
 
   tib.kind = TI_PLAIN_SEQUENCE_SMALL;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TI_PLAIN_SEQUENCE_LARGE;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TI_PLAIN_ARRAY_SMALL;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TI_PLAIN_ARRAY_LARGE;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TI_PLAIN_MAP_SMALL;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TI_PLAIN_MAP_LARGE;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = TI_STRONGLY_CONNECTED_COMPONENT;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = EK_COMPLETE;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
   tib.kind = EK_MINIMAL;
-  EXPECT_FALSE(test.assignable_string(tia, tib));
+  EXPECT_FALSE(test.assignable(tia, tib));
 }
 
 TEST(StringTypesTest, NotAssignable)
