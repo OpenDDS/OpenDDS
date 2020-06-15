dd506ca9ffe6c6cad946a958167ecfdb7276460d
John McCabe <jmccabe@kirintec.com>
2020-06-15 11:26:16 +0100

    Issue #1738 handle SpdpLocalAddress formatted as defined in Dev Guide properly

--- dds/DCPS/RTPS/RtpsDiscovery.cpp
+++ dds/DCPS/RTPS/RtpsDiscovery.cpp
@@ -280,8 +280,8 @@ RtpsDiscovery::Config::discovery_config(ACE_Configuration_Heap& cf)
           config->sedp_local_address(addr);
         } else if (name == "SpdpLocalAddress") {
           ACE_INET_Addr addr;
-          if (addr.set(it->second.c_str())) {
-            ACE_ERROR_RETURN((LM_ERROR,
+          if (addr.set(u_short(0), it->second.c_str())) {
+              ACE_ERROR_RETURN((LM_ERROR,
                               ACE_TEXT("(%P|%t) ERROR: RtpsDiscovery::Config::discovery_config(): ")
                               ACE_TEXT("failed to parse SpdpLocalAddress %C\n"),
                               it->second.c_str()),
