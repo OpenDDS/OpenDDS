8399398ed9b16defcf1fe5c9df8abe0ea3e97400
Clayton Calabrese <calabreseclayton@gmail.com>
2020-06-18 19:52:54 -0500

    Fixes to Alias and adding of alias test. Additional work still needed.

--- tests/DCPS/UnitTests/XtypesExtensibility.cpp
+++ tests/DCPS/UnitTests/XtypesExtensibility.cpp
@@ -156,7 +156,86 @@ TEST(TestDefault, flags_match) {
   EXPECT_TRUE(to_un.minimal.union_type.union_flags & IS_NESTED);
   EXPECT_FALSE(to_un.minimal.union_type.union_flags & IS_AUTOID_HASH);
 }
+//test various alias extensibilities
+TEST(TestAlias, flags_match) {
+  const TypeObject& to_sf = getMinimalTypeObject<extensibility_struct_final_alias_xtag>();
+  EXPECT_TRUE(to_sf.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_FALSE(to_sf.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_FALSE(to_sf.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_sf.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_sf.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
 
+  const TypeObject& to_sa = getMinimalTypeObject<extensibility_struct_appendable_alias_xtag>();
+  EXPECT_FALSE(to_sa.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_TRUE(to_sa.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_FALSE(to_sa.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_sa.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_sa.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
+
+  const TypeObject& to_sm = getMinimalTypeObject<extensibility_struct_mutable_alias_xtag>();
+  EXPECT_FALSE(to_sm.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_FALSE(to_sm.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_TRUE(to_sm.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_sm.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_sm.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
+
+  const TypeObject& to_sd = getMinimalTypeObject<extensibility_struct_default_alias_xtag>();
+  EXPECT_FALSE(to_sd.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_TRUE(to_sd.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_FALSE(to_sd.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_sd.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_sd.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
+
+  const TypeObject& to_uf = getMinimalTypeObject<extensibility_union_final_alias_xtag>();
+  EXPECT_TRUE(to_uf.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_FALSE(to_uf.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_FALSE(to_uf.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_uf.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_uf.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
+
+  const TypeObject& to_ua = getMinimalTypeObject<extensibility_union_appendable_alias_xtag>();
+  EXPECT_FALSE(to_ua.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_TRUE(to_ua.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_FALSE(to_ua.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_ua.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_ua.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
+
+  const TypeObject& to_um = getMinimalTypeObject<extensibility_union_mutable_alias_xtag>();
+  EXPECT_FALSE(to_um.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_FALSE(to_um.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_TRUE(to_um.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_um.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_um.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
+
+  const TypeObject& to_ud = getMinimalTypeObject<extensibility_union_default_alias_xtag>();
+  EXPECT_FALSE(to_ud.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_TRUE(to_ud.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_FALSE(to_ud.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_ud.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_ud.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
+
+  const TypeObject& to_ef = getMinimalTypeObject<extensibility_enum_final_alias_xtag>();
+  EXPECT_TRUE(to_ef.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_FALSE(to_ef.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_FALSE(to_ef.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_ef.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_ef.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
+
+  const TypeObject& to_ea = getMinimalTypeObject<extensibility_enum_appendable_alias_xtag>();
+  EXPECT_FALSE(to_ea.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_TRUE(to_ea.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_FALSE(to_ea.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_ea.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_ea.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
+
+  const TypeObject& to_ed = getMinimalTypeObject<extensibility_enum_default_alias_xtag>();
+  EXPECT_FALSE(to_ed.minimal.alias_type.alias_flags & IS_FINAL);
+  EXPECT_TRUE(to_ed.minimal.alias_type.alias_flags & IS_APPENDABLE);
+  EXPECT_FALSE(to_ed.minimal.alias_type.alias_flags & IS_MUTABLE);
+  EXPECT_FALSE(to_ed.minimal.alias_type.alias_flags & IS_NESTED);
+  EXPECT_FALSE(to_ed.minimal.alias_type.alias_flags & IS_AUTOID_HASH);
+
+}
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
