de670279d111f59db76534730cdfbed6534ea867
Jiang Li <lij@objectcomputing.com>
2020-09-28 12:13:37 -0500

    omit default xcdr1

--- dds/DCPS/RTPS/ParameterListConverter.cpp
+++ dds/DCPS/RTPS/ParameterListConverter.cpp
@@ -746,9 +746,11 @@ void add_DataRepresentationQos(ParameterList& param_list, const DDS::DataReprese
 {
   DDS::DataRepresentationQosPolicy dr_qos;
   dr_qos.value = DCPS::get_effective_data_rep_qos(ids, reader);
-  Parameter param;
-  param.representation(dr_qos);
-  add_param(param_list, param);
+  if (dr_qos.value.lenght() != 1 || dr_qos.value[0] != DDS::XCDR_DATA_REPRESENTATION) {
+    Parameter param;
+    param.representation(dr_qos);
+    add_param(param_list, param);
+  }
 }
 
 bool to_param_list(const DCPS::DiscoveredWriterData& writer_data,
