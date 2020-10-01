9338f872fe0d7f07326bfc366301e97e8b59617d
Justin R. Wilson <wilsonj@objectcomputing.com>
2020-10-01 11:52:02 -0500

    Fix various bugs related to ConnectionRecord processing

---
+++ dds/DCPS/ConnectionRecords.cpp
@@ -0,0 +1,43 @@
+/*
+ *
+ *
+ * Distributed under the OpenDDS License.
+ * See: http://www.opendds.org/license.html
+ */
+
+#include "ConnectionRecords.h"
+
+#include "BuiltInTopicUtils.h"
+#include "DiscoveryBase.h"
+
+OPENDDS_BEGIN_VERSIONED_NAMESPACE_DECL
+
+namespace OpenDDS {
+namespace DCPS {
+
+void WriteConnectionRecords::execute()
+{
+  DDS::DataReader_var d = bit_sub_->lookup_datareader(DCPS::BUILT_IN_CONNECTION_RECORD_TOPIC);
+  if (!d) {
+    return;
+  }
+
+  DCPS::ConnectionRecordDataReaderImpl* dr = dynamic_cast<ConnectionRecordDataReaderImpl*>(d.in());
+  if (!dr) {
+    return;
+  }
+
+  for (ConnectionRecords::const_iterator pos = records_.begin(), limit = records_.end(); pos != limit; ++pos) {
+    if (pos->first) {
+      dr->store_synthetic_data(pos->second, DDS::NEW_VIEW_STATE);
+    } else {
+      const DDS::InstanceHandle_t ih = dr->lookup_instance(pos->second);
+      dr->set_instance_state(ih, DDS::NOT_ALIVE_DISPOSED_INSTANCE_STATE);
+    }
+  }
+}
+
+} // namespace DCPS
+} // namespace OpenDDS
+
+OPENDDS_END_VERSIONED_NAMESPACE_DECL
---
+++ dds/DCPS/ConnectionRecords.h
@@ -0,0 +1,48 @@
+/*
+ *
+ *
+ * Distributed under the OpenDDS License.
+ * See: http://www.opendds.org/license.html
+ */
+
+#ifndef OPENDDS_DCPS_CONNECTION_RECORDS_H
+#define OPENDDS_DCPS_CONNECTION_RECORDS_H
+
+#include "dds/DCPS/JobQueue.h"
+#include "dds/DdsDcpsCoreTypeSupportImpl.h"
+
+OPENDDS_BEGIN_VERSIONED_NAMESPACE_DECL
+
+namespace OpenDDS {
+namespace DCPS {
+
+typedef std::pair<bool, ConnectionRecord> ACR;
+typedef OPENDDS_VECTOR(ACR) ConnectionRecords;
+
+struct WriteConnectionRecords : public DCPS::JobQueue::Job {
+  WriteConnectionRecords(DDS::Subscriber_var bit_sub,
+                         const ConnectionRecords& records)
+    : bit_sub_(bit_sub)
+    , records_(records)
+  {}
+
+  WriteConnectionRecords(DDS::Subscriber_var bit_sub,
+                         bool action,
+                         const ConnectionRecord& record)
+    : bit_sub_(bit_sub)
+  {
+    records_.push_back(std::make_pair(action, record));
+  }
+
+  void execute();
+
+  DDS::Subscriber_var bit_sub_;
+  ConnectionRecords records_;
+};
+
+} // namespace DCPS
+} // namespace OpenDDS
+
+OPENDDS_END_VERSIONED_NAMESPACE_DECL
+
+#endif  /* OPENDDS_DCPS_CONNECTION_RECORDS_H */
