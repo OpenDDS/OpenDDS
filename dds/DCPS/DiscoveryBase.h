b150bbf54a99735bfad549f757b185a5149f843e
Jeremy Adams <adamsj@objectcomputing.com>
2020-10-13 19:36:13 -0500

    remove extra space

--- dds/DCPS/DiscoveryBase.h
+++ dds/DCPS/DiscoveryBase.h
@@ -1341,7 +1341,6 @@ namespace OpenDDS {
           typename OPENDDS_MAP(OPENDDS_STRING, TopicDetails)::iterator td_iter = topics_.find(topic_name);
           if (td_iter != topics_.end()) {
             if (!reader_local) {
-
               td_iter->second.add_pub_sub_xtypes(reader, writer_type_id, reader_type_id, type_lookup_service_);
             }
 
