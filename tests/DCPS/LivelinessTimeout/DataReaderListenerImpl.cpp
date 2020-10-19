3e3817772e6cf43fdc4a3c8ad5f8aecab4dd4d15
Jiang Li <lij@objectcomputing.com>
2020-10-19 13:52:18 -0500

    unused last_si_

--- tests/DCPS/LivelinessTimeout/DataReaderListenerImpl.cpp
+++ tests/DCPS/LivelinessTimeout/DataReaderListenerImpl.cpp
@@ -7,176 +7,65 @@
 #include "tests/DCPS/FooType4/FooDefTypeSupportC.h"
 #include "tests/DCPS/FooType4/FooDefTypeSupportImpl.h"
 
-DataReaderListenerImpl::DataReaderListenerImpl (void) :
-  deadline_missed_(0),
-  last_si_()
+DataReaderListenerImpl::DataReaderListenerImpl()
+  : deadline_missed_(0)
 {
 }
 
-DataReaderListenerImpl::~DataReaderListenerImpl (void)
-  {
+DataReaderListenerImpl::~DataReaderListenerImpl()
+{
+}
+
+void DataReaderListenerImpl::on_liveliness_changed(
+  DDS::DataReader_ptr reader, const DDS::LivelinessChangedStatus& status)
+{
+  ACE_UNUSED_ARG(reader);
+  // if we have gone from active to inactive, then we missed a deadline
+  if((status.alive_count_change < 0) && (status.not_alive_count_change > 0)) {
+    ++deadline_missed_;
   }
+  ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_liveliness_changed: ")
+    ACE_TEXT("active=%d, inactive=%d, activeDelta=%d, inactiveDelta=%d deadline_missed=%d\n"),
+    status.alive_count, status.not_alive_count, status.alive_count_change,
+    status.not_alive_count_change, deadline_missed_));
+}
 
-void DataReaderListenerImpl::on_requested_deadline_missed (
-    ::DDS::DataReader_ptr reader,
-    const ::DDS::RequestedDeadlineMissedStatus & status
-  )
-  {
-    ACE_UNUSED_ARG(reader);
-    ACE_UNUSED_ARG(status);
-
-    ACE_DEBUG((LM_DEBUG,
-      ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_requested_deadline_missed\n")));
+void DataReaderListenerImpl::on_data_available(DDS::DataReader_ptr reader)
+{
+  Xyz::FooDataReader_var foo_dr = Xyz::FooDataReader::_narrow(reader);
+  if (!foo_dr){
+    ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) Xyz::FooDataReader::_narrow failed.\n")));
   }
-
-void DataReaderListenerImpl::on_requested_incompatible_qos (
-    ::DDS::DataReader_ptr reader,
-    const ::DDS::RequestedIncompatibleQosStatus & status
-  )
-  {
-    ACE_UNUSED_ARG(reader);
-    ACE_UNUSED_ARG(status);
-
-    ACE_DEBUG((LM_DEBUG,
-      ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_requested_incompatible_qos\n")));
-  }
-
-void DataReaderListenerImpl::on_liveliness_changed (
-    ::DDS::DataReader_ptr reader,
-    const ::DDS::LivelinessChangedStatus & status
-  )
-  {
-    ACE_UNUSED_ARG(reader);
-    ACE_UNUSED_ARG(status);
-    // if we have gone from active to inactive, then we missed a deadline
-    if((status.alive_count_change < 0) &&
-       (status.not_alive_count_change > 0))
-    {
-      ++deadline_missed_;
+  const int num_ops_per_thread = 100;
+  Xyz::FooSeq foo(num_ops_per_thread);
+  DDS::SampleInfoSeq si(num_ops_per_thread);
+  DDS::ReturnCode_t status = foo_dr->read(foo, si, num_ops_per_thread,
+    DDS::NOT_READ_SAMPLE_STATE, DDS::ANY_VIEW_STATE, DDS::ANY_INSTANCE_STATE);
+  if (status != DDS::RETCODE_OK) {
+    if (status == DDS::RETCODE_NO_DATA) {
+      ACE_OS::fprintf(stderr, "read returned DDS::RETCODE_NO_DATA\n");
+    } else {
+      ACE_OS::fprintf(stderr, "read - Error: %d\n", status);
     }
-
-    ACE_DEBUG((LM_DEBUG,
-      ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_liveliness_changed: ")
-      ACE_TEXT("active=%d, inactive=%d, ")
-      ACE_TEXT("activeDelta=%d, inactiveDelta=%d ")
-      ACE_TEXT("deadline_missed=%d\n"),
-      status.alive_count,
-      status.not_alive_count,
-      status.alive_count_change,
-      status.not_alive_count_change,
-      deadline_missed_
-    ));
   }
+}
 
-void DataReaderListenerImpl::on_subscription_matched (
-    ::DDS::DataReader_ptr reader,
-    const ::DDS::SubscriptionMatchedStatus & status
-  )
-  {
-    ACE_UNUSED_ARG(reader) ;
-    ACE_UNUSED_ARG(status) ;
+void DataReaderListenerImpl::on_requested_deadline_missed(DDS::DataReader_ptr, const DDS::RequestedDeadlineMissedStatus&)
+{
+  ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_requested_deadline_missed\n")));
+}
 
-    ACE_DEBUG((LM_DEBUG,
-      ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_subscription_matched \n")));
-  }
+void DataReaderListenerImpl::on_requested_incompatible_qos(DDS::DataReader_ptr, const DDS::RequestedIncompatibleQosStatus&)
+{
+  ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_requested_incompatible_qos\n")));
+}
 
-  void DataReaderListenerImpl::on_sample_rejected(
-    ::DDS::DataReader_ptr reader,
-    const DDS::SampleRejectedStatus& status
-  )
-  {
-    ACE_UNUSED_ARG(reader) ;
-    ACE_UNUSED_ARG(status) ;
+void DataReaderListenerImpl::on_subscription_matched(DDS::DataReader_ptr, const DDS::SubscriptionMatchedStatus&)
+{
+  ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_subscription_matched \n")));
+}
 
-    ACE_DEBUG((LM_DEBUG,
-      ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_sample_rejected \n")));
-  }
-
-  void DataReaderListenerImpl::on_data_available(
-    ::DDS::DataReader_ptr reader
-  )
-  {
-    ::Xyz::FooDataReader_var foo_dr =
-        ::Xyz::FooDataReader::_narrow(reader);
-
-    if (CORBA::is_nil (foo_dr.in ()))
-      {
-        ACE_ERROR ((LM_ERROR,
-               ACE_TEXT("(%P|%t) ::Xyz::FooDataReader::_narrow failed.\n")));
-      }
-
-    const int num_ops_per_thread = 100;
-    ::Xyz::FooSeq foo(num_ops_per_thread) ;
-    ::DDS::SampleInfoSeq si(num_ops_per_thread) ;
-
-    DDS::ReturnCode_t status  ;
-    status = foo_dr->read(foo, si,
-                          num_ops_per_thread,
-                          ::DDS::NOT_READ_SAMPLE_STATE,
-                          ::DDS::ANY_VIEW_STATE,
-                          ::DDS::ANY_INSTANCE_STATE);
-
-    if (status == ::DDS::RETCODE_OK)
-      {
-        for (CORBA::ULong i = 0 ; i < si.length() ; i++)
-        {
-          last_si_ = si[i] ;
-        }
-      }
-      else if (status == ::DDS::RETCODE_NO_DATA)
-      {
-        ACE_OS::fprintf (stderr, "read returned ::DDS::RETCODE_NO_DATA\n") ;
-      }
-      else
-      {
-        ACE_OS::fprintf (stderr, "read - Error: %d\n", status) ;
-      }
-  }
-
-  void DataReaderListenerImpl::on_sample_lost(
-    ::DDS::DataReader_ptr reader,
-    const DDS::SampleLostStatus& status
-  )
-  {
-    ACE_UNUSED_ARG(reader) ;
-    ACE_UNUSED_ARG(status) ;
-
-  }
-
-  void DataReaderListenerImpl::on_subscription_disconnected (
-    ::DDS::DataReader_ptr reader,
-    const ::OpenDDS::DCPS::SubscriptionDisconnectedStatus & status
-  )
-  {
-    ACE_UNUSED_ARG(reader) ;
-    ACE_UNUSED_ARG(status) ;
-
-  }
-
-  void DataReaderListenerImpl::on_subscription_reconnected (
-    ::DDS::DataReader_ptr reader,
-    const ::OpenDDS::DCPS::SubscriptionReconnectedStatus & status
-  )
-  {
-    ACE_UNUSED_ARG(reader) ;
-    ACE_UNUSED_ARG(status) ;
-
-  }
-
-  void DataReaderListenerImpl::on_subscription_lost (
-    ::DDS::DataReader_ptr reader,
-    const ::OpenDDS::DCPS::SubscriptionLostStatus & status
-  )
-  {
-    ACE_UNUSED_ARG(reader) ;
-    ACE_UNUSED_ARG(status) ;
-
-  }
-
-  void DataReaderListenerImpl::on_budget_exceeded (
-    ::DDS::DataReader_ptr,
-    const ::OpenDDS::DCPS::BudgetExceededStatus&
-    )
-  {
-  }
-
+void DataReaderListenerImpl::on_sample_rejected(DDS::DataReader_ptr, const DDS::SampleRejectedStatus&)
+{
+  ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) DataReaderListenerImpl::on_sample_rejected \n")));
+}
--- tests/DCPS/LivelinessTimeout/DataReaderListenerImpl.h
+++ tests/DCPS/LivelinessTimeout/DataReaderListenerImpl.h
@@ -15,72 +15,28 @@ class DataReaderListenerImpl
   : public virtual OpenDDS::DCPS::LocalObject<OpenDDS::DCPS::DataReaderListener>
 {
 public:
-  DataReaderListenerImpl (void);
+  DataReaderListenerImpl();
+  virtual ~DataReaderListenerImpl();
 
-  virtual ~DataReaderListenerImpl (void);
+  virtual void on_liveliness_changed(DDS::DataReader_ptr reader, const DDS::LivelinessChangedStatus & status);
+  virtual void on_data_available(DDS::DataReader_ptr reader);
 
-  virtual void on_requested_deadline_missed (
-    ::DDS::DataReader_ptr reader,
-    const ::DDS::RequestedDeadlineMissedStatus & status
-  );
+  // Debug info only
+  virtual void on_requested_deadline_missed(DDS::DataReader_ptr, const DDS::RequestedDeadlineMissedStatus&);
+  virtual void on_requested_incompatible_qos(DDS::DataReader_ptr, const DDS::RequestedIncompatibleQosStatus&);
+  virtual void on_subscription_matched(DDS::DataReader_ptr, const DDS::SubscriptionMatchedStatus&);
+  virtual void on_sample_rejected(DDS::DataReader_ptr, const DDS::SampleRejectedStatus&);
 
- virtual void on_requested_incompatible_qos (
-    ::DDS::DataReader_ptr reader,
-    const ::DDS::RequestedIncompatibleQosStatus & status
-  );
+  // No-op implementations
+  virtual void on_sample_lost(DDS::DataReader_ptr, const DDS::SampleLostStatus&) {}
+  virtual void on_subscription_disconnected(DDS::DataReader_ptr, const OpenDDS::DCPS::SubscriptionDisconnectedStatus&) {}
+  virtual void on_subscription_reconnected(DDS::DataReader_ptr, const OpenDDS::DCPS::SubscriptionReconnectedStatus&) {}
+  virtual void on_subscription_lost(DDS::DataReader_ptr, const OpenDDS::DCPS::SubscriptionLostStatus&) {}
+  virtual void on_budget_exceeded(DDS::DataReader_ptr, const OpenDDS::DCPS::BudgetExceededStatus&) {}
 
-  virtual void on_liveliness_changed (
-    ::DDS::DataReader_ptr reader,
-    const ::DDS::LivelinessChangedStatus & status
-  );
-
-  virtual void on_subscription_matched (
-    ::DDS::DataReader_ptr reader,
-    const ::DDS::SubscriptionMatchedStatus & status
-  );
-
-  virtual void on_sample_rejected(
-    ::DDS::DataReader_ptr reader,
-    const DDS::SampleRejectedStatus& status
-  );
-
-  virtual void on_data_available(
-    ::DDS::DataReader_ptr reader
-  );
-
-  virtual void on_sample_lost(
-    ::DDS::DataReader_ptr reader,
-    const DDS::SampleLostStatus& status
-  );
-
-  virtual void on_subscription_disconnected (
-      ::DDS::DataReader_ptr reader,
-      const ::OpenDDS::DCPS::SubscriptionDisconnectedStatus & status
-    );
-
-  virtual void on_subscription_reconnected (
-      ::DDS::DataReader_ptr reader,
-      const ::OpenDDS::DCPS::SubscriptionReconnectedStatus & status
-    );
-
-  virtual void on_subscription_lost (
-      ::DDS::DataReader_ptr reader,
-      const ::OpenDDS::DCPS::SubscriptionLostStatus & status
-    );
-
-  virtual void on_budget_exceeded (
-      ::DDS::DataReader_ptr,
-      const ::OpenDDS::DCPS::BudgetExceededStatus& status
-    );
-
-  unsigned int deadline_missed() const
-  {
-    return deadline_missed_;
-  }
-
+  unsigned int deadline_missed() const { return deadline_missed_; }
 private:
   unsigned int deadline_missed_;
-  ::DDS::SampleInfo last_si_ ;
 };
 
 #endif /* DATAREADER_LISTENER_IMPL_H  */
