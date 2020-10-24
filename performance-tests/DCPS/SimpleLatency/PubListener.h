07043726370b0af6d8290c9054274433c60e1b36
Timothy Simpson <simpsont@objectcomputing.com>
2020-10-24 09:55:23 -0500

    formatting cleanup

--- performance-tests/DCPS/SimpleLatency/PubListener.h
+++ performance-tests/DCPS/SimpleLatency/PubListener.h
@@ -16,25 +16,25 @@ class AckDataReaderListenerImpl
   : public virtual OpenDDS::DCPS::LocalObject<DDS::DataReaderListener>
 {
 public:
-  AckDataReaderListenerImpl (CORBA::Long size);
+  AckDataReaderListenerImpl(CORBA::Long size);
 
-  void init (DDS::DataReader_ptr dr, DDS::DataWriter_ptr dw, bool use_zero_copy_read);
+  void init(DDS::DataReader_ptr dr, DDS::DataWriter_ptr dw, bool use_zero_copy_read);
 
-  virtual ~AckDataReaderListenerImpl (void);
+  virtual ~AckDataReaderListenerImpl(void);
 
-  virtual void on_requested_deadline_missed (
+  virtual void on_requested_deadline_missed(
     DDS::DataReader_ptr reader,
     const DDS::RequestedDeadlineMissedStatus & status);
 
-  virtual void on_requested_incompatible_qos (
+  virtual void on_requested_incompatible_qos(
     DDS::DataReader_ptr reader,
     const DDS::RequestedIncompatibleQosStatus & status);
 
-  virtual void on_liveliness_changed (
+  virtual void on_liveliness_changed(
     DDS::DataReader_ptr reader,
     const DDS::LivelinessChangedStatus & status);
 
-  virtual void on_subscription_matched (
+  virtual void on_subscription_matched(
     DDS::DataReader_ptr reader,
     const DDS::SubscriptionMatchedStatus & status);
 
@@ -61,9 +61,9 @@ private:
   DDSPerfTest::PubMessageDataWriter_var dw_servant_;
   DDS::InstanceHandle_t handle_;
   CORBA::Long sample_num_;
-  int   done_;
+  int done_;
   ACE_High_Res_Timer timer_;
-  bool  use_zero_copy_;
+  bool use_zero_copy_;
 };
 
 #endif /* DATAREADER_LISTENER_IMPL  */
