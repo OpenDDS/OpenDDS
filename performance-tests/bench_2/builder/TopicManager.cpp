9be95894ae3a77f11c0dc883f5bad9bdfb5eca25
Mike Kuznetsov <kuznetsovm@objectcomputing.com>
2020-11-10 11:29:19 -0600

    Fixes

--- performance-tests/bench_2/builder/TopicManager.cpp
+++ performance-tests/bench_2/builder/TopicManager.cpp
@@ -2,7 +2,8 @@
 
 namespace Builder {
 
-TopicManager::TopicManager(const TopicConfigSeq& configs, DDS::DomainParticipant_var& participant) {
+TopicManager::TopicManager(const TopicConfigSeq& configs, DDS::DomainParticipant_var& participant)
+{
   for (CORBA::ULong i = 0; i < configs.length(); ++i) {
     std::shared_ptr<Topic> topic = std::make_shared<Topic>(configs[i], participant, content_filtered_topics_);
     if (topic) {
@@ -11,7 +12,8 @@ TopicManager::TopicManager(const TopicConfigSeq& configs, DDS::DomainParticipant
   }
 }
 
-std::shared_ptr<Topic> TopicManager::get_topic_by_name(const std::string& name) const {
+std::shared_ptr<Topic> TopicManager::get_topic_by_name(const std::string& name) const
+{
   std::shared_ptr<Topic> result;
   auto it = topics_.find(name);
   if (it != topics_.end()) {
@@ -20,7 +22,8 @@ std::shared_ptr<Topic> TopicManager::get_topic_by_name(const std::string& name) 
   return result;
 }
 
-DDS::ContentFilteredTopic_var TopicManager::get_content_filtered_topic_by_name(const std::string& name) const {
+DDS::ContentFilteredTopic_var TopicManager::get_content_filtered_topic_by_name(const std::string& name) const
+{
   DDS::ContentFilteredTopic_var result;
   auto it = content_filtered_topics_.find(name);
   if (it != content_filtered_topics_.end()) {
@@ -29,7 +32,8 @@ DDS::ContentFilteredTopic_var TopicManager::get_content_filtered_topic_by_name(c
   return result;
 }
 
-bool TopicManager::enable(bool throw_on_error) {
+bool TopicManager::enable(bool throw_on_error)
+{
   bool result = true;
   for (auto it = topics_.begin(); it != topics_.end(); ++it) {
     result &= it->second->enable(throw_on_error);
--- performance-tests/bench_2/builder/TopicManager.h
+++ performance-tests/bench_2/builder/TopicManager.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "dds/DdsDcpsTopicC.h"
+
 #include "Topic.h"
 
 #include <map>
