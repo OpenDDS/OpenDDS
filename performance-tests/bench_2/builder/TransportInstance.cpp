f9fbc552a3c2b2916bf4e56949ac280f864e6768
Timothy Simpson <simpsont@objectcomputing.com>
2020-11-23 22:50:10 -0600

    Update Bench ci_mixed scenario workers to cleanly use dual transports and avoid multicast address collisions

--- performance-tests/bench_2/builder/TransportInstance.cpp
+++ performance-tests/bench_2/builder/TransportInstance.cpp
@@ -7,17 +7,17 @@ namespace Builder {
 TransportInstance::TransportInstance(const TransportInstanceConfig& config)
   : name_(config.name.in())
 {
-  Log::log() << "Checking for instance config '" << name_ << "'" << std::endl;
+  Log::log() << "Checking for transport config '" << name_ << "'" << std::endl;
   config_ = OpenDDS::DCPS::TransportRegistry::instance()->get_config(config.name.in());
   if (!config_) {
-    Log::log() << "Creating instance config '" << name_ << "'" << std::endl;
+    Log::log() << "Creating transport config '" << name_ << "'" << std::endl;
     config_ = OpenDDS::DCPS::TransportRegistry::instance()->create_config(config.name.in());
-    Log::log() << "Checking for instance '" << name_ << "'" << std::endl;
+    Log::log() << "Checking for transport instance '" << name_ << "'" << std::endl;
     inst_ = OpenDDS::DCPS::TransportRegistry::instance()->get_inst(config.name.in());
     if (!inst_) {
       inst_ = OpenDDS::DCPS::TransportRegistry::instance()->create_inst(config.name.in(), config.type.in());
       if (inst_) {
-        Log::log() << "Creating instance '" << name_ << "' of type " << config.type << std::endl;
+        Log::log() << "Creating transport instance '" << name_ << "' of type " << config.type << std::endl;
       } else {
         throw std::runtime_error("unsupported transport instance type");
       }
@@ -25,9 +25,17 @@ TransportInstance::TransportInstance(const TransportInstanceConfig& config)
     config_->instances_.push_back(inst_);
     OpenDDS::DCPS::TransportRegistry::instance()->domain_default_config(config.domain, config_);
   } else {
-    Log::log() << "Checking for instance '" << name_ << "'" << std::endl;
+    Log::log() << "Checking for transport instance '" << name_ << "'" << std::endl;
     inst_ = OpenDDS::DCPS::TransportRegistry::instance()->get_inst(config.name.in());
-    if (inst_ && std::string(config.type.in()) == inst_->transport_type_) {
+    if (!inst_) {
+      inst_ = OpenDDS::DCPS::TransportRegistry::instance()->create_inst(config.name.in(), config.type.in());
+      if (inst_) {
+        Log::log() << "Creating transport instance '" << name_ << "' of type " << config.type << std::endl;
+      }
+      else {
+        throw std::runtime_error("unsupported transport instance type");
+      }
+    } else if (std::string(config.type.in()) == inst_->transport_type_) {
       Log::log() << "Using existing instance '" << name_ << "' of type " << inst_->transport_type_ << std::endl;
     } else {
       throw std::runtime_error("mismatched transport instance type");
