2791ab24da14d0f3b8972521c204ea055afad75a
Clayton Calabrese <calabreseclayton@gmail.com>
2020-12-02 13:36:23 -0600

    comment explainign static lock and spacing fixes

--- dds/DCPS/RTPS/Spdp.cpp
+++ dds/DCPS/RTPS/Spdp.cpp
@@ -2976,6 +2976,7 @@ Spdp::SpdpTransport::join_multicast_group(const DCPS::NetworkInterface& nic,
                  all_interfaces ? "all interfaces" : nic.name().c_str()));
     }
 
+    // Windows 7 has an issue with different threads concurrently calling join for ipv6
     static ACE_Thread_Mutex ipv6_static_lock;
     ACE_GUARD(ACE_Thread_Mutex, g3, ipv6_static_lock);
     if (0 == multicast_ipv6_socket_.join(multicast_ipv6_address_, 1, all_interfaces ? 0 : ACE_TEXT_CHAR_TO_TCHAR(nic.name().c_str()))) {
@@ -3000,8 +3001,8 @@ Spdp::SpdpTransport::join_multicast_group(const DCPS::NetworkInterface& nic,
     }
   }
 #endif
-
 }
+
 void
 Spdp::SpdpTransport::leave_multicast_group(const DCPS::NetworkInterface& nic)
 {
