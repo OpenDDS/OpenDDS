777da769a1ef77334d2ba8fb290dbe50ac82ae42
Timothy Simpson <simpsont@objectcomputing.com>
2020-12-03 17:01:10 -0600

    add sm20 to run_test.pl

--- performance-tests/bench_2/run_test.pl
+++ performance-tests/bench_2/run_test.pl
@@ -57,6 +57,10 @@ elsif ($test->flag('sm10')) {
   $tc_opts .= " showtime_mixed_10 --override-start-time 25";
   $is_rtps_disc = 1;
 }
+elsif ($test->flag('sm20')) {
+  $tc_opts .= " showtime_mixed_20 --override-start-time 35";
+  $is_rtps_disc = 1;
+}
 elsif ($test->flag('sm30')) {
   $tc_opts .= " showtime_mixed_30 --override-start-time 45";
   $is_rtps_disc = 1;
