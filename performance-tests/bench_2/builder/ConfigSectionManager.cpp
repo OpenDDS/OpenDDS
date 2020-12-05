474f596bfd80c1a0df1005ad5fedee1c53edf107
Timothy Simpson <simpsont@objectcomputing.com>
2020-12-05 14:55:51 -0600

    avoid considering or displaying empty name / value config pairs

--- performance-tests/bench_2/builder/ConfigSectionManager.cpp
+++ performance-tests/bench_2/builder/ConfigSectionManager.cpp
@@ -28,13 +28,15 @@ ConfigSectionManager::ConfigSectionManager(const ConfigSectionSeq& seq) {
       }
     }
     for (CORBA::ULong j = 0; j < section.properties.length(); ++j) {
-      Log::log()
-        << "Adding '" << section.properties[j].name
-        <<"' = '" << section.properties[j].value
-        << "' to [" << section.name << "] configuration section" << std::endl;
-      ACE_TString tname(ACE_TEXT_CHAR_TO_TCHAR(section.properties[j].name.in()));
-      ACE_TString tvalue(ACE_TEXT_CHAR_TO_TCHAR(section.properties[j].value.in()));
-      ach.set_string_value(keys.back(), tname.c_str(), tvalue.c_str());
+      if (!std::string(section.properties[j].name).empty()) {
+        Log::log()
+          << "Adding '" << section.properties[j].name
+          <<"' = '" << section.properties[j].value
+          << "' to [" << section.name << "] configuration section" << std::endl;
+        ACE_TString tname(ACE_TEXT_CHAR_TO_TCHAR(section.properties[j].name.in()));
+        ACE_TString tvalue(ACE_TEXT_CHAR_TO_TCHAR(section.properties[j].value.in()));
+        ach.set_string_value(keys.back(), tname.c_str(), tvalue.c_str());
+      }
     }
   }
   TheServiceParticipant->load_configuration(ach, ACE_TEXT(""));
