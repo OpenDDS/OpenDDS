ade8660d61ea55744d078f8079e5f656a9361b01
Mike Kuznetsov <kuznetsovm@objectcomputing.com>
2020-12-10 22:38:13 -0600

    Fixes

--- performance-tests/bench_2/test_controller/main.cpp
+++ performance-tests/bench_2/test_controller/main.cpp
@@ -6,7 +6,7 @@
 #include <json_conversion.h>
 
 #ifdef __GNUC__
-#pragma GCC diagnostic push
+#  pragma GCC diagnostic push
 #  if defined(__has_warning)
 #    if __has_warning("-Wclass-memaccess")
 #      pragma GCC diagnostic ignored "-Wclass-memaccess"
