b213327bd343a2b372f1ab56767779fa1508c4d4
Jiang Li <lij@objectcomputing.com>
2020-12-11 10:10:59 -0600

    removed ACE_ERROR_RETURN so detach_condition can be called

--- tests/Utils/StatusMatching.h
+++ tests/Utils/StatusMatching.h
@@ -9,13 +9,8 @@
 
 namespace Utils {
 
-enum CmpOp {
-  LT,
-  LTE,
-  EQ,
-  GTE,
-  GT
-};
+enum CmpOp { LT, LTE, EQ, GTE, GT };
+const DDS::Duration_t wake_interval = { 3, 0 };
 
 template <typename Entity>
 int wait_match(const Entity& entity, unsigned int count, CmpOp cmp = EQ);
@@ -25,52 +20,39 @@ int wait_match<DDS::DataWriter_var>(const DDS::DataWriter_var& writer, unsigned 
 {
   DDS::StatusCondition_var condition = writer->get_statuscondition();
   condition->set_enabled_statuses(DDS::PUBLICATION_MATCHED_STATUS);
-
   DDS::WaitSet_var ws(new DDS::WaitSet);
   ws->attach_condition(condition);
 
-  DDS::ReturnCode_t stat;
-
-  DDS::ConditionSeq conditions;
   DDS::PublicationMatchedStatus ms = { 0, 0, 0, 0, 0 };
-
-  const DDS::Duration_t wake_interval = { 3, 0 };
-
-  bool stop = false;
-  while (!stop) {
-    stat = writer->get_publication_matched_status(ms);
-    if (stat != DDS::RETCODE_OK) {
-      ACE_ERROR_RETURN((
-                  LM_ERROR,
-                  ACE_TEXT("(%P|%t) ERROR: %N:%l: wait_match() -")
-                  ACE_TEXT(" get_publication_matched_status failed!\n")),
-                 -1);
-    } else {
+  DDS::ConditionSeq conditions;
+  bool match = false;
+  while (!match) {
+    if (writer->get_publication_matched_status(ms) == DDS::RETCODE_OK) {
       if (cmp == LT && ms.current_count < static_cast<CORBA::Long>(num_readers)) {
-        stop = true;
+        match = true;
       } else if (cmp == LTE && ms.current_count <= static_cast<CORBA::Long>(num_readers)) {
-        stop = true;
+        match = true;
       } else if (cmp == EQ && ms.current_count == static_cast<CORBA::Long>(num_readers)) {
-        stop = true;
+        match = true;
       } else if (cmp == GTE && ms.current_count >= static_cast<CORBA::Long>(num_readers)) {
-        stop = true;
+        match = true;
       } else if (cmp == GT && ms.current_count > static_cast<CORBA::Long>(num_readers)) {
-        stop = true;
+        match = true;
       } else {
-        // wait for a change
-        stat = ws->wait(conditions, wake_interval);
-        if ((stat != DDS::RETCODE_OK) && (stat != DDS::RETCODE_TIMEOUT)) {
-          ACE_ERROR_RETURN((LM_ERROR,
-                            ACE_TEXT("(%P|%t) ERROR: %N:%l: wait_match() -")
-                            ACE_TEXT(" wait failed!\n")),
-                           -1);
+        DDS::ReturnCode_t r = ws->wait(conditions, wake_interval);
+        if ((r != DDS::RETCODE_OK) && (r != DDS::RETCODE_TIMEOUT)) {
+          ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) ERROR: %N:%l: wait_match() - wait returned %d\n"), r));
+          break;
         }
       }
+    } else {
+      ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) ERROR: %N:%l: wait_match() - get_publication_matched_status failed!\n")));
+      break;
     }
   }
 
   ws->detach_condition(condition);
-  return 0;
+  return match ? 0 : -1;
 }
 
 template <>
@@ -78,52 +60,39 @@ int wait_match<DDS::DataReader_var>(const DDS::DataReader_var& reader, unsigned 
 {
   DDS::StatusCondition_var condition = reader->get_statuscondition();
   condition->set_enabled_statuses(DDS::SUBSCRIPTION_MATCHED_STATUS);
-
   DDS::WaitSet_var ws(new DDS::WaitSet);
   ws->attach_condition(condition);
 
-  DDS::ReturnCode_t stat;
-
-  DDS::ConditionSeq conditions;
   DDS::SubscriptionMatchedStatus ms = { 0, 0, 0, 0, 0 };
-
-  const DDS::Duration_t wake_interval = { 3, 0 };
-
-  bool stop = false;
-  while (!stop) {
-    stat = reader->get_subscription_matched_status(ms);
-    if (stat != DDS::RETCODE_OK) {
-      ACE_ERROR_RETURN((
-                  LM_ERROR,
-                  ACE_TEXT("(%P|%t) ERROR: %N:%l: wait_match() -")
-                  ACE_TEXT(" get_subscription_matched_status failed!\n")),
-                 -1);
-    } else {
+  DDS::ConditionSeq conditions;
+  bool match = false;
+  while (!match) {
+    if (reader->get_subscription_matched_status(ms) == DDS::RETCODE_OK) {
       if (cmp == LT && ms.current_count < static_cast<CORBA::Long>(num_writers)) {
-        stop = true;
+        match = true;
       } else if (cmp == LTE && ms.current_count <= static_cast<CORBA::Long>(num_writers)) {
-        stop = true;
+        match = true;
       } else if (cmp == EQ && ms.current_count == static_cast<CORBA::Long>(num_writers)) {
-        stop = true;
+        match = true;
       } else if (cmp == GTE && ms.current_count >= static_cast<CORBA::Long>(num_writers)) {
-        stop = true;
+        match = true;
       } else if (cmp == GT && ms.current_count > static_cast<CORBA::Long>(num_writers)) {
-        stop = true;
+        match = true;
       } else {
-        // wait for a change
-        stat = ws->wait(conditions, wake_interval);
-        if ((stat != DDS::RETCODE_OK) && (stat != DDS::RETCODE_TIMEOUT)) {
-          ACE_ERROR_RETURN((LM_ERROR,
-                            ACE_TEXT("(%P|%t) ERROR: %N:%l: wait_match() -")
-                            ACE_TEXT(" wait failed!\n")),
-                           -1);
+        DDS::ReturnCode_t r = ws->wait(conditions, wake_interval);
+        if ((r != DDS::RETCODE_OK) && (r != DDS::RETCODE_TIMEOUT)) {
+          ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) ERROR: %N:%l: wait_match() - wait returned %d\n"), r));
+          break;
         }
       }
+    } else {
+      ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) ERROR: %N:%l: wait_match() - get_subscription_matched_status failed!\n")));
+      break;
     }
   }
 
   ws->detach_condition(condition);
-  return 0;
+  return match ? 0 : -1;
 }
 
 }
