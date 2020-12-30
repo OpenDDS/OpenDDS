92a74e2df05e0f983628a0376035cd993ff142c2
Clayton Calabrese <calabreseclayton@gmail.com>
2020-12-30 16:22:24 -0600

    Android-specific compilation

---
+++ java/dds/OpenDDS/DCPS/FakeAndroidNetworkCallback.java
@@ -0,0 +1,11 @@
+/*
+ *
+ *
+ * Distributed under the OpenDDS License.
+ * See: http://www.opendds.org/license.html
+ */
+
+// Empty file to build on non-android platforms
+
+package OpenDDS.DCPS;
+
