c07f8f4adcc1e098bd0df3cfbc3d5384975c6083
Johnny Willemsen <jwillemsen@remedy.nl>
2021-01-05 12:32:35 +0100

    Make use of std::numeric_limits when we have C++11 support
        * dds/DCPS/TimeDuration.cpp:
        * dds/DCPS/TimePoint_T.cpp:

--- dds/DCPS/TimeDuration.cpp
+++ dds/DCPS/TimeDuration.cpp
@@ -1,6 +1,8 @@
 #include "DCPS/DdsDcps_pch.h" //Only the _pch include should start with DCPS/
-
 #include "TimeDuration.h"
+#ifdef ACE_HAS_CPP11
+#  include <limits>
+#endif /* ACE_HAS_CPP11*/
 
 OPENDDS_BEGIN_VERSIONED_NAMESPACE_DECL
 
@@ -8,7 +10,11 @@ namespace OpenDDS {
 namespace DCPS {
 
 const TimeDuration TimeDuration::zero_value(0, 0);
+#ifdef ACE_HAS_CPP11
+const TimeDuration TimeDuration::max_value(std::numeric_limits<time_t>::max(), ACE_ONE_SECOND_IN_USECS - 1);
+#else
 const TimeDuration TimeDuration::max_value(ACE_Numeric_Limits<time_t>::max(), ACE_ONE_SECOND_IN_USECS - 1);
+#endif /* ACE_HAS_CPP11 */
 
 }
 }
--- dds/DCPS/TimePoint_T.cpp
+++ dds/DCPS/TimePoint_T.cpp
@@ -1,4 +1,7 @@
 #include "TimePoint_T.h"
+#ifdef ACE_HAS_CPP11
+#  include <limits>
+#endif /* ACE_HAS_CPP11*/
 
 OPENDDS_BEGIN_VERSIONED_NAMESPACE_DECL
 
@@ -8,8 +11,13 @@ namespace DCPS {
 template<typename AceClock>
 const TimePoint_T<AceClock> TimePoint_T<AceClock>::zero_value(ACE_Time_Value(0, 0));
 
+#ifdef ACE_HAS_CPP11
+template<typename AceClock>
+const TimePoint_T<AceClock> TimePoint_T<AceClock>::max_value(ACE_Time_Value(std::numeric_limits<time_t>::max(), ACE_ONE_SECOND_IN_USECS - 1));
+#else
 template<typename AceClock>
 const TimePoint_T<AceClock> TimePoint_T<AceClock>::max_value(ACE_Time_Value(ACE_Numeric_Limits<time_t>::max(), ACE_ONE_SECOND_IN_USECS - 1));
+#endif /* ACE_HAS_CPP11 */
 
 template<typename AceClock>
 AceClock TimePoint_T<AceClock>::clock;
