f696b6e46787970b1d2201ce24fbf5d1c56eb3a1
Johnny Willemsen <jwillemsen@remedy.nl>
2021-01-07 14:26:25 +0100

    Use ace/Version.h, the OCI version of VERSION.txt has too much content
        * tools/scripts/show_build_config.pl:

--- tools/scripts/show_build_config.pl
+++ tools/scripts/show_build_config.pl
@@ -35,7 +35,7 @@ sub show_config {
   my $opendds_version = ($dds_root eq '.' ? '' : "$dds_root/") . 'VERSION.txt';
   show_file($opendds_version);
 
-  show_file("$ace_root/VERSION.txt");
+  show_file("$ace_root/ace/Version.h");
 
   show_file("$ace_root/bin/MakeProjectCreator/config/default.features");
   show_file("$ace_root/include/makeinclude/platform_macros.GNU");
