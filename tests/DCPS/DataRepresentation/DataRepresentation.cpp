0210efa554037afbef5e8efad7314f5c93cda8fd
Clayton Calabrese <calabreseclayton@gmail.com>
2021-01-19 12:53:32 -0600

    Test was failing on static builds due to lack of these includes

--- tests/DCPS/DataRepresentation/DataRepresentation.cpp
+++ tests/DCPS/DataRepresentation/DataRepresentation.cpp
@@ -17,6 +17,12 @@
 #include <dds/DdsDcpsInfrastructureC.h>
 #include <dds/DCPS/DCPS_Utils.h>
 
+#ifdef ACE_AS_STATIC_LIBS
+#include "dds/DCPS/InfoRepoDiscovery/InfoRepoDiscovery.h"
+#include "dds/DCPS/RTPS/RtpsDiscovery.h"
+#include "dds/DCPS/transport/rtps_udp/RtpsUdp.h"
+#endif
+
 #include <vector>
 #include <sstream>
 
