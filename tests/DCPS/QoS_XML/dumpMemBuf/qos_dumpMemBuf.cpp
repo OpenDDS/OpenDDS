fa12c722ffa16e3c6739c63b40b49fd787a13a2c
Danilo C. Zanella <danilo.zanella@iag.usp.br>
2021-02-23 08:38:11 +0000

    Added dumpMemBuf test to check new feature of QOS_XML_Handler library parsing xml from memory buffer

---
+++ tests/DCPS/QoS_XML/dumpMemBuf/qos_dumpMemBuf.cpp
@@ -0,0 +1,136 @@
+#include <fstream>
+#include <streambuf>
+#include "dds/DCPS/QOS_XML_Handler/XML_MemBuf_Intf.h"
+#include "dds/DdsDcpsC.h"
+
+int ACE_TMAIN (int, ACE_TCHAR *[])
+{
+  int retval = 0;
+  try
+  {
+    // File name and profile name in respective file
+    const char * fileName = "qos.xml";
+    const char * profileName = "TestProfile";
+    const char * topicName = "TopicName";
+
+    // read file and create string
+    std::ifstream ifs(fileName);
+    std::stringstream buffer;
+    buffer << ifs.rdbuf();
+
+    OpenDDS::DCPS::QOS_XML_MemBuf_Handler xml_membuf;
+    // Add env variable and path to search schemas
+    xml_membuf.add_search_path("DDS_ROOT","/docs/schema/");
+    // initialize and parse XML
+    DDS::ReturnCode_t const retcode = xml_membuf.init(buffer.str().c_str());
+
+    if (retcode == DDS::RETCODE_OK)
+    {
+      DDS::ReturnCode_t retcode_qos;
+      ::DDS::DataWriterQos dw_qos;
+      retcode_qos = xml_membuf.get_datawriter_qos (
+          dw_qos,
+          profileName,
+          topicName);
+      if (retcode_qos != DDS::RETCODE_OK)
+      {
+        ACE_ERROR ((LM_ERROR, "MAIN - "
+              "get_datawriter_qos return an error. Retcode <%d>\n",
+              retcode_qos));
+        ++retval;
+      }
+      if (dw_qos.history.kind != ::DDS::KEEP_ALL_HISTORY_QOS)
+      {
+        ACE_ERROR ((LM_ERROR, "MAIN - "
+              "get_datawriter_qos return an invalid history kind.\n"));
+        ++retval;
+      }
+      if (dw_qos.history.depth != 5)
+      {
+        ACE_ERROR ((LM_ERROR, "MAIN - "
+              "get_datawriter_qos return an invalid history depth.\n"));
+        ++retval;
+      }
+
+      ::DDS::DataReaderQos dr_qos;
+      retcode_qos = xml_membuf.get_datareader_qos (
+          dr_qos,
+          profileName,
+          topicName);
+      if (retcode_qos != DDS::RETCODE_OK)
+      {
+        ACE_ERROR ((LM_ERROR, "MAIN - "
+              "get_datareader_qos return an error. Retcode <%d>\n",
+              retcode_qos));
+        ++retval;
+      }
+
+      ::DDS::TopicQos tp_qos;
+      retcode_qos = xml_membuf.get_topic_qos (
+          tp_qos,
+          profileName,
+          topicName);
+      if (retcode_qos != DDS::RETCODE_OK)
+      {
+        ACE_ERROR ((LM_ERROR, "MAIN - "
+              "get_topic_qos return an error. Retcode <%d>\n",
+              retcode_qos));
+        ++retval;
+      }
+
+      ::DDS::PublisherQos pub_qos;
+      retcode_qos = xml_membuf.get_publisher_qos (
+          pub_qos,
+          profileName);
+      if (retcode_qos != DDS::RETCODE_OK)
+      {
+        ACE_ERROR ((LM_ERROR, "MAIN - "
+              "get_publisher_qos return an error. Retcode <%d>\n",
+              retcode_qos));
+        ++retval;
+      }
+
+      ::DDS::SubscriberQos sub_qos;
+      retcode_qos = xml_membuf.get_subscriber_qos (
+          sub_qos,
+          profileName);
+      if (retcode_qos != DDS::RETCODE_OK)
+      {
+        ACE_ERROR ((LM_ERROR, "MAIN - "
+              "get_subscriber_qos return an error. Retcode <%d>\n",
+              retcode_qos));
+        ++retval;
+      }
+
+      ::DDS::DomainParticipantQos dp_qos;
+      retcode_qos = xml_membuf.get_participant_qos (
+          dp_qos,
+          profileName);
+      if (retcode_qos != DDS::RETCODE_OK)
+      {
+        ACE_ERROR ((LM_ERROR, "MAIN - "
+              "get_participant_qos return an error. Retcode <%d>\n",
+              retcode_qos));
+        ++retval;
+      }
+    }
+    else
+    {
+      ACE_ERROR ((LM_ERROR, "MAIN - Init return an error. Retcode <%d>\n",
+            retcode));
+      ++retval;
+    }
+  }
+  catch (const CORBA::Exception& ex)
+  {
+    ex._tao_print_exception ("QOS_Dump::main\n");
+    return -1;
+  }
+  catch (...)
+  {
+    ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unexpected exception\n")));
+    return 1;
+  }
+
+  return retval;
+}
---
+++ tests/DCPS/QoS_XML/dumpMemBuf/run_test.pl
@@ -0,0 +1,42 @@
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+     & eval 'exec perl -S $0 $argv:q'
+     if 0;
+
+# -*- perl -*-
+
+use lib "$ENV{ACE_ROOT}/bin";
+use lib "$ENV{DDS_ROOT}/bin";
+use PerlDDS::Run_Test;
+use strict;
+
+my $test = new PerlDDS::TestFramework();
+$test->process("qos_dumpMemBufTest", 'qos_dumpMemBufTest');
+$test->start_process("qos_dumpMemBufTest");
+my $result = $test->finish(60);
+if ($result != 0) {
+  print STDERR "ERROR: test returned $result\n";
+  exit 1;
+}
+
+exit 0;
+
+#eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+#     & eval 'exec perl -S $0 $argv:q'
+#     if 0;
+#
+## -*- perl -*-
+#
+#use lib "$ENV{ACE_ROOT}/bin";
+#use PerlACE::TestTarget;
+#
+#my $program = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+#
+#$PROG = $program->CreateProcess ("qos_dump");
+#$program_status = $PROG->SpawnWaitKill ($program->ProcessStartWaitInterval ());
+#
+#if ($program_status != 0) {
+#    print STDERR "ERROR: qos_dump returned $program_status\n";
+#    exit 1;
+#}
+#
+#exit 0;
