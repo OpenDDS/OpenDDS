e305cd2d292a4b941bb16eae652317f9b01c82fe
Johnny Willemsen <jwillemsen@remedy.nl>
2021-02-26 16:16:13 +0100

    Update tests/DCPS/QueryCondition/QueryConditionTest.cpp
    Co-authored-by: Adam Mitz <mitza@objectcomputing.com>

--- tests/DCPS/QueryCondition/QueryConditionTest.cpp
+++ tests/DCPS/QueryCondition/QueryConditionTest.cpp
@@ -651,7 +651,7 @@ bool run_change_parameter_test(const DomainParticipant_var& dp,
   }
 
   if (query_cond->set_query_parameters(params_three) != RETCODE_ERROR) {
-    cerr << "ERROR: Setting 3 parameters for query condition with 2 token should have failed " << endl;
+    cerr << "ERROR: Setting 3 parameters for query condition with 2 tokens should have failed" << endl;
     return false;
   }
 
