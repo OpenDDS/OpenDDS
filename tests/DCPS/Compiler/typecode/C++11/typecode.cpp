13bd1ca6fd9c165b3c4c1b792462812f459adea8
Clayton Calabrese <calabreseclayton@gmail.com>
2021-04-16 17:27:59 -0500

    added typecode test. C++11 should not currently compile

---
+++ tests/DCPS/Compiler/typecode/C++11/typecode.cpp
@@ -0,0 +1,27 @@
+/***
+ * This test is meant to test the annotations set by typeflags.
+ * Currently IS_AUTOID_HASH is not implemented so that should always return false.
+ ***/
+#include "typecodeTypeSupportImpl.h"
+
+#include <gtest/gtest.h>
+
+using namespace OpenDDS::DCPS;
+
+TEST(TestAny, flags_match)
+{      
+  typecode::my_struct struct_in;
+  struct_in.my_long(42);
+  typecode::my_struct * struct_out_ptr;
+  CORBA::Any my_any;
+  my_any <<= struct_in;
+  my_any >>= struct_out_ptr;
+  
+  EXPECT_EQ(struct_out_ptr->my_long(), 42);
+}
+
+int main(int argc, char* argv[])
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
