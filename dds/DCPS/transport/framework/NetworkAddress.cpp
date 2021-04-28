5bb3696771df7ce989fb361ad6dc73b709351444
Timothy Simpson <simpsont@objectcomputing.com>
2021-04-28 09:10:26 -0500

    disable mapped, always log

--- dds/DCPS/transport/framework/NetworkAddress.cpp
+++ dds/DCPS/transport/framework/NetworkAddress.cpp
@@ -491,7 +491,7 @@ ACE_INET_Addr choose_single_coherent_address(const ACE_INET_Addr& address, bool 
 
   do {
     if (copy == ACE_INET_Addr()) {
-      VDBG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
+      ACE_DEBUG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
             "Considering Address %C:%d - IGNORING EMPTY ADDRESS\n",
             copy.get_host_addr(), copy.get_port_number()));
       continue;
@@ -502,24 +502,24 @@ ACE_INET_Addr choose_single_coherent_address(const ACE_INET_Addr& address, bool 
 #ifdef ACE_HAS_IPV6
     if (temp.get_type() == AF_INET6 && !temp.is_multicast()) {
       if (temp.is_loopback()) {
-        VDBG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
+        ACE_DEBUG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
               "Considering Address %C:%d (%C) - ADDING TO IPv6 LOOPBACK LIST\n",
               temp.get_host_addr(), temp.get_port_number(), temp.get_host_name()));
         set6_loopback.insert(temp);
       } else if (temp.is_ipv4_mapped_ipv6() || temp.is_ipv4_compat_ipv6()) {
 #ifndef IPV6_V6ONLY
-        VDBG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
+        ACE_DEBUG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
               "Considering Address %C:%d (%C) - ADDING TO IPv6 MAPPED / COMPATIBLE IPv4 LIST\n",
               temp.get_host_addr(), temp.get_port_number(), temp.get_host_name()));
         set6_mapped_v4.insert(temp);
 #endif  // ! IPV6_V6ONLY
       } else if (temp.is_linklocal()) {
-        VDBG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
+        ACE_DEBUG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
               "Considering Address %C:%d (%C) - ADDING TO IPv6 LINK-LOCAL LIST\n",
               temp.get_host_addr(), temp.get_port_number(), temp.get_host_name()));
         set6_linklocal.insert(temp);
       } else {
-        VDBG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
+        ACE_DEBUG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
               "Considering Address %C:%d (%C) - ADDING TO IPv6 NORMAL LIST\n",
               temp.get_host_addr(), temp.get_port_number(), temp.get_host_name()));
         set6.insert(temp);
@@ -527,7 +527,7 @@ ACE_INET_Addr choose_single_coherent_address(const ACE_INET_Addr& address, bool 
     }
 #endif // ACE_HAS_IPV6
     if (temp.get_type() == AF_INET && !temp.is_multicast()) {
-      VDBG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
+      ACE_DEBUG((LM_DEBUG, "(%P|%t) NetworkAddress::choose_single_coherent_address() - "
             "Considering Address %C:%d (%C) - ADDING TO IPv4 NORMAL LIST\n",
             temp.get_host_addr(), temp.get_port_number(), temp.get_host_name()));
       set4.insert(temp);
@@ -551,9 +551,9 @@ ACE_INET_Addr choose_single_coherent_address(const ACE_INET_Addr& address, bool 
   if (!set6.empty()) {
     return *set6.begin();
   }
-  if (!set6_mapped_v4.empty()) {
-    return *set6_mapped_v4.begin();
-  }
+  //if (!set6_mapped_v4.empty()) {
+  //  return *set6_mapped_v4.begin();
+  //}
 #endif // ACE_HAS_IPV6
 
   if (!set4.empty()) {
