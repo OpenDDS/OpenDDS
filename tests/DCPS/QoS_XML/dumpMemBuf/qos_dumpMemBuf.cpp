0423d8dafce53939bbb9f5c4c93d82de4743c07f
Danilo C. Zanella <danilo.zanella@iag.usp.br>
2021-05-09 00:06:44 +0000

    Improving tests to complain with new features of QOS_XML_Handler

--- tests/DCPS/QoS_XML/dumpMemBuf/qos_dumpMemBuf.cpp
+++ tests/DCPS/QoS_XML/dumpMemBuf/qos_dumpMemBuf.cpp
@@ -3,22 +3,21 @@
 #include "dds/DCPS/QOS_XML_Handler/XML_MemBuf_Intf.h"
 #include "dds/DdsDcpsC.h"
 
-int ACE_TMAIN (int, ACE_TCHAR *[])
+int testXML(const char* fileName,
+            const ACE_TCHAR* profileName,
+            const ACE_TCHAR* topicName,
+            OpenDDS::DCPS::QOS_XML_MemBuf_Handler& xml_membuf)
 {
   int retval = 0;
+
   try
   {
-    // File name and profile name in respective file
-    const char * fileName = "qos.xml";
-    const ACE_TCHAR * profileName = ACE_TEXT("TestProfile");
-    const ACE_TCHAR * topicName = ACE_TEXT("TopicName");
 
     // read file and create string
     std::ifstream ifs(fileName);
     std::stringstream buffer;
     buffer << ifs.rdbuf();
 
-    OpenDDS::DCPS::QOS_XML_MemBuf_Handler xml_membuf;
     // Add env variable and path to search schemas
     xml_membuf.add_search_path(ACE_TEXT("DDS_ROOT"),ACE_TEXT("/docs/schema/"));
     // initialize and parse XML
@@ -35,20 +34,22 @@ int ACE_TMAIN (int, ACE_TCHAR *[])
       if (retcode_qos != DDS::RETCODE_OK)
       {
         ACE_ERROR ((LM_ERROR, "MAIN - "
-              "get_datawriter_qos return an error. Retcode <%d>\n",
-              retcode_qos));
+              "%s : get_datawriter_qos return an error. Retcode <%d>\n",
+              fileName, retcode_qos));
         ++retval;
       }
       if (dw_qos.history.kind != ::DDS::KEEP_ALL_HISTORY_QOS)
       {
         ACE_ERROR ((LM_ERROR, "MAIN - "
-              "get_datawriter_qos return an invalid history kind.\n"));
+              "%s : get_datawriter_qos return an invalid history kind.\n",
+              fileName));
         ++retval;
       }
       if (dw_qos.history.depth != 5)
       {
         ACE_ERROR ((LM_ERROR, "MAIN - "
-              "get_datawriter_qos return an invalid history depth.\n"));
+              "%s : get_datawriter_qos return an invalid history depth.\n",
+              fileName));
         ++retval;
       }
 
@@ -60,8 +61,8 @@ int ACE_TMAIN (int, ACE_TCHAR *[])
       if (retcode_qos != DDS::RETCODE_OK)
       {
         ACE_ERROR ((LM_ERROR, "MAIN - "
-              "get_datareader_qos return an error. Retcode <%d>\n",
-              retcode_qos));
+              "%s : get_datareader_qos return an error. Retcode <%d>\n",
+              fileName, retcode_qos));
         ++retval;
       }
 
@@ -73,8 +74,8 @@ int ACE_TMAIN (int, ACE_TCHAR *[])
       if (retcode_qos != DDS::RETCODE_OK)
       {
         ACE_ERROR ((LM_ERROR, "MAIN - "
-              "get_topic_qos return an error. Retcode <%d>\n",
-              retcode_qos));
+              "%s : get_topic_qos return an error. Retcode <%d>\n",
+              fileName, retcode_qos));
         ++retval;
       }
 
@@ -85,8 +86,8 @@ int ACE_TMAIN (int, ACE_TCHAR *[])
       if (retcode_qos != DDS::RETCODE_OK)
       {
         ACE_ERROR ((LM_ERROR, "MAIN - "
-              "get_publisher_qos return an error. Retcode <%d>\n",
-              retcode_qos));
+              "%s : get_publisher_qos return an error. Retcode <%d>\n",
+              fileName, retcode_qos));
         ++retval;
       }
 
@@ -97,8 +98,8 @@ int ACE_TMAIN (int, ACE_TCHAR *[])
       if (retcode_qos != DDS::RETCODE_OK)
       {
         ACE_ERROR ((LM_ERROR, "MAIN - "
-              "get_subscriber_qos return an error. Retcode <%d>\n",
-              retcode_qos));
+              "%s : get_subscriber_qos return an error. Retcode <%d>\n",
+              fileName, retcode_qos));
         ++retval;
       }
 
@@ -109,17 +110,18 @@ int ACE_TMAIN (int, ACE_TCHAR *[])
       if (retcode_qos != DDS::RETCODE_OK)
       {
         ACE_ERROR ((LM_ERROR, "MAIN - "
-              "get_participant_qos return an error. Retcode <%d>\n",
-              retcode_qos));
+              "%s : get_participant_qos return an error. Retcode <%d>\n",
+              fileName, retcode_qos));
         ++retval;
       }
     }
     else
     {
-      ACE_ERROR ((LM_ERROR, "MAIN - Init return an error. Retcode <%d>\n",
-            retcode));
+      ACE_ERROR ((LM_ERROR, "%s - Init return an error. Retcode <%d>\n",
+            fileName, retcode));
       ++retval;
     }
+
   }
   catch (const CORBA::Exception& ex)
   {
@@ -130,6 +132,110 @@ int ACE_TMAIN (int, ACE_TCHAR *[])
   {
     ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unexpected exception\n")));
     return 1;
+  }
+
+  return retval;
+};
+
+int ACE_TMAIN (int, ACE_TCHAR *[])
+{
+  int retval = 0;
+
+  // Skip DEBUG messages
+  ACE_LOG_MSG->priority_mask (LM_ERROR | LM_WARNING | LM_INFO, ACE_Log_Msg::PROCESS);
+
+  // Test first file
+  // File name and profile name in respective file
+  // topic name in respective profile name
+  std::string fileName;
+  std::string profileName;
+  std::string topicName;
+  fileName = "test.xml";
+  profileName = "TestProfile";
+  topicName = "TopicName";
+  
+  OpenDDS::DCPS::QOS_XML_MemBuf_Handler xml1;
+  retval += testXML(fileName.c_str(),
+                    ACE_TEXT_CHAR_TO_TCHAR(profileName.c_str()),
+                    ACE_TEXT_CHAR_TO_TCHAR(topicName.c_str()),
+                    xml1);
+
+  // Test second file
+  // File name and profile name in respective file
+  // topic name in respective profile name
+  fileName = "append2test.xml";
+  profileName = "TestProfile2";
+  topicName = "TopicName";
+  
+  OpenDDS::DCPS::QOS_XML_MemBuf_Handler xml2;
+  retval += testXML(fileName.c_str(),
+                    ACE_TEXT_CHAR_TO_TCHAR(profileName.c_str()),
+                    ACE_TEXT_CHAR_TO_TCHAR(topicName.c_str()),
+                    xml2);
+
+  // Test xml_membuf operations
+  // append a profile
+  profileName = "TestA";
+  dds::qosProfile profile = xml2.getProfile(ACE_TEXT_CHAR_TO_TCHAR(profileName.c_str()));
+  DDS::ReturnCode_t retcode;
+  retcode = xml1.addQoSProfile(profile);
+  if (retcode != DDS::RETCODE_OK)
+  {
+    ACE_ERROR ((LM_ERROR, "MAIN - "
+          "Cannot append profile name <%s>. Retcode <%d>\n",
+          profileName.c_str(), retcode));
+    return retval;
+  }
+  // Check if it was inserted
+  dds::qosProfile testA = xml2.getProfile(ACE_TEXT_CHAR_TO_TCHAR(profileName.c_str()));
+  if (profileName.compare(testA.name().c_str()))
+  {
+    ACE_ERROR ((LM_ERROR, "MAIN - "
+          "Cannot get last profile name <%s> inserted. Retcode <%d>\n",
+          profileName.c_str(), retcode));
+    return retval;
+  }
+
+
+  // Remove profile 
+  retcode = xml1.delQoSProfile(ACE_TEXT_CHAR_TO_TCHAR(profileName.c_str()));
+  if (retcode != DDS::RETCODE_OK)
+  {
+    ACE_ERROR ((LM_ERROR, "MAIN - "
+          "Cannot remove profile name <%s>. Retcode <%d>\n",
+          profileName.c_str(), retcode));
+    return retval;
+  }
+
+  // append existing profile name
+  profileName = "TestProfile";
+  profile = xml2.getProfile(ACE_TEXT_CHAR_TO_TCHAR(profileName.c_str()));
+  retcode = xml1.addQoSProfile(profile);
+  if (retcode == DDS::RETCODE_OK)
+  {
+    ACE_ERROR ((LM_ERROR, "MAIN - "
+          "Unexpected insertion of profile name <%s>.\n",
+          profileName.c_str()));
+    return retval;
+  }
+
+  // append list of profiles
+  int profileNum = xml2.length();
+  if (profileNum != 5)
+  {
+    ACE_ERROR ((LM_ERROR, "MAIN - "
+          "Wrong number (%d) of profiles in <%s>.\n",
+          profileNum, fileName.c_str()));
+    return -1;
+  }
+
+  const dds::qosProfile_seq& profiles = xml2.get();
+  retcode = xml1.addQoSProfileSeq(profiles);
+  if (retcode != DDS::RETCODE_OK)
+  {
+    ACE_ERROR ((LM_ERROR, "MAIN - "
+          "Cannot append profile list. RetCode %d\n",retcode));
+    return ++retval;
   }
 
   return retval;
