bb6b8e71488a92c84906ee38802f6ef1d573ded3
Justin R. Wilson <wilsonj@objectcomputing.com>
2021-05-12 10:32:46 -0500

    From review

--- tools/rtpsrelay/GuidPartitionTable.h
+++ tools/rtpsrelay/GuidPartitionTable.h
@@ -1,6 +1,8 @@
 #ifndef RTPSRELAY_GUID_PARTITION_TABLE_H_
 #define RTPSRELAY_GUID_PARTITION_TABLE_H_
 
+#include "Config.h"
+
 #include "lib/PartitionIndex.h"
 #include "lib/RelayTypeSupportImpl.h"
 #include "lib/Utility.h"
@@ -29,66 +31,7 @@ public:
   }
 
   // Insert a reader/writer guid and its partitions.
-  void insert(const OpenDDS::DCPS::GUID_t& guid, const DDS::StringSeq& partitions)
-  {
-    ACE_GUARD(ACE_Thread_Mutex, g, mutex_);
-
-    StringSet parts;
-    for (CORBA::ULong idx = 0; idx != partitions.length(); ++idx) {
-      parts.insert(partitions[idx].in());
-    }
-    if (parts.empty()) {
-      // Special case for empty list of partitions.
-      parts.insert("");
-    }
-
-    const auto& x = guid_to_partitions_[guid];
-
-    std::vector<std::string> to_add;
-    std::set_difference(parts.begin(), parts.end(), x.begin(), x.end(), std::back_inserter(to_add));
-
-    std::vector<std::string> to_remove;
-    std::set_difference(x.begin(), x.end(), parts.begin(), parts.end(), std::back_inserter(to_remove));
-
-    if (to_add.empty() && to_remove.empty()) {
-      // No change.
-      return;
-    }
-
-    SpdpReplay spdp_replay;
-    populate_replay(spdp_replay, guid, to_add);
-
-    StringSet globally_new;
-    {
-      const auto r = guid_to_partitions_.insert(std::make_pair(guid, StringSet()));
-      r.first->second.insert(to_add.begin(), to_add.end());
-      for (const auto& part : to_add) {
-        const auto q = partition_to_guid_.insert(std::make_pair(part, OrderedGuidSet()));
-        q.first->second.insert(guid);
-        partition_index_.insert(part, guid);
-        if (q.second) {
-          globally_new.insert(part);
-        }
-      }
-      for (const auto& part : to_remove) {
-        r.first->second.erase(part);
-        partition_to_guid_[part].erase(guid);
-        partition_index_.remove(part, guid);
-        if (partition_to_guid_[part].empty()) {
-          partition_to_guid_.erase(part);
-        }
-      }
-      if (r.first->second.empty()) {
-        guid_to_partitions_.erase(r.first);
-      }
-    }
-
-    add_globally_new(globally_new);
-
-    if (spdp_replay_writer_->write(spdp_replay, DDS::HANDLE_NIL) != DDS::RETCODE_OK) {
-      ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) ERROR: failed to write Relay Partitions\n")));
-    }
-  }
+  void insert(const OpenDDS::DCPS::GUID_t& guid, const DDS::StringSeq& partitions);
 
   void remove(const OpenDDS::DCPS::GUID_t& guid)
   {
@@ -97,14 +40,17 @@ public:
     const auto pos = guid_to_partitions_.find(guid);
     if (pos != guid_to_partitions_.end()) {
       for (const auto& partition : pos->second) {
-        partition_to_guid_[partition].erase(guid);
         partition_index_.remove(partition, guid);
-        if (partition_to_guid_[partition].empty()) {
-          partition_to_guid_.erase(partition);
+        const auto pos2 = partition_to_guid_.find(partition);
+        if (pos2 != partition_to_guid_.end()) {
+          pos2->second.erase(guid);
+          if (pos2->second.empty()) {
+            partition_to_guid_.erase(pos2);
+          }
         }
       }
+      guid_to_partitions_.erase(pos);
     }
-    guid_to_partitions_.erase(guid);
   }
 
   // Look up the partitions for the participant from.
@@ -122,16 +68,8 @@ public:
     }
   }
 
-  void lookup(GuidSet& guids, const StringSet& partitions) const
-  {
-    ACE_GUARD(ACE_Thread_Mutex, g, mutex_);
-
-    for (const auto& part : partitions) {
-      partition_index_.lookup(part, guids);
-    }
-  }
-
-  void lookup(GuidSet& guids, const StringSequence& partitions) const
+  template <typename T>
+  void lookup(GuidSet& guids, const T& partitions) const
   {
     ACE_GUARD(ACE_Thread_Mutex, g, mutex_);
 
