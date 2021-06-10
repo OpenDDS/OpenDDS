2d5cc635ad25e1877a5998fe5bbf60d9de12f4f9
Adam Mitz <mitza@objectcomputing.com>
2021-06-10 17:14:39 -0500

    Adapted the DelayedDurable test for this scenario

--- tests/DCPS/DelayedDurable/DelayedDurable.cpp
+++ tests/DCPS/DelayedDurable/DelayedDurable.cpp
@@ -1,14 +1,21 @@
 #include "DelayedDurableTypeSupportImpl.h"
 
+#include <tests/Utils/StatusMatching.h>
+
 #include <dds/DCPS/Marked_Default_Qos.h>
 #include <dds/DCPS/Service_Participant.h>
+#include <dds/DCPS/PoolAllocator.h>
 #include <dds/DCPS/WaitSet.h>
+
 #include <dds/DCPS/transport/framework/TransportSendStrategy.h>
 #ifdef ACE_AS_STATIC_LIBS
 #  include <dds/DCPS/RTPS/RtpsDiscovery.h>
 #  include <dds/DCPS/transport/rtps_udp/RtpsUdp.h>
 #endif
 
+#include <ace/ace_wchar.h>
+
+#include <fstream>
 #include <set>
 
 using namespace DDS;
@@ -39,6 +46,9 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
   bool writer = false;
   bool reader = false;
   bool large_samples = false;
+  bool has_early_reader = false;
+  int argv_index_report_file = 0;
+
   for (int i = 1; i < argc; ++i) {
     ACE_TString arg(argv[i]);
     if (arg == ACE_TEXT("--verbose")) {
@@ -49,6 +59,10 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
       reader = true;
     } else if (arg == ACE_TEXT("--large-samples")) {
       large_samples = true;
+    } else if (arg == ACE_TEXT("--has-early-reader")) {
+      has_early_reader = true;
+    } else if (arg == ACE_TEXT("--report-last-value")) {
+      argv_index_report_file = ++i;
     } else {
       ACE_ERROR((LM_ERROR, "ERROR: Invalid argument: %s\n", argv[i]));
       return 1;
@@ -56,6 +70,9 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
   }
 
   bool failed = false;
+  const int num_instances = has_early_reader ? 1 : scale(100, large_samples);
+  const char* const name = writer ? "writer" : "reader";
+  ACE_DEBUG((LM_DEBUG, "(%P|%t) %C starting at %T\n", name));
 
   if (writer) {
     Publisher_var pub = dp->create_publisher(PUBLISHER_QOS_DEFAULT, 0,
@@ -67,6 +84,10 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
                                                DEFAULT_STATUS_MASK);
     PropertyDataWriter_var pdw = PropertyDataWriter::_narrow(dw);
 
+    if (has_early_reader) {
+      Utils::wait_match(dw, 1);
+    }
+
     Property p;
     if (large_samples) {
       p.extra.length(large_sample_seq_size);
@@ -77,28 +98,34 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
       p.extra.length(0);
     }
 
-    for (int i = 1; i <= scale(100, large_samples); ++i) {
+    for (int i = 1; i <= num_instances; ++i) {
       p.key = i;
       p.value = i;
       pdw->write(p, HANDLE_NIL);
     }
 
     for (int c = 1; c < 75; ++c) {
-      for (int i = 1; i <= scale(100, large_samples); i += scale(5, large_samples)) {
+      for (int i = 1; i <= num_instances; i += scale(5, large_samples)) {
         p.key = i;
         p.value = i + c;
         pdw->write(p, HANDLE_NIL);
       }
-      ACE_OS::sleep(ACE_Time_Value(0, 500 * 1000)); // 1/2 sec
+      if (!has_early_reader) {
+        ACE_OS::sleep(ACE_Time_Value(0, 500 * 1000)); // 1/2 sec
+      }
       if (verbose) {
         ACE_DEBUG((LM_DEBUG, "writer: Count: %d\n", c));
       }
     }
+
     DDS::Duration_t duration = {DDS::DURATION_INFINITE_SEC, DDS::DURATION_INFINITE_NSEC};
     pdw->wait_for_acknowledgments(duration);
-  } else if (reader) {
-    ACE_DEBUG((LM_DEBUG, "Reader starting at %T\n"));
 
+    if (has_early_reader) {
+      Utils::wait_match(dw, 2);
+    }
+
+  } else if (reader) {
     Subscriber_var sub = dp->create_subscriber(SUBSCRIBER_QOS_DEFAULT, 0,
                                                DEFAULT_STATUS_MASK);
     DataReaderQos dr_qos;
@@ -114,8 +141,9 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
     WaitSet_var ws = new WaitSet;
     ws->attach_condition(dr_rc);
     unsigned counter = 0;
-    const unsigned minimum_sample_count = large_samples ? 95 : 981;
+    const unsigned minimum_sample_count = has_early_reader ? 1 : large_samples ? 95 : 981;
     std::set<int> instances;
+    int last_value = 0;
     while (true) {
       ConditionSeq active;
       const Duration_t max_wait = {10, 0};
@@ -136,8 +164,9 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
         for (unsigned int i = 0; i < data.length(); ++i) {
           ++counter;
           instances.insert(data[i].key);
+          last_value = data[i].value;
           if (verbose) {
-            ACE_DEBUG((LM_DEBUG, "reader: Counter: %u Instance: %d\n", counter, data[i].key));
+            ACE_DEBUG((LM_DEBUG, "reader: Counter: %u Instance: %d Value: %d\n", counter, data[i].key, data[i].value));
           }
 
           if (counter == minimum_sample_count) {
@@ -156,7 +185,7 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
         minimum_sample_count, counter));
       failed = true;
     }
-    for (int i = 1; i <= scale(100, large_samples); ++i) {
+    for (int i = 1; i <= num_instances; ++i) {
       if (instances.count(i) == 0) {
         ACE_ERROR((LM_ERROR, "ERROR: Reader Missing Instance %d\n", i));
         failed = true;
@@ -164,10 +193,22 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
     }
     ws->detach_condition(dr_rc);
     dr->delete_readcondition(dr_rc);
+
+    if (argv_index_report_file) {
+      const OpenDDS::DCPS::String filename = ACE_TEXT_ALWAYS_CHAR(argv[argv_index_report_file]);
+      std::ofstream file(filename.c_str());
+      file << last_value;
+    }
+
+    if (has_early_reader) {
+      Utils::wait_match(dr, 0);
+    }
+
   } else {
     ACE_ERROR((LM_ERROR, "ERROR: Must pass either --writer or --reader\n"));
     return 1;
   }
+  ACE_DEBUG((LM_DEBUG, "(%P|%t) %C ending at %T\n", name));
 
   topic = 0;
   dp->delete_contained_entities();
