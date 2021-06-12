f0ef340ef0be320e3e17be9d7374effeed51494b
Timothy Simpson <simpsont@objectcomputing.com>
2021-06-12 15:18:13 -0500

    attempt to guard against random dns failures impacting test

--- tests/unit-tests/dds/DCPS/transport/framework/NetworkAddress.cpp
+++ tests/unit-tests/dds/DCPS/transport/framework/NetworkAddress.cpp
@@ -123,14 +123,20 @@ TEST(network_address_test, choose_single_coherent_address_double)
 
   ACE_INET_Addr addr1 = choose_single_coherent_address("www.bing.com:80", false);
   ACE_INET_Addr addr2 = choose_single_coherent_address("www.bing.com:80", false);
-  EXPECT_NE(addr1, ACE_INET_Addr());
-  EXPECT_NE(addr2, ACE_INET_Addr());
-  EXPECT_EQ(addr1, addr2);
-  EXPECT_EQ(addr2, addr1);
+
+  // Since tihs test winds up doing real DNS lookups which might fail, allow the result to be be empty but expect it to be consistant
+
+  if (addr1 == ACE_INET_Addr()) {
+    EXPECT_NE(addr2, ACE_INET_Addr());
+  } else {
+    EXPECT_NE(addr2, ACE_INET_Addr());
+    EXPECT_EQ(addr1, addr2);
+    EXPECT_EQ(addr2, addr1);
 #if defined ACE_HAS_IPV6 && defined IPV6_V6ONLY
-  EXPECT_FALSE(addr1.is_ipv4_mapped_ipv6());
-  EXPECT_FALSE(addr2.is_ipv4_mapped_ipv6());
+    EXPECT_FALSE(addr1.is_ipv4_mapped_ipv6());
+    EXPECT_FALSE(addr2.is_ipv4_mapped_ipv6());
 #endif
+  }
 }
 
 TEST(network_address_test, choose_single_coherent_address_double_self)
@@ -157,17 +163,24 @@ TEST(network_address_test, choose_single_coherent_address_triple)
   ACE_INET_Addr addr1 = choose_single_coherent_address("www.hp.com:587", false);
   ACE_INET_Addr addr2 = choose_single_coherent_address("www.hp.com:587", false);
   ACE_INET_Addr addr3 = choose_single_coherent_address("www.hp.com:587", false);
-  EXPECT_NE(addr1, ACE_INET_Addr());
-  EXPECT_NE(addr2, ACE_INET_Addr());
-  EXPECT_NE(addr3, ACE_INET_Addr());
-  EXPECT_EQ(addr1, addr2);
-  EXPECT_EQ(addr2, addr3);
-  EXPECT_EQ(addr3, addr1);
+
+  // Since tihs test winds up doing real DNS lookups which might fail, allow the result to be be empty but expect it to be consistant
+
+  if (addr1 == ACE_INET_Addr()) {
+    EXPECT_EQ(addr2, ACE_INET_Addr());
+    EXPECT_EQ(addr3, ACE_INET_Addr());
+  } else {
+    EXPECT_NE(addr2, ACE_INET_Addr());
+    EXPECT_NE(addr3, ACE_INET_Addr());
+    EXPECT_EQ(addr1, addr2);
+    EXPECT_EQ(addr2, addr3);
+    EXPECT_EQ(addr3, addr1);
 #if defined ACE_HAS_IPV6 && defined IPV6_V6ONLY
-  EXPECT_FALSE(addr1.is_ipv4_mapped_ipv6());
-  EXPECT_FALSE(addr2.is_ipv4_mapped_ipv6());
-  EXPECT_FALSE(addr3.is_ipv4_mapped_ipv6());
+    EXPECT_FALSE(addr1.is_ipv4_mapped_ipv6());
+    EXPECT_FALSE(addr2.is_ipv4_mapped_ipv6());
+    EXPECT_FALSE(addr3.is_ipv4_mapped_ipv6());
 #endif
+  }
 }
 
 #if defined ACE_HAS_IPV6
