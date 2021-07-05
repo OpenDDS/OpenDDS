756324d959f0511c095f7f6c81be9d6d1c8862b3
Phil Mesnier <mesnierp@objectcomputing.com>
2021-07-04 21:44:57 -0500

    apply a portable preamble to the run_test script.

--- tests/DCPS/Compiler/typeobject_hash_consistency/run_test.pl
+++ tests/DCPS/Compiler/typeobject_hash_consistency/run_test.pl
@@ -1,16 +1,26 @@
-#!/usr/bin/perl
-    $gencmd = "opendds_idl -Sa -St --no-dcps-data-type-warnings sample.idl";
-    system($gencmd);
-    system("mv sampleTypeSupportImpl.cpp pass1");
-    system($gencmd);
-    $result = system("diff pass1 sampleTypeSupportImpl.cpp");
-    system("rm pass1 sampleT*");
-    if ($result == "")
-    {
-        print "test PASSED\n";
-        exit 0;
-    }
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+    & eval 'exec perl -S $0 $argv:q'
+    if 0;
 
-    print "test FAILED\n";
-    exit 1;
+use Env (DDS_ROOT);
+use lib "$DDS_ROOT/bin";
+use Env (ACE_ROOT);
+use lib "$ACE_ROOT/bin";
+use PerlDDS::Run_Test;
+use strict;
+
+my $gencmd = "opendds_idl -Sa -St --no-dcps-data-type-warnings sample.idl";
+system($gencmd);
+system("mv sampleTypeSupportImpl.cpp pass1");
+system($gencmd);
+my $result = system("diff pass1 sampleTypeSupportImpl.cpp");
+system("rm pass1 sampleT*");
+if ($result == "")
+{
+    print "test PASSED\n";
+    exit 0;
+}
+
+print "test FAILED\n";
+exit 1;
 
