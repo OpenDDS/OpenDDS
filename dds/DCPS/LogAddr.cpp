a22e586afbbf39f3ef69ac323474ff0a9acaf3d9
Adam Mitz <mitza@objectcomputing.com>
2021-07-09 18:58:49 -0500

    Merge pull request #2795 from lij-oci/LogAddr
    LogAddr
    
    (cherry picked from commit b73f55caed6a7b9522870b31d0a0b9003daf4a62)

---
+++ dds/DCPS/LogAddr.cpp
@@ -0,0 +1,56 @@
+#include "DCPS/DdsDcps_pch.h"
+
+#include "LogAddr.h"
+#include "Definitions.h"
+
+#include <stdio.h>
+
+OPENDDS_BEGIN_VERSIONED_NAMESPACE_DECL
+
+namespace OpenDDS {
+namespace DCPS {
+
+const size_t LogAddr::BufSize = AddrToStringSize;
+
+const String LogAddr::ip(const ACE_INET_Addr& addr)
+{
+  char s[BufSize] = {'\0'};
+  return String(addr.get_host_addr(s, BufSize));
+}
+
+const String LogAddr::port(const ACE_INET_Addr& addr)
+{
+  const size_t SZ = 8;
+  char s[SZ] = {'\0'};
+  snprintf(s, SZ, "%d", addr.get_port_number());
+  return String(s);
+}
+
+const String LogAddr::host(const ACE_INET_Addr& addr)
+{
+  char s[BufSize] = {'\0'};
+  addr.get_host_name(s, BufSize);
+  return String(s);
+}
+
+LogAddr::LogAddr(const ACE_INET_Addr& addr, Option opt)
+{
+  if (opt == IpPort) {
+    addr_ = ip(addr) + ':' + port(addr);
+  } else if (opt == HostPort) {
+    addr_ = host(addr) + ':' + port(addr);
+  } else if (opt == Ip) {
+    addr_ = ip(addr);
+  } else if (opt == Port) {
+    addr_ = port(addr);
+  } else if (opt == Host) {
+    addr_ = host(addr);
+  } else if (opt == IpPortHost) {
+    addr_ = ip(addr) + ':' + port(addr) + " (" + host(addr) + ')';
+  }
+}
+
+} // namespace DCPS
+} // namespace OpenDDS
+
+OPENDDS_END_VERSIONED_NAMESPACE_DECL
