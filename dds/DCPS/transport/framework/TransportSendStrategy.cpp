500af9520332c16d7682299b42caf1d80282b797
Johnny Willemsen <jwillemsen@remedy.nl>
2021-08-13 16:45:23 +0200

    Move VDBG out of debug level check
        * dds/DCPS/transport/framework/TransportSendStrategy.cpp:

--- dds/DCPS/transport/framework/TransportSendStrategy.cpp
+++ dds/DCPS/transport/framework/TransportSendStrategy.cpp
@@ -1483,12 +1483,12 @@ TransportSendStrategy::direct_send(bool do_relink)
     } else if ((outcome == OUTCOME_PEER_LOST) ||
                (outcome == OUTCOME_SEND_ERROR)) {
       if (outcome == OUTCOME_SEND_ERROR) {
-        if (Transport_debug_level > 0) {
-          VDBG((LM_WARNING,
-                "(%P|%t) WARNING: Problem detected in "
-                "send buffer management: %p.\n",
-                "send_bytes"));
+        VDBG((LM_WARNING,
+              "(%P|%t) WARNING: Problem detected in "
+              "send buffer management: %p.\n",
+              "send_bytes"));
 
+        if (Transport_debug_level > 0) {
           transport_.config().dump();
         }
       } else {
