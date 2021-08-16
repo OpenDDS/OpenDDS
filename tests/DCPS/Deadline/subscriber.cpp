dc6a0ec323a3d1498354b44f5b674000564ae1c5
Jiang Li <lij@objectcomputing.com>
2021-08-16 12:03:17 -0500

    put listener in reader 2

--- tests/DCPS/Deadline/subscriber.cpp
+++ tests/DCPS/Deadline/subscriber.cpp
@@ -44,10 +44,10 @@ private:
   void test_incompatible_deadline_qos();
   bool check_status(const DDS::InstanceHandle_t& i1r1, const DDS::InstanceHandle_t& i1r2,
                     const DDS::InstanceHandle_t& i2r1, const DDS::InstanceHandle_t& i2r2,
-                    const CORBA::Long expected, CORBA::Long& delta2);
+                    const CORBA::Long expected, CORBA::Long& delta1);
   bool get_status(DDS::RequestedDeadlineMissedStatus& s1, DDS::RequestedDeadlineMissedStatus& s2);
   bool check_instance(const int dr, const DDS::InstanceHandle_t& last, const DDS::InstanceHandle_t& i1, const DDS::InstanceHandle_t& i2) const;
-  bool check_change(DDS::RequestedDeadlineMissedStatus& s1, DDS::RequestedDeadlineMissedStatus& s2, CORBA::Long& delta2) const;
+  bool check_change(DDS::RequestedDeadlineMissedStatus& s1, DDS::RequestedDeadlineMissedStatus& s2, CORBA::Long& delta1) const;
 
   Domain domain_;
   DDS::Subscriber_var sub_;
@@ -82,11 +82,10 @@ Subscriber::Subscriber(int argc, ACE_TCHAR* argv[]) : domain_(argc, argv, "Subsc
     qos.deadline.period.nanosec = Domain::R_Deadline.nanosec;
     qos.reliability.kind = DDS::RELIABLE_RELIABILITY_QOS;
 
-    ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) DataReaderQos deadline.period (%d sec)\n"), qos.deadline.period.sec));
-    ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) create 2 readers and dr1_ has a listener\n")));
-    // Reader1/Reader2 test deadline expiration handling code with/without listener callback
-    dr1_ = sub_->create_datareader(domain_.topic().in(), qos, listener_.in(), OpenDDS::DCPS::DEFAULT_STATUS_MASK);
-    dr2_ = sub_->create_datareader(domain_.topic().in(), qos, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+    ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) create 2 readers with deadline.period (%d sec)\n"), qos.deadline.period.sec));
+    // To test deadline expiration handling without/with listener callback
+    dr1_ = sub_->create_datareader(domain_.topic().in(), qos, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+    dr2_ = sub_->create_datareader(domain_.topic().in(), qos, listener_.in(), OpenDDS::DCPS::DEFAULT_STATUS_MASK);
     if (!dr1_ || !dr2_) {
       throw std::runtime_error("create_datareader failed.");
     }
@@ -118,8 +117,8 @@ int Subscriber::run()
   const DDS::InstanceHandle_t ins2r1 = mdr1->lookup_instance(msg);
   const DDS::InstanceHandle_t ins2r2 = mdr2->lookup_instance(msg);
 
-  CORBA::Long delta2 = 0;
-  if (!check_status(ins1r1, ins1r2, ins2r1, ins2r2, Domain::N_Instance, delta2)) {
+  CORBA::Long delta1 = 0;
+  if (!check_status(ins1r1, ins1r2, ins2r1, ins2r2, Domain::N_Instance, delta1)) {
     return 1;
   }
 
@@ -128,7 +127,7 @@ int Subscriber::run()
   // Wait for another set of deadline periods(5 + 11 secs).
   // During this period, the writers continue to write all samples with .5 second interval.
   ACE_OS::sleep(no_miss_period.value());
-  if (!check_status(ins1r1, ins1r2, ins2r1, ins2r2, 3 * Domain::N_Instance, delta2)) {
+  if (!check_status(ins1r1, ins1r2, ins2r1, ins2r2, 3 * Domain::N_Instance, delta1)) {
     return 1;
   }
 
@@ -207,7 +206,7 @@ void Subscriber::test_incompatible_deadline_qos()
 
 bool Subscriber::check_status(const DDS::InstanceHandle_t& i1r1, const DDS::InstanceHandle_t& i1r2,
                               const DDS::InstanceHandle_t& i2r1, const DDS::InstanceHandle_t& i2r2,
-                              const CORBA::Long expected, CORBA::Long& delta2)
+                              const CORBA::Long expected, CORBA::Long& delta1)
 {
   ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) Subscriber::check_status expected=%d\n"), expected));
   DDS::RequestedDeadlineMissedStatus s1;
@@ -229,7 +228,7 @@ bool Subscriber::check_status(const DDS::InstanceHandle_t& i1r1, const DDS::Inst
                s1.total_count, s2.total_count, expected));
     return false;
   }
-  if (!check_change(s1, s2, delta2)) {
+  if (!check_change(s1, s2, delta1)) {
     return false;
   }
   return true;
@@ -258,13 +257,13 @@ bool Subscriber::check_instance(const int dr, const DDS::InstanceHandle_t& last,
   return true;
 }
 
-bool Subscriber::check_change(DDS::RequestedDeadlineMissedStatus& s1, DDS::RequestedDeadlineMissedStatus& s2, CORBA::Long& delta2) const
+bool Subscriber::check_change(DDS::RequestedDeadlineMissedStatus& s1, DDS::RequestedDeadlineMissedStatus& s2, CORBA::Long& delta1) const
 {
   ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) Subscriber::check_change\n")));
-  // check if the total count changed compared to the last time we got it through dr1's listener;
-  // dr2 has no listener, so the total_count_change should match the total count
-  CORBA::Long delta1 = s1.total_count - listener_i_->requested_deadline_total_count();
-  delta2 = s2.total_count - delta2; // also save for the next check
+  // total_count_change of dr1 (no listener) should match the total_count minus last time change
+  // total_count_change of dr2 should match the total_count minus that from listener
+  delta1 = s1.total_count - delta1; // also save for the next check
+  CORBA::Long delta2 = s2.total_count - listener_i_->requested_deadline_total_count();
   if (s1.total_count_change != delta1 || s2.total_count_change != delta2) {
     ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) ERROR: missed requested deadline change (%d, %d) != (%d, %d)\n"),
                s1.total_count_change, s2.total_count_change, delta1, delta2));
