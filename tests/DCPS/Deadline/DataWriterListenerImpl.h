4673851d2660586e9d6c16ac9722c3dbceb05a51
Adam Mitz <mitza@objectcomputing.com>
2021-08-23 10:14:20 -0500

    Merge pull request #2910 from lij-oci/Deadline_test
    Deadline test fix
    
    (cherry picked from commit 9dad2b33fa8545472a9db5e30320ee97dccc67f4)

--- tests/DCPS/Deadline/DataWriterListenerImpl.h
+++ tests/DCPS/Deadline/DataWriterListenerImpl.h
@@ -21,47 +21,47 @@ class DataWriterListenerImpl
   : public virtual OpenDDS::DCPS::LocalObject<OpenDDS::DCPS::DataWriterListener>
 {
 public:
-  DataWriterListenerImpl (void);
+  DataWriterListenerImpl();
 
-  virtual void on_offered_deadline_missed (
-      ::DDS::DataWriter_ptr writer,
-      const ::DDS::OfferedDeadlineMissedStatus& status);
+  bool wait_matched(long count, const OpenDDS::DCPS::TimeDuration& max_wait) const;
+  CORBA::Long offered_deadline_total_count() const;
 
-  virtual void on_offered_incompatible_qos (
-      ::DDS::DataWriter_ptr writer,
-      const ::DDS::OfferedIncompatibleQosStatus& status);
+  virtual void on_offered_deadline_missed(
+      DDS::DataWriter_ptr writer,
+      const DDS::OfferedDeadlineMissedStatus& status);
 
-  virtual void on_liveliness_lost (
-      ::DDS::DataWriter_ptr writer,
-      const ::DDS::LivelinessLostStatus& status);
+  virtual void on_publication_matched(
+      DDS::DataWriter_ptr writer,
+      const DDS::PublicationMatchedStatus& status);
 
-  virtual void on_publication_matched (
-      ::DDS::DataWriter_ptr writer,
-      const ::DDS::PublicationMatchedStatus& status);
+  virtual void on_offered_incompatible_qos(
+      DDS::DataWriter_ptr writer,
+      const DDS::OfferedIncompatibleQosStatus& status);
 
-  virtual void on_publication_disconnected (
-      ::DDS::DataWriter_ptr reader,
-      const ::OpenDDS::DCPS::PublicationDisconnectedStatus& status);
+  virtual void on_liveliness_lost(
+      DDS::DataWriter_ptr writer,
+      const DDS::LivelinessLostStatus& status);
 
-  virtual void on_publication_reconnected (
-      ::DDS::DataWriter_ptr reader,
-      const ::OpenDDS::DCPS::PublicationReconnectedStatus& status);
+  virtual void on_publication_disconnected(
+      DDS::DataWriter_ptr reader,
+      const OpenDDS::DCPS::PublicationDisconnectedStatus& status);
 
-  virtual void on_publication_lost (
-      ::DDS::DataWriter_ptr writer,
-      const ::OpenDDS::DCPS::PublicationLostStatus& status);
+  virtual void on_publication_reconnected(
+      DDS::DataWriter_ptr reader,
+      const OpenDDS::DCPS::PublicationReconnectedStatus& status);
 
-  bool wait_matched(
-    long count, const OpenDDS::DCPS::TimeDuration& max_wait) const;
-
-  CORBA::Long offered_deadline_total_count (void) const;
+  virtual void on_publication_lost(
+      DDS::DataWriter_ptr writer,
+      const OpenDDS::DCPS::PublicationLostStatus& status);
 
 protected:
-  virtual ~DataWriterListenerImpl (void);
+  virtual ~DataWriterListenerImpl();
 
 private:
-  mutable ACE_Thread_Mutex mutex_;
-  mutable OpenDDS::DCPS::ConditionVariable<ACE_Thread_Mutex> matched_condition_;
+  typedef ACE_Thread_Mutex Mutex;
+  typedef ACE_Guard<Mutex> Lock;
+  mutable Mutex mutex_;
+  mutable OpenDDS::DCPS::ConditionVariable<Mutex> matched_condition_;
   long matched_;
   CORBA::Long offered_deadline_total_count_;
 };
