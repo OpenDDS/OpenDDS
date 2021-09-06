4a7a10eb28363a57f77cc6a9ddbd8799d9f21ec4
Fred Hornsey <hornseyf@objectcomputing.com>
2021-09-06 00:30:25 -0500

    htmlproofer.sh Strip Full Workflow URL Prefix

--- htmlproofer.sh
+++ htmlproofer.sh
@@ -1,15 +1,22 @@
-site_dir=_site
+site_dir="${site_dir:-_site}"
 
-if grep -RI --quiet 'href="/pages/objectcomputing/OpenDDS/' $site_dir
+# Absolute URLs to the website with a domain wont work unless we are serving
+# from that address at the same time. Use --url-swap to replace the URLs with
+# just an absolute path.
+url_swap_list=()
+if grep -RI --quiet 'href="/pages/objectcomputing/OpenDDS/' "$site_dir"
 then
-  # This gets inserted in the workflow, but we need to remove it for
-  # htmlproofer.
-  strip_url='\/pages\/objectcomputing\/OpenDDS'
+  url_swap_list+=(
+    '^https?\:\/\/github\.com\/pages\/objectcomputing\/OpenDDS:'
+    '^\/pages\/objectcomputing\/OpenDDS:'
+  )
 else
-  # Absolute URLs to the website wont work unless we are serving at the same
-  # time, replace with root-relative.
-  strip_url='https?\:\/\/localhost\:4000'
+  url_swap_list+=(
+    '^https?\:\/\/localhost\:4000:'
+  )
 fi
+url_swap_arg="$(printf ",%s" "${url_swap_list[@]}")"
+url_swap_arg="${url_swap_arg:1}"
 
 file_ignore_list=(
   "$site_dir/documents/ExcelRTD/userguide.html"
@@ -26,15 +33,15 @@ url_ignore_arg="$(printf ",%s" "${url_ignore_list[@]}")"
 url_ignore_arg="${url_ignore_arg:1}"
 
 # NOTE: $(:) is a nop command that can be used like an inline comment.
-exec bundle exec htmlproofer  $site_dir \
+exec bundle exec htmlproofer "$site_dir" \
   --check-html \
   --check-img-http \
   $(: TODO: --enforce-https \ ) \
+  --url-swap "$url_swap_arg" \
   --file-ignore "$file_ignore_arg" \
   --url-ignore "$url_ignore_arg" \
   $(: 'Removing this adds almost 500 errors. Ignore for the time being.') \
   --empty-alt-ignore \
   $(: 'Sites like github.com wont like it if we make too many requests too quickly.') \
   --hydra-config '{ "max_concurrency": 1 }' \
-  --url-swap "$strip_url:" \
   "$@"
