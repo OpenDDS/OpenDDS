e6d75b5473a512e7ae95dfbc349f1bdfcdf63acd
Jiang Li <lij@objectcomputing.com>
2021-09-14 12:38:48 -0500

    remove repeated call to LogAddr::ip

--- dds/DCPS/RTPS/Spdp.cpp
+++ dds/DCPS/RTPS/Spdp.cpp
@@ -613,13 +613,13 @@ Spdp::get_ice_endpoint_if_added()
   return tport_->ice_endpoint_added_ ? tport_->get_ice_endpoint() : 0;
 }
 
-bool ip_in_locator_list(const ACE_INET_Addr& from, const DCPS::LocatorSeq& locators)
+bool ip_in_locator_list(const OpenDDS::DCPS::String& ip, const DCPS::LocatorSeq& locators)
 {
   bool locator_found = false;
   for (CORBA::ULong i = 0; i < locators.length(); ++i) {
     ACE_INET_Addr addr;
     if (locator_to_address(addr, locators[i], true) == 0) {
-      if (DCPS::LogAddr::ip(from) == DCPS::LogAddr::ip(addr)) {
+      if (ip == DCPS::LogAddr::ip(addr)) {
         locator_found = true;
         break;
       }
@@ -980,7 +980,7 @@ Spdp::data_received(const DataSubmessage& data,
     return;
   }
 
-  if (!ip_in_locator_list(from, pdata.participantProxy.metatrafficUnicastLocatorList)) {
+  if (!ip_in_locator_list(DCPS::LogAddr::ip(from), pdata.participantProxy.metatrafficUnicastLocatorList)) {
     ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) Spdp::data_received - IP not in locator list: %C\n"), DCPS::LogAddr(from).c_str()));
     return;
   }
