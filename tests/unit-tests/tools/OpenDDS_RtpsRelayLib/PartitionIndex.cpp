7f5bccb6d5736adb15b13016c05838821c38110f
Justin R. Wilson <wilsonj@objectcomputing.com>
2021-10-21 12:09:04 -0500

    Fix tests

--- tests/unit-tests/tools/OpenDDS_RtpsRelayLib/PartitionIndex.cpp
+++ tests/unit-tests/tools/OpenDDS_RtpsRelayLib/PartitionIndex.cpp
@@ -29,7 +29,7 @@ TEST(tools_rtpsrelay_lib_PartitionIndex, maintest)
 
   {
     // Test the empty string.
-    PartitionIndex<GuidSet, GuidTransformer> pi;
+    PartitionIndex<GuidSet, GuidToParticipantGuid> pi;
     pi.insert("", guid1);
 
     GuidSet expected;
@@ -62,7 +62,7 @@ TEST(tools_rtpsrelay_lib_PartitionIndex, maintest)
 
   {
     // Test a literal.
-    PartitionIndex<GuidSet, GuidTransformer> pi;
+    PartitionIndex<GuidSet, GuidToParticipantGuid> pi;
     pi.insert("apple", guid1);
 
     GuidSet expected;
@@ -99,7 +99,7 @@ TEST(tools_rtpsrelay_lib_PartitionIndex, maintest)
 
   {
     // Test a pattern.
-    PartitionIndex<GuidSet, GuidTransformer> pi;
+    PartitionIndex<GuidSet, GuidToParticipantGuid> pi;
     pi.insert("**a[pq][!rs]?**", guid1);
 
     GuidSet expected;
@@ -136,7 +136,7 @@ TEST(tools_rtpsrelay_lib_PartitionIndex, maintest)
 
   {
     // Test multiple literals and patterns.
-    PartitionIndex<GuidSet, GuidTransformer> pi;
+    PartitionIndex<GuidSet, GuidToParticipantGuid> pi;
     pi.insert("apple", guid1);
     pi.insert("orange", guid2);
     pi.insert("*a*e*", guid3);
