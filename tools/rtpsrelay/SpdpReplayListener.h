78433f178923a7e153f10746be7e814f68238619
Fred Hornsey <hornseyf@objectcomputing.com>
2021-10-28 20:50:38 -0500

    Add Counts of Remote Relay Publications to Stats
    To make sure clusters of relays are communicating with each other, add
    count of matches for the SPDP Relay, Relay Partitions, and Relay
    Addresses DataReaders to the Relay Statistics.

--- tools/rtpsrelay/SpdpReplayListener.h
+++ tools/rtpsrelay/SpdpReplayListener.h
@@ -3,17 +3,22 @@
 
 #include "ListenerBase.h"
 #include "RelayHandler.h"
+#include "RelayStatisticsReporter.h"
 
 namespace RtpsRelay {
 
 class SpdpReplayListener : public ListenerBase {
 public:
-  SpdpReplayListener(SpdpHandler& spdp_handler);
+  SpdpReplayListener(SpdpHandler& spdp_handler,
+    RelayStatisticsReporter& relay_statistics_reporter);
 
 private:
-  void on_data_available(DDS::DataReader_ptr /*reader*/) override;
+  void on_data_available(DDS::DataReader_ptr reader) override;
+  void on_subscription_matched(DDS::DataReader_ptr reader,
+                               const DDS::SubscriptionMatchedStatus& status) override;
 
   SpdpHandler& spdp_handler_;
+  RelayStatisticsReporter& relay_statistics_reporter_;
 };
 
 }
