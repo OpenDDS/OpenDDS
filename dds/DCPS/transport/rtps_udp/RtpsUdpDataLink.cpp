14aa88bbbaeb1e89d1075f8c534c8bf44bd94787
Timothy Simpson <simpsont@objectcomputing.com>
2021-11-10 14:36:29 +0000

    fix inversion issue

--- dds/DCPS/transport/rtps_udp/RtpsUdpDataLink.cpp
+++ dds/DCPS/transport/rtps_udp/RtpsUdpDataLink.cpp
@@ -1385,9 +1385,9 @@ RtpsUdpDataLink::customize_queue_element(TransportQueueElement* element)
   const RepoId pub_id = element->publication_id();
   GUIDSeq_var peers = peer_ids(pub_id);
 
-  ACE_GUARD_RETURN(ACE_Thread_Mutex, guard, writers_lock_, 0);
-
   bool require_iq = requires_inline_qos(peers);
+
+  ACE_GUARD_RETURN(ACE_Thread_Mutex, guard, writers_lock_, 0);
 
   const RtpsWriterMap::iterator rw = writers_.find(pub_id);
   MetaSubmessageVec meta_submessages;
