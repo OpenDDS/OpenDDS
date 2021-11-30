d61cc46bad06ffe7b9b02324e996cfcd34d392f6
Fred Hornsey <hornseyf@objectcomputing.com>
2021-11-30 00:20:50 -0600

    Cleanup and Improve dynamic-data-monitor
    - Cleaned up unused code.
    - Setup RTPS programmatically, so an ini file isn't always required.
    - Put dynamic-data-monitor in `$DDS_ROOT/bin`.
    - Improved argument parsing:
      - Replaced `ACE_Arg_Shifter` with a more systematic approach with
        stricter checks loosely based on argument parsing in the bench 2
        programs.
      - Added a `--help` message.
      - Added a `--writer-count` option to display the number of writers
        when they associate and disassociate.

--- tools/dynamicdatamonitor/DynamicDataMonitor.cpp
+++ tools/dynamicdatamonitor/DynamicDataMonitor.cpp
@@ -9,55 +9,206 @@
 #include <dds/DCPS/Marked_Default_Qos.h>
 #include <dds/DCPS/Service_Participant.h>
 #include <dds/DCPS/XTypes/DynamicData.h>
+#include <dds/DCPS/RTPS/RtpsDiscovery.h>
+#include <dds/DCPS/transport/framework/TransportRegistry.h>
+#include <dds/DCPS/transport/framework/TransportConfig.h>
+#include <dds/DCPS/transport/framework/TransportInst.h>
 #if defined ACE_AS_STATIC_LIBS && !defined OPENDDS_SAFETY_PROFILE
 #  include <dds/DCPS/RTPS/RtpsDiscovery.h>
 #  include <dds/DCPS/transport/rtps_udp/RtpsUdp.h>
 #endif
 
-#include <ace/Arg_Shifter.h>
-#include <ace/Semaphore.h>
-#include <ace/Thread_Semaphore.h>
-
 #include <iostream>
 #include <sstream>
 
 using namespace OpenDDS::DCPS;
+using namespace OpenDDS::RTPS;
 
-long num_samples = 0;
-long num_seconds = 0;
-// parse the command line arguments
-int parse_args (int& argc, ACE_TCHAR* argv[])
+typedef std::vector<std::string> Args;
+typedef Args::iterator ArgsIt;
+typedef std::pair<bool, ArgsIt> AfterOpt;
+
+AfterOpt get_option(Args& args, const std::string& opt)
 {
-  u_long mask =  ACE_LOG_MSG->priority_mask(ACE_Log_Msg::PROCESS);
-  ACE_LOG_MSG->priority_mask(mask | LM_TRACE | LM_DEBUG, ACE_Log_Msg::PROCESS);
-  ACE_Arg_Shifter arg_shifter(argc, argv);
-
-  while (arg_shifter.is_anything_left()) {
-    // options:
-    //  -samples num_samples       defaults to infinite
-    //  -time num_seconds          defaults to infinite
-
-    const ACE_TCHAR* currentArg = 0;
-
-    if ((currentArg = arg_shifter.get_the_parameter(ACE_TEXT("--samples"))) != 0) {
-      num_samples = ACE_OS::atoi(currentArg);
-      arg_shifter.consume_arg();
-    } else if ((currentArg = arg_shifter.get_the_parameter(ACE_TEXT("--time"))) != 0) {
-      num_seconds = ACE_OS::atoi(currentArg);
-      arg_shifter.consume_arg();
-    } else {
-      arg_shifter.ignore_arg();
+  for (ArgsIt args_it = args.begin(); args_it != args.end(); ++args_it) {
+    if (*args_it == "--") {
+      // Remaining arguments should be interpreted as positional arguments
+      return AfterOpt(false, args.end());
+    }
+    if (opt.length() <= args_it->length()) {
+      const bool starts_with = args_it->substr(0, opt.length()) == opt;
+      const bool same_size = args_it->length() == opt.length();
+      if (starts_with && !same_size && args_it->at(opt.length()) == '=') {
+        // Form is OPT=VALUE, replace current with VALUE
+        *args_it = args_it->substr(opt.length() + 1);
+        return AfterOpt(true, args_it);
+      } else if (same_size && starts_with) {
+        // Form is OPT VALUE?, return next argument
+        return AfterOpt(true, args.erase(args_it));
+      }
     }
   }
-  // Indicates successful parsing of the command line
+  return AfterOpt(false, args.end());
+}
+
+bool has_option(Args& args, const std::string& opt)
+{
+  return get_option(args, opt).first;
+}
+
+std::string get_option_argument(Args& args, AfterOpt& after_opt, const std::string& opt)
+{
+  ArgsIt& args_it = after_opt.second;
+  if (args_it == args.end()) {
+    std::cerr << "ERROR: Option " << opt << " requires an argument" << std::endl;
+    throw 1;
+  }
+  const std::string value = *args_it;
+  args_it = args.erase(args_it);
+  return value;
+}
+
+unsigned get_option_argument_uint(Args& args, AfterOpt& after_opt, const std::string& opt)
+{
+  unsigned value;
+  try {
+    value = static_cast<unsigned>(std::stoul(get_option_argument(args, after_opt, opt)));
+  } catch (const std::exception&) {
+    std::cerr << "ERROR: Option " << opt
+      << " requires an argument that's a valid positive number" << std::endl;
+    throw 1;
+  }
+  return value;
+}
+
+void check_for_unknown_options(Args& args)
+{
+  for (ArgsIt args_it = args.begin(); args_it != args.end(); ++args_it) {
+    if (*args_it == "--") {
+      // Remaining arguments should be interpreted as positional arguments
+      return;
+    }
+    if (args_it->length() && args_it->at(0) == '-') {
+      std::cerr << "ERROR: Option " << *args_it << " is invalid" << std::endl;
+      throw 1;
+    }
+  }
+}
+
+unsigned get_pos_argument_uint(Args& args, size_t pos, const std::string& what)
+{
+  unsigned value;
+  try {
+    value = static_cast<unsigned>(std::stoul(args[pos]));
+  } catch (const std::exception&) {
+    std::cerr << "ERROR: positional argument " << what
+      << " should be a valid positive number" << std::endl;
+    throw 1;
+  }
+  return value;
+}
+
+std::string prog_name;
+
+std::string topic_name;
+std::string type_name;
+DDS::DomainId_t domainid = 0;
+unsigned num_samples = 0;
+unsigned num_seconds = 0;
+bool help = false;
+bool writer_count = false;
+
+void print_usage(bool for_error = false)
+{
+  std::ostream& os = for_error ? std::cout : std::cerr;
+  os <<
+    "usage: " << prog_name << " [OPTIONS] TOPIC_NAME TYPE_NAME DOMAIN_ID\n"
+    "usage: " << prog_name << " [--help|-h]\n";
+  if (for_error) {
+    os << "See -h for more details\n";
+  }
+}
+
+void print_help()
+{
+  print_usage();
+  std::cout <<
+    "\n"
+    "Print samples written to the given topic when the remotes support DDS XTypes\n"
+    "complete TypeObjects.\n"
+    "\n"
+    "Positional Arguments:\n"
+    "  TOPIC_NAME              The name of the topic to listen for.\n"
+    "  TYPE_NAME               The full name (including any modules) of the topic\n"
+    "                          type.\n"
+    "  DOMAIN_ID               The DDS Domain to participant in.\n"
+    "\n"
+    "Options\n"
+    "  -h | --help             Displays this message.\n"
+    "  -w | --writer-count     Print number of associated writers when they change.\n"
+    "                          Default is not to.\n"
+    "  --samples COUNT         Wait for at least this number of samples and exit.\n"
+    "                          May actually print more. Default is to print samples\n"
+    "                          forever.\n"
+    "  --time SECONDS          Print samples for an ammount of seconds and exit.\n"
+    "                          Default to print samples forever.\n";
+}
+
+// parse the command line arguments
+int parse_args(int argc, ACE_TCHAR* argv[])
+{
+  unsigned long mask = ACE_LOG_MSG->priority_mask(ACE_Log_Msg::PROCESS);
+  ACE_LOG_MSG->priority_mask(mask | LM_TRACE | LM_DEBUG, ACE_Log_Msg::PROCESS);
+
+  prog_name = ACE_TEXT_ALWAYS_CHAR(argv[0]);
+  std::vector<std::string> args;
+  for (int i = 1; i < argc; ++i) {
+    args.push_back(ACE_TEXT_ALWAYS_CHAR(argv[i]));
+  }
+
+  try {
+    // Parse options
+    if (has_option(args, "-h") || has_option(args, "--help")) {
+      print_help();
+      std::exit(0);
+    }
+    writer_count = has_option(args, "-w") || has_option(args, "--writer-count");
+    {
+      const std::string opt = "--samples";
+      AfterOpt after_opt = get_option(args, opt);
+      if (after_opt.first) {
+        num_samples = get_option_argument_uint(args, after_opt, opt);
+      }
+    }
+    {
+      const std::string opt = "--time";
+      AfterOpt after_opt = get_option(args, opt);
+      if (after_opt.first) {
+        num_seconds = get_option_argument_uint(args, after_opt, opt);
+      }
+    }
+    check_for_unknown_options(args);
+
+    // Parse positional arguments
+    if (args.size() != 3) {
+      std::cerr << "ERROR: incorrect number of arguments: expected 3, received " << args.size() << "\n";
+      throw 1;
+    }
+    topic_name = args[0];
+    type_name = args[1];
+    domainid = get_pos_argument_uint(args, 2, "DOMAIN_ID");
+  } catch(int value) {
+    print_usage(true);
+    return value;
+  }
+
   return 0;
 }
 
-class TestRecorderListener : public RecorderListener {
+class RecorderListenerImpl : public RecorderListener {
 public:
-  explicit TestRecorderListener(DDS::GuardCondition_var gc)
-    : sem_(0)
-    , sample_count_(0)
+  explicit RecorderListenerImpl(DDS::GuardCondition_var gc)
+    : sample_count_(0)
     , gc_(gc)
   {
   }
@@ -69,7 +220,7 @@ public:
     String my_type;
     String indent;
     if (!OpenDDS::XTypes::print_dynamic_data(dd, my_type, indent)) {
-      ACE_ERROR((LM_ERROR, "(%P|%t) ERROR: TestRecorderListener::on_sample_data_received: "
+      ACE_ERROR((LM_ERROR, "(%P|%t) ERROR: RecorderListenerImpl::on_sample_data_received: "
         "Failed to read dynamic data\n"));
     }
     std::cout << my_type;
@@ -82,56 +233,45 @@ public:
   virtual void on_recorder_matched(Recorder*,
                                    const ::DDS::SubscriptionMatchedStatus& status)
   {
-    if (status.current_count == 1 && DCPS_debug_level >= 4) {
-        ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) TestRecorderListener::on_recorder_matched:"
-          " a writer connected to recorder\n")));
-    } else if (status.current_count == 0 && status.total_count > 0) {
-      if (DCPS_debug_level >= 4) {
-        ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) TestRecorderListener::on_recorder_matched:"
-          " a writer disconnected with recorder\n")));
-      }
-      sem_.release();
+    if (writer_count) {
+      std::cout << "Listening to " << status.current_count << " writer(s)\n";
     }
   }
 
-  int wait(const ACE_Time_Value& tv)
-  {
-    ACE_Time_Value timeout = ACE_OS::gettimeofday() + tv;
-    return sem_.acquire(timeout);
-  }
-
-  long sample_count() const
+  unsigned sample_count() const
   {
     return sample_count_;
   }
+
 private:
-  ACE_Thread_Semaphore sem_;
-  long sample_count_;
+  unsigned sample_count_;
   DDS::GuardCondition_var gc_;
 };
 
-
-int run_test(int argc, ACE_TCHAR* argv[])
+int run(int argc, ACE_TCHAR* argv[])
 {
   int ret_val = 0;
   try {
+    TransportConfig_rch transport_config =
+      TheTransportRegistry->create_config("default_rtps_transport_config");
+    TransportInst_rch transport_inst =
+      TheTransportRegistry->create_inst("default_rtps_transport", "rtps_udp");
+    transport_config->instances_.push_back(transport_inst);
+    TheTransportRegistry->global_config(transport_config);
+
     DDS::DomainParticipantFactory_var dpf =
       TheParticipantFactoryWithArgs(argc, argv);
-    parse_args(argc, argv);
-    if (argc != 4) {
-      if (log_level >= LogLevel::Error) {
-        ACE_ERROR((LM_ERROR,
-                   "(%P|%t) ERROR: main():"
-                   " dynamic-data-monitor was passed an incorrect number of arguments:"
-                   " expected 3, received %d\n"
-                   "Format should be ./dynamic-data-monitor [topic name] [type name] [domain id]\n", argc-1));
-      }
-      return 1;
+    ret_val = parse_args(argc, argv);
+    if (ret_val) {
+      return ret_val;
     }
-    const ACE_TCHAR* topic_name = argv[1];
-    const ACE_TCHAR* type_name = argv[2];
-    DDS::DomainId_t domainid = ACE_OS::atoi(argv[3]);
-    Service_Participant* service = TheServiceParticipant;
+
+    OpenDDS::RTPS::RtpsDiscovery_rch disc =
+      make_rch<OpenDDS::RTPS::RtpsDiscovery>("rtps_disc");
+    disc->use_xtypes(OpenDDS::RTPS::RtpsDiscoveryConfig::XTYPES_COMPLETE);
+    Service_Participant* const service = TheServiceParticipant;
+    service->add_discovery(static_rchandle_cast<Discovery>(disc));
+    service->set_repo_domain(domainid, disc->key());
 
     // Create DomainParticipant
     DDS::DomainParticipant_var participant =
@@ -153,8 +293,8 @@ int run_test(int argc, ACE_TCHAR* argv[])
     {
       DDS::Topic_var topic =
         service->create_typeless_topic(participant,
-                                       ACE_TEXT_ALWAYS_CHAR(topic_name),
-                                       ACE_TEXT_ALWAYS_CHAR(type_name),
+                                       topic_name.c_str(),
+                                       type_name.c_str(),
                                        true,
                                        TOPIC_QOS_DEFAULT,
                                        0,
@@ -167,7 +307,7 @@ int run_test(int argc, ACE_TCHAR* argv[])
         return 1;
       }
 
-      RcHandle<TestRecorderListener> recorder_listener = make_rch<TestRecorderListener>(gc);
+      RcHandle<RecorderListenerImpl> recorder_listener = make_rch<RecorderListenerImpl>(gc);
 
       DDS::SubscriberQos sub_qos;
       participant->get_default_subscriber_qos(sub_qos);
@@ -176,6 +316,7 @@ int run_test(int argc, ACE_TCHAR* argv[])
       dr_qos.representation.value.length(1);
       dr_qos.representation.value[0] = DDS::XCDR2_DATA_REPRESENTATION;
       dr_qos.reliability.kind = DDS::RELIABLE_RELIABILITY_QOS;
+
       // Create Recorder
       Recorder_var recorder =
         service->create_recorder(participant,
@@ -183,13 +324,13 @@ int run_test(int argc, ACE_TCHAR* argv[])
                                  sub_qos,
                                  dr_qos,
                                  recorder_listener);
-
       if (!recorder.in()) {
         if (log_level >= LogLevel::Error) {
           ACE_ERROR((LM_ERROR, "(%P|%t) ERROR: main(): create_recorder failed!\n"));
         }
         return 1;
       }
+
       DDS::Duration_t timeout = { DDS::DURATION_INFINITE_SEC, DDS::DURATION_INFINITE_NSEC };
       if (num_seconds > 0) {
         timeout.sec = num_seconds;
@@ -203,6 +344,7 @@ int run_test(int argc, ACE_TCHAR* argv[])
         }
         return 1;
       }
+
       ws->detach_condition(gc);
       service->delete_recorder(recorder);
     }
@@ -224,7 +366,9 @@ int run_test(int argc, ACE_TCHAR* argv[])
 
 int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
 {
-  int ret = run_test(argc, argv);
-  ACE_Thread_Manager::instance()->wait();
+  int ret = run(argc, argv);
+  if (!ret) {
+    ACE_Thread_Manager::instance()->wait();
+  }
   return ret;
 }
