f265146916c4507cc1655ea389f7960526888bb8
Adam Mitz <mitza@objectcomputing.com>
2022-01-11 22:16:13 -0600

    Merge pull request #3266 from iguessthislldo/igtd/more-secure-static
    Fix RtpsRelay Smoke Test for Static Builds
    
    (cherry picked from commit 6533f4674af42add79f633994193a47f5465fa75)
    
    # Conflicts:
    #	performance-tests/bench/worker/Bench_Worker.mpc

--- performance-tests/bench/worker/main.cpp
+++ performance-tests/bench/worker/main.cpp
@@ -1,18 +1,3 @@
-#include "dds/DCPS/Service_Participant.h"
-
-#include <ace/Proactor.h>
-#ifdef ACE_HAS_AIO_CALLS
-#include <ace/POSIX_CB_Proactor.h>
-#endif
-#include <dds/DCPS/transport/framework/TransportRegistry.h>
-
-#ifdef ACE_AS_STATIC_LIBS
-#include <dds/DCPS/transport/rtps_udp/RtpsUdp.h>
-#include <dds/DCPS/transport/udp/Udp.h>
-#include <dds/DCPS/transport/tcp/Tcp.h>
-#include <dds/DCPS/transport/multicast/Multicast.h>
-#endif
-
 #include "BenchC.h"
 #ifdef __GNUC__
 #pragma GCC diagnostic push
@@ -55,15 +40,33 @@
 #include "WorkerParticipantListener.h"
 #include "WriteAction.h"
 
+#include <util.h>
+#include <json_conversion.h>
+
+#include <dds/DCPS/Service_Participant.h>
+#include <dds/DCPS/transport/framework/TransportRegistry.h>
+#ifdef ACE_AS_STATIC_LIBS
+#  include <dds/DCPS/RTPS/RtpsDiscovery.h>
+#  include <dds/DCPS/transport/rtps_udp/RtpsUdp.h>
+#  include <dds/DCPS/transport/udp/Udp.h>
+#  include <dds/DCPS/transport/tcp/Tcp.h>
+#  include <dds/DCPS/transport/multicast/Multicast.h>
+#  ifdef OPENDDS_SECURITY
+#    include <dds/DCPS/security/BuiltInPlugins.h>
+#  endif
+#endif
+
+#include <ace/Proactor.h>
+#ifdef ACE_HAS_AIO_CALLS
+#  include <ace/POSIX_CB_Proactor.h>
+#endif
+
 #include <cmath>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <thread>
-
-#include <util.h>
-#include <json_conversion.h>
 
 using Builder::Log;
 using Builder::ZERO;
