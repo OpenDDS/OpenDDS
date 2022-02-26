101ab32ee1f3ba59944dd95bef122f8a2b64ff79
Adam Mitz <mitza@objectcomputing.com>
2022-02-26 12:54:14 -0600

    Merge pull request #3367 from iguessthislldo/igtd/cmake-install
    Fix Scoreboard Issues with CMake Install Test
    
    (cherry picked from commit fbfce178b9f8d47d8ba0b22ae42cf901a2f2fe55)

--- tools/scripts/install_test_framework.pl
+++ tools/scripts/install_test_framework.pl
@@ -8,14 +8,23 @@ use warnings;
 use File::Basename qw(dirname basename);
 use File::Copy qw(copy);
 use File::Path qw(make_path);
+use Module::Load::Conditional qw(can_load);
+use FindBin;
 
 BEGIN {
   require lib;
-  if (exists($ENV{ACE_ROOT}) && exists($ENV{DDS_ROOT})) {
+  if (exists($ENV{ACE_ROOT}) && -d "$ENV{ACE_ROOT}/bin/PerlACE") {
     lib->import("$ENV{ACE_ROOT}/bin");
-    lib->import("$ENV{DDS_ROOT}/bin");
+  }
+  elsif (exists($ENV{ACE_SOURCE_ROOT}) && -d "$ENV{ACE_SOURCE_ROOT}/bin/PerlACE") {
+    lib->import("$ENV{ACE_SOURCE_ROOT}/bin");
+  }
+  if (!can_load(modules => {'PerlACE::Process' => undef})) {
+    die("ERROR: Couldn't load PerlACE::Process\n" .
+      "Set ACE_ROOT or ACE_SOURCE_ROOT to the ACE source or add PerlACE to PERL5LIB");
   }
 }
+use lib "$FindBin::RealBin/../../bin";
 use PerlDDS::Process;
 
 die("ERROR: please pass the install prefix") unless (scalar(@ARGV) == 1);
