f382cc6ba063d42bae7ebfab581f89c0beae75b0
Adam Mitz <mitza@objectcomputing.com>
2022-04-21 08:44:07 -0500

    Merge pull request #3440 from simpsont-oci/more_thrasher_cleanup
    More Thrasher Cleanup, CI Coverage Expansion
    
    (cherry picked from commit 428eadc9b301f19c1f56a66edee37bbdcccc56c0)

--- tests/DCPS/Thrasher/DataReaderListenerImpl.h
+++ tests/DCPS/Thrasher/DataReaderListenerImpl.h
@@ -18,23 +18,24 @@
 class DataReaderListenerImpl : public virtual OpenDDS::DCPS::LocalObject<DDS::DataReaderListener>
 {
 public:
-  DataReaderListenerImpl(const std::size_t expected_samples, const char* progress_fmt);
+  DataReaderListenerImpl(size_t publisher_count, size_t samples_per_publisher, const char* progress_fmt);
   virtual ~DataReaderListenerImpl();
 
   void wait_received() const;
-  int check_received(const size_t n_publishers) const;
+  int check_received() const;
   virtual void on_data_available(DDS::DataReader_ptr reader);
 
-  virtual void on_requested_deadline_missed(DDS::DataReader_ptr, const DDS::RequestedDeadlineMissedStatus&){}
-  virtual void on_requested_incompatible_qos(DDS::DataReader_ptr, const DDS::RequestedIncompatibleQosStatus&){}
-  virtual void on_liveliness_changed(DDS::DataReader_ptr, const DDS::LivelinessChangedStatus&){}
-  virtual void on_subscription_matched(DDS::DataReader_ptr, const DDS::SubscriptionMatchedStatus&){}
-  virtual void on_sample_rejected(DDS::DataReader_ptr, const DDS::SampleRejectedStatus&){}
-  virtual void on_sample_lost(DDS::DataReader_ptr, const DDS::SampleLostStatus&){}
+  virtual void on_requested_deadline_missed(DDS::DataReader_ptr, const DDS::RequestedDeadlineMissedStatus&) {}
+  virtual void on_requested_incompatible_qos(DDS::DataReader_ptr, const DDS::RequestedIncompatibleQosStatus&) {}
+  virtual void on_liveliness_changed(DDS::DataReader_ptr, const DDS::LivelinessChangedStatus&) {}
+  virtual void on_subscription_matched(DDS::DataReader_ptr, const DDS::SubscriptionMatchedStatus&) {}
+  virtual void on_sample_rejected(DDS::DataReader_ptr, const DDS::SampleRejectedStatus&) {}
+  virtual void on_sample_lost(DDS::DataReader_ptr, const DDS::SampleLostStatus&) {}
 
 private:
-  typedef OPENDDS_MAP(size_t, OPENDDS_SET(size_t)) TaskSamplesMap;
-  bool received_all(const size_t x, const size_t y);
+  typedef OPENDDS_MAP(size_t, OPENDDS_SET(size_t)) ReceivedSamplesMap;
+  bool received_all() const;
+  bool update_and_check(size_t x, size_t y);
 #ifdef ACE_HAS_CPP11
   typedef std::mutex Mutex;
   typedef std::condition_variable Condition;
@@ -46,9 +47,13 @@ private:
 #endif
   mutable Mutex mutex_;
   mutable Condition condition_;
-  const size_t expected_samples_;
-  size_t received_samples_;
-  TaskSamplesMap task_samples_map_;
+  const size_t publisher_count_;
+  const size_t samples_per_publisher_;
+  const size_t maximum_possible_samples_;
+  const size_t expected_total_samples_;
+  const size_t expected_samples_per_publisher_;
+  size_t received_total_samples_;
+  ReceivedSamplesMap received_samples_map_;
   ProgressIndicator progress_;
 };
 
--- tests/DCPS/Thrasher/Subscriber.cpp
+++ tests/DCPS/Thrasher/Subscriber.cpp
@@ -16,9 +16,8 @@
 
 #include <stdexcept>
 
-Subscriber::Subscriber(const DDS::DomainId_t domainId, const std::size_t n_pub_threads, const std::size_t expected_samples, const bool durable)
+Subscriber::Subscriber(DDS::DomainId_t domainId, size_t n_pub_threads, size_t expected_samples, bool durable)
   : domainId_(domainId)
-  , n_pub_threads_(n_pub_threads)
   , expected_samples_(expected_samples)
   , durable_(durable)
 {
@@ -47,7 +46,7 @@ Subscriber::Subscriber(const DDS::DomainId_t domainId, const std::size_t n_pub_t
       throw std::runtime_error("create_subscriber failed.");
     }
     // Create DataReader
-    listener_i_ = new DataReaderListenerImpl(expected_samples_, "(%P|%t)  sub %d%% (%d samples received)\n");
+    listener_i_ = new DataReaderListenerImpl(n_pub_threads, expected_samples_ / n_pub_threads, "(%P|%t)  sub %d%% (%d samples received)\n");
     listener_ = listener_i_;
     if (!listener_) {
       throw std::runtime_error("DataReaderListenerImpl creation failed.");
@@ -60,7 +59,9 @@ Subscriber::Subscriber(const DDS::DomainId_t domainId, const std::size_t n_pub_t
     }
 #ifndef OPENDDS_NO_OWNERSHIP_PROFILE
     qos.history.kind = DDS::KEEP_ALL_HISTORY_QOS;
+    qos.history.depth = static_cast<CORBA::Long>(expected_samples * 2);
 #endif
+
     reader_ = sub->create_datareader(topic.in(), qos, listener_.in(), OpenDDS::DCPS::DEFAULT_STATUS_MASK);
     if (!reader_) {
       throw std::runtime_error("create_datareader failed.");
@@ -105,5 +106,5 @@ void Subscriber::cleanup()
 int Subscriber::wait_and_check_received() const
 {
   listener_i_->wait_received();
-  return listener_i_->check_received(n_pub_threads_);
+  return listener_i_->check_received();
 }
