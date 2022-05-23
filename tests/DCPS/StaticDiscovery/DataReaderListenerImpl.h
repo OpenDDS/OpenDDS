181f684ccf73b1fea4680dd000bc6e6c1b82e919
Adam Mitz <mitza@objectcomputing.com>
2022-05-23 14:56:21 -0500

    Merge pull request #3517 from jrw972/opendds-557
    StaticDiscovery test uses publication handles
    
    (cherry picked from commit aea73447fb5492d60c59481c2654be8a27b6c430)

--- tests/DCPS/StaticDiscovery/DataReaderListenerImpl.h
+++ tests/DCPS/StaticDiscovery/DataReaderListenerImpl.h
@@ -11,6 +11,7 @@
 #include <dds/DdsDcpsSubscriptionC.h>
 #include <dds/DCPS/LocalObject.h>
 #include <dds/DCPS/Definitions.h>
+#include <dds/DCPS/GuidUtils.h>
 
 #include <string>
 #include <vector>
@@ -22,11 +23,10 @@ typedef void (*callback_t)(bool);
 class DataReaderListenerImpl
   : public virtual OpenDDS::DCPS::LocalObject<DDS::DataReaderListener> {
 public:
-  DataReaderListenerImpl(const std::string& id, bool reliable, bool expect_all_samples, const std::vector<std::string>& writers, const int total_writers, const int expected_samples, callback_t done_callback, DDS::Subscriber_ptr subscriber, bool check_bits)
+  DataReaderListenerImpl(const std::string& id, bool reliable, bool expect_all_samples, const int total_writers, const int expected_samples, callback_t done_callback, DDS::Subscriber_ptr subscriber, bool check_bits)
     : id_(id)
     , reliable_(reliable)
     , expect_all_samples_(expect_all_samples)
-    , writers_(writers)
     , total_writers_(total_writers)
     , expected_samples_(expected_samples)
     , previous_count_(0)
@@ -85,14 +85,13 @@ private:
   std::string id_;
   bool reliable_;
   bool expect_all_samples_;
-  const std::vector<std::string>& writers_;
   const int total_writers_;
   const int expected_samples_;
   int previous_count_;
   int received_samples_;
   typedef std::set<int> SampleSet;
-  typedef std::map<int, SampleSet> SampleSetMap;
-  SampleSetMap ph_received_samples_;
+  typedef std::map<OpenDDS::DCPS::GUID_t, SampleSet, OpenDDS::DCPS::GUID_tKeyLessThan> SampleSetMap;
+  SampleSetMap guid_received_samples_;
   callback_t done_callback_;
   bool builtin_read_error_;
 #ifndef DDS_HAS_MINIMUM_BIT
