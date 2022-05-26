0de693a56f758ed959461d5c57e2db45308ab6fe
Timothy Simpson <simpsont@objectcomputing.com>
2022-05-26 14:47:02 -0500

    Add note for operator assumptions

--- dds/DCPS/transport/rtps_udp/RoutedGuidPair.h
+++ dds/DCPS/transport/rtps_udp/RoutedGuidPair.h
@@ -17,12 +17,16 @@ namespace DCPS {
 #pragma pack(push, 1)
 struct OpenDDS_Rtps_Udp_Export RoutedGuidPair {
   RoutedGuidPair(const GUID_t& src, const GUID_t dst) : src_(src), dst_(dst) {}
+
   bool operator<(const RoutedGuidPair& rhs) const {
     return std::memcmp(static_cast<const void*>(&src_), static_cast<const void*>(&rhs.src_), 2 * sizeof(GUID_t)) < 0;
   }
   bool operator==(const RoutedGuidPair& rhs) const {
     return std::memcmp(static_cast<const void*>(&src_), static_cast<const void*>(&rhs.src_), 2 * sizeof(GUID_t)) == 0;
   }
+
+  // Note: The comparison operators for RoutedGuidPair assume a tightly packed pair of GUIDs
+  // If you intend to modify or add to these members, you will also need to update the comparison operators defined above
   GUID_t src_;
   GUID_t dst_;
 };
