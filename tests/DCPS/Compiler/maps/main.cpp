7fe533a0883f9d0cd7f6f77b09e39e338bd3adb6
Tyler Mayoff <tyler@tylermayoff.com>
2022-07-10 15:30:01 -0400

    Updated tests

--- tests/DCPS/Compiler/maps/main.cpp
+++ tests/DCPS/Compiler/maps/main.cpp
@@ -6,22 +6,44 @@
 
 const OpenDDS::DCPS::Encoding encoding(OpenDDS::DCPS::Encoding::KIND_UNALIGNED_CDR);
 
-TEST(Maps, MapMarshalling) 
+template<typename T> 
+T TestMarshalling(OpenDDS::DCPS::Serializer strm, T original) {
+  strm << original;
+  T t;
+  strm >> t;
+
+  return t;
+}
+
+TEST(MapsMarshalling, MapIntInt)
 {
   OpenDDS::DCPS::Message_Block_Ptr b(new ACE_Message_Block(100000));
   OpenDDS::DCPS::Serializer strm(b.get(), encoding);
 
   Data expectedData;
-  expectedData.intTest(10);
   expectedData.intIntMap()[10] = 10;
 
-  strm << expectedData;
-
-  Data testData;
-  strm >> testData;
-
+  Data testData = TestMarshalling(strm, expectedData);
+  
   EXPECT_EQ(testData.intIntMap(), expectedData.intIntMap());
   EXPECT_EQ(testData.intIntMap()[10], expectedData.intIntMap()[10]);
+}
+
+TEST(MapsMarshalling, MapStringStrct)
+{
+  OpenDDS::DCPS::Message_Block_Ptr b(new ACE_Message_Block(100000));
+  OpenDDS::DCPS::Serializer strm(b.get(), encoding);
+
+  Data expectedData;
+
+  TestStruct stru;
+  stru.msg("World");
+  expectedData.stringStructsMap()["Hello"] = stru;
+
+  auto testData = TestMarshalling(strm, expectedData);
+
+  // EXPECT_EQ(testData.stringStructsMap(), expectedData.stringStructsMap());
+  EXPECT_EQ(testData.stringStructsMap()["Hello"].msg(), expectedData.stringStructsMap()["Hello"].msg());
 }
 
 int main(int argc, char* argv[])
