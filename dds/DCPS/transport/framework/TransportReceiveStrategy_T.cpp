6344d7df7bb9c540f42ba76a854d91d2776c6822
Adam Mitz <mitza@objectcomputing.com>
2022-11-02 09:04:39 -0500

    allow empty samples

--- dds/DCPS/transport/framework/TransportReceiveStrategy_T.cpp
+++ dds/DCPS/transport/framework/TransportReceiveStrategy_T.cpp
@@ -822,9 +822,11 @@ TransportReceiveStrategy<TH, DSH>::handle_dds_input(ACE_HANDLE fd)
         VDBG((LM_DEBUG,"(%P|%t) DBG:   "
               "Now dispatch the sample to the DataLink\n"));
 
-        ReceivedDataSample rds(*payload_);
-        payload_->release();
-        payload_ = 0;
+        ReceivedDataSample rds = payload_ ? ReceivedDataSample(*payload_) : ReceivedDataSample();
+        if (payload_) {
+          payload_->release();
+          payload_ = 0;
+        }
         if (this->data_sample_header_.into_received_data_sample(rds)) {
 
           if (this->data_sample_header_.more_fragments()
