727d1d59cdd07fe3c44dba92dfd04b71fce578fe
Chad Elliott <elliottc@objectcomputing.com>
2022-11-04 07:00:16 -0500

    Simplify statically linking the FACE and idl2jni plugins into opendds_idl without code changes. Modified to allow multiple plugins to be statically linked.

--- dds/idl/opendds_idl/static_mapping.cpp
+++ dds/idl/opendds_idl/static_mapping.cpp
@@ -3,21 +3,31 @@
 #if defined (ACE_AS_STATIC_LIBS)
 #include <ace/SString.h>
 
-#define STATICALLY_LINKED_LANGUAGE_MAPPING(NAME) \
-  extern "C" { LanguageMapping* opendds_idl_ ## NAME ## _allocator(); } \
-  BE_BuiltinInterface::language_mapping_allocator \
-  static_language_mapping(const ACE_TString& key) { \
-    if (key == #NAME) { \
-      return opendds_idl_ ## NAME ## _allocator; \
-    } \
-    return 0; \
+#define LANGUAGE_MAPPING_FWD(NAME) \
+  extern "C" { LanguageMapping* opendds_idl_ ## NAME ## _allocator(); }
+#define LANGUAGE_MAPPING_CHECK(NAME) \
+  if (key == #NAME) { \
+    return opendds_idl_ ## NAME ## _allocator; \
   }
 
-// In the event that you want to statically link the FACE language map into
-// opendds_idl, you will need to do two things:
-//   1) Uncomment the macro below.
-//   2) Add the FACE language map library to the link line for opendds_idl.
+// In the event that you want to add a statically linked the language map into
+// opendds_idl, you will need to do three things:
+//   1) Use the LANGUAGE_MAPPING_FWD macro to declare the language map.
+//   2) Use the LANGUAGE_MAPPING_CHECK macro inside the
+//      static_language_mapping function.
+//   3) Add the language map library to the link line for opendds_idl.
 //
-//STATICALLY_LINKED_LANGUAGE_MAPPING(face);
+
+#if defined(STATICALLY_LINK_FACE)
+LANGUAGE_MAPPING_FWD(face)
+#endif
+
+BE_BuiltinInterface::language_mapping_allocator
+static_language_mapping(const ACE_TString& key) {
+#if defined(STATICALLY_LINK_FACE)
+  LANGUAGE_MAPPING_CHECK(face)
+#endif
+  return 0;
+}
 
 #endif
--- dds/idl/opendds_idl/static_plugin.cpp
+++ dds/idl/opendds_idl/static_plugin.cpp
@@ -3,20 +3,31 @@
 #if defined (ACE_AS_STATIC_LIBS)
 #include <ace/SString.h>
 
-#define STATICALLY_LINKED_BACKEND_PLUGIN(NAME) \
-  extern "C" { BE_Interface* NAME ## _instance(); } \
-  interface_instance_t static_be_plugin(const ACE_TString& key) { \
-    if (key == #NAME) { \
-      return  NAME ## _instance; \
-    } \
-    return 0; \
+#define BACKEND_PLUGIN_FWD(NAME) \
+  extern "C" { BE_Interface* NAME ## _instance(); }
+#define BACKEND_PLUGIN_CHECK(NAME) \
+  if (key == #NAME) { \
+    return  NAME ## _instance; \
   }
 
-// In the event that you want to statically link the idl2jni backend into
-// opendds_idl, you will need to do two things:
-//   1) Uncomment the macro below.
-//   2) Add the idl2jni backend library to the link line for opendds_idl.
+// In the event that you want to add a statically linked backend into
+// opendds_idl, you will need to do three things:
+//   1) Use the BACKEND_PLUGIN_FWD macro to declared the backend plugin.
+//   2) Use the BACKEND_PLUGIN_CHECK macro inside the static_be_plugin
+//      function.
+//   3) Add the backend library to the link line for opendds_idl.
 //
-//STATICALLY_LINKED_BACKEND_PLUGIN(idl2jni);
+
+#if defined(STATICALLY_LINK_IDL2JNI)
+BACKEND_PLUGIN_FWD(idl2jni)
+#endif
+
+interface_instance_t static_be_plugin(const ACE_TString& key)
+{
+#if defined(STATICALLY_LINK_IDL2JNI)
+  BACKEND_PLUGIN_CHECK(idl2jni)
+#endif
+    return 0;
+}
 
 #endif
