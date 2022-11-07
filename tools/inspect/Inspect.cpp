0624c164e305e04b8ade8a44ad80b1116051d190
Adam Kutell <adamk@acm.org>
2022-11-07 09:01:52 -0600

    Move RtpsDiscoveryConfig into its own files (from RtpsDiscovery.cpp and .h)
    Move text into new files
    Modify files that depend on the class

--- tools/inspect/Inspect.cpp
+++ tools/inspect/Inspect.cpp
@@ -10,11 +10,11 @@
 #include <dds/DCPS/Service_Participant.h>
 #include <dds/DCPS/XTypes/DynamicDataImpl.h>
 #include <dds/DCPS/RTPS/RtpsDiscovery.h>
+#include <dds/DCPS/RTPS/RtpsDiscoveryConfig.h>
 #include <dds/DCPS/transport/framework/TransportRegistry.h>
 #include <dds/DCPS/transport/framework/TransportConfig.h>
 #include <dds/DCPS/transport/framework/TransportInst.h>
 #if defined ACE_AS_STATIC_LIBS && !defined OPENDDS_SAFETY_PROFILE
-#  include <dds/DCPS/RTPS/RtpsDiscovery.h>
 #  include <dds/DCPS/transport/rtps_udp/RtpsUdp.h>
 #endif
 
