c3857042856c5f83d38ca682657eead9530d7c67
Son Dinh <dinhs@objectcomputing.com>
2022-11-11 09:50:20 -0600

    Move DynamicDataBase functions to cpp

--- dds/DCPS/XTypes/DynamicDataBase.h
+++ dds/DCPS/XTypes/DynamicDataBase.h
@@ -7,10 +7,10 @@
 #define OPENDDS_DCPS_XTYPES_DYNAMIC_DATA_BASE_H
 
 #ifndef OPENDDS_SAFETY_PROFILE
+#  include "DynamicTypeImpl.h"
 
-#include "DynamicTypeImpl.h"
-
-#include <dds/DCPS/LocalObject.h>
+#  include <dds/DCPS/debug.h>
+#  include <dds/DCPS/LocalObject.h>
 
 OPENDDS_BEGIN_VERSIONED_NAMESPACE_DECL
 
@@ -19,145 +19,18 @@ namespace XTypes {
 
 class DynamicDataBase : public virtual DCPS::LocalObject<DDS::DynamicData> {
 public:
-  DynamicDataBase()
-  {}
-
-  DynamicDataBase(DDS::DynamicType_ptr type)
-    : type_(get_base_type(type))
-  {}
+  DynamicDataBase();
+  DynamicDataBase(DDS::DynamicType_ptr type);
 
   DDS::ReturnCode_t get_descriptor(DDS::MemberDescriptor*& value, MemberId id)
-  {
-    DDS::DynamicTypeMember_var dtm;
-    if (type_->get_member(dtm, id) != DDS::RETCODE_OK) {
-      return DDS::RETCODE_ERROR;
-    }
-    return dtm->get_descriptor(value);
-  }
-
-  DDS::MemberId get_member_id_by_name(const char* name)
-  {
-    const TypeKind tk = type_->get_kind();
-    switch (tk) {
-    case TK_BOOLEAN:
-    case TK_BYTE:
-    case TK_INT16:
-    case TK_INT32:
-    case TK_INT64:
-    case TK_UINT16:
-    case TK_UINT32:
-    case TK_UINT64:
-    case TK_FLOAT32:
-    case TK_FLOAT64:
-    case TK_FLOAT128:
-    case TK_INT8:
-    case TK_UINT8:
-    case TK_CHAR8:
-    case TK_CHAR16:
-    case TK_ENUM:
-      return MEMBER_ID_INVALID;
-    case TK_STRING8:
-    case TK_STRING16:
-    case TK_SEQUENCE:
-    case TK_ARRAY:
-      // Elements of string, sequence, array must be accessed by index.
-      return MEMBER_ID_INVALID;
-    case TK_MAP:
-      // Values in map can be accessed by strings which is converted from map keys.
-      // But need to find out how this conversion works. In the meantime, only allow
-      // accessing map using index.
-      return MEMBER_ID_INVALID;
-    case TK_BITMASK:
-    case TK_STRUCTURE:
-    case TK_UNION: {
-      DDS::DynamicTypeMember_var member;
-      if (type_->get_member_by_name(member, name) != DDS::RETCODE_OK) {
-        return MEMBER_ID_INVALID;
-      }
-      DDS::MemberDescriptor_var descriptor;
-      if (member->get_descriptor(descriptor) != DDS::RETCODE_OK) {
-        return MEMBER_ID_INVALID;
-      }
-      if (tk == TK_BITMASK) {
-        // Bitmask's flags don't have ID, so use index instead.
-        return descriptor->index();
-      } else {
-        return descriptor->id();
-      }
-    }
-    }
-    if (DCPS::log_level >= DCPS::LogLevel::Notice) {
-      ACE_ERROR((LM_NOTICE, "(%P|%t) NOTICE: DynamicDataBase::get_member_id_by_name:"
-                 " Calling on an unexpected type %C\n", typekind_to_string(tk)));
-    }
-  return MEMBER_ID_INVALID;
-  }
+  DDS::MemberId get_member_id_by_name(const char* name);
 
 protected:
-
   /// Verify that a given type is primitive or string or wstring.
   ///
-  bool is_type_supported(TypeKind tk, const char* func_name)
-  {
-    if (!is_primitive(tk) && tk != TK_STRING8 && tk != TK_STRING16) {
-      if (DCPS::log_level >= DCPS::LogLevel::Notice) {
-        ACE_ERROR((LM_NOTICE, "(%P|%t) NOTICE: DynamicDataXcdrReadImpl::is_type_supported:"
-                   " Called function %C on an unsupported type (%C)\n",
-                   func_name, typekind_to_string(tk)));
-      }
-      return false;
-    }
-    return true;
-  }
-
-  bool is_primitive(TypeKind tk) const
-  {
-    switch (tk) {
-    case TK_BOOLEAN:
-    case TK_BYTE:
-    case TK_INT8:
-    case TK_UINT8:
-    case TK_CHAR8:
-    case TK_INT16:
-    case TK_UINT16:
-    case TK_CHAR16:
-    case TK_INT32:
-    case TK_UINT32:
-    case TK_FLOAT32:
-    case TK_INT64:
-    case TK_UINT64:
-    case TK_FLOAT64:
-    case TK_FLOAT128:
-      return true;
-    default:
-      return false;
-    }
-  }
-
-  bool get_index_from_id(DDS::MemberId id, ACE_CDR::ULong& index, ACE_CDR::ULong bound) const
-  {
-    // The mapping from id to index must be consistent with get_member_id_at_index
-    // for these types. In particular, index and id are equal given that it doesn't
-    // go out of bound.
-    switch (type_->get_kind()) {
-    case TK_STRING8:
-    case TK_STRING16:
-    case TK_SEQUENCE:
-    case TK_MAP:
-      if (bound == 0 || id < bound) {
-        index = id;
-        return true;
-      }
-      break;
-    case TK_BITMASK:
-    case TK_ARRAY:
-      if (id < bound) {
-        index = id;
-        return true;
-      }
-    }
-    return false;
-  }
+  bool is_type_supported(TypeKind tk, const char* func_name);
+  bool is_primitive(TypeKind tk) const;
+  bool get_index_from_id(DDS::MemberId id, ACE_CDR::ULong& index, ACE_CDR::ULong bound) const;
 
   /// The actual (i.e., non-alias) DynamicType of the associated type.
   DDS::DynamicType_var type_;
