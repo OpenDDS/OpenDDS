aa7a1d51e29371cff2a1439375b49bade4e9747b
Chad Elliott <elliott_c@ociweb.com>
2022-12-15 08:13:21 -0600

    Added a wrapper header for gtest.h and gmock.h to disable the deprecated copy warning.

---
+++ tests/Utils/gtestWrapper.h
@@ -0,0 +1,17 @@
+#ifndef OPENDDS_TEST_GTESTWRAPPER_H
+#define OPENDDS_TEST_GTESTWRAPPER_H
+
+#ifdef __GNUC__
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wdeprecated-copy"
+#endif
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#ifdef __GNUC__
+#  pragma GCC diagnostic pop
+#endif
+
+#endif
+
