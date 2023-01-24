521feb434a5beaf3409cbb857df64b50730fdbfb
Adam Mitz <mitza@objectcomputing.com>
2023-01-24 09:36:17 -0600

    Merge pull request #3999 from simpsont-oci/rcobject_stress_test
    Add RcObject (et al.) Stress Test

---
+++ dds/DCPS/Atomic.h
@@ -0,0 +1,41 @@
+#ifndef OPENDDS_DCPS_ATOMIC_H
+#define OPENDDS_DCPS_ATOMIC_H
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#  pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#ifdef ACE_HAS_CPP11
+#  include <atomic>
+#else
+#  include <ace/Atomic_Op.h>
+#  include <ace/Thread_Mutex.h>
+#endif
+
+OPENDDS_BEGIN_VERSIONED_NAMESPACE_DECL
+
+namespace OpenDDS {
+namespace DCPS {
+
+#ifdef ACE_HAS_CPP11
+template <typename T>
+using Atomic = std::atomic<T>;
+#else
+template <typename T>
+class Atomic : public ACE_Atomic_Op<ACE_Thread_Mutex, T>
+{
+public:
+  typedef ACE_Atomic_Op<ACE_Thread_Mutex, T> Base;
+  Atomic() : Base() {}
+  explicit Atomic(T desired) : Base(desired) {}
+  inline operator T() const { return Base::value(); }
+};
+#endif
+
+} // DCPS
+} // OPENDDS
+
+OPENDDS_END_VERSIONED_NAMESPACE_DECL
+
+#endif /* end of include guard: OPENDDS_DCPS_ATOMIC_H */
+
--- dds/DCPS/RcObject.h
+++ dds/DCPS/RcObject.h
@@ -9,17 +9,12 @@
 #include "dds/Versioned_Namespace.h"
 
 #include "dcps_export.h"
+#include "Atomic.h"
 #include "PoolAllocationBase.h"
 #include "RcHandle_T.h"
 
 #include <ace/Guard_T.h>
 #include <ace/Synch_Traits.h>
-
-#ifdef ACE_HAS_CPP11
-#  include <atomic>
-#else
-#  include <ace/Atomic_Op.h>
-#endif
 
 OPENDDS_BEGIN_VERSIONED_NAMESPACE_DECL
 
@@ -31,10 +26,10 @@ namespace DCPS {
   class OpenDDS_Dcps_Export WeakObject : public PoolAllocationBase
   {
   public:
+
     WeakObject(RcObject* ptr)
-      : ref_count_(1)
-      , ptr_(ptr)
-      , expired_(false)
+      : ptr_(ptr)
+      , ref_count_(1)
     {
     }
 
@@ -53,7 +48,7 @@ namespace DCPS {
     }
 
     RcObject* lock();
-    bool set_expire();
+    bool check_expire(Atomic<long>& count);
 
   private:
 #ifdef ACE_HAS_CPP11
@@ -81,8 +76,7 @@ namespace DCPS {
 
     virtual void _remove_ref()
     {
-      const long new_count = --ref_count_;
-      if (new_count == 0 && weak_object_->set_expire()) {
+      if (weak_object_->check_expire(ref_count_)) {
         delete this;
       }
     }
@@ -90,11 +84,7 @@ namespace DCPS {
     /// This accessor is purely for debugging purposes
     long ref_count() const
     {
-#ifdef ACE_HAS_CPP11
       return ref_count_;
-#else
-      return ref_count_.value();
-#endif
     }
 
     WeakObject* _get_weak_object() const
@@ -110,12 +100,8 @@ namespace DCPS {
     {}
 
   private:
-#ifdef ACE_HAS_CPP11
-    std::atomic<long> ref_count_;
-#else
-    ACE_Atomic_Op<ACE_SYNCH_MUTEX, long> ref_count_;
-#endif
-    WeakObject*  weak_object_;
+    Atomic<long> ref_count_;
+    WeakObject* weak_object_;
 
     RcObject(const RcObject&);
     RcObject& operator=(const RcObject&);
@@ -124,20 +110,21 @@ namespace DCPS {
   inline RcObject* WeakObject::lock()
   {
     ACE_Guard<ACE_SYNCH_MUTEX> guard(mx_);
-    if (!expired_) {
+    if (ptr_) {
       ptr_->_add_ref();
-      return ptr_;
     }
-    return 0;
+    return ptr_;
   }
 
-  inline bool WeakObject::set_expire()
+  inline bool WeakObject::check_expire(Atomic<long>& count)
   {
     ACE_Guard<ACE_SYNCH_MUTEX> guard(mx_);
-    if (!expired_ && ptr_->ref_count() == 0) {
-      expired_ = true;
+    const long new_count = --count;
+    if (new_count == 0 && ptr_) {
+      ptr_ = 0;
+      return true;
     }
-    return expired_;
+    return false;
   }
 
   template <typename T>
