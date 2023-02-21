bcbf8077b0d6a02891674a7c59d91b8975b50750
Chad Elliott <elliottc@objectcomputing.com>
2023-02-21 12:33:23 -0600

    Removed code duplication.

--- dds/idl/marshal_generator.cpp
+++ dds/idl/marshal_generator.cpp
@@ -4126,15 +4126,12 @@ void marshal_generator::gen_union_default(AST_UnionBranch* branch, const std::st
   AST_Type* br = resolveActualType(branch->field_type());
   const Classification br_cls = classify(br);
 
-  if (br_cls & (CL_SEQUENCE | CL_ARRAY)) {
+  if (br_cls & (CL_SEQUENCE | CL_ARRAY | CL_STRUCTURE | CL_UNION)) {
     be_global->impl_ << " " << scoped(branch->field_type()->name()) << " "
                      << getWrapper("tmp", branch->field_type(), WD_INPUT) << ";\n";
-  }
-
-  if (br_cls & (CL_STRUCTURE | CL_UNION)) {
-    be_global->impl_ << " " << scoped(branch->field_type()->name()) << " "
-                     << getWrapper("tmp", branch->field_type(), WD_INPUT) << ";\n"
-                     << "  " << varname << "." << branch->local_name()->get_string() << "(tmp);\n";
+    if (br_cls & (CL_STRUCTURE | CL_UNION)) {
+      be_global->impl_ << "  " << varname << "." << branch->local_name()->get_string() << "(tmp);\n";
+    }
   }
 
   be_global->impl_ << type_to_default("  ", branch->field_type(),
