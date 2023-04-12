bee11b89c552a26b41287b10a8e7ffeb5d1acad0
Johnny Willemsen <jwillemsen@remedy.nl>
2023-04-12 10:24:00 +0200

    Add new complex types test
        * tests/DCPS/Compiler/complex_types/C++11/complex_types_C++11.mpc:
        * tests/DCPS/Compiler/complex_types/C++11/main.cpp:
        * tests/DCPS/Compiler/complex_types/complex_types.idl:
        * tests/DCPS/Compiler/complex_types/complex_types.mpc:
        * tests/DCPS/Compiler/complex_types/main.cpp:
          Added.

---
+++ tests/DCPS/Compiler/complex_types/C++11/main.cpp
@@ -0,0 +1,6 @@
+#include "complex_typesTypeSupportImpl.h"
+
+int main(int, char*[])
+{
+  return 0;
+}
---
+++ tests/DCPS/Compiler/complex_types/main.cpp
@@ -0,0 +1,6 @@
+#include "complex_typesTypeSupportImpl.h"
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}
