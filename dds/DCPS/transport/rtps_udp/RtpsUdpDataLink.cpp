609cbc97778576691b2d5b21f1eae02c90e152c4
Son Dinh <dinhs@objectcomputing.com>
2023-06-14 14:46:19 -0500

    Update check

--- dds/DCPS/transport/rtps_udp/RtpsUdpDataLink.cpp
+++ dds/DCPS/transport/rtps_udp/RtpsUdpDataLink.cpp
@@ -3187,22 +3187,16 @@ RtpsUdpDataLink::RtpsWriter::process_acknack(const RTPS::AckNackSubmessage& ackn
 
   const ReaderInfo_rch& reader = ri->second;
   const SequenceNumber max_sn = expected_max_sn(reader);
-
-#ifdef OPENDDS_SECURITY
-  if (!is_pvs_writer_) {
-#endif
-    if (ack > max_sn) {
-      if (transport_debug.log_dropped_messages) {
-        ACE_DEBUG((LM_DEBUG, "(%P|%t) {transport_debug.log_dropped_messages} "
-                   "RtpsUdpDataLink::RtpsWriter::process_acknack: %C -> %C "
-                   "Received sequence number (%q) > expected max sequence number (%q)\n",
-                   LogGuid(src).c_str(), LogGuid(id_).c_str(), ack.getValue(), max_sn.getValue()));
-      }
-      return;
+  const SequenceNumber sn_received_by_reader = ack.previous();
+  if (sn_received_by_reader > max_sn) {
+    if (transport_debug.log_dropped_messages) {
+    ACE_DEBUG((LM_DEBUG, "(%P|%t) {transport_debug.log_dropped_messages} "
+               "RtpsUdpDataLink::RtpsWriter::process_acknack: %C -> %C "
+               "Received sequence number (%q) > expected max sequence number (%q)\n",
+               LogGuid(src).c_str(), LogGuid(id_).c_str(), sn_received_by_reader.getValue(), max_sn.getValue()));
     }
-#ifdef OPENDDS_SECURITY
+    return;
   }
-#endif
 
   SequenceNumber previous_acked_sn = reader->acked_sn();
   const bool count_is_not_zero = acknack.count.value != 0;
