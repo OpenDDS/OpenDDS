f058c170b6858408a3aba9b69eb38721e5c567a9
Fred Hornsey <hornseyf@unityfoundation.io>
2023-06-14 03:21:52 -0500

    Windows Workaround For OpenDDS_Utils

--- cmake/opendds_build_helpers.cmake
+++ cmake/opendds_build_helpers.cmake
@@ -2,17 +2,23 @@ function(_opendds_alias target)
   # This is the name the target should be exported from CMake-build OpenDDS as
   # or imported in the MPC-built OpenDDS CMake package. For consistency and
   # convenience, alias it so we can use it within the OpenDDS CMake build.
-  string(REPLACE "OpenDDS_" "" user_name "${target}")
-  set(user_name "OpenDDS::${user_name}")
+  string(REPLACE "OpenDDS_" "" name "${target}")
+  set(name "OpenDDS::${name}")
   get_target_property(target_type ${target} TYPE)
   if(target_type MATCHES "LIBRARY")
-    add_library("${user_name}" ALIAS "${target}")
+    add_library("${name}" ALIAS "${target}")
   else()
-    add_executable("${user_name}" ALIAS "${target}")
+    add_executable("${name}" ALIAS "${target}")
   endif()
 endfunction()
 
 function(_opendds_library target)
+  set(no_value_options)
+  set(single_value_options EXPORT_SYMBOLS_NAME)
+  set(multi_value_options)
+  cmake_parse_arguments(arg
+    "${no_value_options}" "${single_value_options}" "${multi_value_options}" ${ARGN})
+
   _opendds_alias(${target})
 
   # Put library in BINARY_DIR/lib
@@ -23,14 +29,17 @@ function(_opendds_library target)
   )
 
   get_target_property(target_type ${target} TYPE)
-  string(TOUPPER "${target}" uppercase_target)
+  if(NOT DEFINED arg_EXPORT_SYMBOLS_NAME)
+    set(arg_EXPORT_SYMBOLS_NAME "${target}")
+  endif()
+  string(TOUPPER "${arg_EXPORT_NAME}" export_symbols_name)
   if(target_type STREQUAL "SHARED_LIBRARY")
     # Define macro for export header
-    target_compile_definitions(${target} PRIVATE "${uppercase_target}_BUILD_DLL")
+    target_compile_definitions(${target} PRIVATE "${export_symbols_name}_BUILD_DLL")
   elseif(target_type STREQUAL "STATIC_LIBRARY")
     # Define macro for dds/DCPS/InitStaticLibs.h and other files
-    string(REPLACE "OPENDDS_" "" short_uppercase_target "${uppercase_target}")
-    target_compile_definitions(${target} PUBLIC "OPENDDS_${short_uppercase_target}_HAS_DLL=0")
+    string(REPLACE "OPENDDS_" "" short_export_symbols_name "${export_symbols_name}")
+    target_compile_definitions(${target} PUBLIC "OPENDDS_${short_export_symbols_name}_HAS_DLL=0")
   else()
     message(FATAL_ERROR "Target ${target} has unexpected type ${target_type}")
   endif()
