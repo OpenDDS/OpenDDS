fdf767f47dfd070591172481d21da69b1c97b667
Tyler Mayoff <tyler@tylermayoff.com>
2023-11-16 11:56:27 -0500

    Setup optional test

---
+++ tests/DCPS/Compiler/optional/main.cpp
@@ -0,0 +1,16 @@
+#include "key_annotationTypeSupportImpl.h"
+
+#include <dds/DCPS/Serializer.h>
+#include <dds/DCPS/TypeSupportImpl.h>
+#include <dds/DCPS/FilterEvaluator.h>
+
+#include <ace/ACE.h>
+#include <ace/Log_Msg.h>
+
+
+int ACE_TMAIN(int, ACE_TCHAR**)
+{
+  
+
+  return failed;
+}
