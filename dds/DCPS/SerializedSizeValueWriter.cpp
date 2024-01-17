afc506ac498cdb1fa385576a3e989911473102ee
Son Dinh <dinhs@objectcomputing.com>
2024-01-17 12:27:47 -0600

    Handle union

--- dds/DCPS/SerializedSizeValueWriter.cpp
+++ dds/DCPS/SerializedSizeValueWriter.cpp
@@ -7,18 +7,6 @@ namespace DCPS {
 
 void SerializedSizeValueWriter::begin_aggregated(DDS::ExtensibilityKind extensibility)
 {
-}
-
-void SerializedSizeValueWriter::end_aggregated()
-{
-}
-
-void SerializedSizeValueWriter::begin_aggregated_member(bool optional, bool present = true)
-{
-}
-
-void SerializedSizeValueWriter::begin_struct(DDS::ExtensibilityKind extensibility)
-{
   // This is where we keep a bookmark for when the aggregated value starts in the stream.
   // To derive the size of the whole struct or union, we need to
   // find out the size of paddings in front of the Dheader.
@@ -42,24 +30,28 @@ void SerializedSizeValueWriter::begin_struct(DDS::ExtensibilityKind extensibilit
   // For final or appendable struct, only member which is an appendable or mutable struct (or
   // in general type that requires Dheader) will need its own entry in state_ and in the cache.
 
+  // Always create a new Metadata instance when encountering a struct.
+  // But only struct whose size is needed, in order to fill its Dheader or
+  // the Emheader (and possibly Nextint) in case it's a member of a mutable
+  // struct, has a cache entry for its size.
+  // Also, for mutable struct, each of its members has its size cached,
+  // regardless of whether it is a nested member or not.
   if (!state_.empty()) {
     const DDS::ExtensibilityKind enclosing_ek = state_.top().extensibility;
     if (enclosing_ek == DDS::FINAL || enclosing_ek == DDS::APPENDABLE) {
       if (extensibility == DDS::FINAL) {
         // We don't need to compute and cache the size for this struct.
         // Instead, what we need is the total size of the closest ancestor type for which
-        // the size is required for a Dheader or a Emheader.
-        // So we don't need to cache the size of this struct.
-        // The concrete write_* methods will add the size of for the concrete members of this struct.
+        // the size is required by a Dheader or a Emheader.
+        // Copy the current size so that the total size can be built up continuously
+        // in the deeper nesting levels.
         Metadata metadata(extensibility);
-        // Copy so that the total size can be built up continuously.
         metadata.total_size = state_.top().total_size;
         state_.push(metadata);
         return;
       } else {
-        // This struct is either APPENDABLE or MUTABLE. Thus, it needs
-        // its own entry in state_ and its size needs to be cached. Since we know the
-        // the first thing in this struct is Dheader, we can perform alignment here.
+        // The alignment before the Dheader is accounted in size of the containing type.
+        // And then we can compute the size of this struct separately.
         encoding_.align(state_.top().total_size, uint32_cdr_size);
         state_.push(Metadata(extensibility));
         serialized_size_delimiter(encoding_, state_.top().total_size);
@@ -70,8 +62,7 @@ void SerializedSizeValueWriter::begin_struct(DDS::ExtensibilityKind extensibilit
         state_.top().cache_pos = size_cache_.size() - 1;
         return;
       }
-    } else {
-      // Enclosing struct is MUTABLE.
+    } else { // Enclosing struct is MUTABLE.
       // Regardless of the extensibility of this struct, we must track its size for the
       // corresponding Emheader in the enclosing struct.
       // Since this is a member of a mutable struct, there was a call to
@@ -96,7 +87,7 @@ void SerializedSizeValueWriter::begin_struct(DDS::ExtensibilityKind extensibilit
   }
 }
 
-void SerializedSizeValueWriter::end_struct()
+void SerializedSizeValueWriter::end_aggregated()
 {
   Metadata& state = state_.top();
   const DDS::ExtensibilityKind extensibility = state.extensibility;
@@ -113,18 +104,16 @@ void SerializedSizeValueWriter::end_struct()
     const DDS::ExtensibilityKind enclosing_ek = state_.top().extensibility;
     if (enclosing_ek == DDS::FINAL || enclosing_ek == DDS::APPENDABLE) {
       if (extensibility == DDS::FINAL) {
-        // Copy the accumulated size back to the containing entry.
+        // Copy the accumulated size back to the entry for the containing type.
         state_.top().total_size = total_size;
         return;
       } else {
-        // This struct is APPENDABLE or MUTABLE.
-        // Here the total size of this struct is ready, now update the total size of
+        // The total size of this struct is ready, now update the total size of
         // the containing struct. Also update the cached size for this struct.
         size_cache_[pos] = total_size;
         state_.top().total_size += total_size;
       }
-    } else {
-      // Enclosing struct is MUTABLE.
+    } else { // Enclosing struct is MUTABLE.
       size_cache_[pos] = total_size;
       state_.top().total_size += total_size;
     }
@@ -134,8 +123,7 @@ void SerializedSizeValueWriter::end_struct()
   }
 }
 
-void SerializedSizeValueWriter::begin_struct_member(const char* name, bool optional,
-                                                    bool present = true)
+void SerializedSizeValueWriter::begin_aggregated_member(bool optional, bool present = true)
 {
   Metadata& state = state_.top();
   const DDS::ExtensibilityKind extensibility = state.extensibility;
@@ -149,6 +137,22 @@ void SerializedSizeValueWriter::begin_struct_member(const char* name, bool optio
   if (extensibility == DDS::MUTABLE && present) {
     serialized_size_parameter_id(encoding_, total_size, mutable_running_total);
   }
+}
+
+void SerializedSizeValueWriter::begin_struct(DDS::ExtensibilityKind extensibility)
+{
+  begin_aggregated(extensibility);
+}
+
+void SerializedSizeValueWriter::end_struct()
+{
+  end_aggregated();
+}
+
+void SerializedSizeValueWriter::begin_struct_member(const char* /*name*/, bool optional,
+                                                    bool present = true)
+{
+  begin_aggregated_member(optional, present);
 }
 
 void SerializedSizeValueWriter::end_struct_member()
@@ -168,11 +172,7 @@ void SerializedSizeValueWriter::end_union()
 
 void SerializedSizeValueWriter::begin_discriminator()
 {
-  const DDS::ExtensibilityKind extensibility = state_.top().extensibility;
-  size_t& mutable_running_total = state_.top().mutable_running_total;
-  if (extensibility == DDS::MUTABLE) {
-    serialized_size_parameter_id(encoding_, size_, mutable_running_total);
-  }
+  begin_aggregated_member(0, false);
 }
 
 void SerializedSizeValueWriter::end_discriminator()
@@ -183,7 +183,7 @@ void SerializedSizeValueWriter::end_discriminator()
 void SerializedSizeValueWriter::begin_union_member(const char* /*name*/, bool optional,
                                                    bool present = true)
 {
-  begin_aggregated_member(optional(), present);
+  begin_aggregated_member(0, optional, present);
 }
 
 void SerializedSizeValueWriter::end_union_member()
@@ -191,6 +191,9 @@ void SerializedSizeValueWriter::end_union_member()
   return;
 }
 
+// Array can be treated similar to final/appendable struct with each element is a member.
+// If the element type is primitive, it is similar to final struct.
+// If the element type is not primitive, it is similar to appendable struct with Dheader.
 void SerializedSizeValueWriter::begin_array(DDS::TypeKind elem_tk)
 {
   if (!is_primitive(elem_tk)) {
@@ -203,6 +206,11 @@ void SerializedSizeValueWriter::end_array()
   return;
 }
 
+// Sequence is similar to final or appendable struct with each element is a member.
+// If element type is primitive, it can be treated as final struct which has no Dheader.
+// If element type is not primitive, it can be treated as appendable struct which has Dheader.
+// In both case, elements are serialized back-to-back similarly to final/appendable struct.
+// One difference is that sequence has length ahead of all the elements.
 void SerializedSizeValueWriter::begin_sequence(DDS::TypeKind elem_tk)
 {
   if (!is_primitive(elem_tk)) {
@@ -226,6 +234,8 @@ void SerializedSizeValueWriter::end_element()
   return;
 }
 
+// When this is a member of a mutable struct, its size needs to be recorded
+// so that we can write the Emheader (and Nextint) later.
 void SerializedSizeValueWriter::write_boolean(ACE_CDR::Boolean /*value*/)
 {
   primitive_serialized_size_boolean(encoding_, state_.top().total_size);
