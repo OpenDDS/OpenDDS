418e109cc114dbae4c38d61fc439edf798c4db5a
Tyler <tyler@tylermayoff.com>
2024-02-02 22:20:04 -0500

    Update dds/idl/langmap_generator.cpp
    Co-authored-by: Fred Hornsey <fred@hornsey.us>

--- dds/idl/langmap_generator.cpp
+++ dds/idl/langmap_generator.cpp
@@ -1523,8 +1523,8 @@ struct Cxx11Generator : GeneratorBase
     }
 
     const bool is_optional = be_global->is_optional(field);
-    const std::string lang_field_type = be_global->is_optional(field) ? "std::optional<" + generator_->map_type(field) + ">" : generator_->map_type(field);
-    if (be_global->is_optional(field)) {
+    const std::string lang_field_type = is_optional ? "std::optional<" + generator_->map_type(field) + ">" : generator_->map_type(field);
+    if (is_optional) {
       be_global->add_include("<optional>", BE_GlobalData::STREAM_LANG_H);
     }
 
