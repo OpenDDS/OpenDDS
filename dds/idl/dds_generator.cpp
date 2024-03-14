b3ef2d35828f7cc459c91f290c3daf41e0eaba90
Adam Mitz <mitza@objectcomputing.com>
2024-03-14 12:39:19 -0500

    Support keywords

--- dds/idl/dds_generator.cpp
+++ dds/idl/dds_generator.cpp
@@ -138,7 +138,7 @@ bool dds_generator::gen_enum_helper(AST_Enum*, UTL_ScopedName* name,
   const std::string underscores = scoped_helper(name, "_"),
     scope = be_global->language_mapping() == BE_GlobalData::LANGMAP_CXX11
             ? scoped(name) + "::"
-            : module_scope(name),
+            : "",
     fwd_decl = be_global->value_reader_writer() ? "" : "class EnumHelper;\n",
     helper_decl = "const EnumHelper* gen_" + underscores + "_helper",
     decl_prefix = (be_global->export_macro() == "")
@@ -153,9 +153,12 @@ bool dds_generator::gen_enum_helper(AST_Enum*, UTL_ScopedName* name,
     "const ListEnumHelper::Pair gen_" << underscores << "_pairs[] = {\n";
 
   for (size_t i = 0; i < contents.size(); ++i) {
-    const std::string idl_name = canonical_name(contents[i]);
+    const std::string idl_name = canonical_name(contents[i]),
+      cxx_name = be_global->language_mapping() == BE_GlobalData::LANGMAP_CXX11
+                 ? contents[i]->local_name()->get_string()
+                 : scoped(contents[i]->name());
     be_global->impl_ <<
-      "  {\"" << idl_name << "\", static_cast<ACE_CDR::Long>(" << scope << idl_name << ")},\n";
+      "  {\"" << idl_name << "\", static_cast<ACE_CDR::Long>(" << scope << cxx_name << ")},\n";
   }
 
   be_global->impl_ <<
