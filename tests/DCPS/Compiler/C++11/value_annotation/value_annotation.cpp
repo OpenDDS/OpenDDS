aa08b18573b6de598500a59a18e24049377d890e
Adam Mitz <mitza@objectcomputing.com>
2024-03-19 17:28:33 -0500

    started test

--- tests/DCPS/Compiler/C++11/value_annotation/value_annotation.cpp
+++ tests/DCPS/Compiler/C++11/value_annotation/value_annotation.cpp
@@ -1,6 +1,56 @@
 #include "value_annotationTypeSupportImpl.h"
 
-int ACE_MAIN(int argc, char* argv[])
+#include <cstdlib>
+#include <cstring>
+
+using namespace OpenDDS::DCPS;
+
+int ACE_TMAIN(int, ACE_TCHAR*[])
 {
-  return 0;
+  bool failed = false;
+
+  if (static_cast<uint32_t>(TestValueAnnotation::NON_ZERO) != 99) {
+    ACE_ERROR((LM_ERROR, "ERROR: NON_ZERO wasn't 99\n"));
+    failed = true;
+  }
+  if (static_cast<uint32_t>(TestValueAnnotation::SIX) != 6) {
+    ACE_ERROR((LM_ERROR, "ERROR: SIX wasn't 6\n"));
+    failed = true;
+  }
+  if (static_cast<uint32_t>(TestValueAnnotation::TEN) != 10) {
+    ACE_ERROR((LM_ERROR, "ERROR: TEN wasn't 10\n"));
+    failed = true;
+  }
+
+  const EnumHelper& helper = *gen_TestValueAnnotation_helper;
+  if (helper.valid("invalid")) {
+    ACE_ERROR((LM_ERROR, "ERROR: invalid was valid\n"));
+    failed = true;
+  }
+  if (helper.valid(0)) {
+    ACE_ERROR((LM_ERROR, "ERROR: 0 was valid\n"));
+    failed = true;
+  }
+
+  if (helper.valid("FIVE")) {
+    if (helper.get_value("FIVE") != 5) {
+      ACE_ERROR((LM_ERROR, "ERROR: FIVE's value wasn't 5\n"));
+      failed = true;
+    }
+  } else {
+    ACE_ERROR((LM_ERROR, "ERROR: FIVE was invalid\n"));
+    failed = true;
+  }
+
+  if (helper.valid(5)) {
+    if (std::strcmp(helper.get_name(5), "FIVE")) {
+      ACE_ERROR((LM_ERROR, "ERROR: 5's name wasn't FIVE\n"));
+      failed = true;
+    }
+  } else {
+    ACE_ERROR((LM_ERROR, "ERROR: 5 was invalid\n"));
+    failed = true;
+  }
+
+  return failed ? EXIT_FAILURE : EXIT_SUCCESS;
 }
