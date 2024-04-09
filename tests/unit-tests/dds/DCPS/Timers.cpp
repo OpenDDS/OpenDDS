8ecb6205c8d74322c4d9ca791dc960ae57db9365
Adam Mitz <mitza@objectcomputing.com>
2024-04-09 16:37:25 -0500

    Unit test for timers: use the same ACE_Reactor impl on all platforms

--- tests/unit-tests/dds/DCPS/Timers.cpp
+++ tests/unit-tests/dds/DCPS/Timers.cpp
@@ -1,6 +1,6 @@
 #include <dds/DCPS/Timers.h>
 
-#include <ace/Reactor.h>
+#include <ace/Select_Reactor.h>
 
 #include <gtest/gtest.h>
 
@@ -24,15 +24,15 @@ struct TestEventHandler : RcEventHandler {
 TEST(dds_DCPS_Timers, test)
 {
   RcHandle<TestEventHandler> handler = make_rch<TestEventHandler>();
-  ACE_Reactor* const reactor = ACE_Reactor::instance();
+  ACE_Reactor reactor(new ACE_Select_Reactor, true);
 
   ACE_DEBUG((LM_DEBUG, "%T schedule\n"));
-  const Timers::TimerId id = Timers::schedule(reactor, *handler, 0, TimeDuration::from_msec(10));
+  const Timers::TimerId id = Timers::schedule(&reactor, *handler, 0, TimeDuration::from_msec(10));
   ASSERT_NE(id, Timers::InvalidTimerId);
 
-  ACE_Time_Value one_sec(1, 0);
-  reactor->handle_events(one_sec);
+  ACE_Time_Value one_sec(1);
+  reactor.handle_events(one_sec);
 
   ASSERT_EQ(handler->calls_, 1);
-  Timers::cancel(reactor, id);
+  Timers::cancel(&reactor, id);
 }
