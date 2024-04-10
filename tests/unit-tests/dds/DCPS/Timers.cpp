2816ccf75c3bd9d812d99d286fa446c85b2f2c87
Adam Mitz <mitza@objectcomputing.com>
2024-04-10 19:51:23 +0000

    extended test

--- tests/unit-tests/dds/DCPS/Timers.cpp
+++ tests/unit-tests/dds/DCPS/Timers.cpp
@@ -13,7 +13,6 @@ struct TestEventHandler : RcEventHandler {
 
   int handle_timeout(const ACE_Time_Value&, const void*)
   {
-    ACE_DEBUG((LM_DEBUG, "%T timeout\n"));
     ++calls_;
     return 0;
   }
@@ -21,12 +20,11 @@ struct TestEventHandler : RcEventHandler {
   int calls_;
 };
 
-TEST(dds_DCPS_Timers, test)
+TEST(dds_DCPS_Timers, test_oneshot)
 {
   RcHandle<TestEventHandler> handler = make_rch<TestEventHandler>();
   ACE_Reactor reactor(new ACE_Select_Reactor, true);
 
-  ACE_DEBUG((LM_DEBUG, "%T schedule\n"));
   const Timers::TimerId id = Timers::schedule(&reactor, *handler, 0, TimeDuration::from_msec(10));
   ASSERT_NE(id, Timers::InvalidTimerId);
 
@@ -34,5 +32,20 @@ TEST(dds_DCPS_Timers, test)
   reactor.handle_events(one_sec);
 
   ASSERT_EQ(handler->calls_, 1);
+  Timers::cancel(&reactor, id);
+}
+
+TEST(dds_DCPS_Timers, test_repeat)
+{
+  RcHandle<TestEventHandler> handler = make_rch<TestEventHandler>();
+  ACE_Reactor reactor(new ACE_Select_Reactor, true);
+
+  const Timers::TimerId id = Timers::schedule(&reactor, *handler, 0, TimeDuration(), TimeDuration::from_msec(10));
+  ASSERT_NE(id, Timers::InvalidTimerId);
+
+  ACE_Time_Value one_sec(1);
+  reactor.run_reactor_event_loop(one_sec);
+
+  ASSERT_GT(handler->calls_, 10);
   Timers::cancel(&reactor, id);
 }
