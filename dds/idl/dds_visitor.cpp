6c248b5e89e60a75b4974a076a8923b7144ff262
Adam Mitz <mitza@objectcomputing.com>
2025-02-14 15:24:06 -0600

    opendds_idl: options to write TypeObjects to a separate file

--- dds/idl/dds_visitor.cpp
+++ dds/idl/dds_visitor.cpp
@@ -68,7 +68,7 @@ namespace {
 dds_visitor::dds_visitor(AST_Decl* scope, bool java_ts_only)
   : scope_(scope), error_(false), java_ts_only_(java_ts_only)
 {
-  if (!be_global->no_default_gen()) {
+  if (!be_global->no_default_gen() || be_global->gen_typeobject_override()) {
     gen_target_.add_generator(&to_gen_);
     const bool generate_xtypes = !be_global->suppress_xtypes() && !java_ts_only;
     to_gen_.produce_output(generate_xtypes);
@@ -76,7 +76,8 @@ dds_visitor::dds_visitor(AST_Decl* scope, bool java_ts_only)
     if (generate_xtypes && be_global->old_typeobject_encoding()) {
       to_gen_.use_old_typeobject_encoding();
     }
-
+  }
+  if (!be_global->no_default_gen()) {
     if (be_global->value_reader_writer()) {
       gen_target_.add_generator(&value_reader_generator_);
       gen_target_.add_generator(&value_writer_generator_);
