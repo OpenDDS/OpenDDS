c59110e7cbf3dea0afed8d2974592cf2a641e1f8
Adam Mitz <mitza@objectcomputing.com>
2025-03-19 12:14:05 -0500

    Additional use case for flexible type support
    The GUID of a remote peer may be known before discovery starts
    
    (cherry picked from commit a82a2c9c5edb4ec3febfa8e96267e0f6700e7bc9)
    
    # Conflicts:
    #	dds/DCPS/RTPS/Sedp.cpp
    #	dds/DCPS/RTPS/Spdp.cpp

--- dds/DCPS/RTPS/Spdp.h
+++ dds/DCPS/RTPS/Spdp.h
@@ -407,6 +407,9 @@ private:
 #endif
   XTypes::TypeLookupService_rch type_lookup_service_;
 
+  typedef OPENDDS_MAP_CMP(GUID_t, DCPS::String, GUID_tKeyLessThan) GuidToString;
+  GuidToString flexible_types_pre_discovery_;
+
   // Participant:
   const DDS::DomainId_t domain_;
   DCPS::GUID_t guid_;
