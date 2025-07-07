ee3611f71437bbae02a1b4c434b105e7a968037f
Adam Mitz <mitza@objectcomputing.com>
2025-07-07 19:36:03 +0000

    fixed merge

--- dds/DCPS/Service_Participant.cpp
+++ dds/DCPS/Service_Participant.cpp
@@ -150,7 +150,6 @@ Service_Participant::Service_Participant()
   ORB_argv_(false /*substitute_env_args*/),
 #endif
   time_source_()
-  , monitor_factory_(0)
   , priority_min_(0)
   , priority_max_(0)
   , shut_down_(false)
