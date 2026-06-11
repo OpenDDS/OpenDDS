f6d2d8acda1fe0270de38125d16a6377cb79d5e5
Son Dinh <dinhs@objectcomputing.com>
2026-06-11 17:31:02 -0500

    Add cross relay test

--- tests/DCPS/RtpsRelay/Smoke/run_async_disc_test.pl
+++ tests/DCPS/RtpsRelay/Smoke/run_async_disc_test.pl
@@ -25,41 +25,62 @@ $test->{dcps_transport_debug_level} = 1;
 # will manually set -DCPSConfigFile
 $test->{add_transport_config} = 0;
 
-# Allow publisher to do asynchronous discovery when reconnecting
-my $async_discovery_opts = " -CertificateIdPattern CN\\s*=\\s*(Tahoe).* -LogAsyncDiscovery 1";
+# Topology:
+# publisher <====> relay1 <====> relay2 <====> subscriber
+# Async discovery is enabled for relay1 but disabled for relay2.
+# subscriber is permanently connected to relay2.
+# relay1 builds up async discovery cache for publisher the first time it connects to relay1.
+# When publisher reconnects to relay1, the constructed cache should be used.
 
 # Require security
-my $relay_opts = "-Id relay" .
-                 $async_discovery_opts .
-                 " -LogDiscovery 1" .
-                 " -LogActivity 1" .
-                 " -LogRelayStatistics 3" .
-                 " -DCPSConfigFile relay1.ini" .
-                 " -ApplicationDomain 42" .
-                 " -VerticalAddress 4444" .
-                 " -HorizontalAddress 127.0.0.1:11444" .
-                 " -UserData thisrelay" .
-                 " -IdentityCA ../../../security/certs/identity/identity_ca_cert.pem" .
-                 " -PermissionsCA ../../../security/certs/permissions/permissions_ca_cert.pem" .
-                 " -IdentityCertificate ../../../security/certs/identity/test_participant_01_cert.pem" .
-                 " -IdentityKey ../../../security/certs/identity/test_participant_01_private_key.pem" .
-                 " -Governance governance_signed.p7s" .
-                 " -Permissions permissions_relay_signed.p7s" .
-                 " -DCPSSecurity 1";
+sub get_relay_args {
+    my $n = shift;
+    my $port_digit = 3 + $n;
+    my $async_discovery_opts = $n == 1 ? "-CertificateIdPattern CN\\s*=\\s*(Tahoe).* -LogAsyncDiscovery 1" : "";
+    return join(' ',
+        "-Id relay${n}",
+        $async_discovery_opts,
+        "-LogDiscovery 1",
+        "-LogActivity 1",
+        "-LogRelayStatistics 3",
+        "-DCPSConfigFile relay${n}.ini",
+        "-ApplicationDomain 42",
+        "-VerticalAddress ${port_digit}444",
+        "-HorizontalAddress 127.0.0.1:11${port_digit}44",
+        "-MetaDiscoveryAddress 127.0.0.1:808${n}",
+        "-UserData relay${n}",
+        "-IdentityCA ../../../security/certs/identity/identity_ca_cert.pem",
+        "-PermissionsCA ../../../security/certs/permissions/permissions_ca_cert.pem",
+        "-IdentityCertificate ../../../security/certs/identity/test_participant_01_cert.pem",
+        "-IdentityKey ../../../security/certs/identity/test_participant_01_private_key.pem",
+        "-Governance governance_signed.p7s",
+        "-Permissions permissions_relay_signed.p7s",
+        "-DCPSSecurity 1"
+    );
+}
 
-# Use the same relay for both publisher and subscriber
-my $common_ini = " pub_rtps.ini";
+my $pub_ini = " pub_rtps.ini";
+my $sub_ini = " sub_rtps.ini";
+if (!$test->flag('cross_relay')) {
+    $sub_ini = $pub_ini;
+}
 
-$test->process("relay", "$ENV{DDS_ROOT}/bin/RtpsRelay", $relay_opts);
-$test->process("publisher", "publisher", "-ORBDebugLevel 1 -DCPSConfigFile". $common_ini . " -DCPSSecurity 1");
-$test->process("publisher2", "publisher", "-ORBDebugLevel 1 -DCPSConfigFile". $common_ini . " -DCPSSecurity 1 -r");
-$test->process("subscriber", "subscriber", "-ORBDebugLevel 1 -DCPSConfigFile" . $common_ini . " -DCPSSecurity 1 -r");
+$test->process("relay1", "$ENV{DDS_ROOT}/bin/RtpsRelay", get_relay_args(1));
+$test->process("relay2", "$ENV{DDS_ROOT}/bin/RtpsRelay", get_relay_args(2)) if $test->flag('cross_relay');
+$test->process("publisher", "publisher", "-ORBDebugLevel 1 -DCPSConfigFile". $pub_ini . " -DCPSSecurity 1");
+$test->process("publisher2", "publisher", "-ORBDebugLevel 1 -DCPSConfigFile". $pub_ini . " -DCPSSecurity 1 -r");
+$test->process("subscriber", "subscriber", "-ORBDebugLevel 1 -DCPSConfigFile" . $sub_ini . " -DCPSSecurity 1 -r");
 
 # First, connect the first publisher with the subscriber
-$test->start_process("relay");
-sleep 3;
+if ($test->flag('cross_relay')) {
+    $test->start_process("relay2");
+    sleep 3;
+}
+
 $test->start_process("subscriber");
 sleep 3;
+$test->start_process("relay1");
+sleep 10;
 $test->start_process("publisher");
 sleep 3;
 $test->stop_process(5, "publisher");
@@ -71,6 +92,9 @@ sleep 3;
 
 $test->stop_process(5, "subscriber");
 $test->stop_process(5, "publisher2");
-$test->kill_process(5, "relay");
+$test->kill_process(5, "relay1");
+if ($test->flag('cross_relay')) {
+    $test->kill_process(5, "relay2");
+}
 
 exit $test->finish(15);
